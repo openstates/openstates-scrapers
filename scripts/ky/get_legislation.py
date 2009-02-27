695b906e9ea8159b7345343bb31f9ef0d6a6d452
James Turk <james.p.turk@gmail.com>
2009-02-27 16:55:39 -0500

    switched to requring yield

--- scripts/ky/get_legislation.py
+++ scripts/ky/get_legislation.py
@@ -1,46 +1,46 @@
 import urllib, urllib2
 import re
-import time
 from BeautifulSoup import BeautifulSoup
 
 # ugly hack
 import sys
 sys.path.append('.')
-from pyutils.legislation import LegislationScraper
+from pyutils.legislation import run_legislation_scraper
 
-class KentuckyScraper(LegislationScraper):
-    def scrape_legislation(self, chamber, year, download):
-        # this needs to call scrape_session
-        # sessions = ['%sRS','%sSS','%sS2']
-        pass
+def scrape_session(chamber, session):
+    if chamber == 'upper':
+        chamber_abbr = 'H'
+        bill_abbr = 'HB'
+    elif chamber == 'lower':
+        chamber_abbr = 'S'
+        bill_abbr = 'SB'
 
-    def scrape_session(self, chamber, session, download):
-        if chamber == 'upper':
-            chamber_abbr = 'H'
-            bill_abbr = 'HB'
-        elif chamber == 'lower':
-            chamber_abbr = 'S'
-            bill_abbr = 'SB'
+    index_file = "http://www.lrc.ky.gov/record/%s/bills_%s.htm" % (session, chamber_abbr)
+    print index_file
+    req = urllib2.Request(index_file)
+    response = urllib2.urlopen(req)
+    doc = response.read()
+    soup = BeautifulSoup(doc)
+    re_str = "%s\d{1,4}.htm" % bill_abbr
+    links = soup.findAll(href=re.compile(re_str))
 
-        local_filename = ''
-        index_file = "http://www.lrc.ky.gov/recarch/%s/bills_%s.htm" % (session, chamber_abbr)
-        req = urllib2.Request(index_file)
-        response = urllib2.urlopen(req)
-        doc = response.read()
-        soup = BeautifulSoup(doc)
-        re_str = "%s\d{1,4}.htm" % bill_abbr
-        links = soup.findAll(href=re.compile(re_str))
+    for link in links:
+        bill_id = link['href'].replace('.htm', '')
+        bill_url = "http://www.lrc.ky.gov/recarch/%s/%s" % (session, link['href'])
 
-        for link in links:
-            bill_id = link['href'].replace('.htm', '')
-            bill_url = "http://www.lrc.ky.gov/recarch/%s/%s" % (session, link['href'])
+        #if download:
+        #    local_filename = 'data/ky/legislation/%s%s%s.htm' % (chamber, session, bill_id) 
+        #    urllib.urlretrieve(bill_url, local_filename)
+        #    time.sleep(0.5)
 
-            if download:
-                local_filename = 'data/ky/legislation/%s%s%s.htm' % (chamber, session, bill_id) 
-                urllib.urlretrieve(bill_url, local_filename)
-                time.sleep(0.5)
+        yield {'state':'KY', 'chamber':chamber, 'session':session,
+               'bill_id':bill_id, 'remote_url':bill_url}
 
-            self.add_bill('KY', chamber, session, bill_id, bill_url, local_filename)
+def scrape_legislation(chamber, year):
+    yy = str(year)[2:]
+    for session in ['%sRS','%sSS','%sS2']:
+        for bill in scrape_session(chamber, session % yy):
+            yield bill
 
 if __name__ == '__main__':
-    KentuckyScraper('test.csv').run()
+    run_legislation_scraper(scrape_legislation)
--- scripts/pyutils/legislation.py
+++ scripts/pyutils/legislation.py
@@ -1,8 +1,7 @@
 from optparse import make_option, OptionParser
 import csv
 
-class LegislationScraper(object):
-
+def run_legislation_scraper(get_bills_func):
     option_list = (
         make_option('-y', '--year', action='append', dest='years', 
                     help='year(s) to scrape'),
@@ -14,31 +13,22 @@ class LegislationScraper(object):
                     default=False, help='store bills locally'),
     )
 
-    def __init__(self, filename):
-        options, spares = OptionParser(option_list=self.option_list).parse_args()
-        self.years = options.years
-        self.chambers = []
-        if options.upper:
-            self.chambers.append('upper')
-        if options.lower:
-            self.chambers.append('lower')
-        self.download = options.download
-        self.filename = filename
-        self.fields = ('state', 'chamber', 'session', 'bill_id', 'remote_url', 'local_filename')
-        self.outfile = csv.DictWriter(open(filename, 'w'), self.fields)
+    options, spares = OptionParser(option_list=option_list).parse_args()
+    years = options.years
+    chambers = []
+    if options.upper:
+        chambers.append('upper')
+    if options.lower:
+        chambers.append('lower')
+    download = options.download
+    fields = ('state', 'chamber', 'session', 'bill_id', 'remote_url')
+    output = csv.DictWriter(open('output.csv', 'w'), fields)
 
-    def scrape_legislation(self, chamber, year, download):
-        raise NotImplementedError('scrape_legislation should be defined in a subclass of LegislationScraper')
+    #all_funcs = chain(*[get_bills_func(chamber, year)
+    #                    for year in years for chamber in chambers])
+    #saucebrush.run_recipe(all_funcs, saucebrush.emitters.CSVEmitter(open('test.csv', 'w'), fields))
 
-    def add_bill(self, state, chamber, session, bill_id, remote_url, local_filename=''):
-        # this will keep track of what has been seen before, etc.
-        self.outfile.writerow({'state': state, 'chamber':chamber,
-                               'session':session, 'bill_id': bill_id,
-                               'remote_url': remote_url,
-                               'local_filename': local_filename})
-
-    def run(self):
-        for year in self.years:
-            for chamber in self.chambers:
-                self.scrape_legislation(chamber, year, self.download)
-
+    for chamber in chambers:
+        for year in years:
+            for bill in get_bills_func(chamber, year):
+                output.writerow(bill)
