e218e3cd37c97e56e1ad6eefcff232d79c4affd4
Rebecca Shapiro <rshapiro@front4.(none)>
2009-03-02 14:08:07 -0500

    updated CT parser to use function style and yield

--- scripts/ct/get_legislation.py
+++ scripts/ct/get_legislation.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python
 
 #Bill numbering:
-#SB 1-5000. (errors 1072..)
+#SB 1-5000. (errors around 1072?)
 #HB 5001-9999
 
-#http://cga.ct.gov/asp/cgabillstatus/cgabillstatus.asp?selBillType=Bill&bill_num=100&which_year=2009
 
 from BeautifulSoup import BeautifulSoup
 import re
@@ -14,45 +13,45 @@ import time
 # ugly hack
 import sys
 sys.path.append('.')
-from legislation import LegislationScraper
+from pyutils.legislation import run_legislation_scraper
 
-class ConnecticutScraper(LegislationScraper):
-    def scrape_legislation(self,chamber,year,download):
-        if chamber == 'upper':
-            chamber_abbr = 'H'
-            min = 5001
-            max = 9999
-        elif chamber == 'lower':
-            chamber_abbr = 'S'
-            min = 1
-            max = 5000
+def scrape_legislation(chamber,year):
+    if chamber == 'upper':
+        min = 5001
+        max = 9999
+    elif chamber == 'lower':
+        min = 1
+        max = 5000
         local_filename = ''
-
-        for i in range(min,max+1):
-            index_file ='http://cga.ct.gov/asp/cgabillstatus/cgabillstatus.asp?selBillType=Bill&bill_num=%d&which_year=%s'\
-                %(i,year)
-            req = urllib2.Request(index_file)
-            response = urllib2.urlopen(req)
-            doc = response.read()
-            soup = BeautifulSoup(doc)
-            if soup.find("div",{"class":"CGASubHeader"}) == None:
-                continue #bill does not exist
-            else: 
-                ahref = soup.find("table",{"id":"CGABillText"}).find("a")
-                regexp = re.compile("href=\"(\S*)\"")
-                bill_url = regexp.search(str(ahref))
-                bill_url = bill_url.group(1)
-                bill_url = "http://cga.ct.gov"+bill_url
-                if download:
-                    local_filename = 'data/ct/legislation/%s%s%04d.htm' %(chamber,year,i)
-                    urllib.urlretrieve(bill_url,local_filename)
-                    time.sleep(0.5)
-                else:
-                    local_filename = ''
+        
+    for i in range(min,max+1):
+        #obtain html
+        index_file ='http://cga.ct.gov/asp/cgabillstatus/cgabillstatus.asp?selBillType=Bill&bill_num=%d&which_year=%s'\
+            %(i,year)
+        req = urllib2.Request(index_file)
+        response = urllib2.urlopen(req)
+        doc = response.read()
+        soup = BeautifulSoup(doc)
+        
+            #check to see legislation exists
+        if soup.find("div",{"class":"CGASubHeader"}) == None:
+            continue #bill does not exist
+        else: 
+            ahref = soup.find("table",{"id":"CGABillText"}).find("a")
+            regexp = re.compile("href=\"(\S*)\"")
+            bill_url = regexp.search(str(ahref))
+            bill_url = bill_url.group(1)
+            bill_url = "http://cga.ct.gov"+bill_url
+            #if download:
+            #    local_filename = 'data/ct/legislation/%s%s%04d.htm' %(chamber,year,i)
+            #    urllib.urlretrieve(bill_url,local_filename)
+            #    time.sleep(0.5)
+            #else:
+            #    local_filename = ''
                 
-                self.add_bill('CT',chamber,year,i,bill_url,local_filename)
-                print 'CT'+" "+chamber+" "+str(year)+" "+str(i)+" "+bill_url+" "+local_filename
+            yield {'state':'CT','chamber':chamber,'session':year,
+                   'bill_id':i,'remote_url':bill_url}
                 
     
 if __name__ == '__main__':
-    ConnecticutScraper('test.csv').run()
+    run_legislation_scraper(scrape_legislation)
