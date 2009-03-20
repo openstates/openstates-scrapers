4313ffe28622d7d189a07465d52bfe6f36e7f63e
Michael Stephens <mike@mindleak.com>
2009-03-20 11:34:55 +0800

    PA: cleanup and comments
    Signed-off-by: Sunlight Labs <git@sunlightlabs.com>

--- scripts/pa/get_legislation.py
+++ scripts/pa/get_legislation.py
@@ -10,13 +10,14 @@ sys.path.append('.')
 from pyutils.legislation import run_legislation_scraper
 
 def scrape_session(chamber, year, session=0):
+    # Data from 1969 (!) on is available
+    assert int(year) >= 1969, "no data available before 1969"
+    assert int(year) <= dt.date.today().year, "can't look into the future"
+
     if chamber == 'upper':
         bill_abbr = 'S'
     elif chamber == 'lower':
         bill_abbr = 'H'
-
-    assert int(year) >= 1969, "no data available before 1969"
-    assert int(year) <= dt.date.today().year, "can't look into the future"
 
     # Get session years
     if int(year) % 2 == 1:
@@ -25,21 +26,23 @@ def scrape_session(chamber, year, session=0):
     else:
         # If second year of session just ignore
         return
-    
-    url = 'http://www.legis.state.pa.us/cfdocs/legis/bi/BillIndx.cfm?sYear=%s&sIndex=%i&bod=%s' % (y1, session, bill_abbr)
-    print url
-    soup = BeautifulSoup(urllib2.urlopen(url).read())
 
+    # Get the bill list
+    bill_list_url = 'http://www.legis.state.pa.us/cfdocs/legis/bi/BillIndx.cfm?sYear=%s&sIndex=%i&bod=%s' % (y1, session, bill_abbr)
+    print bill_list_url
+    bill_list = BeautifulSoup(urllib2.urlopen(bill_list_url).read())
+
+    # Get all bill links
     re_str = "body=%s&type=B&bn=\d+" % bill_abbr
-    links = soup.findAll(href=re.compile(re_str))
+    links = bill_list.findAll(href=re.compile(re_str))
 
     for link in links:
         bill_number = link.contents[0]
         bill_id = bill_abbr + 'B' + bill_number
+        print "Getting %s" % bill_id
 
+        # Get info page
         info_url = 'http://www.legis.state.pa.us/cfdocs/billinfo/billinfo.cfm?syear=%s&sind=%i&body=%s&type=B&BN=%s' % (y1, session, bill_abbr, bill_number)
-        print info_url
-        
         info_page = BeautifulSoup(urllib2.urlopen(info_url).read())
         pn_table = info_page.find('div', {"class": 'pn_table'})
         
