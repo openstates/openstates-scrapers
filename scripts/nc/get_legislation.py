f4517c9715838eb6444fb2a13640a4502a8d2bf1
James Turk <james.p.turk@gmail.com>
2009-03-23 10:35:35 -0400

    updated wrapper

--- scripts/nc/get_legislation.py
+++ scripts/nc/get_legislation.py
@@ -5,116 +5,113 @@ from BeautifulSoup import BeautifulSoup
 
 # ugly hack
 import sys
-sys.path.append('.')
-#from pyutils.legislation import run_legislation_scraper, NoDataForYear
+sys.path.append('./scripts')
+from pyutils.legislation import LegislationScraper, NoDataForYear
 
 def clean_legislators(s):
     s = s.replace('&nbsp;', ' ').strip()
     return [l.strip() for l in s.split(';') if l]
 
-def add_bill(bill):
-    print 'bill: %r' % bill
+class NCLegislationScraper(LegislationScraper):
 
-def add_sponsor(bill, sponsor_type, sponsor):
-    print 'sponsor: %r %r %r' % (bill, sponsor_type, sponsor)
+    state = 'nc'
 
-def add_action(bill, chamber, action, date):
-    print 'action %r %r %r' % (bill, chamber, action)
+    def get_bill_info(self, session, bill_id):
+        bill_detail_url = 'http://www.ncga.state.nc.us/gascripts/BillLookUp/BillLookUp.pl?Session=%s&BillID=%s' % (session, bill_id)
 
-def get_bill_info(session, bill_id):
-    bill_detail_url = 'http://www.ncga.state.nc.us/gascripts/BillLookUp/BillLookUp.pl?Session=%s&BillID=%s' % (session, bill_id)
+        # parse the bill data page, finding the latest html text
+        bill_data = urllib.urlopen(bill_detail_url).read()
+        bill_soup = BeautifulSoup(bill_data)
+        links = bill_soup.findAll('a')
+        best_url = None
+        for link in links:
+            if link.has_key('href') and link['href'].startswith('/Sessions'):
+                if not best_url or link['href'].endswith('.html'):
+                    best_url = link['href']
+        best_url = 'http://www.ncga.state.nc.us' + best_url
 
-    # parse the bill data page, finding the latest html text
-    bill_data = urllib.urlopen(bill_detail_url).read()
-    bill_soup = BeautifulSoup(bill_data)
-    links = bill_soup.findAll('a')
-    best_url = None
-    for link in links:
-        if link.has_key('href') and link['href'].startswith('/Sessions'):
-            if not best_url or link['href'].endswith('.html'):
-                best_url = link['href']
-    best_url = 'http://www.ncga.state.nc.us' + best_url
-
-    # REMOVE
-    chamber = 'X'
-
-    bill = {'state':'NC', 'chamber':chamber, 'session':session, 'bill_id':bill_id, 'remote_url':best_url}
-    add_bill(bill)
-
-    # grab primary and cosponsors from table[6]
-    tables = bill_soup.findAll('table')
-    sponsor_rows = tables[6].findAll('tr')
-    sponsors = clean_legislators(sponsor_rows[1].td.contents[0])
-    for leg in sponsors:
-        add_sponsor(bill, 'primary', leg)
-    cosponsors = clean_legislators(sponsor_rows[2].td.contents[0])
-    for leg in cosponsors:
-        add_sponsor(bill, 'cosponsor', leg)
-
-    # easier to read actions from the rss.. but perhaps favor less HTTP requests?
-    rss_url = 'http://www.ncga.state.nc.us/gascripts/BillLookUp/BillLookUp.pl?Session=%s&BillID=%s&view=history_rss' % (session, bill_id)
-    rss_data = urllib.urlopen(rss_url).read()
-    rss_soup = BeautifulSoup(rss_data)
-    # title looks like 'House Chamber: action'
-    for item in rss_soup.findAll('item'):
-        action = item.title.contents[0]
-        pieces = item.title.contents[0].split(' Chamber: ')
-        if len(pieces) == 2:
-            chamber = pieces[0]
-            action = pieces[1]
+        if bill_id[0] == 'H':
+            chamber = 'lower'
         else:
-            chamber = None
-            action = pieces[0]
-        date = item.pubdate.contents[0]
-        add_action(bill, chamber, action, date)
+            chamber = 'upper'
 
-def scrape_session(chamber, session):
-    url = 'http://www.ncga.state.nc.us/gascripts/SimpleBillInquiry/displaybills.pl?Session=%s&tab=Chamber&Chamber=%s' % (session, chamber)
-    data = urllib.urlopen(url).read()
-    soup = BeautifulSoup(data)
+        self.add_bill(chamber, session, bill_id, '-title-fix-me-', best_url)
 
-    rows = soup.findAll('table')[5].findAll('tr')[1:]
-    for row in rows:
-        td = row.find('td')
-        bill_id = td.a.contents[0]
-        get_bill_info(session, bill_id)
+        # grab primary and cosponsors from table[6]
+        tables = bill_soup.findAll('table')
+        sponsor_rows = tables[6].findAll('tr')
+        sponsors = clean_legislators(sponsor_rows[1].td.contents[0])
+        for leg in sponsors:
+            self.add_sponsorship(chamber, session, bill_id, 'primary', leg)
+        cosponsors = clean_legislators(sponsor_rows[2].td.contents[0])
+        for leg in cosponsors:
+            self.add_sponsorship(chamber, session, bill_id, 'cosponsor', leg)
 
-def scrape_legislation(chamber, year):
-    year_mapping = {
-        #'1985': ('1985',),
-        #'1986': ('1985E1',),
-        #'1987': ('1987',),
-        #'1988': (),
-        #'1989': ('1989', '1989E1'),
-        #'1990': ('1989E2',),
-        #'1991': ('1991E1', '1991'),
-        #'1992': (),
-        #'1993': ('1993',),
-        #'1994': ('1993E1',),
-        #'1995': ('1995',),
-        '1996': ('1995E1', '1995E2'),
-        '1997': ('1997',),
-        '1998': ('1997E1',),
-        '1999': ('1999E1', '1999'),
-        '2000': ('1999E2',),
-        '2001': ('2001',),
-        '2002': ('2001E1',),
-        '2003': ('2003', '2002E1', '2003E2'),
-        '2004': ('2003E3',),
-        '2005': ('2005',),
-        '2006': (),
-        '2007': ('2007E1', '2007'),
-        '2008': ('2007E2',),
-        '2009': ('2009',),
-    }
-    chamber = {'lower':'House', 'upper':'Senate'}[chamber]
+        # easier to read actions from the rss.. but perhaps favor less HTTP requests?
+        rss_url = 'http://www.ncga.state.nc.us/gascripts/BillLookUp/BillLookUp.pl?Session=%s&BillID=%s&view=history_rss' % (session, bill_id)
+        rss_data = urllib.urlopen(rss_url).read()
+        rss_soup = BeautifulSoup(rss_data)
+        # title looks like 'House Chamber: action'
+        for item in rss_soup.findAll('item'):
+            action = item.title.contents[0]
+            pieces = item.title.contents[0].split(' Chamber: ')
+            if len(pieces) == 2:
+                action_chamber = pieces[0]
+                action = pieces[1]
+            else:
+                action_chamber = None
+                action = pieces[0]
+            date = item.pubdate.contents[0]
+            self.add_action(chamber, session, bill_id, action_chamber, action, date)
 
-    if year not in year_mapping:
-        raise NoDataForYear(year)
+    def scrape_session(self, chamber, session):
+        url = 'http://www.ncga.state.nc.us/gascripts/SimpleBillInquiry/displaybills.pl?Session=%s&tab=Chamber&Chamber=%s' % (session, chamber)
+        data = urllib.urlopen(url).read()
+        print url
+        print data
+        soup = BeautifulSoup(data)
 
-    for session in year_mapping[year]:
-        for bill in scrape_session(chamber, session):
-            yield bill
+        rows = soup.findAll('table')[5].findAll('tr')[1:]
+        for row in rows:
+            td = row.find('td')
+            bill_id = td.a.contents[0]
+            self.get_bill_info(session, bill_id)
 
-#if __name__ == '__main__':
-#    run_legislation_scraper(scrape_legislation)
+    def scrape_bills(self, chamber, year):
+        year_mapping = {
+            #'1985': ('1985',),
+            #'1986': ('1985E1',),
+            #'1987': ('1987',),
+            #'1988': (),
+            #'1989': ('1989', '1989E1'),
+            #'1990': ('1989E2',),
+            #'1991': ('1991E1', '1991'),
+            #'1992': (),
+            #'1993': ('1993',),
+            #'1994': ('1993E1',),
+            #'1995': ('1995',),
+            '1996': ('1995E1', '1995E2'),
+            '1997': ('1997',),
+            '1998': ('1997E1',),
+            '1999': ('1999E1', '1999'),
+            '2000': ('1999E2',),
+            '2001': ('2001',),
+            '2002': ('2001E1',),
+            '2003': ('2003', '2002E1', '2003E2'),
+            '2004': ('2003E3',),
+            '2005': ('2005',),
+            '2006': (),
+            '2007': ('2007E1', '2007'),
+            '2008': ('2007E2',),
+            '2009': ('2009',),
+        }
+        chamber = {'lower':'House', 'upper':'Senate'}[chamber]
+
+        if year not in year_mapping:
+            raise NoDataForYear(year)
+
+        for session in year_mapping[year]:
+            self.scrape_session(chamber, session)
+
+if __name__ == '__main__':
+    NCLegislationScraper().run()
--- scripts/pyutils/legislation.py
+++ scripts/pyutils/legislation.py
@@ -22,6 +22,7 @@ def run_legislation_scraper(get_bills_func):
         make_option('--lower', action='store_true', dest='lower', default=False,
                     help='scrape lower chamber'),
     )
+    raise DeprecationWarning('run_legislator_method is deprecated')
 
     options, spares = OptionParser(option_list=option_list).parse_args()
     years = options.years
@@ -47,4 +48,85 @@ def run_legislation_scraper(get_bills_func):
                     pass
                 else:
                     raise
+
+
+class LegislationScraper(object):
+    option_list = (
+        make_option('-y', '--year', action='append', dest='years',
+                    help='year(s) to scrape'),
+        make_option('--all', action='store_true', dest='all_years',
+                    default=False, help='scrape all data (overrides --year)'),
+        make_option('--upper', action='store_true', dest='upper', default=False,
+                    help='scrape upper chamber'),
+        make_option('--lower', action='store_true', dest='lower', default=False,
+                    help='scrape lower chamber'),
+    )
+    common_fields = ('bill_state', 'bill_chamber', 'bill_session', 'bill_id')
+    bill_fields = common_fields + ('bill_name', 'bill_url')
+    sponsor_fields = common_fields + ('sponsor_type', 'sponsor_name')
+    action_fields = common_fields + ('action_chamber', 'action_text', 'action_date')
+
+    def __init__(self):
+        if not hasattr(self, 'state'):
+            raise Exception('LegislationScrapers must have a state attribute')
+
+        bill_filename = 'data/%s/legislation.csv' % self.state
+        self.bill_csv = csv.DictWriter(open(bill_filename, 'w'), 
+                                       self.bill_fields, extrasaction='ignore')
+        sponsor_filename = 'data/%s/sponsorships.csv' % self.state
+        self.sponsor_csv = csv.DictWriter(open(sponsor_filename, 'w'),
+                                          self.sponsor_fields, 
+                                          extrasaction='ignore')
+        action_filename = 'data/%s/actions.csv' % self.state
+        self.action_csv = csv.DictWriter(open(action_filename, 'w'),
+                                         self.action_fields,
+                                         extrasaction='ignore')
+
+    def add_bill(self, bill_chamber, bill_session, bill_id, bill_name, bill_url, **kwargs):
+        row = {'bill_state': self.state, 'bill_chamber': bill_chamber,
+               'bill_session': bill_session, 'bill_id': bill_id,
+               'bill_name': bill_name, 'bill_url': bill_url}
+        row.update(kwargs)
+        self.bill_csv.writerow(row)
+
+    def add_sponsorship(self, bill_chamber, bill_session, bill_id, sponsor_type, sponsor_name, **kwargs):
+        row = {'bill_state': self.state, 'bill_chamber': bill_chamber,
+               'bill_session': bill_session, 'bill_id': bill_id,
+               'sponsor_type': sponsor_type, 'sponsor_name': sponsor_name}
+        row.update(kwargs)
+        self.sponsor_csv.writerow(row)
+
+    def add_action(self, bill_chamber, bill_session, bill_id, action_chamber, action_text, action_date, **kwargs):
+        row = {'bill_state': self.state, 'bill_chamber': bill_chamber,
+               'bill_session': bill_session, 'bill_id': bill_id,
+               'action_chamber': action_chamber, 'action_text': action_text,
+               'action_date': action_date}
+        row.update(kwargs)
+        self.action_csv.writerow(row)
+
+    def scrape_bills(self, chamber, year):
+        raise NotImplementedError('LegislatorScrapers must define a scrape_bills method')
+
+    def run(self):
+        options, spares = OptionParser(option_list=self.option_list).parse_args()
+        years = options.years
+        if options.all_years:
+            years = [str(y) for y in range(1969, datetime.datetime.now().year+1)]
+        chambers = []
+        if options.upper:
+            chambers.append('upper')
+        if options.lower:
+            chambers.append('lower')
+        if not chambers:
+            chambers = ['upper', 'lower']
+
+        for chamber in chambers:
+            for year in years:
+                try:
+                    self.scrape_bills(chamber, year)
+                except NoDataForYear, e:
+                    if options.all_years:
+                        pass
+                    else:
+                        raise
 
