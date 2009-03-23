5bf6a919ef90877c45ad3dfb1819d61b89b7b422
root <matt@neworganizing.com>
2009-03-23 21:39:23 +0000

    rewrote senate to work in class

--- scripts/mo/get_legislation.py
+++ scripts/mo/get_legislation.py
@@ -13,7 +13,17 @@ import sys
 sys.path.append('.')
 from pyutils.legislation import LegislationScraper, NoDataForYear
 
+def soup_web(url):
+    # get the file, parse it with BeautifulSoup
+    req = urllib2.Request(url)
+    response = urllib2.urlopen(req)
+    doc = response.read()
+    soup = BeautifulSoup(doc)#this gives us an index page
+    return soup
+    
+
 class MOLegislationScraper(LegislationScraper):
+    state = 'mo'
     def scrape_bills(self,chamber,year):
         print "chamber: %s, year: %s" % (chamber, year)
         if chamber == 'upper':
@@ -23,7 +33,7 @@ class MOLegislationScraper(LegislationScraper):
         else:
             print >> sys.stder, "Need to pick a chamber"
 
-    def scrape_senate(self,year)
+    def scrape_senate(self,year):
     
         #we only have data from 2005-2009
         assert int(year) >= 2005, NoDataForYear(year)
@@ -36,49 +46,88 @@ class MOLegislationScraper(LegislationScraper):
         root_url ='http://www.senate.mo.gov/'+year2+'info/BTS_Web/'
         index_file = root_url + 'BillList.aspx?SessionType=R'
     
-        # get the file, parse it with BeautifulSoup
-        req = urllib2.Request(index_file)
-        response = urllib2.urlopen(req)
-        doc = response.read()
-        soup = BeautifulSoup(doc)#this gives us an index page
-    
+        soup = soup_web(index_file)
     
         # each bill is in it's own table (nested in a larger table)
         bill_tables = soup.findAll(id="Table2")
     
         if bill_tables == None:
-            yield 
+            return
     
         for bill_table in bill_tables:
     
-            bill_id      = ''
-            bill_desc    = ''
+#            bill_id      = ''
+#            bill_desc    = ''
             bill_url     = ''
-            bill_sponsor = ''
+#            bill_sponsor = ''
     
-            bill_id_cell = bill_table.find(id=re.compile("BillNum"))
-            if bill_id_cell != None:
-                bill_id = bill_id_cell.b.font.string
-    
-            bill_desc_cell = bill_table.find(id=re.compile("BriefDesc"))
-            if bill_desc_cell != None:
-                bill_desc = bill_desc_cell.font.string
-    
-            bill_desc_cell = bill_table.find(id=re.compile("Sponsor"))
-            if bill_sponsor != None:
-                bill_sponsor = bill_desc_cell.b.font.string
-    
+#            bill_id_cell = bill_table.find(id=re.compile("BillNum"))
+#            if bill_id_cell != None:
+#                bill_id = bill_id_cell.b.font.string
+#    
+#            bill_desc_cell = bill_table.find(id=re.compile("BriefDesc"))
+#            if bill_desc_cell != None:
+#                bill_desc = bill_desc_cell.font.string
+#    
+#            bill_desc_cell = bill_table.find(id=re.compile("Sponsor"))
+#            if bill_sponsor != None:
+#                bill_sponsor = bill_desc_cell.b.font.string
+#    
             # here we just search the whole table string to get 
             # the BillID that the MO senate site uses
             m = re.search(r"BillID=(\d*)", str(bill_table))
             if m != None:
                 bill_web_id = m.group(1)
                 bill_url= root_url + 'Bill.aspx?SessionType=R&BillID='+bill_web_id
-    
-            yield {'state':'M0','chamber':'upper','session':year,
-                'bill_id':bill_id,'remote_url':bill_url,
-                'name':bill_desc,'bill_sponsor':bill_sponsor}
+                self.read_senate_billpage(bill_url, year)
 
+
+    def read_senate_billpage(self, bill_url, year):
+        soup = soup_web(bill_url)
+
+        # get all the info needed to record the bill
+        bill_id   = soup.find(id="lblBillNum").string
+	print bill_id
+        bill_name = soup.find(id="lblBillTitle").string
+        bill_desc = soup.find(id="lblBriefDesc").string
+        bill_lr   = soup.find(id="lblLRNum").string
+
+#        added_info = {'LR': bill_lr, 'desc':bill_desc}
+        self.add_bill('upper',year, bill_id, bill_name, bill_url,added_info, 'bill_lr'=bill_lr, 'bill_desc'=bill_desc)
+
+
+        # get the sponsors and cosponsors
+        bill_sponsor = soup.find(id="hlSponsor").string
+        bill_sponsor_link = soup.find(id="hlSponsor").href
+
+#        added_info = {'sponsor_link':bill_sponsor_link}
+        self.add_sponsorship('upper',year,bill_id,'primary',bill_sponsor,'sponsor_link'=bill_sponsor_link)
+
+        cosponsor_tag = soup.find(id="hlCoSponsors")
+        if cosponsor_tag != None:
+            self.read_senate_cosponsors(cosponsor_url.href, bill_id, year)
+
+        # get the actions
+        action_url = soup.find(id="hlAllActions").href
+        #TODO: parse senate actions
+        
+
+    def read_senate_cosponsors(url, bill_id, year):
+	print bill_id + ": sponsors"
+        soup = soup_web(url)
+        cosponsor_table = soup.find(id="dgCoSponsors")
+        cosponsors = cosponsor_table.findAll("tr")
+        for cosponsor_row in cosponsors:
+            #cosponsors include district, so parse that out
+            cosponsor_string = cosponsor_row.font.string
+            m = re.search("(.*),",cosponsor_string)
+            cosponsor = m.group(1)
+
+            cosponsor_url = cosponsor_row.a.href
+            #added_info = {'sponsor_link':cosponsor_url}
+            self.add_sponsorship('upper',year,bill_id,'cosponsor',cosponsor,'sponsor_link'=cosponsor_url)
+        
+    
 
     def scrape_house(year):
         chamber_abbr='s'
@@ -141,14 +190,14 @@ class MOLegislationScraper(LegislationScraper):
                         bill_sponsor = bill_link.nextSibling.next.next.string.strip()
                         bill_info = str(bill.nextSibling.next.next.next).strip()
     
-      bill_info = re.sub(r"(\r|\n|\s)+", " ", bill_info)
+                bill_info = re.sub(r"(\r|\n|\s)+", " ", bill_info)
     
-      # parse out the description and the LR number
-      m = re.match("(.*?)\(LR# (\d.*?)\)", bill_info, re.DOTALL)
-      bill_desc = m.group(1)
-      bill_lr = m.group(2)
+                # parse out the description and the LR number
+                m = re.match("(.*?)\(LR# (\d.*?)\)", bill_info, re.DOTALL)
+                bill_desc = m.group(1)
+                bill_lr = m.group(2)
     
-                    yield {'state':'M0','chamber':'lower','session':session,
+                yield {'state':'M0','chamber':'lower','session':session,
                         'bill_id':bill_id,'remote_url':bill_url,
                         'name':bill_desc,'bill_sponsor':bill_sponsor,
                         'lr':bill_lr }
