e77cf092aca71e1448ef1baff7fe8968de755692
Michael Stephens <mike@mindleak.com>
2009-03-24 06:22:13 +0800

    FL: work with new wrapper
    Signed-off-by: Sunlight Labs <git@sunlightlabs.com>

--- scripts/fl/get_legislation.py
+++ scripts/fl/get_legislation.py
@@ -6,65 +6,69 @@ from BeautifulSoup import BeautifulSoup
 
 # ugly hack
 import sys
-sys.path.append('.')
-from pyutils.legislation import run_legislation_scraper
+sys.path.append('./scripts')
+from pyutils.legislation import LegislationScraper, NoDataForYear
 
-def scrape_session(chamber, year, special=''):
-    # Data is available from 1998 on
-    assert int(year) >= 1998, "no data available before 1998"
-    assert int(year) <= dt.date.today().year, "can't look into the future"
+class FLLegislationScraper(LegislationScraper):
 
-    if chamber == 'upper':
-        chamber_name = 'Senate'
-        bill_abbr = 'S'
-    elif chamber == 'lower':
-        chamber_name = 'House'
-        bill_abbr = 'H'
+    state = 'fl'
 
-    # Base url for bills sorted by first letter of title
-    base_url = 'http://www.flsenate.gov/Session/index.cfm?Mode=Bills&BI_Mode=ViewBySubject&Letter=%s&Year=%s&Chamber=%s'
-    session = year + special
+    def scrape_session(self, chamber, year, special=''):
+        if chamber == 'upper':
+            chamber_name = 'Senate'
+            bill_abbr = 'S'
+        elif chamber == 'lower':
+            chamber_name = 'House'
+            bill_abbr = 'H'
 
-    # Bill ID format
-    bill_re = re.compile("%s (\d{4}%s)" % (bill_abbr, special))
+        # Base url for bills sorted by first letter of title
+        base_url = 'http://www.flsenate.gov/Session/index.cfm?Mode=Bills&BI_Mode=ViewBySubject&Letter=%s&Year=%s&Chamber=%s'
+        session = year + special
+
+        # Bill ID format
+        bill_re = re.compile("%s (\d{4}%s)" % (bill_abbr, special))
     
-    # Go through all sorted bill list pages
-    for letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
-        bill_list_url = base_url % (letter, session, chamber_name)
-        print bill_list_url
-        bill_list = BeautifulSoup(urllib2.urlopen(bill_list_url).read())
-        
-        # Bill ID's are bold
-        for b in bill_list.findAll('b'):
-            if not b.string:
-                continue
+        # Go through all sorted bill list pages
+        for letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+            bill_list_url = base_url % (letter, session, chamber_name)
+            print bill_list_url
+            bill_list = BeautifulSoup(urllib2.urlopen(bill_list_url).read())
             
-            match = bill_re.search(b.string)
-            if match:
-                # Bill ID and number
-                bill_id = match.group(0)
-                bill_number = match.group(1)
+            # Bill ID's are bold
+            for b in bill_list.findAll('b'):
+                if not b.string:
+                    continue
+            
+                match = bill_re.search(b.string)
+                if match:
+                    # Bill ID and number
+                    bill_id = match.group(0)
+                    bill_number = match.group(1)
 
-                # Get bill name
-                bill_name = b.parent.findNext('td').find('a').string.strip()
-                print "Getting %s: %s" % (bill_id, bill_name)
-                
-                # Generate bill text url
-                if chamber == 'upper':
-                    bill_file = "sb%s.html" % bill_number
-                else:
-                    # House bills have two extra 0's at end
-                    bill_file = "hb%s00.html" % bill_number
-                bill_url = 'http://www.flsenate.gov/cgi-bin/view_page.pl?File=%s&Directory=session/%s/%s/bills/billtext/html/' % (bill_file, session, chamber_name)
+                    # Get bill name
+                    bill_name = b.parent.findNext('td').find('a').string.strip()
+                    print "Getting %s: %s" % (bill_id, bill_name)
 
-                yield {'state':'FL', 'chamber':chamber, 'session':session,
-                       'bill_id':bill_id, 'remote_url':bill_url}
+                    # Generate bill text url
+                    if chamber == 'upper':
+                        bill_file = "sb%s.html" % bill_number
+                    else:
+                        # House bills have two extra 0's at end
+                        bill_file = "hb%s00.html" % bill_number
 
-def scrape_year(chamber, year):
-    # These are all the session types that I've seen
-    for session in ['', 'A', 'B', 'C', 'D', 'O']:
-        for bill in scrape_session(chamber, year, session):
-            yield bill
+                    bill_url = 'http://www.flsenate.gov/cgi-bin/view_page.pl?File=%s&Directory=session/%s/%s/bills/billtext/html/' % (bill_file, session, chamber_name)
+
+                    self.add_bill(chamber, session, bill_id,
+                                  bill_name, bill_url)
+
+    def scrape_bills(self, chamber, year):
+        # Data available for 1998 on
+        if int(year) < 1998 or int(year) > dt.date.today().year:
+            raise NoDataForYear(year)
+
+        # These are all the session types that I've seen
+        for session in ['', 'A', 'B', 'C', 'D', 'O']:
+            self.scrape_session(chamber, year, session)
 
 if __name__ == '__main__':
-    run_legislation_scraper(scrape_year)
+    FLLegislationScraper().run()
