4a3d7c58b7419b0347e2bf9fe119e8def51ff13f
Mr Man <mr@man.com>
2009-04-05 14:28:53 +0800

    Working version, with complete functionality
    Signed-off-by: Sunlight Labs <git@sunlightlabs.com>

--- scripts/nj/get_legislation.rb
+++ scripts/nj/get_legislation.rb
@@ -1,8 +1,10 @@
+# !/usr/bin/ruby
+# This module scrapes legislation for New Jersey
+
 module NewJersey
   require File.join(File.dirname(__FILE__), '..', 'rbutils', 'legislation')
-  require 'mechanize'
-  include Scrapable
   
+  include Scrapable
   
   class Bill
     attr_reader :bill_id, :year, :chamber, :actions, :versions, :sponsors, :session
@@ -17,18 +19,17 @@ module NewJersey
       @actions = []
       @session = "#{year} to #{year.to_i+1} Legislative Session"
     end
-    
-    
+
     def add_versions(version,link)
-      @versions << {:version => version,:link => "http://www.njleg.state.nj.us/Bills/#{link}"} 
+      @versions << {:version_name => version,:version_url => "http://www.njleg.state.nj.us#{link}"} 
     end
     
     def add_sponsor(sponsor, type)
-      @sponsors << {:sponsor => sponsor,:type => type}
+      @sponsors << {:sponsor_name => sponsor,:sponsor_type => type}
     end
     
     def add_action(date,action)
-      @actions << {:action => action,:date => date}
+      @actions << {:action_chamber=> @chamber, :action_text => action,:action_date => date}
     end
     
     def to_hash
@@ -45,40 +46,40 @@ module NewJersey
   
   class NJLegislationSite
     
-    def initialize (year, chamber)
+    attr_reader :record_count, :page_count
+    
+    def initialize (chamber,year)
+      @year = year
+      @chamber = chamber
+      
+      if (bill_db.nil?)
+        raise Scrapable::NoDataForYearError.new(year, "Please make sure year is even(i.e., 1996,1998,2000..) and inbetween 1996 and current year")
+      end
+      
       @base_uri = "http://www.njleg.state.nj.us"
       @agent = WWW::Mechanize.new
       @agent.get(@base_uri)
-      @billpage = fetch_bill_page() #Initializing the Session for that Year
-      @year = year
-      @chamber = chamber
+      @agent.post("#{@base_uri}/bills/bills0001.asp","DBNAME" => bill_db)
+      @billpage = fetch_bill_page
+      fetch_page_count
     end
     
-    def fetch_page_count
-      @billpage
-    end
     
-    def bill_numbers(page=1)
-      a_bill_numbers = []
-      s_bill_numbers = []
-      
-      if(@billpage.nil?)
-        doc = Hpricot(fetch_bill_page(page).body)
-          doc.search("//a[@title='View Detail Bill Information']").each do |bill_element|
-            bill_number = (bill_element.inner_html).to_s().slice(/[AS][0-9]{1,10}/)
-            get_chamber_from_bill_no(bill_number)=="lower" ? a_bill_numbers << bill_number : s_bill_numbers << bill_number        
-          end
-        @chamber == "lower" ? a_bill_numbers :  s_bill_numbers
-      else
-        nil
+    def fetch_bills_by_page(page)
+      result = []
+      _billnumbers = bill_numbers(page)
+      _billnumbers.each do |bill_id|
+        result << fetch_bill(bill_id)
       end
+      result
     end
     
     def fetch_bill(bill_id)
+      puts "Processing #{bill_id}"
       bill = Bill.new(bill_id,@year,@chamber)
       bdp = Hpricot(fetch_bill_detail_page(bill_id).body) #Fetching Bill Detail Page(bdp) for the bill
       
-      #Scraping Legislation
+      #Parsing Legislation
       bdp.search('//td[@bgcolor="#fede7e"]').each do |element|
         element.search('//font[@color="maroon"]').each do |l|
             bill.legislation = (l.inner_html).to_s.strip
@@ -139,18 +140,31 @@ module NewJersey
     end
     
     def fetch_bill_page(page=1)
-      if !bill_db.nil?
-        # Get the options page to the set the appropriate cookies
-        puts "Fetching for year #{@year}"
-        @agent.post("#{@base_uri}/bills/bills0001.asp","DBNAME" => bill_db)
-        @agent.post("#{@base_uri}/bills/BillsByNumber.asp","GoToPage" => page)
-      else
-        nil
-      end
+      !bill_db.nil? ? @agent.post("#{@base_uri}/bills/BillsByNumber.asp","GoToPage" => page) : nil
     end
     
     def fetch_bill_detail_page(bill_id)
       !bill_db.nil? ? @agent.post("#{@base_uri}/bills/BillView.asp","BillNumber" => bill_id, "LastSession" => "") : nil
+     end
+     
+     def fetch_page_count
+       doc = Hpricot(@billpage.body)   
+       temp = doc.search("//table[@height='780']//tr//td//table")[1].at("//tr/td/div/font/b/font")
+       page_totals = temp.to_plain_text.gsub(/\D/," ").strip.split(" ")
+       @record_count = page_totals[0].to_i
+       @page_count = page_totals[2].to_i
+     end
+
+
+     def bill_numbers(page=1)
+       a_bill_numbers = []
+       s_bill_numbers = []
+       doc = Hpricot(fetch_bill_page(page).body)
+       doc.search("//a[@title='View Detail Bill Information']").each do |bill_element|
+         bill_number = (bill_element.inner_html).to_s().slice(/[AS][0-9]{1,10}/)
+         get_chamber_from_bill_no(bill_number)=="lower" ? a_bill_numbers << bill_number : s_bill_numbers << bill_number        
+       end
+       @chamber == "lower" ? a_bill_numbers : s_bill_numbers
      end
   end
  
@@ -158,20 +172,28 @@ module NewJersey
     "nj"
   end
   
-  def self.scrape_bills(chamber,year)
-  njs = NJLegislationSite.new(year,chamber)
-    #njs.bill_numbers(year,chamber).each do |bill_no|
-      #bill = njs.fetch_bill(bill_no,year,chamber)
-      #pp bill
-    #end
-    njs.bill_numbers.each do |bill_id|
-       pp njs.fetch_bill(bill_id)
+  def self.scrape_bills(chamber, year)
+    njs = NJLegislationSite.new(chamber,year.to_i)  
+    puts "Total Records found: #{njs.record_count}"
+    njs.page_count.times do |page_no|
+      puts "Processing Page #{page_no+1} of #{njs.page_count}"
+      bills = njs.fetch_bills_by_page(page_no+1)
+      bills.each do |bill|
+        common_hash = bill.to_hash
+        add_bill(common_hash)
+        bill.sponsors.each do |sponsor|
+          add_sponsorship(common_hash.merge(sponsor))
+        end
+        bill.versions.each do |version|
+          add_bill_version(common_hash.merge(version))
+        end
+        bill.actions.each do |action|
+          add_action(common_hash.merge(action))
+        end
+      end
     end
-   
-    #puts njs.fetch_page_count(year)
-    #raise Scrapable::NoDataForYearError.new(@year, "Year has to be even number between 1996-2008")
   end
+  
 end
 
-
-NewJersey.scrape_bills("upper",2006)
+NewJersey.run
