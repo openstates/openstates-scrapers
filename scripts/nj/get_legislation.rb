becbc3a5e67cafcb431a1a0a36f6308ca7cab907
Mr Man <mr@man.com>
2009-04-05 08:27:07 +0800

    Refactored the code a little, improved performance
    Signed-off-by: Sunlight Labs <git@sunlightlabs.com>

--- scripts/nj/get_legislation.rb
+++ scripts/nj/get_legislation.rb
@@ -3,6 +3,7 @@ module NewJersey
   require 'mechanize'
   include Scrapable
   
+  
   class Bill
     attr_reader :bill_id, :year, :chamber, :actions, :versions, :sponsors, :session
     attr_writer :legislation
@@ -14,12 +15,9 @@ module NewJersey
       @versions = []
       @sponsors = []
       @actions = []
-      session
-    end
-    
-    def session
       @session = "#{year} to #{year.to_i+1} Legislative Session"
     end
+    
     
     def add_versions(version,link)
       @versions << {:version => version,:link => "http://www.njleg.state.nj.us/Bills/#{link}"} 
@@ -32,44 +30,53 @@ module NewJersey
     def add_action(date,action)
       @actions << {:action => action,:date => date}
     end
+    
+    def to_hash
+      {
+        :bill_state => 'nj',
+        :bill_chamber => @chamber,
+        :bill_session => @session,
+        :bill_id => @bill_id,
+        :bill_name => @legislation
+      }
+    end
   end
   
   
   class NJLegislationSite
     
-    def initialize
+    def initialize (year, chamber)
       @base_uri = "http://www.njleg.state.nj.us"
       @agent = WWW::Mechanize.new
       @agent.get(@base_uri)
+      @billpage = fetch_bill_page() #Initializing the Session for that Year
+      @year = year
+      @chamber = chamber
     end
     
-    def bill_numbers(year,chamber)
-      bp = fetch_bill_page(year) #This page is used for getting total no. of pages
+    def fetch_page_count
+      @billpage
+    end
+    
+    def bill_numbers(page=1)
       a_bill_numbers = []
       s_bill_numbers = []
-      pages = 20 #Extract Total Pages from Bill Page and store, currenlty testing for 5 Pages
       
-      if(!bp.nil?)
-        pages.times do |page|
-          doc = Hpricot(fetch_bill_page(year,page+1).body)
-            doc.search("//a[@title='View Detail Bill Information']").each do |bill_element|
-              bill_number = (bill_element.inner_html).to_s().slice(/[AS][0-9]{1,10}/)
-              if bill_number.include?("A")
-                a_bill_numbers << bill_number
-              else
-                s_bill_numbers << bill_number
-              end         
-            end
-        end
-        return chamber == :lower ? a_bill_numbers :  s_bill_numbers
+      if(@billpage.nil?)
+        doc = Hpricot(fetch_bill_page(page).body)
+          doc.search("//a[@title='View Detail Bill Information']").each do |bill_element|
+            bill_number = (bill_element.inner_html).to_s().slice(/[AS][0-9]{1,10}/)
+            get_chamber_from_bill_no(bill_number)=="lower" ? a_bill_numbers << bill_number : s_bill_numbers << bill_number        
+          end
+        @chamber == "lower" ? a_bill_numbers :  s_bill_numbers
       else
         nil
       end
     end
     
-    def fetch_bill(bill_id,year,chamber)
-      bill = Bill.new(bill_id,year,chamber)
-      bdp = Hpricot(fetch_bill_detail_page(bill_id,year).body) #Fetching Bill Detail Page(bdp) for the bill
+    def fetch_bill(bill_id)
+      bill = Bill.new(bill_id,@year,@chamber)
+      bdp = Hpricot(fetch_bill_detail_page(bill_id).body) #Fetching Bill Detail Page(bdp) for the bill
       
       #Scraping Legislation
       bdp.search('//td[@bgcolor="#fede7e"]').each do |element|
@@ -114,8 +121,8 @@ module NewJersey
     
     private
     
-    def get_bill_db(year)
-        case year
+    def bill_db
+        case @year
           when 1996; "LIS9697"
           when 1998; "LIS9899"
           when 2000; "LIS2000"
@@ -127,40 +134,44 @@ module NewJersey
         end
     end
     
-    def fetch_bill_page(year, page=1)
-      year_db=get_bill_db(year)
-      if !year_db.nil?
+    def get_chamber_from_bill_no(bill_no)
+      bill_no.include?("A") ? "lower" : "upper"
+    end
+    
+    def fetch_bill_page(page=1)
+      if !bill_db.nil?
         # Get the options page to the set the appropriate cookies
-        @agent.post("#{@base_uri}/bills/bills0001.asp","DBNAME" => year_db)
-        return @agent.post("#{@base_uri}/bills/BillsByNumber.asp","GoToPage" => page)
+        puts "Fetching for year #{@year}"
+        @agent.post("#{@base_uri}/bills/bills0001.asp","DBNAME" => bill_db)
+        @agent.post("#{@base_uri}/bills/BillsByNumber.asp","GoToPage" => page)
       else
-        raise Scrapable::NoDataForYearError.new(year, "Year has to be even number between 1996-2008")
+        nil
       end
     end
     
-    def fetch_bill_detail_page(bill_id,year)
-      #Don't call fetch_bill_page here, even though some code is repeated, avoid calling bills0001.asp again
-      year_db=get_bill_db(year)
-      if !year_db.nil?
-        @agent.post("#{@base_uri}/bills/bills0001.asp","DBNAME" => year_db)
-        return @agent.post("#{@base_uri}/bills/BillView.asp","BillNumber" => bill_id, "LastSession" => "")
-      else
-        raise Scrapable::NoDataForYearError.new(year, "Year has to be even number between 1996-2008")
-      end
-    end
+    def fetch_bill_detail_page(bill_id)
+      !bill_db.nil? ? @agent.post("#{@base_uri}/bills/BillView.asp","BillNumber" => bill_id, "LastSession" => "") : nil
+     end
   end
  
-  def self.scrape_bills
-  njs = NJLegislationSite.new
-    njs.bill_numbers(2006,:lower).each do |bno|
-      puts "==  Bill  =="
-      bill = njs.fetch_bill(bno,2006,:lower)
-      pp bill
+  def self.state
+    "nj"
+  end
+  
+  def self.scrape_bills(chamber,year)
+  njs = NJLegislationSite.new(year,chamber)
+    #njs.bill_numbers(year,chamber).each do |bill_no|
+      #bill = njs.fetch_bill(bill_no,year,chamber)
+      #pp bill
+    #end
+    njs.bill_numbers.each do |bill_id|
+       pp njs.fetch_bill(bill_id)
     end
    
-    #pp njs.fetch_bill("A15",2008,:lower)
+    #puts njs.fetch_page_count(year)
+    #raise Scrapable::NoDataForYearError.new(@year, "Year has to be even number between 1996-2008")
   end
 end
 
 
-NewJersey.scrape_bills
+NewJersey.scrape_bills("upper",2006)
