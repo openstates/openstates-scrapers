b1a0d54d61d244697bb5b054dd9af7cb360dd70c
Michael Stephens <mike@mindleak.com>
2009-05-31 05:33:37 -0400

    grab biographical data on state legislators from votesmart

---
+++ backend/get_bio.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+from couchdb.client import Server
+import argparse
+from votesmart import votesmart
+
+
+votesmart.apikey = 'API_KEY'
+
+
+def get_bio(state, year, verbose=False):
+
+    def log(msg):
+        if verbose:
+            print "%s: %s" % (state, msg)
+
+    # Setup couch connection
+    server = Server('http://localhost:5984')
+    assert state in server
+    db = server[state]
+
+    # Get list of reps from votesmart
+    candidates = votesmart.candidates.getByOfficeState(8, state.upper(),
+                                                       electionYear=year)
+    elected = filter(lambda cand: cand.electionStatus == 'Won', candidates)
+    officials = {}
+    for official in elected:
+        if official.lastName in officials:
+            officials[official.lastName].append(official)
+        else:
+            officials[official.lastName] = [official]
+
+    # Go through CouchDB legislators
+    for leg in db.view('app/leg-by-year', include_docs=True)[int(year)]:
+        doc = leg.doc
+
+        if doc['last_name'] in officials:
+            for match in officials[doc['last_name']]:
+                # This is not state-agnostic
+                if match.title == 'Representative':
+                    chamber = 'lower'
+                else:
+                    chamber = 'upper'
+
+                if (match.firstName == doc['first_name'] and
+                    chamber == doc['chamber']):
+                    # Found match
+                    log("Getting bio data for %s (%s)" %
+                        (doc['fullname'], doc['chamber']))
+
+                    bio = votesmart.candidatebio.getBio(match.candidateId)
+                    doc['votesmart'] = {}
+                    for key in bio.__dict__:
+                        doc['votesmart'][key] = bio.__dict__[key]
+
+                    db.update([doc])
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Grab biographical information on state legislators")
+    parser.add_argument('--year', '-y', action='append')
+    parser.add_argument('states', metavar='state', nargs='+',
+                        help='the state(s) to import')
+    parser.add_argument('-v', '--verbose', action='store_true')
+    # TODO: couch host option, maybe option for non-default DB name
+
+    args = parser.parse_args()
+
+    for state in args.states:
+        for year in args.year:
+            get_bio(state, year, args.verbose)
