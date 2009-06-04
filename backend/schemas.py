42c15eb708d0b324c5c1560e8ff0a3581ea686e3
Michael Stephens <mike@mindleak.com>
2009-06-03 20:39:48 -0400

    bill and metadata schemas

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,7 +1,48 @@
 #!/usr/bin/env python
-from couchdb.schema import Document, TextField, ListField, View
+from couchdb.schema import *
+
+# Some of the code in this file relies on patches I've made to couchdb-python.
+# They've been submitted upstream but until they're applied you can grab
+# them from:
+# http://code.google.com/p/couchdb-python/issues/detail?id=76
+# http://code.google.com/p/couchdb-python/issues/detail?id=77
+
+class StateMetadata(Document):
+    type = TextField(default='state_metadata')
+    state_name = TextField()
+    legislature_name = TextField()
+    lower_chamber_name = TextField()
+    upper_chamber_name = TextField()
+    lower_title = TextField()
+    upper_title = TextField()
+    lower_term = IntegerField()
+    upper_term = IntegerField()
+    sessions = ListField(TextField())
+    session_details = DictField()
+
+    @classmethod
+    def get(cls, db):
+        # There should only be one document of this type, named state_medata
+        return cls.load(db, 'state_metadata')
+
+    def sessions_adjacent(self, session1, session2):
+        """
+        Returns true if session1 and session2 ocurred consecutively (in
+        either order).
+        """
+        if session1 in self.sessions:
+            index = self.sessions.index(session1)
+            if index > 0 and self.sessions[index - 1] == session2:
+                return True
+            if ((index + 1) < len(self.sessions) and
+                self.sessions[index + 1] == session2):
+                return True
+        return False
+
 
 class Legislator(Document):
+    type = TextField(default='legislator')
+    state = TextField()
     fullname = TextField()
     first_name = TextField()
     last_name = TextField()
@@ -11,3 +52,41 @@ class Legislator(Document):
     chamber = TextField()
     district = TextField()
     sessions = ListField(TextField())
+
+
+class Bill(Document):
+    type = TextField(default='bill')
+    state = TextField()
+    bill_id = TextField()
+    chamber = TextField()
+    session = TextField()
+    title = TextField()
+
+    sponsors = ListField(DictField(Schema.build(
+        leg_id=TextField(),
+        type=TextField(),
+        name=TextField())))
+
+    versions = ListField(DictField(Schema.build(
+        url=TextField(),
+        name=TextField())))
+
+    votes = ListField(DictField(Schema.build(
+        date=TextField(),
+        threshold=TextField(),
+        motion=TextField(),
+        chamber=TextField(),
+        location=TextField(),
+        passed=BooleanField(),
+        yes_count=TextField(),
+        no_count=TextField(),
+        other_count=TextField(),
+        yes_votes=ListField(DictField(Schema.build(
+            leg_id=TextField(),
+            name=TextField()))),
+        no_votes=ListField(DictField(Schema.build(
+            leg_id=TextField(),
+            name=TextField()))),
+        other_votes=ListField(DictField(Schema.build(
+            leg_id=TextField(),
+            name=TextField()))))))
