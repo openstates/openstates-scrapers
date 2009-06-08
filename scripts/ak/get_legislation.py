661b8eee112cae91fc9216961955fe5d87bbe37e
Michael Stephens <mike@mindleak.com>
2009-06-08 06:29:23 -0400

    AK: new style, scrape_legislators

--- scripts/ak/get_legislation.py
+++ scripts/ak/get_legislation.py
@@ -7,37 +7,98 @@ import html5lib
 # ugly hack
 import sys
 sys.path.append('./scripts')
-from pyutils.legislation import LegislationScraper, NoDataForYear
+from pyutils.legislation import *
 
 class AKLegislationScraper(LegislationScraper):
 
     state = 'ak'
     soup_parser = html5lib.HTMLParser(tree=html5lib.treebuilders.getTreeBuilder('beautifulsoup')).parse
 
-    def __init__(self):
-        super(AKLegislationScraper, self).__init__()
+    metadata = {
+        'state_name': 'Alaska',
+        'legislature_name': 'The Alaska State Legislature',
+        'lower_chamber_name': 'House of Representatives',
+        'upper_chamber_name': 'Senate',
+        'lower_title': 'Representative',
+        'upper_title': 'Senator',
+        'lower_term': 2,
+        'upper_term': 4,
+        'sessions': ['18', '19', '20', '21', '22', '23', '24',
+                     '25', '26'],
+        'session_details': {
+            '18': {'years': [1993, 1994], 'sub_sessions': [],
+                   'election_year': 1992},
+            '19': {'years': [1995, 1996], 'sub_sessions': [],
+                   'election_year': 1994},
+            '20': {'years': [1997, 1998], 'sub_sessions': [],
+                   'election_year': 1996},
+            '21': {'years': [1999, 2000], 'sub_sessions': [],
+                   'election_year': 1998},
+            '22': {'years': [2001, 2002], 'sub_sessions': [],
+                   'election_year': 2000},
+            '23': {'years': [2003, 2004], 'sub_sessions': [],
+                   'election_year': 2002},
+            '24': {'years': [2005, 2006], 'sub_sessions': [],
+                   'election_year': 2004},
+            '25': {'years': [2007, 2008], 'sub_sessions': [],
+                   'election_year': 2006},
+            '26': {'years': [2009, 2010], 'sub_sessions': [],
+                   'election_year': 2008},
+        }}
 
-        self.bill_subject_fields = ('bill_state', 'bill_chamber', 'bill_session',
-                                    'bill_id', 'bill_subject')
-        bill_subject_filename = 'data/%s/subjects.csv' % self.state
-        self.subject_csv = csv.DictWriter(open(bill_subject_filename, 'w'),
-                                          self.bill_subject_fields,
-                                          extrasaction='ignore')
+    def scrape_legislators(self, chamber, year):
+        # Data available for 1993 on
+        if int(year) < 1993 or int(year) > dt.date.today().year:
+            raise NoDataForYear(year)
 
-    def add_subject(self, bill_chamber, bill_session, bill_id, bill_subject,
-                    **kwargs):
-        row = {'bill_state': self.state, 'bill_chamber': bill_chamber,
-               'bill_session': bill_session,
-               'bill_id': bill_id, 'bill_subject': bill_subject}
-        row.update(kwargs)
-        self.subject_csv.writerow(row)
+        # Expect first year of session (odd)
+        if int(year) % 2 != 1:
+            raise NoDataForYear(year)
+
+        if chamber == 'upper':
+            chamber_abbr = 'H'
+        else:
+            chamber_abbr = 'S'
+
+        session = 18 + ((int(year) - 1993) / 2)
+
+        leg_list_url = "http://www.legis.state.ak.us/basis/commbr_info.asp?session=%d" % session
+        leg_list = self.soup_parser(self.urlopen(leg_list_url))
+
+        leg_re = "get_mbr_info.asp\?member=.+&house=%s&session=%d" % (
+            chamber_abbr, session)
+        links = leg_list.findAll(href=re.compile(leg_re))
+
+        for link in links:
+            member_url = "http://www.legis.state.ak.us/basis/" + link['href']
+            member_page = self.soup_parser(self.urlopen(member_url))
+
+            full_name = member_page.findAll('h3')[1].contents[0]
+            full_name = ' '.join(full_name.split(' ')[1:])
+            full_name = re.sub('\s+', ' ', full_name).strip()
+
+            first_name = full_name.split(' ')[0]
+            last_name = full_name.split(' ')[-1]
+            middle_name = ' '.join(full_name.split(' ')[1:-1])
+
+            code = link['href'][24:27]
+
+            district = member_page.find(text=re.compile("District:"))
+            district = district.strip().split(' ')[-1]
+
+            party = member_page.find(text=re.compile("Party: "))
+            party = ' '.join(party.split(' ')[1:])
+
+            self.add_legislator(Legislator(session, chamber, district,
+                                           full_name, first_name,
+                                           last_name, middle_name,
+                                           party=party, code=code))
 
     def scrape_session(self, chamber, year):
-        # What about joint resolutions, etc.? Just ignoring them for now.
         if chamber == 'upper':
-            bill_abbr = 'SB'
+            bill_abbr = 'SB|SCR|SJR'
         elif chamber == 'lower':
-            bill_abbr = 'HB'
+            bill_abbr = 'HB|HCR|HJR'
 
         # Sessions last 2 years, 1993-1994 was the 18th
         session = 18 + ((int(year) - 1993) / 2)
@@ -49,7 +110,8 @@ class AKLegislationScraper(LegislationScraper):
 
         # Get bill list
         bill_list_url = 'http://www.legis.state.ak.us/basis/range_multi.asp?session=%i&date1=%s&date2=%s' % (session, date1, date2)
-        self.be_verbose("Getting bill list for %s %s (this may take a long time)." % (chamber, session))
+        self.log("Getting bill list for %s %s (this may take a long time)." %
+                 (chamber, session))
         bill_list = self.soup_parser(self.urlopen(bill_list_url))
 
         # Find bill links
@@ -59,7 +121,7 @@ class AKLegislationScraper(LegislationScraper):
         for link in links:
             bill_id = link.contents[0].replace(' ', '')
             bill_name = link.parent.parent.findNext('td').find('font').contents[0].strip()
-            self.add_bill(chamber, session, bill_id, bill_name.strip())
+            bill = Bill(session, chamber, bill_id, bill_name.strip())
 
             # Get the bill info page and strip malformed t
             info_url = "http://www.legis.state.ak.us/basis/%s" % link['href']
@@ -73,15 +135,13 @@ class AKLegislationScraper(LegislationScraper):
                 spons_str)
             if sponsors_match:
                 sponsors = sponsors_match.group(2).split(',')
-                self.add_sponsorship(chamber, session, bill_id, 'primary',
-                                     sponsors[0].strip())
+                bill.add_sponsor('primary', sponsors[0].strip())
+
                 for sponsor in sponsors[1:]:
-                    self.add_sponsorship(chamber, session, bill_id, 'cosponsor',
-                                         sponsor.strip())
+                    bill.add_sponsor('cosponsor', sponsor.strip())
             else:
                 # Committee sponsorship
-                self.add_sponsorship(chamber, session, bill_id, 'primary',
-                                     spons_str.strip())
+                bill.add_sponsor('committee', spons_str.strip())
 
             # Get actions
             act_rows = info_page.findAll('table', 'myth')[1].findAll('tr')[1:]
@@ -98,14 +158,14 @@ class AKLegislationScraper(LegislationScraper):
 
                 action = cols[3].font.contents[0].strip()
 
-                self.add_action(chamber, session, bill_id, act_chamber,
-                                action, act_date)
+                bill.add_action(act_chamber, action, act_date)
 
             # Get subjects
+            bill['subjects'] = []
             subject_link_re = re.compile('.*subject=\w+$')
             for subject_link in info_page.findAll('a', href=subject_link_re):
                 subject = subject_link.contents[0].strip()
-                self.add_subject(chamber, session, bill_id, subject)
+                bill['subjects'].append(subject)
 
             # Get versions
             text_list_url = "http://www.legis.state.ak.us/basis/get_fulltext.asp?session=%s&bill=%s" % (session, bill_id)
@@ -114,8 +174,9 @@ class AKLegislationScraper(LegislationScraper):
             for text_link in text_list.findAll('a', href=text_link_re):
                 text_name = text_link.parent.previousSibling.contents[0].strip()
                 text_url = "http://www.legis.state.ak.us/basis/%s" % text_link['href']
-                self.add_bill_version(chamber, session, bill_id,
-                                      text_name, text_url)
+                bill.add_version(text_name, text_url)
+
+            self.add_bill(bill)
 
     def scrape_bills(self, chamber, year):
         # Data available for 1993 on
