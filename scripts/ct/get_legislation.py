03f9c2d187797c815c4f1fc357ca80d8f0c9666b
bx <bx@dunkel.(none)>
2009-06-26 05:30:39 +0800

    ct voting in progress
    Signed-off-by: Sunlight Labs <git@sunlightlabs.com>

--- scripts/ct/get_legislation.py
+++ scripts/ct/get_legislation.py
@@ -15,8 +15,17 @@ import sys
 sys.path.append('./scripts')
 from pyutils.legislation import *
 
-
-
+class CTVote():
+    def __init__():
+        url=None
+        date=None
+        yeas=None
+        neys=None
+        others=None
+        passed=None
+        chamber=None
+        url=None
+        
 class CTLegislationScraper(LegislationScraper):
     #some constants
     state = 'ct'
@@ -120,7 +129,75 @@ class CTLegislationScraper(LegislationScraper):
         #not all bills have votes
         vote_table = soup.find("table",{"id":"CGAVotes"})
         if vote_table != None:
-            print vote_table
+            for line in vote_table:
+                line = str(line)
+                line = line.strip()
+                if (len(line) > 0) and (line.find("CGAWhiteOnBlue") < 0):
+                    regexp = re.compile("href=\"(\S*)\"")
+                    vote_url = regexp.search(line)
+                    vote_url = vote_url.group(1)
+                    vote_url = "http://cga.ct.gov"+vote_url
+                    self.scrape_votes(vote_url)
+
+        print "-----------------------"
+                
+
+    #url is the url where the vote info page is.  Returns Vote object
+    def scrape_votes(self,url):
+        soup = BeautifulSoup(urllib2.urlopen(urllib2.Request(url)).read())
+        date=None
+        motion=None
+        yays=-1
+        neys=-1
+        others=-1
+        passed=None
+        chamber=None
+        
+        fonts = soup.findAll('font')
+        if len(fonts) > 4: #data is vaguely structured
+            for line in fonts:
+                #this could be sped up.
+                line = str(line.contents)
+                line = line.strip()
+                if line.find("Taken on") > -1:
+                    #then the text is in the form of: "Take on <date> <reason>"
+                    split = line.split(None,3)
+                    date = split[2]
+                    motion=split[3]
+                    print split
+                elif line.find("Those voting Yea") > -1:
+                    print line + "- yea"
+                elif line.find("Those voting Nay") > -1:
+                    print line + "- nay"
+                elif line.find("Those absent and not voting") > -1:
+                    print line + "- other"
+                elif (line.find("Necessary for Adoption") > -1) or (line.find("Necessary for Passage") > -1):
+                    print line + "- necessary"
+        else:
+            #data is mostly unstructured. Have to sift through a string
+            data = soup.find('pre')
+            #print data.contents
+            lines = data.contents[len(data.contents)-1]
+            lines = lines.strip()
+            exp = re.compile(r'\n+|\r+|\f+')
+            lines = exp.split(lines)
+            print lines
+            line=""
+            if line.find("Taken on") > -1:
+                #then the text is in the form of: "Take on <date> <reason>"
+                split = line.split(None,3)
+                date = split[2]
+                motion=split[3]
+                print split
+            elif line.find("Those voting Yea") > -1:
+                print line + "- yea"
+            elif line.find("Those voting Nay") > -1:
+                print line + "- nay"
+            elif line.find("Those absent and not voting") > -1:
+                print line + "- other"
+            elif (line.find("Necessary for Adoption") > -1) or (line.find("Necessary for Passage") > -1):
+                print line + "- necessary"
+
 
 
 #all CT bill pages have a bug in them (three quotation marks in a row 
@@ -128,5 +205,7 @@ class CTLegislationScraper(LegislationScraper):
 #returns a version of the html without this bug
 def cleanup_html(html):
     return html.replace('"""','"')
+
+
 if __name__ == '__main__':
     CTLegislationScraper().run()
