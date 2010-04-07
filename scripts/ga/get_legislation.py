1be1f091fcd2fea546e6eb0d3a73cdfce28cf43c
Michael Stephens <mstephens@sunlightfoundation.com>
2010-04-07 02:22:15 -0400

    GA: consistent session names

--- scripts/ga/get_legislation.py
+++ scripts/ga/get_legislation.py
@@ -50,17 +50,17 @@ class GALegislationScraper(LegislationScraper):
         'lower_title': 'Representative',
         'upper_term': 2,
         'lower_term': 2,
-        'sessions': ['1995-1996', '1997-1998', '1999-2000', '2001-2002',
-                     '2003-2004', '2005-2006', '2007-2008', '2009-2010',],
+        'sessions': ['1995_1996', '1997_1998', '1999_2000', '2001_2002',
+                     '2003_2004', '2005_2006', '2007_2008', '2009_2010',],
         'session_details': {
-            '1995-1996': {'years': [1995, 1996], 'sub_sessions': []},
-            '1997-1998': {'years': [1997, 1998], 'sub_sessions': []},
-            '1999-2000': {'years': [1999, 2000], 'sub_sessions': []},
-            '2001-2002': {'years': [2001, 2002], 'sub_sessions': []},
-            '2003-2004': {'years': [2003, 2004], 'sub_sessions': []},
-            '2005-2006': {'years': [2005, 2006], 'sub_sessions': []},
-            '2007-2008': {'years': [2007, 2008], 'sub_sessions': []},
-            '2009-2010': {'years': [2009, 2010], 'sub_sessions': []},
+            '1995_1996': {'years': [1995, 1996], 'sub_sessions': []},
+            '1997_1998': {'years': [1997, 1998], 'sub_sessions': []},
+            '1999_2000': {'years': [1999, 2000], 'sub_sessions': []},
+            '2001_2002': {'years': [2001, 2002], 'sub_sessions': []},
+            '2003_2004': {'years': [2003, 2004], 'sub_sessions': []},
+            '2005_2006': {'years': [2005, 2006], 'sub_sessions': []},
+            '2007_2008': {'years': [2007, 2008], 'sub_sessions': []},
+            '2009_2010': {'years': [2009, 2010], 'sub_sessions': []},
             }
         }
 
