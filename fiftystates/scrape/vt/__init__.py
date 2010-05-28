a3259777da89eb26bb00bcf295fff7c1ea0a4053
Michael Stephens <mstephens@sunlightfoundation.com>
2010-05-28 09:59:07 -0400

    VT metadata

---
+++ fiftystates/scrape/vt/__init__.py
@@ -0,0 +1,20 @@
+metadata = dict(
+    state_name='Vermont',
+    legislature_name='Vermont General Assembly',
+    upper_chamber_name='Senate',
+    lower_chamber_name='House of Representatives',
+    upper_title='Senator',
+    lower_title='Representative',
+    upper_term=2,
+    lower_term=2,
+    sessions=[],
+    session_details={},
+)
+
+# Populate 'sessions' and 'session_details'
+for year in [y for y in xrange(1987, 2010) if y % 2]:
+    session = "%d-%d" % (year, year + 1)
+    metadata['sessions'].append(session)
+    metadata['session_details'][session] = dict(
+        years=(year, year + 1),
+        sub_sessions=[])
