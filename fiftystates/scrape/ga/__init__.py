f5d51ed6c939c0844c75e292bf9407519a773f21
James Turk <james.p.turk@gmail.com>
2010-06-08 17:17:30 -0400

    move GA to new style

---
+++ fiftystates/scrape/ga/__init__.py
@@ -0,0 +1,22 @@
+metadata = {
+    'state_name': 'Georgia',
+    'legislature_name': 'Georgia General Assembly',
+    'upper_chamber_name': 'Senate',
+    'lower_chamber_name': 'House of Representatives',
+    'upper_title': 'Senator',
+    'lower_title': 'Representative',
+    'upper_term': 2,
+    'lower_term': 2,
+    'sessions': ['1995_1996', '1997_1998', '1999_2000', '2001_2002',
+                 '2003_2004', '2005_2006', '2007_2008', '2009_2010',],
+    'session_details': {
+        '1995_1996': {'years': [1995, 1996], 'sub_sessions': []},
+        '1997_1998': {'years': [1997, 1998], 'sub_sessions': []},
+        '1999_2000': {'years': [1999, 2000], 'sub_sessions': []},
+        '2001_2002': {'years': [2001, 2002], 'sub_sessions': []},
+        '2003_2004': {'years': [2003, 2004], 'sub_sessions': []},
+        '2005_2006': {'years': [2005, 2006], 'sub_sessions': []},
+        '2007_2008': {'years': [2007, 2008], 'sub_sessions': []},
+        '2009_2010': {'years': [2009, 2010], 'sub_sessions': []},
+        }
+    }
