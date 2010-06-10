8223bd5b32d8921845930697a795ce2b59712f37
James Turk <james.p.turk@gmail.com>
2010-06-09 20:07:15 -0400

    ND new style

---
+++ fiftystates/scrape/nd/__init__.py
@@ -0,0 +1,28 @@
+metadata = {
+    'state': 'nd',
+    'state_name': 'North Dakota',
+    'legislature_name': 'North Dakota Legislative Assembly',
+    'upper_chamber_name': 'Senate',
+    'lower_chamber_name': 'House of Representatives',
+    'upper_title': 'Senator',
+    'lower_title': 'Representative',
+    'upper_term': 4,
+    'lower_term': 4,
+    'sessions': ['1997', '1999', '2001', '2003', '2005', '2007', '2009'],
+    'session_details': {
+        '1997': {'years': [1997, 1998], 'sub_sessions': [], 
+                 'alternate': '55th', 'number': 55},
+        '1999': {'years': [1999, 2000], 'sub_sessions': [], 
+                 'alternate': '56th', 'number': 56},
+        '2001': {'years': [2001, 2002], 'sub_sessions': [], 
+                 'alternate': '57th', 'number': 57},
+        '2003': {'years': [2003, 2004], 'sub_sessions': [], 
+                 'alternate': '58th', 'number': 58},
+        '2005': {'years': [2005, 2006], 'sub_sessions': [], 
+                 'alternate': '59th', 'number': 59},
+        '2007': {'years': [2007, 2008], 'sub_sessions': [], 
+                 'alternate': '60th', 'number': 60},
+        '2009': {'years': [2009, 2010], 'sub_sessions': [], 
+                 'alternate': '61th', 'number': 61},
+        }
+}
