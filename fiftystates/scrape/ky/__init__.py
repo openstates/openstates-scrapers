ccb9bc9727a59e6261efce33918ba1bdf47cc61e
James Turk <james.p.turk@gmail.com>
2010-06-09 21:27:15 -0400

    KY new style

---
+++ fiftystates/scrape/ky/__init__.py
@@ -0,0 +1,14 @@
+metadata = {
+    'state_name': 'Kentucky',
+    'legislature_name': 'Kentucky General Assembly',
+    'lower_chamber_name': 'House of Representatives',
+    'upper_chamber_name': 'Senate',
+    'lower_title': 'Representative',
+    'upper_title': 'Senator',
+    'lower_term': 2,
+    'upper_term': 4,
+    'sessions': ['2009'],
+    'session_details': {
+        '2009': {'years': [2009],
+                 'sub_sessions': ['2009 Special Session']}}}
+
