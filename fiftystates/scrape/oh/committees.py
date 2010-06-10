08e074864ff09ad8f3f7d1cecce5f1fd3be9e6fb
Dan Schneiderman <dschneiderman@sunlightfoundation.com>
2010-06-10 16:55:32 -0400

    Finished scraping Senate and Joint commitees.

--- fiftystates/scrape/oh/committees.py
+++ fiftystates/scrape/oh/committees.py
@@ -1,7 +1,7 @@
 import re
 
 from fiftystates.scrape import NoDataForYear
-from fiftystates.scrape.committees import Scraper, Scrape
+from fiftystates.scrape.committees import CommitteeScraper, Committee
 from fiftystates.scrape.oh.utils import clean_committee_name
 
 import lxml.etree
@@ -23,20 +23,39 @@ class OHCommitteeScraper(CommitteeScraper):
 
     def scrape_reps_comm(self, chamber, year):
 
-        #id range for senate committees on their website
-        for id in range(92, 123):
+        save_chamber = chamber
 
-            comm_url = 'http://www.house.state.oh.us/index.php?option=com_displaycommittees&task=2&type=Regular&committeeId=' + str(id)
+        #id range for senate committees on their website
+        for comm_id in range(87, 123):
+
+            chamber = save_chamber
+
+            comm_url = 'http://www.house.state.oh.us/index.php?option=com_displaycommittees&task=2&type=Regular&committeeId=' + str(comm_id)
             with self.urlopen(comm_url) as page:
                 root = lxml.etree.fromstring(page, lxml.etree.HTMLParser())
 
+                comm_name = root.xpath('string(//table/tr[@class="committeeHeader"]/td)')
+                comm_name = comm_name.replace("/", " ")                
+                
+                #joint legislative committiees
+                if comm_id < 92:
+                    chamber = "joint_legislation"
 
-                comm_name = root.xpath('string(div[@class="componentheading"])')
                 committee = Committee(chamber, comm_name)
-
-                for el in root.xpath('table/tr/td[@class="committee"]/a'):
-                    rep = el.text
-                    committee.add_member(rep)
+               
+                path = '/html/body[@id="bd"]/div[@id="ja-wrapper"]/div[@id="ja-containerwrap-f"]/div[@id="ja-container"]/div[@id="ja-mainbody-f"]/div[@id="ja-contentwrap"]/div[@id="ja-content"]/table/tr[position() >=3]'
+                
+                for el in root.xpath(path):
+                    rep1 = el.xpath('string(td[1]/a)')
+                    rep2 = el.xpath('string(td[4]/a)')
+                    committee.add_member(rep1)
+                    committee.add_member(rep2)
+                    
+                committee.add_source(comm_url)
+                self.save_committee(committee)
+                #print "\n"
+                #print committee
+                #print "\n"
 
 
     def scrape_senate_comm(self, chamber, year):
