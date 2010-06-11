f17698474582f926fc50a20ce82d10e16487c099
Dan Schneiderman <dschneiderman@sunlightfoundation.com>
2010-06-11 15:11:21 -0400

    Finshed House committees

--- fiftystates/scrape/me/committees.py
+++ fiftystates/scrape/me/committees.py
@@ -11,7 +11,7 @@ class MECommitteeScraper(CommitteeScraper):
 
     def scrape(self, chamber, year):
         self.save_errors=False
-        if year! != '2009':
+        if year != '2009':
             raise NoDataForYear(year)
 
         if chamber == 'upper':
@@ -20,9 +20,32 @@ class MECommitteeScraper(CommitteeScraper):
             self.scrape_reps_comm(chamber, year)
 
 
-    def scrape_reps_comm(self, chamber, year):
-        print "Needs to be worked on"
+    def scrape_reps_comm(self, chamber, year):        
+        
+       url = 'http://www.maine.gov/legis/house/hsecoms.htm'
 
+       with self.urlopen(url) as page:
+            root = lxml.etree.fromstring(page, lxml.etree.HTMLParser())
+
+            count = 0
+
+            for n in range(1, 12, 2):
+                path = 'string(//body/center[%s]/h1/a)' % (n)
+                comm_name = root.xpath(path)
+                committee = Committee(chamber, comm_name)
+                count = count + 1                
+
+                path2 = '/html/body/ul[%s]/li/a' % (count)
+
+                for el in root.xpath(path2):
+                   rep = el.text
+                   if rep.find('(') != -1:
+                        mark = rep.find('(')
+                        rep = rep[15: mark]
+                   committee.add_member(rep)
+                committee.add_source(url)
+                
+                self.save_committee(committee)
 
     def scrape_senate_comm(chamber, year):
         print "Needs to be worked on"
