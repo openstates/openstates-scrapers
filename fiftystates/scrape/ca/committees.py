e942ff28ee4fb07760dc6aa08a8676b348c72dcf
Michael Stephens <mstephens@sunlightfoundation.com>
2010-06-11 15:39:21 -0400

    CA: assembly committees

---
+++ fiftystates/scrape/ca/committees.py
@@ -0,0 +1,37 @@
+import re
+
+from fiftystates.scrape.committees import CommitteeScraper, Committee
+
+import lxml.html
+
+
+class CACommitteeScraper(CommitteeScraper):
+    state = 'ca'
+
+    def scrape(self, chamber, year):
+        if chamber != 'lower' or year != '2009':
+            return
+
+        list_url = 'http://www.assembly.ca.gov/acs/comDir.asp'
+        with self.urlopen(list_url) as list_page:
+            list_page = lxml.html.fromstring(list_page)
+            list_page.make_links_absolute(list_url)
+
+            for a in list_page.xpath('//ul/a'):
+                comm = Committee('lower', a.text.strip())
+                self.scrape_committee_members(comm, a.attrib['href'])
+                self.save_committee(comm)
+
+    def scrape_committee_members(self, committee, url):
+        # break out of frame
+        url = url.replace('newcomframeset.asp', 'welcome.asp')
+
+        with self.urlopen(url) as page:
+            page = lxml.html.fromstring(page)
+
+            for a in page.xpath('//tr/td/font/a'):
+                if re.match('^(mailto:|javascript:)', a.attrib['href']):
+                    continue
+
+                name = a.xpath('string(ul)').strip()
+                committee.add_member(name)
