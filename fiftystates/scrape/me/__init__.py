c9cb76cd4feab4bd71179968e9827d9e23c1c817
Dan Schneiderman <dschneiderman@sunlightfoundation.com>
2010-06-11 12:48:38 -0400

    Initial commit. Prolly wont change this file.

---
+++ fiftystates/scrape/me/__init__.py
@@ -0,0 +1,13 @@
+metadata = dict(
+    state_name='Maine',
+    legislature_name='Maine Legislature',
+    upper_chamber_name='Senate',
+    lower_chamber_name='House of Representatives',
+    upper_title='Senator',
+    lower_title='Representative',
+    upper_term=2,
+    lower_term=2,
+    sessions=['124'],
+    session_details={
+        '124': {'years': [2009, 2010], 'sub_sessions': [] }},
+)
