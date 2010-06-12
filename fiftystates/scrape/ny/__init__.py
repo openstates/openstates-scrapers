dd95f42030256872c6a205e793e53b4d0eb82726
Ben Woosley <ben.woosley@gmail.com>
2010-06-12 17:05:58 -0400

    Initial NY metadata (no sessions)

---
+++ fiftystates/scrape/ny/__init__.py
@@ -0,0 +1,13 @@
+metadata = dict(
+  name='New York',
+  abbreviation='ny',
+  legislature_name='New York Legislature',
+  upper_chamber_name='Senate',
+  lower_chamber_name='Assembly',
+  upper_chamber_title='Senator',
+  lower_chamber_title='Assembly Member',
+  upper_chamber_term=2,
+  lower_chamber_term=2,
+  sessions=[],
+  session_details={}
+)
