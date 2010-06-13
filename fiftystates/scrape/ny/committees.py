c00bf3267cc0baf414eb91291c7bcebc7a27d659
Ben Woosley <ben.woosley@gmail.com>
2010-06-12 21:13:10 -0400

    Scrape NY Assembly committes and members, but not subcommittees

--- fiftystates/scrape/ny/committees.py
+++ fiftystates/scrape/ny/committees.py
@@ -3,6 +3,8 @@ import datetime as dt
 
 from fiftystates.scrape.committees import CommitteeScraper, Committee
 
+# TODO scrape assembly subcommittees
+
 class NYCommitteeScraper(CommitteeScraper):
     state = "ny"
 
@@ -19,6 +21,36 @@ class NYCommitteeScraper(CommitteeScraper):
     def scrape_assembly(self):
         """Scrape Assembly Committees"""
         assembly_committees_url = "http://assembly.state.ny.us/comm/"
+
+        with self.urlopen(assembly_committees_url) as html:
+            doc = lxml.html.fromstring(html)
+            standing_committees, subcommittees, legislative_commissions, task_forces = doc.cssselect('#sitelinks ul')
+            committee_paths = set([l.get('href') for l in standing_committees.cssselect("li a[href]")
+                              if l.get("href").startswith('?sec=mem')])
+
+        for committee_path in committee_paths:
+            committee_url = assembly_committees_url+committee_path
+            with self.urlopen(committee_url) as chtml:
+                cdoc = lxml.html.fromstring(chtml)
+                for h in cdoc.cssselect("#content .pagehdg"):
+                    if h.text:
+                        committee_name = h.text.split('Committee Members')[0].strip()
+                        break
+
+                committee = Committee("lower", committee_name)
+                committee.add_source(committee_url)
+                members = cdoc.cssselect("#sitelinks")[0]
+
+                first = 1
+                for member in members.iter('span'):
+                    member = member.xpath('li/a')[0].text
+                    if first == 1:
+                        committee.add_member(member, 'chair')
+                        first = 0
+                    else:
+                        committee.add_member(member)
+
+                self.save_committee(committee)
 
     def scrape_senate(self):
         """Scrape Senate Committees"""
