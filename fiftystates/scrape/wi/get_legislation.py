fd6205d033a7dace74f3bbf6bd64f1ae1f2dac3e
James Turk <james.p.turk@gmail.com>
2010-06-17 17:36:00 -0400

    wi: new __init__.py with metadata

--- fiftystates/scrape/wi/get_legislation.py
+++ fiftystates/scrape/wi/get_legislation.py
@@ -217,32 +217,3 @@ class WisconsinScraper(LegislationScraper):
         for c in map(lambda x: x.text_content().split('(')[0], list(cmts)):
             legislator.add_role('committee member', session, committee=c.strip())
         return legislator
-
-
-    def scrape_metadata(self):
-        sessions = []
-        session_details = {}
-
-        with self.soup_context("http://www.legis.state.wi.us/") as session_page:
-            for option in session_page.find(id='session').findAll('option'):
-                year = int(re.findall(r'[0-9]+', option.string)[0])
-                text = option.string.strip()
-                if not year in self.internal_sessions:
-                    self.internal_sessions[year] = []
-                    session_details[year] = {'years': [year], 'sub_sessions':[] }
-                    sessions.append(year)
-                session_details[year]['sub_sessions'].append(text)
-                self.internal_sessions[year].append([option['value'], text])
-        return {
-            'state_name': 'Wisconsin',
-            'legislature_name': 'Wisconsin State Legislature',
-            'lower_chamber_name': 'Assembly',
-            'upper_chamber_name': 'Senate',
-            'lower_title': 'Representative',
-            'upper_title': 'Senator',
-            'lower_term': 2,
-            'upper_term': 4,
-            'sessions': sessions,
-            'session_details': session_details
-        }
-
