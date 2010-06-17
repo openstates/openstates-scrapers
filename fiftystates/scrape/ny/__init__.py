72dd3ddb42766dfe70d62521b12dd85151f530f7
Michael Stephens <mstephens@sunlightfoundation.com>
2010-06-17 10:29:57 -0400

    NY VS legislator data

---
+++ fiftystates/scrape/ny/__init__.py
@@ -0,0 +1,12 @@
+metadata = dict(
+    name='New York',
+    abbreviation='ny',
+    legislature_name='New York Legislature',
+    lower_chamber_name='Assembly',
+    upper_chamber_name='Senate',
+    lower_chamber_title='Assembly Member',
+    upper_chamber_title='Senator',
+    lower_chamber_term=2,
+    upper_chamber_term=2,
+    sessions=[],
+    )
