6e59be5c6e6c81eb127d9069644e9d436327da71
Dan Schneiderman <schneidy@csh.rit.edu>
2010-06-21 10:46:05 -0400

    Initial commit.

---
+++ fiftystates/scrape/nv/__init__.py
@@ -0,0 +1,13 @@
+metadata = dict(
+    state_name='Nevada',
+    legislature_name='Nevada Legislature',
+    upper_chamber_name='Senate',
+    lower_chamber_name='Assembly',
+    upper_title='Senator',
+    lower_title='Representative',
+    upper_term=4,
+    lower_term=2,
+    sessions=['75'],
+    session_details={
+        '75': {'years': [2009, 2010], 'sub_sessions': [] }},
+)
