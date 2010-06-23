79efe84acdb4dd1bee09970a445545410c2a7f2f
Gabriel J. Pérez <gabrieljoel@gamil.com>
2010-06-23 14:01:13 -0400

    Added stubs for Colorado

---
+++ fiftystates/scrape/co/bills.py
@@ -0,0 +1,33 @@
+from fiftystates.scrape import ScrapeError, NoDataForYear
+from fiftystates.scrape.votes import Vote
+from fiftystates.scrape.bills import BillScraper, Bill
+
+import lxml.html
+import re, contextlib
+import datetime as dt
+
+class COBillScraper(BillScraper):
+    state = 'co'
+    
+    @contextlib.contextmanager
+    def lxml_context(self, url, sep=None, sep_after=True):
+        try:
+            body = self.urlopen(url)
+        except:
+            body = self.urlopen("http://www.google.com")
+        
+        if sep != None: 
+            if sep_after == True:
+                before, itself, body = body.rpartition(sep)
+            else:
+                body, itself, after = body.rpartition(sep)    
+        
+        elem = lxml.html.fromstring(body)
+        
+        try:
+            yield elem
+        except:
+            raise
+
+    def scrape(self, chamber, year):
+        pass
