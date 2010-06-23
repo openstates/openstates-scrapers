32725ecbffbabe848dc152fef7bc666c4ff46ac5
Gabriel J. Pérez <gabrieljoel@gamil.com>
2010-06-23 12:17:43 -0400

    Votes are scraped

--- fiftystates/scrape/hi/bills.py
+++ fiftystates/scrape/hi/bills.py
@@ -92,34 +92,48 @@ class HIBillScraper(BillScraper):
                                 votes_parts = action_text.text_content().split(";")
                                 voters = []
                                 
+                                motion_text, sep, after = action_text.text_content().partition(" The votes were as follows:")
+                                
                                 for vp in votes_parts:
                                     before, sep, after = vp.partition("(s)")
                                     voters_list = after.split(", ")
                                     voters_list[0] = voters_list[0].lstrip(" ")
                                     voters_list[-1] = voters_list[-1].rstrip(". ")                          
                                     voters.append(voters_list)
-                                    
-                                for v in voters:
-                                    print len(v)
-                                    print v
                                 
-                                
-                                votes = {"Ayes":0, "Ayes with reservations":0, "Noes":0, "Excused":0}
+                                #Ayes, Ayes with reservations, Noes, Excused
                                 
                                 vote_counts = [0, 0, 0, 0]
                                 
                                 for i, t in enumerate(votes_parts):
                                     match = re.search("[0-9]+", t)
                                     if (match != None):
-                                        vote_counts[i] = match.group(0)
+                                        vote_counts[i] = int(match.group(0))
                                 
+                                action_date = dt.datetime.strptime(action_date.text_content(), '%m/%d/%Y') 
                                 
-                              
+                                if(actor_house == 'H'):
+                                    vote_house = "lower"
+                                else:
+                                    vote_house = "upper"
                                 
+                                vote = Vote(vote_house, action_date, motion_text, True, \
+                                             vote_counts[0], vote_counts[2], vote_counts[1] + vote_counts[3])
+                                vote.add_source(bill_page_url)
                                 
-                                
+                                for yes_voter in voters[0]:
+                                    vote.yes(yes_voter)
+                                for no_voter in voters[2]:
+                                    vote.no(no_voter)
+                                for other_voter in voters[1]:
+                                    vote.other(other_voter)
+                                for other_voter in voters[2]:
+                                    vote.other(other_voter)  
+                                    
+                                print vote_house, action_date, motion_text                 
+       
                                                            
-                            bill.add_action(actor, action_text, dt.datetime.strptime(action_date.text_content(), '%m/%d/%Y'))
+                            bill.add_action(actor, action_text, action_date)
                         
                         versions_page_url = "http://www.capitol.hawaii.gov/session2009/getstatus.asp?query=" \
                          + type + bill_number + "&showtext=on&currpage=1"
