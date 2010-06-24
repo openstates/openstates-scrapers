5a5e4100877996c0989d67bfffc5410e9129b2c6
Dan Schneiderman <schneidy@csh.rit.edu>
2010-06-24 17:32:06 -0400

    Added in all special cases for Assembly. Can now go all the way back to 2001 for both committees.

--- fiftystates/scrape/nv/committees.py
+++ fiftystates/scrape/nv/committees.py
@@ -36,7 +36,7 @@ class NVCommitteeScraper(CommitteeScraper):
         if chamber == 'upper':
             self.scrape_senate_comm(chamber, insert, session)
         elif chamber == 'lower':
-            self.scrape_assem_comm(chamber, insert, year)
+            self.scrape_assem_comm(chamber, insert, year, session)
 
 
     def scrape_senate_comm(self, chamber, insert, session):
@@ -108,7 +108,7 @@ class NVCommitteeScraper(CommitteeScraper):
                 comm.add_source(leg_url)
                 self.save_committee(comm)
             
-    def scrape_assem_comm(self, chamber, insert, year):
+    def scrape_assem_comm(self, chamber, insert, year, session):
 
         committees = self.scrape_comm(chamber, insert, session)
 
@@ -118,48 +118,88 @@ class NVCommitteeScraper(CommitteeScraper):
 
             with self.urlopen(leg_url) as page:
                 root = lxml.etree.fromstring(page, lxml.etree.HTMLParser())
-                
+               
                 comm_name = root.xpath('string(/html/body/div[@id="content"]/h1)')
-                if year == '2007':
-                    mark = 15
-                elif year == '2009':
-                    mark = 22
-                else:
-                    mark = 0
 
-                comm_name = comm_name[0: len(comm_name) - mark]
-                if committee[0:3] == 'EPE':
-                    comm_name = 'Elections, Procedures, Ethics, and Constitutional Amendments'
-                if comm_name.split()[0] == 'Assembly':
-                    comm_name = comm_name[9: len(comm_name)]
-                comm = Committee(chamber, comm_name)
+                #special cases for each session to grab the name
+                if session == 73:
+                    comm_name = root.xpath('string(/html/body/div[@id="content"]/h1)')
+                elif session == 72:
+                    comm_name = root.xpath('string(/html/body/h2[1]/font)')
+                elif session == 71:
+                    comm_name = root.xpath('string(/html/body/h2)')
+                elif committee == 'NR.cfm' and session != 72 and session != 71:
+                    comm_name = root.xpath('string(/html/body/div[@id="content"]/h2)')
+
+                #Marking for grabbing only the name of the committee
+                startmark = comm_name.find("Assembly")
+                if startmark == -1:
+                    startmark = 0
+                else:
+                    startmark = 9
+                endmark = comm_name.find(str(session))
+
+                if session <= 73:
+                    comm_name = comm_name[startmark: len(comm_name)]
+                else:
+                    comm_name = comm_name[startmark: endmark - 3]
+
+                comm_name = comm_name.replace(' \r\n ', '')
+
+
+                if committee == 'EPE.cfm' and (year == '2005' or year == '2007'):
+                    note1 = root.xpath('string(/html/body/div[@id="content"]/ul[1]/li[1])') 
+                    note2 = root.xpath('string(/html/body/div[@id="content"]/ul[1]/li[2])')
+                    comm = Committee(chamber, comm_name, note1 = note1, note2 = note2)
+                else:
+                    comm = Committee(chamber, comm_name) 
                 count = 0    
 
                 #special case
-                if committee[0:3] == 'EPE' and year == 2009:
+                if committee == 'EPE.cfm' and year == '2009':
                     special_name1 = root.xpath('string(/html/body/div[@id="content"]/p/a[1])')
-                    special_name1 = special_name1.split()[0] + special_name1.split()[1]
+                    special_name1 = special_name1.split()[0] + " " + special_name1.split()[1]
                     name1_2ndrole = "Constitutional Amendments Vice Chair"                    
 
                     special_name2 = root.xpath('string(/html/body/div[@id="content"]/p/a[2])')
-                    special_name2 = special_name2.split()[0] + special_name2.split()[1]
+                    special_name2 = special_name2.split()[0] + " " + special_name2.split()[1]
                     name2_2ndrole = "Elections Procedures and Ethics Vice Chair"
 
                     comm.add_member(special_name1, role="Elections Procedures and Ethics Chair", name1_2ndrole = name1_2ndrole)
-                    comm.add_member(special_name2, role="Constitutional Admendments Chair", name2_2ndrole = name2_2ndrole)
+                    comm.add_member(special_name2, role="Constitutional Admendments Chair", name2_2ndrole = name2_2ndrole)                   
 
+                #paths for grabbing names
+                if session == 73 or session == 71:
+                    path = '//li'
+                elif session == 72:
+                    path = '/html/body/ul/li'
+                else:
+                    path = '/html/body/div[@id="content"]/ul/li'
 
-                for mr in root.xpath('/html/body/div[@id="content"]/ul/li'):
+                #grabbing names
+                for mr in root.xpath(path):
                     name = mr.xpath('string(a)')
                     name = name.strip()
+
+                    if session == 72 or session == 71:
+                        name = mr.xpath('string()')
+                        name = name.replace('\r\n', '')
+                        name = name.replace(' -Vice Chair', '')
+                        name = name.replace(' -Chair', '')
+                        name = name.replace('-Chair', '')
+                        name = name.replace('\u', '')
+                        name = name.replace('\u00a0', '')
+                        name = name.replace('  ', ' ')
+
                     count = count + 1
-                    if count == 1 and committee[0:3] != 'EPE':
+                    if count == 1 and committee[0:3] != 'EPE' and session != 72:
                         role = 'Chair'
-                    elif count == 2 and committee[0:3] != 'EPE':
+                    elif count == 2 and committee[0:3] != 'EPE' and session != 72:
                         role = 'Vice Chair'
                     else:
                         role = 'member'
-                    comm.add_member(name, role = role)
+                    if len(name) > 0:
+                        comm.add_member(name, role = role) 
                 comm.add_source(leg_url)
                 self.save_committee(comm)
              
@@ -174,8 +214,12 @@ class NVCommitteeScraper(CommitteeScraper):
         leg_url = 'http://www.leg.state.nv.us/Session/' + insert  + '/Committees/' + chamber_letter + '_Committees/' 
         committees = []
         
-        if session == 72 or session == 71:
+        if (session == 72 or session == 71) and chamber_letter == 'S':
             path = '/html/body/table[2]/tr/td[1]/b/font/a'
+        elif (session == 72) and chamber_letter == 'A':
+            path = '/html/body/table[2]/tr/td/font/b/a'
+        elif (session == 71) and chamber_letter == 'A':
+            path = '/html/body/table[2]/tr/td/b/font/a'
         else:
             path = '/html/body/div[@id="content"]/table/tr/td/font/b/a'
 
@@ -186,4 +230,4 @@ class NVCommitteeScraper(CommitteeScraper):
                 if web_end != 'COW.cfm':
                     committees.append(web_end)
         return committees            
-    
+       
