d1c5c1d59ebdb8f926fca6ea12bdd811c3dda61c
Gabriel J. Pérez <gabrieljoel@gamil.com>
2010-06-30 13:55:39 -0400

    scrapes bill title, sponsors and document

--- fiftystates/scrape/co/bills.py
+++ fiftystates/scrape/co/bills.py
@@ -30,4 +30,32 @@ class COBillScraper(BillScraper):
             raise
 
     def scrape(self, chamber, year):
-        pass
+        # Data prior to 1997 is contained in pdfs
+        if year < '1997':
+            raise NoDataForYear(year)
+        
+        bills_url = "http://www.leg.state.co.us/CLICS/CLICS" + year + "A/csl.nsf/%28bf-1%29?OpenView&Count=2000"
+        with self.lxml_context(bills_url) as bills_page:
+            table_rows = bills_page.cssselect('tr')
+            for row in table_rows:
+                print "row"
+                row_elements = row.cssselect('td')
+                
+                bill_document = row_elements[0]
+                bill_document.make_links_absolute("http://www.leg.state.co.us")
+                links = []
+                for element, attribute, link, pos in bill_document.iterlinks():
+                    links.append(link)
+                if len(links) > 1:
+                    bill_document_link = links[0]
+                    print bill_document_link
+                
+                    title_and_sponsors = row_elements[1]
+                    title_and_sponsors_elements = title_and_sponsors.cssselect('font')
+                    title = title_and_sponsors_elements[0].text_content()
+                    print title
+                    
+#                for re in row_elements:
+#                    print "row element"
+#                    print re.text_content()
+               
