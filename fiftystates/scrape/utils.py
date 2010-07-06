2b2331595fa5184810f9bb310e3fe0f79847bef2
Michael Stephens <mstephens@sunlightfoundation.com>
2010-07-06 11:44:37 -0400

    pdf_to_html and pdf_to_lxml

--- fiftystates/scrape/utils.py
+++ fiftystates/scrape/utils.py
@@ -1,5 +1,6 @@
 import os
 import tempfile
+import subprocess
 
 def pdf_to_text(filename):
     (fd, temp_path) = tempfile.mkstemp()
@@ -11,3 +12,13 @@ def pdf_to_text(filename):
     os.remove(temp_path)
 
     return text
+
+def pdf_to_html(filename):
+    pipe = subprocess.Popen(["pdftohtml", "-stdout", filename],
+                            stdout=subprocess.PIPE).stdout
+    return pipe.read()
+
+def pdf_to_lxml(filename):
+    import lxml.html
+    text = pdf_to_html(filename)
+    return lxml.html.fromstring(text)
