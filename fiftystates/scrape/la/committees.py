4f41b58194cf5d7bb877452445111e6eccfcca0b
Michael Stephens <mstephens@sunlightfoundation.com>
2010-07-06 17:17:50 -0400

    LA: house committees

--- fiftystates/scrape/la/committees.py
+++ fiftystates/scrape/la/committees.py
@@ -47,3 +47,47 @@ class LACommitteeScraper(CommitteeScraper):
                 committee.add_member(name)
 
         self.save_committee(committee)
+
+    def scrape_house(self):
+        url = "http://house.louisiana.gov/H_Reps/H_Reps_CmtesFull.asp"
+        comm_cache = {}
+        with self.urlopen(url) as text:
+            page = lxml.html.fromstring(text)
+
+            for row in page.xpath("//table[@bordercolorlight='#EAEAEA']/tr"):
+                cells = row.xpath('td')
+
+                name = cells[0].xpath('string()').strip()
+
+                font = cells[1].xpath('font')[0]
+                committees = []
+
+                committees.append(font.text.strip())
+                for br in font.xpath('br'):
+                    if br.text:
+                        committees.append(br.text.strip())
+                    if br.tail:
+                        committees.append(br.tail)
+
+                for comm_name in committees:
+                    mtype = 'member'
+                    if comm_name.endswith(', Chairman'):
+                        mtype = 'chairman'
+                        comm_name = comm_name.replace(', Chairman', '')
+                    elif comm_name.endswith(', Vice Chair'):
+                        mtype = 'vice chair'
+                        comm_name = comm_name.replace(', Vice Chair', '')
+                    elif comm_name.endswith(', Ex Officio'):
+                        mtype = 'ex officio'
+                        comm_name = comm_name.replace(', Ex Officio', '')
+
+                    try:
+                        committee = comm_cache[comm_name]
+                    except KeyError:
+                        committee = Committee('lower', comm_name)
+                        comm_cache[comm_name] = committee
+
+                    committee.add_member(name, mtype)
+
+            for committee in comm_cache.values():
+                self.save_committee(committee)
