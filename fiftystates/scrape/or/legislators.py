2fc7abb48911abdf7a4d89e8a53687d4a6233d5d
Gabriel J. Pérez <gabrieljoel@gamil.com>
2010-07-06 17:25:33 -0400

    Further develop OR stubs

---
+++ fiftystates/scrape/or/legislators.py
@@ -0,0 +1,33 @@
+from fiftystates.scrape import ScrapeError, NoDataForYear
+from fiftystates.scrape.legislators import LegislatorScraper, Legislator
+
+import lxml.html
+import re, contextlib
+
+class ORLegislatorScraper(LegislatorScraper):
+    state = 'or'
+    
+    @contextlib.contextmanager
+    def lxml_context(self, url, sep=None, sep_after=True):
+        try:
+            body = self.urlopen(url)
+        except:
+            body = self.urlopen("http://www.google.com")
+        
+        if sep != None: 
+            if sep_after == True:
+                before, itself, body = body.rpartition(sep)
+            else:
+                body, itself, after = body.rpartition(sep)    
+        
+        elem = lxml.html.fromstring(body)
+        
+        try:
+            yield elem
+        except:
+            print "FAIL"
+            #self.show_error(url, body)
+            raise
+
+    def scrape(self, chamber, year):
+        pass
