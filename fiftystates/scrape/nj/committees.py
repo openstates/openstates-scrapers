fabab3f09b266cbe75f09b912a7a6e16bcfe979a
Dan Schneiderman <schneidy@csh.rit.edu>
2010-07-08 17:46:15 -0400

    Grabbing names of the committees from a local dbf file.

--- fiftystates/scrape/nj/committees.py
+++ fiftystates/scrape/nj/committees.py
@@ -5,117 +5,55 @@ from fiftystates.scrape.committees import CommitteeScraper, Committee
 from fiftystates.scrape.nv.utils import clean_committee_name
 
 import lxml.etree
+from dbfpy import dbf
 
 class NJCommitteeScraper(CommitteeScraper):
     state = 'nj'
 
     def scrape(self, chamber, year):
         self.save_errors=False
-        if year < 2009:
+
+        if year < 1996:
             raise NoDataForYear(year)
+        elif year == 1996:
+            year_abr = 9697
+        elif year == 1998:
+            year_abr = 9899
+        else:
+            year_abr = year
+
+        session = (int(year) - 2010) + 214
 
         if chamber == 'upper':
-            self.scrape_committees(chamber, year)
+            self.scrape_committees(year_abr, session)
         elif chamber == 'lower':
-            self.scrape_committees(chamber, year)
+            self.scrape_committees(year_abr, session)
 
-    def scrape_committees(self, chamber, year):
+    def scrape_committees(self, year_abr, session):
 
-            if chamber == 'lower':
-                letter = 'A'
-                num_pages = 6
-            elif chamber == 'upper':
-                letter = 'S'
-                num_pages = 5 
-            comm_url = 'http://www.njleg.state.nj.us/committees/Committees.asp?House=%s' % (letter)
-            for number in range(1, num_pages):
-                body = 'MoveRec=%s' % number
-                            
-                with self.urlopen(comm_url, 'POST', body) as page:
-                    root = lxml.etree.fromstring(page, lxml.etree.HTMLParser())
-                    comm_count = 5
-                    curr_comm = ''
-                    curr_name = []
-                    curr = False
-                    for mr in root.xpath('//table'):        
-                        #Committee Names
-                        if comm_count % 2 == 1:
-                            path = 'string(//tr[%s]//font/b)' % (comm_count)
-                            name = root.xpath(path).split()
-                            comm_name = ''
-                            for part in name:
-                                comm_name = comm_name + part + " "
-                            comm_name = comm_name[0: len(comm_name) - 1]
-                            if len(name) > 0:
-                                curr_comm = comm_name
-                                curr = True                                
-                            curr_name = name
-                        # Leg Names
-                        if ((comm_count % 2 == 0) and (len(curr_name) > 0) or (comm_count == 11)) and curr == True:
-                            curr = False
-                            if comm_count != 11:
-                                comm = Committee(chamber, curr_comm)
-                                path = '//tr[%s]/td/a/font' % (comm_count)
-                            else:
-                                comm = Committee(chamber, curr_comm)
-                                path = '//tr[12]/td/a/font'
-                            count = 0
-                            for el in root.xpath(path):
-                                leg = el.xpath('string()').split()
-                                leg_name = ''                                
-                                count = count + 1
-                                if len(leg) == 2:
-                                    leg[0] = leg[0].replace(',', '')
-                                    leg_name = leg[1] + " " + leg[0]
-                                elif len(leg) == 3:
-                                    leg[0] = leg[0].replace(',', '')
-                                    leg_name = leg[1] + " " + leg[2] + " " + leg[0]
+        members_url = 'ftp://www.njleg.state.nj.us/ag/%sdata/COMEMB.DBF' % (year_abr)
+        comm_info__url = 'ftp://www.njleg.state.nj.us/ag/%sdata/COMMITT.DBF' % (year_abr)
 
-                                if count == 1:
-                                    role = 'Chair'
-                                elif count == 2:
-                                    role = 'Vice-Chair'
-                                else:
-                                    role = 'member'
-                                comm.add_member(leg_name, role)
+        members_db = dbf.Dbf("COMEMB.DBF")
+        info_db = dbf.Dbf("COMMITT.DBF")
 
-                            comm.add_source(comm_url)
-                            self.save_committee(comm)
-                        
-                        comm_count = comm_count + 1
+        comm_dictionary = {}
 
+        for name_rec in info_db:
+            abr = name_rec["code"]
+            comm_name = name_rec["descriptio"]
 
-                if (chamber == 'upper' and number != 4 ) or (chamber == 'lower'):
-                    #Special case
-                    #Committee Name
-                    path = 'string(//tr[13]//font/b)'
-                    name = root.xpath(path).split()
-                    comm_name = ''
-                    for part in name:
-                        comm_name = comm_name + part + " "
-                    comm_name = comm_name[0: len(comm_name) - 1]
+            comm_dictionary[abr] = comm_name
 
-                    #Members
-                    comm = Committee(chamber, comm_name)
-                    path = '//tr[14]/td/a/font' 
-                    count = 0
-                    for el in root.xpath(path):
-                        leg = el.xpath('string()').split()
-                        leg_name = ''
-                        count = count + 1
-                        if len(leg) == 2:
-                            leg[0] = leg[0].replace(',', '')
-                            leg_name = leg[1] + " " + leg[0]
-                        elif len(leg) == 3:
-                            leg[0] = leg[0].replace(',', '')
-                            leg_name = leg[1] + " " + leg[2] + " " + leg[0]
-
-                        if count == 1:
-                            role = 'Chair'
-                        elif count == 2:
-                            role = 'Vice-Chair'
-                        else:
-                            role = 'member'
-                        comm.add_member(leg_name, role)
-                    comm.add_source(comm_url)
-                    self.save_committee(comm)
+        for member_rec in members_db:
+            abrv = member_rec["code"]
+            comm_name = comm_dictionary[abrv]
+            
+            if abrv[0] == "A":
+                chamber = "General Assembly"
+            elif abrv[0] == "S":
+                chamber = "Senate"
+            leg = member_rec["member"]            
+            comm = Committee(chamber, comm_name)
+            comm.add_member(leg)
+            self.save_committee(comm) 
