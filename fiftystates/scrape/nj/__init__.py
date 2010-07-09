dddd006ed2ff0b1c5deb10ca46a2c52cee4b2eb3
Dan Schneiderman <schneidy@csh.rit.edu>
2010-07-09 17:41:58 -0400

    Switched the init file to include terms instead of years.

--- fiftystates/scrape/nj/__init__.py
+++ fiftystates/scrape/nj/__init__.py
@@ -7,8 +7,12 @@ metadata = dict(
     lower_title='Representative',
     upper_term="http://en.wikipedia.org/wiki/New_Jersey_Legislature#Elections_and_terms",
     lower_term=2,
-    sessions=['214'],
-    session_details={
-        '214': {'years': [2010], 'sub_sessions': []} },
+    terms=[
+        {'name': '2000-2001', 'sessions': ['209'], 'start_year': 2000, 'end_year': 2001},
+        {'name': '2002-2003', 'sessions': ['210'], 'start_year': 2002, 'end_year': 2003},
+        {'name': '2004-2005', 'sessions': ['211'], 'start_year': 2004, 'end_year': 2005},
+        {'name': '2006-2007', 'sessions': ['212'], 'start_year': 2006, 'end_year': 2007},
+        {'name': '2008-2009', 'sessions': ['213'], 'start_year': 2008, 'end_year': 2009},
+        {'name': '2010-2011', 'sessions': ['214'], 'start_year': 2010, 'end_year': 2011},]
 )
 
