41be7457520f5f18b0f2572805d8a66314154e07
Dan Schneiderman <schneidy@csh.rit.edu>
2010-07-12 14:01:30 -0400

    Grabs main information and actions.

--- fiftystates/scrape/nj/bills.py
+++ fiftystates/scrape/nj/bills.py
@@ -12,19 +12,55 @@ import scrapelib
 class NJBillScraper(BillScraper):
     state = 'nj'
 
-    def scrape(self, chamber, year):
+    def scrape(self, chamber, session):
 
-        if year < 1996:
+        session = int(session)
+        if session < 209:
             raise NoDataForPeriod(year)
-        elif year == 1996:
-            year_abr = 9697
-        elif year == 1998:
-            year_abr = 9899
         else:
-            year_abr = year
+            year_abr = 2010
 
-        session = (int(year) - 2010) + 214
-        self.scrape_bill_pages(year, session, year_abr)
+        self.scrape_bill_pages(session, year_abr)
 
-    def scrape_bill_pages(self, year, session, year_abr):
+    def scrape_bill_pages(self, session, year_abr):
 
+        main_bill_url = 'ftp://www.njleg.state.nj.us/ag/%sdata/MAINBILL.DBF' % (year_abr)
+        MAINBILL_dbf, resp = self.urlretrieve(main_bill_url)
+        main_bill_db = dbf.Dbf(MAINBILL_dbf)
+        bill_dict = {}
+
+        #Main information
+        #print main_bill_db[2]
+        for rec in main_bill_db:
+            bill_type = rec["billtype"]
+            bill_number = int(rec["billnumber"])
+            bill_id = bill_type + str(bill_number)
+            title = rec["synopsis"]
+            if bill_type[0] == 'A':
+                chamber = "General Assembly"
+            else:
+                chamber = "Senate"
+            bill = Bill(session, chamber, bill_id, title)
+            bill.add_source(main_bill_url)
+            bill_dict[bill_id] = bill
+
+        bill_action_url = 'ftp://www.njleg.state.nj.us/ag/%sdata/BILLHIST.DBF' % (year_abr)
+        ACTION_dbf, resp = self.urlretrieve(bill_action_url)
+        bill_action_db = dbf.Dbf(ACTION_dbf)
+
+        #Actions
+        #print bill_action_db[2]
+        for rec in bill_action_db:
+            bill_type = rec["billtype"]
+            bill_number = int(rec["billnumber"])
+            bill_id = bill_type + str(bill_number)
+            bill = bill_dict[bill_id]
+            action = rec["action"]
+            date = rec["dateaction"]
+            actor = rec["house"]
+            comment = rec["comment"]
+            bill.add_action(actor, action, str(date), comment = comment)
+            bill.add_source(bill_action_url)
+
+
+        
