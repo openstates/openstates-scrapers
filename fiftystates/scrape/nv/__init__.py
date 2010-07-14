1596616fb705c2fcc5ee92ec7d3535716df168a5
Dan Schneiderman <schneidy@csh.rit.edu>
2010-07-14 17:58:59 -0400

    Updated metadata.

--- fiftystates/scrape/nv/__init__.py
+++ fiftystates/scrape/nv/__init__.py
@@ -7,11 +7,11 @@ metadata = dict(
     lower_title='Representative',
     upper_term=4,
     lower_term=2,
-    sessions=['75'],
-    session_details={
-        '71': {'start_year': 2001, 'end_year': 2002, 'sub_sessions': [] },
-        '72': {'start_year': 2003, 'end_year': 2004, 'sub_sessions': [] },
-        '73': {'start_year': 2005, 'end_year': 2006, 'sub_sessions': [] },
-        '74': {'start_year': 2007, 'end_year': 2008, 'sub_sessions': [] },
-        '75': {'start_year': 2009, 'end_year': 2010, 'sub_sessions': [] }},
+    terms = [
+        {'name': '2001-2002', 'sessions': ['71'], 'start_year': 2001, 'end_year': 2002},
+        {'name': '2003-2004', 'sessions': ['72'], 'start_year': 2003, 'end_year': 2004},
+        {'name': '2005-2006', 'sessions': ['73'], 'start_year': 2005, 'end_year': 2006},
+        {'name': '2007-2008', 'sessions': ['74'], 'start_year': 2007, 'end_year': 2008},
+        {'name': '2009-2010', 'sessions': ['75'], 'start_year': 2009, 'end_year': 2010},
+    ]
 )
