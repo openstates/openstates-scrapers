0d4bd92aeab1868d4aa0923ef09c4fa62fe6dcea
Dan Schneiderman <schneidy@csh.rit.edu>
2010-07-19 16:11:03 -0400

    Fixed special sessions. They now use the input of year + special + session (ex. 2010Special26).

--- fiftystates/scrape/nv/__init__.py
+++ fiftystates/scrape/nv/__init__.py
@@ -8,10 +8,10 @@ metadata = dict(
     upper_term=4,
     lower_term=2,
     terms = [
-        {'name': '2001-2002', 'sessions': ['Special17', 'Special18', '71'], 'start_year': 2001, 'end_year': 2002},
-        {'name': '2003-2004', 'sessions': ['Special19', 'Special20', 'Special21', '72'], 'start_year': 2003, 'end_year': 2004},
-        {'name': '2005-2006', 'sessions': ['Special22', '73'], 'start_year': 2005, 'end_year': 2006},
-        {'name': '2007-2008', 'sessions': ['Special23', 'Special24', 'Special25', '74'], 'start_year': 2007, 'end_year': 2008},
-        {'name': '2009-2010', 'sessions': ['Special26', '75'], 'start_year': 2009, 'end_year': 2010},
+        {'name': '2001-2002', 'sessions': ['2001Special17', '2002Special18', '71'], 'start_year': 2001, 'end_year': 2002},
+        {'name': '2003-2004', 'sessions': ['2003Special19', '2003Special20', '2004Special21', '72'], 'start_year': 2003, 'end_year': 2004},
+        {'name': '2005-2006', 'sessions': ['2005Special22', '73'], 'start_year': 2005, 'end_year': 2006},
+        {'name': '2007-2008', 'sessions': ['2007Special23', '2008Special24', '2008Special25', '74'], 'start_year': 2007, 'end_year': 2008},
+        {'name': '2009-2010', 'sessions': ['2010Special26', '75'], 'start_year': 2009, 'end_year': 2010},
     ]
 )
