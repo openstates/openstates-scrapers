bdc0ae464580285b4054b213b2b8d2a916d70d28
Gabriel J. Pérez <gabrieljoel@gamil.com>
2010-07-21 13:12:22 -0400

    Added stubs for PR

---
+++ fiftystates/scrape/pr/__init__.py
@@ -0,0 +1,24 @@
+status = dict(
+    bills=False,
+    bill_versions=False,
+    sponsors=False,
+    actions=False,
+    votes=False,
+    legislators=False,
+    contributors=['Gabriel J. Perez-Irizarry'],
+    notes="",
+)
+
+metadata = dict(
+    name='Puerto Rico',
+    abbreviation='pr',
+    legislature_name='Legislative Assembly of Puerto Rico',
+    upper_chamber_name='Senate',
+    lower_chamber_name='House of Representatives',
+    upper_chamber_title='Senator',
+    lower_chamber_title='Representative',
+    upper_chamber_term=4,
+    lower_chamber_term=4,
+    sessions=[],
+    session_details={}
+)
