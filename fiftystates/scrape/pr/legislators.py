8a485734524ff7e1a20c4d635257a6455c17b1bf
Gabriel J. Pérez <gabrieljoel@gamil.com>
2010-07-24 02:39:27 -0400

    Scrape most leg data for PR

--- fiftystates/scrape/pr/legislators.py
+++ fiftystates/scrape/pr/legislators.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from fiftystates.scrape import ScrapeError, NoDataForYear
 from fiftystates.scrape.legislators import LegislatorScraper, Legislator
 
@@ -28,12 +30,6 @@ class PRLegislatorScraper(LegislatorScraper):
             print "FAIL"
             #self.show_error(url, body)
             raise
-        
-    # From the itertools docs's recipe section 
-    def grouper(self, n, iterable, fillvalue=None):
-        "grouper(3, 'ABCDEFG', 'x') --> ABC DEF Gxx"
-        args = [iter(iterable)] * n
-        return itertools.izip_longest(fillvalue=fillvalue, *args) 
 
     def scrape(self, chamber, year):
         # Legislator data only available for the 2009 session
@@ -74,14 +70,6 @@ class PRLegislatorScraper(LegislatorScraper):
                         party = data_elements[2].text_content()
                         phone_no = data_elements[3].text_content()
                         email = data_elements[4].text_content()
-                        
-                            
-#                        grouped_elements = self.grouper(4, data_elements)
-#                        for name, party, phone_no, email in grouped_elements:
-#                            print name.text_content()
-#                            print party.text_content()
-#                            print phone_no.text_content()
-#                            print email.text_content()
                     
                     
         else:
@@ -91,16 +79,32 @@ class PRLegislatorScraper(LegislatorScraper):
                 leg_acu_table = tables[5]
                 legs_dist = leg_dist_table.cssselect('td')
                 legs_acu = leg_acu_table.cssselect('td')
+                # last one is empty
+                legs_acu.pop()
                 
                 for l in legs_dist:
-                    print len(l.cssselect('font')[0].cssselect('br'))
-                    party = l.cssselect('font')[1].text_content()
-                    pic_link_part = l.cssselect('a')[0].iterlinks().next()[2]
+                    leg_data = l.cssselect('font')
+                    name_dist_party = leg_data[0].text_content()
+                    name, sep, dist_party = name_dist_party.partition('Distrito')
+                    dist = re.search('[0-9]+', dist_party).group(0)
+                    party = leg_data[1].text_content()
+                    name = name.lstrip()
+                    link_part = l.cssselect('a')[0].iterlinks().next()[2]
+                    link = 'http://www.camaraderepresentantes.org/' + link_part
+                    imgs = l.cssselect('img')
+                    pic_link_part = imgs[0].iterlinks().next()[2]
+                    pic_link = 'http://www.camaraderepresentantes.org/' + pic_link_part
+                
+                
+                for l in legs_acu:
+                    link_part = l.iterlinks().next()[2]
+                    link = 'http://www.camaraderepresentantes.org/' + link_part
+                    pic_link_part = l.cssselect('img')[0].iterlinks().next()[2]
+                    pic_link = 'http://www.camaraderepresentantes.org/' + pic_link_part
+                    name_party = l.text_content().lstrip()
+                    match = re.search('PNP|PPD', name_party)
+                    name = name_party[:-4]
+                    party = match.group(0)
                     
-#                
-#                for l in legs_acu:
-#                    print l.text_content()
-#                    
-#                
-#                
+                   
         
