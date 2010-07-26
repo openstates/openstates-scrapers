5f8e08999d254b40054651bdcf92121b39f7a9fc
Dan Schneiderman <schneidy@csh.rit.edu>
2010-07-26 13:42:30 -0400

    Fixed chambers and removed repeating opening of pages.

--- fiftystates/scrape/nv/bills.py
+++ fiftystates/scrape/nv/bills.py
@@ -61,7 +61,7 @@ class NVBillScraper(BillScraper):
                         session = insert
                     bill = Bill(session, chamber, bill_id, title)
 
-                    primary, secondary = self.scrape_sponsors(page_path)
+                    primary, secondary = self.scrape_sponsors(page)
 
                     if primary[0] == 'By:':
                         primary.pop(0)
@@ -79,8 +79,8 @@ class NVBillScraper(BillScraper):
                     for leg in secondary:
                         bill.add_sponsor('cosponsor', leg)
 
-                    self.scrape_actions(page_path, bill, "upper")
-                    self.scrape_votes(page_path, bill, "upper", insert, title, year)
+                    self.scrape_actions(page, bill, "upper")
+                    self.scrape_votes(page, bill, "upper", insert, title, year)
                     bill.add_source(page_path)
                     self.save_bill(bill)
 
@@ -106,7 +106,7 @@ class NVBillScraper(BillScraper):
                         session = insert
                     bill = Bill(session, chamber, bill_id, title)
 
-                    primary, secondary = self.scrape_sponsors(page_path)
+                    primary, secondary = self.scrape_sponsors(page)
                     
                     if primary[0] == 'By:':
                         primary.pop(0)
@@ -124,8 +124,8 @@ class NVBillScraper(BillScraper):
                     for leg in secondary:
                         bill.add_sponsor('cosponsor', leg)
 
-                    self.scrape_actions(page_path, bill, "lower")
-                    self.scrape_votes(page_path, bill, "lower", insert, title, year)
+                    self.scrape_actions(page, bill, "lower")
+                    self.scrape_votes(page, bill, "lower", insert, title, year)
                     bill.add_source(page_path)
                     self.save_bill(bill)
 
@@ -142,93 +142,89 @@ class NVBillScraper(BillScraper):
         return links
 
 
-    def scrape_sponsors(self, url):
+    def scrape_sponsors(self, page):
         primary = []
-        with self.urlopen(url) as page:
-            root = lxml.etree.fromstring(page, lxml.etree.HTMLParser())
-            path = 'string(/html/body/div[@id="content"]/table[1]/tr[4]/td)'
-            sponsors = root.xpath(path)
-            sponsors = sponsors.replace(', ', '')
-            sponsors = sponsors.split()
+        root = lxml.etree.fromstring(page, lxml.etree.HTMLParser())
+        path = 'string(/html/body/div[@id="content"]/table[1]/tr[4]/td)'
+        sponsors = root.xpath(path)
+        sponsors = sponsors.replace(', ', '')
+        sponsors = sponsors.split()
 
-            if '(Bolded' in sponsors:
-                sponsors.remove('By:')
-                sponsors.remove('(Bolded')
-                sponsors.remove('name')
-                sponsors.remove('indicates')
-                sponsors.remove('primary')
-                sponsors.remove('sponsorship)')          
+        if '(Bolded' in sponsors:
+            sponsors.remove('By:')
+            sponsors.remove('(Bolded')
+            sponsors.remove('name')
+            sponsors.remove('indicates')
+            sponsors.remove('primary')
+            sponsors.remove('sponsorship)')          
 
-            for mr in root.xpath('/html/body/div[@id="content"]/table[1]/tr[4]/td/b[position() > 2]'):
-                name = mr.xpath('string()')
-                name = name.replace(' ', '')
-                primary.append(name)
+        for mr in root.xpath('/html/body/div[@id="content"]/table[1]/tr[4]/td/b[position() > 2]'):
+            name = mr.xpath('string()')
+            name = name.replace(' ', '')
+            primary.append(name)
 
-            for unwanted in primary:
-                if unwanted in sponsors:
-                    sponsors.remove(unwanted)
+        for unwanted in primary:
+            if unwanted in sponsors:
+                sponsors.remove(unwanted)
 
-            if len(primary) == 0:
-                primary = sponsors
-                sponsors = []
+        if len(primary) == 0:
+            primary = sponsors
+            sponsors = []
 
-            return primary, sponsors
+        return primary, sponsors
 
-    def scrape_actions(self, url, bill, actor):
-        with self.urlopen(url) as page:
-            root = lxml.etree.fromstring(page, lxml.etree.HTMLParser())
-            path = '/html/body/div[@id="content"]/table/tr/td/p[1]'
-            count = 6
-            for mr in root.xpath(path):
-                date = mr.xpath('string()')
-                date = date.split()[0] + " " + date.split()[1] + " " + date.split()[2]
-                date = datetime.strptime(date, "%b %d, %Y")
-                count = count + 1
-                action_path = '/html/body/div[@id="content"]/table[%s]/tr/td/ul/li' % (count)
-                for el in root.xpath(action_path):
-                    action = el.xpath('string()')
-                    bill.add_action(actor, action, date)
+    def scrape_actions(self, page, bill, actor):
+        root = lxml.etree.fromstring(page, lxml.etree.HTMLParser())
+        path = '/html/body/div[@id="content"]/table/tr/td/p[1]'
+        count = 6
+        for mr in root.xpath(path):
+            date = mr.xpath('string()')
+            date = date.split()[0] + " " + date.split()[1] + " " + date.split()[2]
+            date = datetime.strptime(date, "%b %d, %Y")
+            count = count + 1
+            action_path = '/html/body/div[@id="content"]/table[%s]/tr/td/ul/li' % (count)
+            for el in root.xpath(action_path):
+                action = el.xpath('string()')
+                bill.add_action(actor, action, date)
 
-    def scrape_votes(self, bill_url, bill, chamber, insert, motion, year):
-        with self.urlopen(bill_url) as page:
-            root = lxml.etree.fromstring(page, lxml.etree.HTMLParser())
-            url_path = ('/html/body/div[@id="content"]/table[5]/tr/td/a')
-            for mr in root.xpath(url_path):
-                url_end = mr.xpath('string(@href)')
-                vote_url = 'http://www.leg.state.nv.us/Session/%s/Reports/%s' % (insert, url_end)
-                
-                with self.urlopen(vote_url) as page:
-                    root = lxml.etree.fromstring(page, lxml.etree.HTMLParser())
+    def scrape_votes(self, bill_page, bill, chamber, insert, motion, year):
+        root = lxml.etree.fromstring(bill_page, lxml.etree.HTMLParser())
+        url_path = ('/html/body/div[@id="content"]/table[5]/tr/td/a')
+        for mr in root.xpath(url_path):
+            url_end = mr.xpath('string(@href)')
+            vote_url = 'http://www.leg.state.nv.us/Session/%s/Reports/%s' % (insert, url_end)
+            bill.add_source(vote_url)    
+            with self.urlopen(vote_url) as page:
+                root = lxml.etree.fromstring(page, lxml.etree.HTMLParser())
 
-                    date = root.xpath('string(/html/body/center/font)').split()[-1]
-                    date = date + "-" + str(year)
-                    date = datetime.strptime(date, "%m-%d-%Y")
-                    #FUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU
-                    yes_count = root.xpath('string(/html/body/center/table/tr/td[1])').split()[0]
-                    no_count = root.xpath('string(/html/body/center/table/tr/td[2])').split()[0]
-                    excused = root.xpath('string(/html/body/center/table/tr/td[3])').split()[0]
-                    not_voting = root.xpath('string(/html/body/center/table/tr/td[4])').split()[0]
-                    absent = root.xpath('string(/html/body/center/table/tr/td[5])').split()[0]
+                date = root.xpath('string(/html/body/center/font)').split()[-1]
+                date = date + "-" + str(year)
+                date = datetime.strptime(date, "%m-%d-%Y")
+                yes_count = root.xpath('string(/html/body/center/table/tr/td[1])').split()[0]
+                no_count = root.xpath('string(/html/body/center/table/tr/td[2])').split()[0]
+                excused = root.xpath('string(/html/body/center/table/tr/td[3])').split()[0]
+                not_voting = root.xpath('string(/html/body/center/table/tr/td[4])').split()[0]
+                absent = root.xpath('string(/html/body/center/table/tr/td[5])').split()[0]
                     
-                    if yes_count > no_count:
-                        passed = True
-                    else:
-                        passed = False
+                if yes_count > no_count:
+                    passed = True
+                else:
+                    passed = False
 
-                    vote = Vote(chamber, date, motion, passed, yes_count, no_count, '', not_voting = not_voting, absent = absent)
+                vote = Vote(chamber, date, motion, passed, yes_count, no_count, '', not_voting = not_voting, absent = absent)
 
-                    for el in root.xpath('/html/body/table[2]/tr'):
-                        name = el.xpath('string(td[1])').strip()
-                        full_name = ''
-                        for part in name:
-                            full_name = full_name + part + " "
-                        name = str(name)
-                        vote_result = el.xpath('string(td[2])').split()[0]
+                for el in root.xpath('/html/body/table[2]/tr'):
+                    name = el.xpath('string(td[1])').strip()
+                    full_name = ''
+                    for part in name:
+                        full_name = full_name + part + " "
+                    name = str(name)
+                    vote_result = el.xpath('string(td[2])').split()[0]
                         
-                        if vote_result == 'Yea':
-                            vote.yes(name)
-                        elif vote_result == 'Nay':
-                            vote.no(name)
-                        else:
-                            vote.other(name)
-                    bill.add_vote(vote)
+                    if vote_result == 'Yea':
+                        vote.yes(name)
+                    elif vote_result == 'Nay':
+                        vote.no(name)
+                    else:
+                        vote.other(name)
+                bill.add_vote(vote)
