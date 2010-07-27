2e3ff1cc4ea265fec16f84e62f06beaba55ca37e
Dan Schneiderman <schneidy@csh.rit.edu>
2010-07-27 14:39:07 -0400

    Updated titles.

--- fiftystates/scrape/ms/__init__.py
+++ fiftystates/scrape/ms/__init__.py
@@ -3,10 +3,10 @@ metadata = dict(
     legislature_name='Mississippi Legislature',
     upper_chamber_name='Senate',
     lower_chamber_name='House of Representatives',
-    upper_title='Senator',
-    lower_title='Representative',
-    upper_term=4,
-    lower_term=4,
+    upper_chamber_title='Senator',
+    lower_chamber_title='Representative',
+    upper_chamber_term=4,
+    lower_chamber_term=4,
     terms=[
         {'name': '2008-2011', 'sessions': ['2008', '2009', '20091E', '20092E', '20093E', '2010', '20101E'], 'start_year': 2008, 'end_year': 2011},]
 )
