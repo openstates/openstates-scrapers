166fa29036ef19c111d0cc1709b55645c3be16f4
Gabriel J. Pérez <gabrieljoel@gamil.com>
2010-07-27 13:51:33 -0400

    Scrape committees for upper house, PR

--- fiftystates/scrape/pr/committees.py
+++ fiftystates/scrape/pr/committees.py
@@ -1,16 +1,33 @@
 import lxml.html
-import datetime as dt
+import contextlib
 from fiftystates.scrape import NoDataForPeriod
-
 from fiftystates.scrape.committees import CommitteeScraper, Committee
+from fiftystates.scrape.pr.utils import committees_url, grouper
 
-class PRComitteeScraper(CommitteeScraper):
+
+class PRCommitteeScraper(CommitteeScraper):
     state = 'pr'
     
+    @contextlib.contextmanager
+    def lxml_context(self, url):
+        try:
+            body = self.urlopen(url)
+        except:
+            body = self.urlopen("http://www.google.com") 
+        
+        elem = lxml.html.fromstring(body)
+        
+        try:
+            yield elem
+        except:
+            print "FAIL"
+            #self.show_error(url, body)
+            raise
+    
     def scrape(self, chamber, term):
-        # Data available for this year only
-        if int(year) != dt.date.today().year:
-            raise NoDataForPeriod(term)
+        # Data available for this term only
+        if term != '2010-2011':
+            raise NoDataForPeriod(term)  
 
         if chamber == "upper":
             self.scrape_senate()
@@ -18,11 +35,36 @@ class PRComitteeScraper(CommitteeScraper):
             self.scrape_house()
     
     def scrape_senate(self):
-        permanent_commisions = 'http://senadopr.us/Pages/ComisionesPermanentes.aspx'
-        special_commisions = 'http://senadopr.us/Pages/ComisionesEspeciales.aspx'
-        joint_commisions = 'http://senadopr.us/Pages/ComisionesConjuntas.aspx'
+        committees_pages = committees_url('upper')
         
-    
+        link = committees_pages['permanent']
+        with self.lxml_context(link) as perm_committees_page:
+            td_elements = perm_committees_page.cssselect('td')     
+            self.scrape_senate_comittee_data(td_elements[129:201], link)
+        
+        link = committees_pages['special']
+        with self.lxml_context(link) as special_committees_page:
+            td_elements = special_committees_page.cssselect('td')         
+            self.scrape_senate_comittee_data(td_elements[129:138], link)
+        
+        link = committees_pages['joint']
+        with self.lxml_context(link) as joint_committees_page:
+            td_elements = joint_committees_page.cssselect('td')
+            self.scrape_senate_comittee_data(td_elements[129:156], link)  
+            
     def scrape_house(self):
-        permanent_commisions = 'http://www.camaraderepresentantes.org/comisiones.asp'
-        joint_commisions = 'http://www.camaraderepresentantes.org/comisiones3.asp'
+        committees_pages = committees_url('lower')
+        
+    def scrape_senate_comittee_data(self, committee_elements, link):
+        committees_grouped = grouper(3, committee_elements)
+            
+        for commission, type, president in committees_grouped:
+            link_part = commission.iterlinks().next()[2]
+            com_link = 'http://senadopr.us' + link_part
+            name = commission.text_content()
+            com_president = president.text_content()
+
+            com = Committee('upper', name, president = com_president)
+            com.add_source(com_link)
+            com.add_source(link)
+            
