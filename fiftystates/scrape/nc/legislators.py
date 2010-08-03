a1d6e63e7c2e7d352e3cb5596753141f9beb5b7e
James Turk <james.p.turk@gmail.com>
2010-08-03 13:02:23 -0400

    NC: legislators to lxml

--- fiftystates/scrape/nc/legislators.py
+++ fiftystates/scrape/nc/legislators.py
@@ -1,12 +1,9 @@
-import html5lib
 
 from fiftystates.scrape.legislators import LegislatorScraper, Legislator
-from fiftystates.scrape.nc.utils import split_name
+import lxml.html
 
 class NCLegislatorScraper(LegislatorScraper):
     state = 'nc'
-    soup_parser = html5lib.HTMLParser(
-        tree=html5lib.treebuilders.getTreeBuilder('beautifulsoup')).parse
 
     def scrape(self, chamber, term):
         url = "http://www.ncga.state.nc.us/gascripts/members/"\
@@ -18,24 +15,29 @@ class NCLegislatorScraper(LegislatorScraper):
             url += 'Senate'
 
         with self.urlopen(url) as data:
-            leg_list = self.soup_parser(data)
-            leg_table = leg_list.find('div', id='mainBody').find('table')
+            doc = lxml.html.fromstring(data)
+            rows = doc.xpath('//div[@id="mainBody"]/table/tr')
 
-            for row in leg_table.findAll('tr')[1:]:
-                party = row.td.contents[0].strip()
+            for row in rows[1:]:
+                party, district, full_name, counties = row.getchildren()
+
+                party = party.text_content()
                 if party == 'Dem':
                     party = 'Democrat'
                 elif party == 'Rep':
                     party = 'Republican'
 
-                district = row.findAll('td')[1].contents[0].strip()
-                full_name = row.findAll('td')[2].a.contents[0].strip()
+                district = district.text_content()
+
+                notice = full_name.xpath('span')
+                if notice:
+                    notice = notice[0].text_content()
+                else:
+                    notice = None
+                full_name = full_name.xpath('a')[0].text_content()
                 full_name = full_name.replace(u'\u00a0', ' ')
-                (first_name, last_name, middle_name, suffix) = split_name(
-                    full_name)
 
                 legislator = Legislator(term, chamber, district, full_name,
-                                        first_name, last_name, middle_name,
-                                        party, suffix=suffix)
+                                        party=party, notice=notice)
                 legislator.add_source(url)
                 self.save_legislator(legislator)
