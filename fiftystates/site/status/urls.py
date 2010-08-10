901ec14b41162498565b632af36d35014ac1afae
James Turk <james.p.turk@gmail.com>
2010-08-10 14:00:12 -0400

    remove old status stuff

--- fiftystates/site/status/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('fiftystates.site.status.views',
-    url(r'^$', 'status_index', name='status_index'),
-    url(r'^map.svg$', 'map_svg', name='map_svg'),
-)
