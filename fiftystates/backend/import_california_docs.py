38613eba30de2e245afb16cc8964334268f22285
Michael Stephens <mstephens@sunlightfoundation.com>
2010-08-16 11:02:26 -0400

    fix up CA doc import

--- fiftystates/backend/import_california_docs.py
+++ fiftystates/backend/import_california_docs.py
@@ -8,33 +8,35 @@ import gridfs
 from pymongo.son import SON
 
 
-def import_docs(user='', pw='', host='localhost', db='capublic'):
+def import_docs(user='', pw='', host='localhost', db_name='capublic'):
     if user and pw:
         conn_str = 'mysql://%s:%s@' % (user, pw)
     else:
         conn_str = 'mysql://'
 
     conn_str = '%s%s/%s?charset=utf8&unix_socket=/tmp/mysql.sock' % (
-        conn_str, host, db)
+        conn_str, host, db_name)
     engine = create_engine(conn_str)
     Session = sessionmaker(bind=engine)
     session = Session()
 
     fs = gridfs.GridFS(db, collection="documents")
 
-    for version in session.query(CABillVersion):
+    for version in session.query(CABillVersion).filter(
+        CABillVersion.bill_xml != None):
+
         if fs.exists({"metadata": {"ca_version_id": version.bill_version_id}}):
             continue
 
         query = SON([('_id', 'ca')])
         update = SON([('$inc', SON([('seq', 1)]))])
-        seq = db.command(SON([('findandmodify': 'doc_ids'),
+        seq = db.command(SON([('findandmodify', 'doc_ids'),
                               ('query', query),
                               ('update', update),
                               ('new', True),
                               ('upsert', True)]))['value']['seq']
 
-        doc_id = "%s%08d" % seq
+        doc_id = "CAD%08d" % seq
         print "Saving: %s" % doc_id
 
         fs.put(version.bill_xml, _id=doc_id,
