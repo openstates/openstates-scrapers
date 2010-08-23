a7c0772edac5a7c73f48985926a686d4f82fb3bb
Michael Stephens <mstephens@sunlightfoundation.com>
2010-08-23 12:11:10 -0400

    NY: Senate committees from nyss_openlegislation package

--- fiftystates/scrape/ny/committees.py
+++ fiftystates/scrape/ny/committees.py
@@ -4,15 +4,15 @@ from fiftystates.scrape import NoDataForPeriod
 
 from fiftystates.scrape.committees import CommitteeScraper, Committee
 
-# TODO scrape assembly subcommittees
+import nyss_openlegislation.models
+
 
 class NYCommitteeScraper(CommitteeScraper):
     state = "ny"
 
-    def scrape(self, chamber, year):
-        # Data available for this year only
-        if int(year) != dt.date.today().year:
-            raise NoDataForPeriod(year)
+    def scrape(self, chamber, term):
+        if term != '2009-2010':
+            raise NoDataForPeriod(term)
 
         if chamber == "upper":
             self.scrape_senate()
@@ -55,31 +55,10 @@ class NYCommitteeScraper(CommitteeScraper):
 
     def scrape_senate(self):
         """Scrape Senate Committees"""
-        senate_url = "http://www.nysenate.gov"
-        senate_committees_url = senate_url + "/committees"
+        for name, comm in nyss_openlegislation.models.committees.items():
+            committee = Committee('upper', name)
 
-        with self.urlopen(senate_committees_url) as html:
-            doc = lxml.html.fromstring(html)
-            committee_paths = set([l.get("href") for l in doc.cssselect("li a")
-                              if l.get("href", "").find("/committee/") != -1])
+            for member in comm.members:
+                committee.add_member(member.fullname)
 
-        for committee_path in committee_paths:
-            committee_url = senate_url+committee_path
-            with self.urlopen(committee_url) as chtml:
-                cdoc = lxml.html.fromstring(chtml)
-                for h in cdoc.cssselect(".committee_name"):
-                    if h.text:
-                        committee_name = h.text
-                        break
-
-                committee = Committee("upper", committee_name)
-                committee.add_source(committee_url)
-                for l in cdoc.cssselect(".committee-chair a[href]"):
-                    if "/senator/" in l.get("href") and l.text and l.text.startswith("Sen."):
-                        committee.add_member(l.text.split('Sen. ', 1)[1], "chair")
-
-                for l in cdoc.cssselect(".committee-members a[href]"):
-                    if "/senator/" in l.get("href"):
-                        committee.add_member(l.text)
-
-                self.save_committee(committee)
+            self.save_committee(committee)
