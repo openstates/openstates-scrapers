eb9191435492ff77714dbdc61046ee3aeedae28b
Michael Stephens <mstephens@sunlightfoundation.com>
2010-08-27 11:39:49 -0400

    CA: drop trailing sponsor from vote motion

--- fiftystates/scrape/ca/bills.py
+++ fiftystates/scrape/ca/bills.py
@@ -177,6 +177,7 @@ class CABillScraper(BillScraper):
                 motion = re.compile(r'^(Senate|Assembly) ',
                                     re.IGNORECASE).sub('', motion)
                 motion = re.sub(r'^(SCR|SB|AB)\s?\d+ \w+  ', '', motion)
+                motion = re.sub(r' \(\w+\)$', '', motion)
                 motion = re.sub(r'\s+', ' ', motion)
 
                 fsvote = Vote(vote_chamber,
