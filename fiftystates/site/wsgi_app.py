e87987c0cecff0d2cc55635ab11a5b15242249a2
James Turk <james.p.turk@gmail.com>
2010-08-31 15:53:32 -0400

    add wsgi_app to git

---
+++ fiftystates/site/wsgi_app.py
@@ -0,0 +1,8 @@
+import sys, os
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'fiftystates.site.settings'
+
+import django.core.handlers.wsgi
+
+application = django.core.handlers.wsgi.WSGIHandler()
