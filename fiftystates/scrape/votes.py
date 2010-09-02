f8dd382578db0631a84680320cbe9bd5c88ab729
Michael Stephens <mstephens@sunlightfoundation.com>
2010-09-02 15:33:07 -0400

    vote type is optional

--- fiftystates/scrape/votes.py
+++ fiftystates/scrape/votes.py
@@ -37,7 +37,7 @@ class VoteScraper(Scraper):
 
 class Vote(FiftystatesObject):
     def __init__(self, chamber, date, motion, passed,
-                 yes_count, no_count, other_count, type, **kwargs):
+                 yes_count, no_count, other_count, type='other', **kwargs):
         """
         Create a new :obj:`Vote`.
 
