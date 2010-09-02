35f3f2fdb7f3f20f1b528f4deb03a0b1da383187
Michael Stephens <mstephens@sunlightfoundation.com>
2010-09-02 11:55:55 -0400

    PA: update metadata

--- fiftystates/scrape/pa/__init__.py
+++ fiftystates/scrape/pa/__init__.py
@@ -1,5 +1,6 @@
 metadata = dict(
     name='Pennsylvania',
+    abbreviation='pa',
     legislature_name='Pennsylvania General Assembly',
     upper_chamber_name='Senate',
     lower_chamber_name='House of Representatives',
@@ -8,8 +9,10 @@ metadata = dict(
     upper_chamber_term=4,
     lower_chamber_term=2,
     terms=[
-        dict(name='2009-2010', sessions=[
-            '2009-2010',
-            '2009-2010 Special Session #1 (Transportation)'],
-             start_year=2009, end_year=2010)],
+        dict(name='2009-2010', start_year=2009,
+             end_year=2010,
+             sessions=[
+                 '2009-2010',
+                 '2009-2010 Special Session #1 (Transportation)']),
+        ],
     )
