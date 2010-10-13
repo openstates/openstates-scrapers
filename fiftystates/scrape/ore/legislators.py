dd62476e8cdd5357f2a19ed64753e4b49cfd7159
James Turk <james.p.turk@gmail.com>
2010-10-13 11:39:36 -0400

    initial or cleanup

--- fiftystates/scrape/ore/legislators.py
+++ fiftystates/scrape/ore/legislators.py
@@ -1,47 +1,69 @@
 from fiftystates.scrape import ScrapeError, NoDataForPeriod
 from fiftystates.scrape.legislators import LegislatorScraper, Legislator
-from fiftystates.scrape.ore.utils import chambers_url, legs_url, year_from_session
+from fiftystates.scrape.ore.utils import year_from_session
 
 import lxml.html
-import re, csv, urllib
 
 class ORELegislatorScraper(LegislatorScraper):
     state = 'or'
 
-    def scrape(self, chamber, session):
-        if year_from_session(session) != 2010:
-            raise NoDataForPeriod(session)
-        
-        if chamber == 'upper':
-            url_piece = 'senate'
-            url_piece2 = 'senator'
-        else:
-            url_piece = 'house'
-            url_piece2 = 'representative'           
-        
-        chamber_url = chambers_url(url_piece)
-        
-        leg_reader = csv.reader(urllib.urlopen(chamber_url))
-        
-        with self.urlopen(legs_url(url_piece2)) as leg_page_html:
-            leg_page = lxml.html.fromstring(leg_page_html)
-            font_elements = leg_page.cssselect('font')
-            names = {}
-            for fe in font_elements:
-                name_elements = fe.cssselect('a')
-                for ne in name_elements:
-                    if 'Senator' in ne.text_content() or 'Representative' in ne.text_content():
-                        break
-                    name_and_party_list = ne.text_content().split('-')
-                    names[name_and_party_list[0]] = name_and_party_list[1]
-                    
-            district_matches = re.findall("District: ([0-9]+)", leg_page.text_content())
-        
-        # Title,First Name,Last Name,Capitol Address,Capitol Phone,District Address,District Phone,Session Email
-        for row, district, name_and_party in zip(leg_reader, district_matches, names.iteritems()):
-            leg = Legislator(session, chamber, district, name_and_party[0], row[1], row[2], "", name_and_party[1], \
-                              capitol_address=row[3], capitol_phone=row[4], district_adress=row[5], \
-                              district_phone=row[6], session_email=row[7])
-            self.save_legislator(leg)
-            
-        
+    def scrape(self, chamber, term):
+        mtype = {'upper':'senator', 'lower': 'representative'}[chamber]
+
+        extra_fields = {
+            'phone':
+                './phone-numbers/phone-number[@title="Capitol Phone"]/@number',
+            'district_phone':
+                './phone-numbers/phone-number[@title="District Phone"]/@number'
+        }
+
+        addr_fields = {
+            'capitol_address':
+                './addresses/address[@title="Capitol Address"]',
+            'district_address':
+                './addresses/address[@title="District Office Address"]',
+        }
+
+        party_map = {'DEM': 'Democratic', 'REP': 'Republican'}
+
+        with self.urlopen('http://www.leg.state.or.us/xml/members.xml') as html:
+            doc = lxml.html.fromstring(html)
+
+            for member in doc.xpath('//member[@member-type="%s"]' % mtype):
+                first_name = member.get('first-name')
+                last_name = member.get('last-name')
+                party = party_map[member.get('party')]
+
+                # extra_fields
+                extra_dict = {}
+                for name, xpath in extra_fields.iteritems():
+                    result = member.xpath(xpath)
+                    if result:
+                        extra_dict[name] = result[0]
+
+                # address fields
+                for name, xpath in addr_fields.iteritems():
+                    result = member.xpath(xpath)
+                    if result:
+                        extra_dict[name] = '%s %s, %s %s' % (
+                            member.get('street-address'),
+                            member.get('city'),
+                            member.get('state'),
+                            member.get('postal-code'))
+
+
+                leg = Legislator(term, chamber, member.get('district-number'),
+                                 full_name=first_name+' '+last_name,
+                                 first_name=first_name,
+                                 last_name=last_name,
+                                 middle_name=member.get('middle-initial'),
+                                 party=party,
+                                 email=member.get('e-mail'),
+                                 website=member.get('website'),
+                                 oregon_member_id=member.get('leg-member-id'),
+                                 **extra_fields)
+                leg.add_source('http://www.leg.state.or.us/xml/members.xml')
+
+
+                self.save_legislator(leg)
+
--- fiftystates/scrape/ore/utils.py
+++ fiftystates/scrape/ore/utils.py
@@ -6,13 +6,8 @@ def base_url():
     return 'http://www.leg.state.or.us/'
 
 def bills_url():
-    return base_url() + 'bills_laws/billsinfo.htm'
+    return 'http://www.leg.state.or.us/bills_laws/billsinfo.htm'
 
-def chambers_url(url_piece):
-    return base_url() + url_piece + '/' + url_piece + '.csv'
-
-def legs_url(url_piece):
-    return base_url() + 'servlet/XSLT?URL=members.xml&xslURL=members.xsl&member-type=' + url_piece
 
 def year_from_session(session):
     return int(session.split()[0])
