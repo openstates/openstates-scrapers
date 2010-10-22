c1faacadecae814f5913d1d0bb7b8c829992cfc8
James Turk <james.p.turk@gmail.com>
2010-10-22 16:25:39 -0400

    import_state.py and reporting totals

--- fiftystates/backend/import_metadata.py
+++ fiftystates/backend/import_metadata.py
@@ -7,11 +7,8 @@ try:
 except ImportError:
     import simplejson as json
 
-from fiftystates import settings
 from fiftystates.backend import db
-from fiftystates.backend.utils import base_arg_parser, prepare_obj
-
-import argparse
+from fiftystates.backend.utils import prepare_obj
 
 
 def import_metadata(state, data_dir):
@@ -23,21 +20,3 @@ def import_metadata(state, data_dir):
 
     data['_id'] = state
     db.metadata.save(data, safe=True)
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        parents=[base_arg_parser],
-        description=('Import scraped state metadata into a mongo database.'))
-
-    parser.add_argument('--data_dir', '-d', type=str,
-                        help='the base Fifty State data directory')
-
-    args = parser.parse_args()
-
-    if args.data_dir:
-        data_dir = args.data_dir
-    else:
-        data_dir = settings.FIFTYSTATES_DATA_DIR
-
-    import_metadata(args.state, data_dir)
