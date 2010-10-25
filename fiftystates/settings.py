9797570e7ecbd8ed00ea13a79ace779b550e8797
James Turk <james.p.turk@gmail.com>
2010-10-25 17:29:35 -0400

    revert a mistake

--- fiftystates/settings.py
+++ fiftystates/settings.py
@@ -14,5 +14,5 @@ FIFTYSTATES_CACHE_DIR = os.path.abspath(os.path.join(os.path.abspath(
 FIFTYSTATES_ERROR_DIR = os.path.abspath(os.path.join(os.path.abspath(
             os.path.dirname(__file__)), '..', 'errors'))
 
-NIMSP_API_KEY = '33b067cfce326567afd9e60d78a9ef6e'
-VOTESMART_API_KEY = '27d0492dd79ef95c882a1dacad4d960e'
+NIMSP_API_KEY = ''
+VOTESMART_API_KEY = ''
