7bd4d4a2c49e3cdc1204d110adf9c86bbb6b2f54
Matt Grayson <mattgrayson@eitheror.org>
2010-11-20 16:16:09 -0600

    Added URLs for TN legislators

--- fiftystates/scrape/tn/legislators.py
+++ fiftystates/scrape/tn/legislators.py
@@ -5,8 +5,49 @@ import lxml.html
 
 class TNLegislatorScraper(LegislatorScraper):
     state = 'tn'
+    urls = {
+        '99th General Assembly': {
+            'lower': 'http://www.capitol.tn.gov/house/archives/99GA/Members.htm',
+            'upper': None
+        },
+        '100th General Assembly': {
+            'lower': 'http://www.capitol.tn.gov/house/archives/100GA/Members.htm',
+            'upper': None
+        },
+        '101st General Assembly': {
+            'lower': 'http://www.capitol.tn.gov/house/archives/101GA/Members.htm',
+            'upper': None
+        },
+        '102nd General Assembly': {
+            'lower': 'http://www.capitol.tn.gov/house/archives/102GA/Members/Members.htm',
+            'upper': None
+        },
+        '103rd General Assembly': {
+            'lower': 'http://www.capitol.tn.gov/house/archives/103GA/Members/HMembers.htm',
+            'upper': None
+        },
+        '104th General Assembly': {
+            'lower': 'http://www.capitol.tn.gov/house/archives/104GA/Members/HMembers.htm',
+            'upper': None
+        },
+        '105th General Assembly': {
+            'lower': 'http://www.capitol.tn.gov/house/archives/105GA/Members/HMembers.htm',
+            'upper': 'http://www.capitol.tn.gov/senate/archives/105GA/Members/sMembers105a.htm'
+        },
+        '106th General Assembly': {
+            'lower': 'http://www.capitol.tn.gov/house/archives/106GA/Members/index.html',
+            'upper': 'http://www.capitol.tn.gov/senate/archives/106GA/Members/index.html'
+        }
+    }
+
 
     def scrape(self, chamber, term):
-        print chamber
-        print term
-
+        url = self.urls[term][chamber]
+        
+        if url is None:
+            #raise NoDataForPeriod(term)
+            return 'Nuthin'
+        
+        with self.urlopen(url) as page:
+            page = lxml.html.fromstring(page)
+            print page.text_content()
