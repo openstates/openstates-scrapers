010f8d62fc49d742e3f460285cf08dcd0f196dde
Matt Grayson <mattgrayson@eitheror.org>
2010-11-20 21:44:01 -0600

    Working TN legislator scraper

--- fiftystates/scrape/tn/legislators.py
+++ fiftystates/scrape/tn/legislators.py
@@ -45,9 +45,32 @@ class TNLegislatorScraper(LegislatorScraper):
         url = self.urls[term][chamber]
         
         if url is None:
-            #raise NoDataForPeriod(term)
-            return 'Nuthin'
+            raise NoDataForPeriod(term)
         
         with self.urlopen(url) as page:
             page = lxml.html.fromstring(page)
-            print page.text_content()
+            
+            for row in page.xpath("//tr")[1:]:
+                name = row.xpath("td")[0].text_content()
+                name = name.split(",")
+                if len(name) == 2:
+                    fullname = "%s %s" % (name[1].strip(), name[0].strip())
+                elif len(name) == 3:
+                    fullname = "%s %s, %s" % (name[1].strip(), name[0].strip(), name[2].strip())
+                else:
+                    fullname = ' '.join(name).strip()
+                
+                # Most recent general assembly legislators list is slightly different than archived versions
+                if term == "106th General Assembly":
+                    party = row.xpath("td")[1].text_content().strip()
+                    district = row.xpath("td")[3].text_content().replace("District ","").strip()
+                else:
+                    party, district = row.xpath("td")[1].text_content().split("-")
+                    party = party.strip()
+                    district = district.strip()
+                
+                leg = Legislator(term, chamber, district, fullname, party=party)
+                leg.add_source(url)
+                self.save_legislator(leg)
+                
+                
