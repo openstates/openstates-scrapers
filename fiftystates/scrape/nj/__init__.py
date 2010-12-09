aa83bcad5c8027db90165743a343323c29f6a5d1
James Turk <james.p.turk@gmail.com>
2010-12-09 16:08:58 -0500

    NJ: metadata fixes

--- fiftystates/scrape/nj/__init__.py
+++ fiftystates/scrape/nj/__init__.py
@@ -1,3 +1,5 @@
+import datetime
+
 metadata = dict(
     name='New Jersey',
     abbreviation='nj',
@@ -9,11 +11,19 @@ metadata = dict(
     upper_chamber_term="http://en.wikipedia.org/wiki/New_Jersey_Legislature#Elections_and_terms",
     lower_chamber_term=2,
     terms=[
-        {'name': '2000-2001', 'sessions': ['209'], 'start_year': 2000, 'end_year': 2001},
-        {'name': '2002-2003', 'sessions': ['210'], 'start_year': 2002, 'end_year': 2003},
-        {'name': '2004-2005', 'sessions': ['211'], 'start_year': 2004, 'end_year': 2005},
-        {'name': '2006-2007', 'sessions': ['212'], 'start_year': 2006, 'end_year': 2007},
-        {'name': '2008-2009', 'sessions': ['213'], 'start_year': 2008, 'end_year': 2009},
-        {'name': '2010-2011', 'sessions': ['214'], 'start_year': 2010, 'end_year': 2011},]
+        #{'name': '2000-2001', 'sessions': ['209'],
+        # 'start_year': 2000, 'end_year': 2001},
+        #{'name': '2002-2003', 'sessions': ['210'],
+        # 'start_year': 2002, 'end_year': 2003},
+        #{'name': '2004-2005', 'sessions': ['211'],
+        # 'start_year': 2004, 'end_year': 2005},
+        #{'name': '2006-2007', 'sessions': ['212'],
+        # 'start_year': 2006, 'end_year': 2007},
+        #{'name': '2008-2009', 'sessions': ['213'],
+        # 'start_year': 2008, 'end_year': 2009},
+        {'name': '2010-2011', 'sessions': ['214'],
+         'start_year': 2010, 'end_year': 2011},
+    ],
+    session_details={'214': {'start_date': datetime.date(2010, 1, 12)}},
 )
 
