d451a51f53ffbf7c123e8f3320444fa0fd877793
James Turk <james.p.turk@gmail.com>
2010-12-15 14:38:06 -0500

    NJ: bill subjects

--- fiftystates/scrape/nj/utils.py
+++ fiftystates/scrape/nj/utils.py
@@ -1,4 +1,5 @@
 import re
+from dbfpy import dbf
 
 
 def clean_committee_name(comm_name):
@@ -23,5 +24,9 @@ def chamber_name(chamber):
     else:
         return 'assembly'
 
-
-
+class DBFMixin(object):
+    def get_dbf(self, year, name):
+        url = 'ftp://www.njleg.state.nj.us/ag/%sdata/%s.DBF' % (year, name)
+        dbf_file, resp = self.urlretrieve(url)
+        db = dbf.Dbf(dbf_file)
+        return url, db
