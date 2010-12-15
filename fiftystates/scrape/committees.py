bde7777c485101aaad703f0c9ec6e8c3ba544b60
James Turk <james.p.turk@gmail.com>
2010-12-15 11:54:32 -0500

    committee scraper docs

--- fiftystates/scrape/committees.py
+++ fiftystates/scrape/committees.py
@@ -48,6 +48,14 @@ class CommitteeScraper(Scraper):
 class Committee(FiftystatesObject):
     def __init__(self, chamber, committee, subcommittee=None,
                  **kwargs):
+        """
+        Create a Committee.
+
+        :param chamber: the chamber this committee is associated with ('upper',
+            'lower', or 'joint')
+        :param committee: the name of the committee
+        :param subcommittee: the name of the subcommittee (optional)
+        """
         super(Committee, self).__init__('committee', **kwargs)
         self['chamber'] = chamber
         self['committee'] = committee
@@ -55,5 +63,12 @@ class Committee(FiftystatesObject):
         self['members'] = kwargs.get('members', [])
 
     def add_member(self, legislator, role='member', **kwargs):
+        """
+        Add a member to the committee object.
+
+        :param legislator: name of the legislator
+        :param role: role that legislator holds in the committee
+            (eg. chairman) default: 'member'
+        """
         self['members'].append(dict(name=legislator, role=role,
                                     **kwargs))
