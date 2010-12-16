3126dc003fe5b2c05ac72375bc85d125052cc87b
James Turk <james.p.turk@gmail.com>
2010-12-15 23:52:34 -0500

    MI: port to (not so)newapi

---
+++ fiftystates/scrape/mi/__init__.py
@@ -0,0 +1,18 @@
+metadata = {
+    'name': 'Michigan',
+    'abbreviation': 'mi',
+    'legislature_name': 'Michigan Legislature',
+    'lower_chamber_name': 'House of Representatives',
+    'upper_chamber_name': 'Senate',
+    'lower_chamber_title': 'Representative',
+    'upper_chamber_title': 'Senator',
+    'lower_chamber_term': 2,
+    'upper_chamber_term': 4,
+    'terms': [
+        {'name': '2009-2010', 'sessions': ['2009-2010'],
+         'start_year': 2009, 'end_year': 2010},
+    ],
+    'session_details': {
+        '2009-2010': {'type':'primary'},
+    }
+}
