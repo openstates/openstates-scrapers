4dce94ff48e7fcfe1bdf5164a8428c3b22bb7210
Michael Stephens <mstephens@sunlightfoundation.com>
2011-01-07 11:52:04 -0500

    test timestamp conversion util

--- tests/backend/test_utils.py
+++ tests/backend/test_utils.py
@@ -14,7 +14,7 @@ def test_insert_with_id():
             'state': 'ex'}
 
     id_re = r'^EXL\d{6,6}$'
-    
+
     id1 = utils.insert_with_id(obj1)
     assert re.match(id_re, id1)
     assert db.legislators.find_one({'_id': id1})
@@ -23,3 +23,30 @@ def test_insert_with_id():
     assert re.match(id_re, id2)
     assert id2 != id1
     assert db.legislators.find_one({'_id': id2})
+
+
+def test_convert_timestamps():
+    import time
+    import datetime
+
+    dt = datetime.datetime.now().replace(microsecond=0)
+    ts = time.mktime(dt.utctimetuple())
+
+    obj = {'date': ts,
+           'actions': [{'when': ts}, {'date': ts}],
+           'sources': [{'when': ts}, {'date': ts}],
+           'votes': [{'when': ts}, {'date': ts}],
+           'terms': [{'start_date': ts}, {'end_date': ts}],
+           }
+
+    expect = {'date': dt,
+              'actions': [{'when': dt}, {'date': dt}],
+              'sources': [{'when': dt}, {'date': dt}],
+              'votes': [{'when': dt}, {'date': dt}],
+              'terms': [{'start_date': dt}, {'end_date': dt}],
+              }
+
+    assert utils.convert_timestamps(obj) == expect
+
+    # also modifies obj in place
+    assert obj == expect
