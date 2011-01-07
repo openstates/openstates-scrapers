5c2907baecbd0e6912a50df1bf1412381357b1ce
Michael Stephens <mstephens@sunlightfoundation.com>
2011-01-07 14:59:52 -0500

    FL: fix session/term structure

--- fiftystates/scrape/fl/__init__.py
+++ fiftystates/scrape/fl/__init__.py
@@ -9,11 +9,8 @@ metadata = dict(
     upper_chamber_term=4,
     lower_chamber_term=2,
     terms=[
-        {'name': '2009',
-         'sessions': ['2009', '2009 A'],
-         'start_year': 2009, 'end_year': 2009},
-        {'name': '2010',
-         'sessions': ['2010'],
-         'start_year': 2010, 'end_year': 2010},
-        ],
+        {'name': '2009-2010',
+         'sessions': ['2009', '2009 A', '2010']},
+        {'name': '2011-2012',
+         'sessions': ['2011']}],
     )
