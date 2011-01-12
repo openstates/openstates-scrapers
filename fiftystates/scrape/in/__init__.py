878f30f37eb418ccbfd261ffc7cf83e6fff6c2e3
Michael Stephens <mstephens@sunlightfoundation.com>
2011-01-12 16:16:21 -0500

    IN: initial bill scraper

---
+++ fiftystates/scrape/in/__init__.py
@@ -0,0 +1,20 @@
+import datetime
+
+metadata = dict(
+    name='Indiana',
+    abbreviation='in',
+    legislature_name='Indiana General Assembly',
+    upper_chamber_name='Senate',
+    lower_chamber_name='House of Representatives',
+    upper_chamber_title='Senator',
+    lower_chamber_title='Representative',
+    upper_chamber_term=4,
+    lower_chamber_term=2,
+    terms=[
+        {'name': '2011-2012', 'start_year': 2011,
+         'end_year': 2012, 'sessions': ['2011']},
+        ],
+    session_details={
+        '2011': {'start_date': datetime.date(2011, 1, 5)},
+        }
+    )
---
+++ fiftystates/scrape/in/bills.py
@@ -0,0 +1,61 @@
+import datetime
+
+from fiftystates.scrape.bills import BillScraper, Bill
+
+import lxml.html
+
+
+class INBillScraper(BillScraper):
+    state = 'in'
+
+    def scrape(self, chamber, session):
+        url = ("http://www.in.gov/apps/lsa/session/billwatch/billinfo"
+               "?year=%s&session=1&request=all" % session)
+
+        with self.urlopen(url) as page:
+            page = lxml.html.fromstring(page)
+            page.make_links_absolute(url)
+
+            abbrev = {'upper': 'SB', 'lower': 'HB'}[chamber]
+            xpath = "//a[contains(@href, 'doctype=%s')]" % abbrev
+            for link in page.xpath(xpath):
+                bill_id = link.text.strip()
+                self.scrape_bill(session, chamber, bill_id,
+                                 link.attrib['href'])
+
+    def scrape_bill(self, session, chamber, bill_id, url):
+        with self.urlopen(url) as page:
+            page = lxml.html.fromstring(page)
+            page.make_links_absolute(url)
+
+            title = page.xpath("//br")[8].tail.strip()
+
+            bill = Bill(session, chamber, bill_id, title)
+            bill.add_source(url)
+
+            action_link = page.xpath("//a[contains(@href, 'getActions')]")[0]
+            self.scrape_actions(bill, action_link.attrib['href'])
+
+            self.save_bill(bill)
+
+    def scrape_actions(self, bill, url):
+        with self.urlopen(url) as page:
+            page = lxml.html.fromstring(page)
+
+            bill.add_source(url)
+
+            act_table = page.xpath("//table")[1]
+
+            for row in act_table.xpath("tr")[1:]:
+                date = row.xpath("string(td[1])").strip()
+                date = datetime.datetime.strptime(date, "%m/%d/%Y").date()
+
+                chamber = row.xpath("string(td[2])").strip()
+                if chamber == 'S':
+                    chamber = 'upper'
+                elif chamber == 'H':
+                    chamber = 'lower'
+
+                action = row.xpath("string(td[4])").strip()
+
+                bill.add_action(chamber, action, date)
