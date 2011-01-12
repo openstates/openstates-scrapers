6bfc958b6a6e27fac456c6cdbec521c80547f0ba
Michael Stephens <mstephens@sunlightfoundation.com>
2011-01-12 16:33:37 -0500

    IN: legislator scraper

---
+++ fiftystates/scrape/in/legislators.py
@@ -0,0 +1,41 @@
+import re
+
+from fiftystates.scrape import NoDataForPeriod
+from fiftystates.scrape.legislators import LegislatorScraper, Legislator
+
+import lxml.html
+
+
+class INLegislatorScraper(LegislatorScraper):
+    state = 'in'
+
+    def scrape(self, chamber, term):
+        if term != '2011-2012':
+            raise NoDataForPeriod(term)
+
+        chamber_name = {'upper': 'Senate',
+                        'lower': 'House'}[chamber]
+
+        url = ("http://www.in.gov/cgi-bin/legislative/listing/"
+               "listing-2.pl?data=alpha&chamber=%s" % chamber_name)
+
+        with self.urlopen(url) as page:
+            page = lxml.html.fromstring(page)
+
+            for link in page.xpath("//div[@id='col2']/p/a"):
+                name = link.text.strip()
+
+                details = link.getnext().text.strip()
+
+                party = details.split(',')[0]
+                if party == 'Democrat':
+                    party = 'Democratic'
+
+                district = re.search(r'District (\d+)', details).group(1)
+                district = district.lstrip('0')
+
+                leg = Legislator(term, chamber, district, name,
+                                 '', '', '', party)
+                leg.add_source(url)
+
+                self.save_legislator(leg)
