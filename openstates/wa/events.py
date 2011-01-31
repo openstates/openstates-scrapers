d038173a9a528ba08d7ef9010dacef44731c3999
Michael Stephens <mstephens@sunlightfoundation.com>
2011-01-31 11:40:44 -0500

    WA: event scraper (committee hearings)

---
+++ openstates/wa/events.py
@@ -0,0 +1,59 @@
+import os
+import re
+import datetime
+
+from billy.scrape.events import EventScraper, Event
+
+import pytz
+import lxml.etree
+
+
+class WAEventScraper(EventScraper):
+    state = 'wa'
+
+    _tz = pytz.timezone('US/Pacific')
+    _ns = {'wa': "http://WSLWebServices.leg.wa.gov/"}
+
+    def scrape(self, chamber, session):
+        url = "http://wslwebservices.leg.wa.gov/CommitteeMeetingService.asmx/GetCommitteeMeetings?beginDate=2011-01-10T00:00:00&endDate=2012-01-10T00:00:00"
+
+        expected_agency = {'upper': 'Senate', 'lower': 'House'}[chamber]
+
+        with self.urlopen(url) as page:
+            page = lxml.etree.fromstring(page)
+
+            for meeting in page.xpath(
+                "//wa:CommitteeMeeting", namespaces=self._ns):
+
+                cancelled = meeting.xpath(
+                    "string(wa:Cancelled)", namespaces=self._ns).strip()
+                if cancelled.lower() == "true":
+                    continue
+
+                agency = meeting.xpath(
+                    "string(wa:Agency)",
+                    namespaces=self._ns).strip()
+
+                if agency != expected_agency:
+                    continue
+
+                dt = meeting.xpath("string(wa:Date)", namespaces=self._ns)
+                dt = datetime.datetime.strptime(dt, "%Y-%m-%dT%H:%M:%S")
+
+                room = meeting.xpath("string(wa:Room)", namespaces=self._ns)
+                building = meeting.xpath(
+                    "string(wa:Building)", namespaces=self._ns)
+                location = "%s, %s" % (room, building)
+
+                comm = meeting.xpath(
+                    "string(wa:Committees/wa:Committee[1]/wa:Name)",
+                    namespaces=self._ns)
+
+                desc = "Committee Meeting\n%s" % comm
+
+                guid = meeting.xpath(
+                    "string(wa:AgendaId)", namespaces=self._ns)
+
+                event = Event(session, dt, 'committee:meeting',
+                              desc, location=location, _guid=guid)
+                self.save_event(event)
