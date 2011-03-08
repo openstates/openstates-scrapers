f3ba6a64103ca1c6c2f82d6e3b272d4ea843ff3b
Michael Stephens <mstephens@sunlightfoundation.com>
2011-03-08 10:07:55 -0500

    SD: upper/lower_term were renamed a long time ago

--- openstates/sd/__init__.py
+++ openstates/sd/__init__.py
@@ -8,8 +8,8 @@ metadata = dict(
     lower_chamber_name='House of Representatives',
     upper_chamber_title='Senator',
     lower_chamber_title='Representative',
-    upper_term=2,
-    lower_term=2,
+    upper_chamber_term=2,
+    lower_chamber_term=2,
     terms=[
         {'name': '2009-2010', 'start_year': 2009, 'end_year': 2010,
          'sessions': ['2009', '2010']},
