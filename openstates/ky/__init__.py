9d9aa78b45c7147524db7c7433a36bd585f27106
Segovax <segovax@insightbb.com>
2011-03-13 22:31:58 -0400

    add 2011 leg scraper, updating ky metadata

--- openstates/ky/__init__.py
+++ openstates/ky/__init__.py
@@ -1,14 +1,40 @@
-metadata = {
-    'state_name': 'Kentucky',
-    'legislature_name': 'Kentucky General Assembly',
-    'lower_chamber_name': 'House of Representatives',
-    'upper_chamber_name': 'Senate',
-    'lower_title': 'Representative',
-    'upper_title': 'Senator',
-    'lower_term': 2,
-    'upper_term': 4,
-    'sessions': ['2009'],
-    'session_details': {
-        '2009': {'years': [2009],
-                 'sub_sessions': ['2009 Special Session']}}}
-
+metadata = dict(
+    name='Kentucky',
+    abbreviation='ky',
+    legislature_name='Kentucky General Assembly',
+    upper_chamber_name='Senate',
+    lower_chamber_name='House of Representatives',
+    upper_chamber_title='Senator',
+    lower_chamber_title='Representative',
+    upper_chamber_term=4,
+    lower_chamber_term=2,
+    terms=[
+    dict(name='2009', 
+        start_year=2009,
+        end_year=2009,
+        sessions=[
+            '2009 Regular Session',
+            '2009 Special Session'
+            ]),
+    dict(name='2010',
+        start_year=2010,
+        end_year=2010,
+        sessions=[
+            '2010 Regular Session',
+            '2010 Extraordinary Session'
+            ]),
+    dict(name='2011',
+        start_year=2011,
+        end_year=2011,
+        sessions=[
+            '2011 Regular Session'
+            ])
+    ],
+    session_details={
+        '2009 Regular Session': {'type': 'primary'},
+        '2009 Special Session': {'type': 'special'},
+        '2010 Regular Session': {'type': 'primary'},
+        '2010 Extraordinary Session': {'type':'special'},
+        '2011 Regular Session':{'type':'primary'}
+        }
+    )
--- openstates/ky/legislators.py
+++ openstates/ky/legislators.py
@@ -1,32 +1,18 @@
 import re
-from BeautifulSoup import BeautifulSoup
 
 from billy.scrape import NoDataForPeriod
 from billy.scrape.legislators import Legislator, LegislatorScraper
 
-def split_name(full_name):
-    last_name = full_name.split(',')[0]
-    rest = ','.join(full_name.split(',')[1:])
+import lxml.html
 
-    m = re.search('(\w+)\s([A-Z])\.$', rest)
-    if m:
-        first_name = m.group(1)
-        middle_name = m.group(2)
-    else:
-        first_name = rest
-        middle_name = ''
-
-    if last_name.endswith(' Jr.'):
-        first_name += ' Jr.'
-        last_name = last_name.replace(' Jr.', '')
-
-    return (first_name.strip(), last_name.strip(), middle_name.strip())
 
 class KYLegislatorScraper(LegislatorScraper):
     state = 'ky'
 
     def scrape(self, chamber, year):
-        if year != '2009':
+
+        if year != self.metadata['terms'][-1]['name']:
+            # Data only available for current term
             raise NoDataForPeriod(year)
 
         if chamber == 'upper':
@@ -34,47 +20,95 @@ class KYLegislatorScraper(LegislatorScraper):
         else:
             leg_list_url = 'http://www.lrc.ky.gov/house/hsemembers.htm'
 
-        with self.urlopen(leg_list_url) as leg_list:
-            leg_list = BeautifulSoup(leg_list)
-            leg_table = leg_list.find(id="table2")
+        with self.urlopen(leg_list_url) as page:
+            page = lxml.html.fromstring(page)
 
-            for row in leg_table.findAll('tr')[1:]:
-                leg_link = row.findAll('td')[1].font
-                if leg_link: leg_link = leg_link.a
-                if not leg_link:
-                    # Vacant seat
-                    continue
+        for link in page.xpath('//a[@onmouseout="hideImg();"]'):
+            self.scrape_member(chamber, year, link.get('href'))
 
-                full_name = leg_link.contents[0].strip()
+    def scrape_member(self, chamber, year, member_url):
 
-                district = ""
-                for text in row.findAll('td')[2].findAll(text=True):
-                    district += text.strip()
-                district = district.strip()
+        with self.urlopen(member_url) as member_page:
 
-                self.parse_legislator(chamber, year, full_name,
-                                      district, leg_link['href'])
+            member = {}
+            member_root = lxml.html.fromstring(member_page)
 
-    def parse_legislator(self, chamber, year, full_name, district, url):
-        with self.urlopen(url) as leg_page:
-            leg_page = BeautifulSoup(leg_page)
-            name_str = leg_page.find('strong').contents[0].strip()
+            table = member_root.xpath('//body/div[2]/table')[0]
 
-            if name_str.endswith('(D)'):
-                party = 'Democrat'
-            elif name_str.endswith('(R)'):
+            imgtag = member_root.xpath('//body/div[2]/table//img')
+
+            member['photo_url'] = imgtag[0].get('src')
+
+            name_list = [mem.text for mem in table.iterdescendants(tag='strong')][0].split(' ')
+
+            member['full_name'] = ' '.join(name_list[1:-1]).strip()
+
+            party = name_list[-1]
+            party = re.sub(r'\(|\)', '', party)
+            if party == 'R':
                 party = 'Republican'
-            elif name_str.endswith('(I)'):
+            elif party == 'D':
+                party = 'Democratic'
+            elif party == 'I':
                 party = 'Independent'
-            else:
-                party = 'Other'
 
-            full_name = full_name.replace('\n', '').replace('&quot;', '"')
-            full_name = full_name.replace('\t', '').replace('\r', '')
-            (first_name, last_name, middle_name) = split_name(full_name)
+            member['party'] = party
 
-            legislator = Legislator(year, chamber, district, full_name,
-                                    first_name, last_name, middle_name, party)
-            legislator.add_source(url)
+            boldList = [bold.text for bold in table.iterdescendants(tag='b')]
 
-            self.save_legislator(legislator)
+            for item in boldList:
+                if item == None:
+                    continue
+                elif 'District' in item:
+                    district = item.split(' ')[-1]
+                    member['district'] = district.strip()
+                else:
+                    if member.has_key('additionalRoles'):
+                        member['additionalRoles'].append(item)
+                    else:
+                        member['additionalRoles'] = [item]
+
+            contact_rows = member_root.xpath('//body/div[2]/div[1]/table/tr/td/table[1]/tr')
+
+            for row in contact_rows:
+                row_text = self.get_child_text(row)
+                
+                if len(row_text) > 0: 
+                    if row_text[0] == 'Frankfort Address(es)':
+                        member['office_address'] = '\n'.join(row_text[1:])
+
+                    if row_text[0] == 'Phone Number(s)':
+                        for item in row_text:
+                            # Use the first capitol annex phone
+                            if item.startswith('Annex:'):
+                                member['office_phone'] = item.replace('Annex:', '').strip()
+                                break
+
+            leg = Legislator(year, chamber, member['district'], member['full_name'], 
+                            party=member['party'], photo_url=member['photo_url'],
+                            office_address=member['office_address'], 
+                            office_phone=member['office_phone'])
+            leg.add_source(member_url)
+
+            if member.has_key('additionalRoles'):
+                for role in member['additionalRoles']:
+                    leg.add_role(role, year, member['party'])
+
+            self.save_legislator(leg)
+
+    
+
+
+    def get_child_text(self, node):
+
+        text = []
+
+        for item in node.iterdescendants():
+            if item.text != None:
+                if len(item.text.strip()) > 0:
+                    text.append(item.text.strip())
+            if item.tail != None:
+                if len(item.tail.strip()) > 0:
+                    text.append(item.tail.strip())
+        return text
+
