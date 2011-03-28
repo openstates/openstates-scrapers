3ee7afd378ac971774263b74e89122f9d2af632c
James Turk <james.p.turk@gmail.com>
2011-03-28 17:37:17 -0400

    MI: update metadata

--- openstates/mi/__init__.py
+++ openstates/mi/__init__.py
@@ -9,10 +9,10 @@ metadata = {
     'lower_chamber_term': 2,
     'upper_chamber_term': 4,
     'terms': [
-        {'name': '2009-2010', 'sessions': ['2009-2010'],
-         'start_year': 2009, 'end_year': 2010},
+        {'name': '2011-2012', 'sessions': ['2011-2012'],
+         'start_year': 2011, 'end_year': 2012},
     ],
     'session_details': {
-        '2009-2010': {'type':'primary'},
+        '2011-2012': {'type':'primary'},
     }
 }
