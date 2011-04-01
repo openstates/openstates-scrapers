208a7575fc1c195c5d0aa392384e1d6e53c55bad
Michael Stephens <mstephens@sunlightfoundation.com>
2011-04-01 13:03:35 -0400

    CT: bill actions

--- openstates/ct/bills.py
+++ openstates/ct/bills.py
@@ -13,6 +13,14 @@ class CTBillScraper(BillScraper):
         if session != '2011':
             raise NoDataForPeriod(session)
 
+        self.bills = {}
+        self.scrape_bill_info(chamber, session)
+        self.scrape_bill_history()
+
+        for bill in self.bills.itervalues():
+            self.save_bill(bill)
+
+    def scrape_bill_info(self, chamber, session):
         info_url = "ftp://ftp.cga.ct.gov/pub/data/bill_info.csv"
         page = urllib2.urlopen(info_url)
         page = csv.DictReader(page)
@@ -26,4 +34,24 @@ class CTBillScraper(BillScraper):
 
             bill = Bill(session, chamber, bill_id, row['bill_title'])
             bill.add_source(info_url)
-            self.save_bill(bill)
+            self.bills[bill_id] = bill
+
+    def scrape_bill_history(self):
+        history_url = "ftp://ftp.cga.ct.gov/pub/data/bill_history.csv"
+        page = urllib2.urlopen(history_url)
+        page = csv.DictReader(page)
+
+        for row in page:
+            bill_id = row['bill_num']
+
+            try:
+                bill = self.bills[bill_id]
+            except KeyError:
+                continue
+
+            action = row['act_desc']
+            date = row['act_date']
+            date = datetime.datetime.strptime(
+                date, "%Y-%m-%d %H:%M:%S").date()
+
+            bill.add_action(bill['chamber'], action, date)
