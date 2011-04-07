4d9ae9f6ec0301bbe074e2152058173839b5dcaa
James Turk <james.p.turk@gmail.com>
2011-04-07 15:55:53 -0400

    MI: remove print statements

--- openstates/mi/committees.py
+++ openstates/mi/committees.py
@@ -36,14 +36,12 @@ class MICommitteeScraper(CommitteeScraper):
                     for a in cdoc.xpath('//div[@class="servicecolumn2"]//a[starts-with(@href, "http")]'):
                         name = a.text.strip()
                         text = a.xpath('../following-sibling::font[1]/text()')
-                        print text[0],
                         if text[0].startswith('Committee Chair'):
                             role = 'chairman'
                         elif 'Vice-Chair' in text[0]:
                             role = 'vice chairman'
                         else:
                             role = 'member'
-                        print role
                         com.add_member(name, role=role)
 
                     self.save_committee(com)
