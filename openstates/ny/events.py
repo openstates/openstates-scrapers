f789088619b61535e47e9b273b243eb2b42dff41
Michael Stephens <mstephens@sunlightfoundation.com>
2011-04-28 10:43:34 -0400

    NY: assembly committee events

--- openstates/ny/events.py
+++ openstates/ny/events.py
@@ -1,3 +1,5 @@
+import datetime
+
 from billy.scrape.events import EventScraper, Event
 
 import pytz
@@ -12,11 +14,14 @@ class NYEventScraper(EventScraper):
 
     def scrape(self, chamber, session):
         if chamber == 'upper':
-            url = ("http://www.nysenate.gov/calendar/ical/"
-                   "senator%3DAll%2526type%3D3%2526committee%3DAll"
-                   "%2526initiative%3DAll")
-        else:
-            return
+            self.scrape_upper_events(session)
+        elif chamber == 'lower':
+            self.scrape_lower_events(session)
+
+    def scrape_upper_events(self, session):
+        url = ("http://www.nysenate.gov/calendar/ical/"
+               "senator%3DAll%2526type%3D3%2526committee%3DAll"
+               "%2526initiative%3DAll")
 
         with self.urlopen(url) as page:
             cal = icalendar.Calendar.from_string(page)
@@ -49,3 +54,42 @@ class NYEventScraper(EventScraper):
             page = lxml.html.fromstring(page)
 
             return page.xpath("string(//div[@class = 'adr'])").strip()
+
+    def scrape_lower_events(self, session):
+        url = "http://assembly.state.ny.us/leg/?sh=hear"
+
+        year = datetime.date.today().year
+
+        with self.urlopen(url) as page:
+            page = lxml.html.fromstring(page)
+
+            for td in page.xpath("//td[@bgcolor='#99CCCC']"):
+                desc = td.xpath("string(following-sibling::td/strong)")
+                if 'Senate Standing Committee' in desc:
+                    # We should pick these up from the upper scraper
+                    continue
+
+                location = td.xpath(
+                    "string(../following-sibling::tr[2]/td[2])")
+
+                date = ' '.join(td.text.split()[0:2]).strip()
+
+                time = td.xpath("../following-sibling::tr[3]/td[2]")[0]
+                time = time.text.split('-')[0].strip()
+
+                when = "%s %d %s" % (date, year, time)
+                when = when.replace('.', '')
+
+                try:
+                    when = datetime.datetime.strptime(
+                        when, "%b %d %Y %I:%M %p")
+                except ValueError:
+                    when = datetime.datetime.strptime(
+                        when, "%B %d %Y %I:%M %p")
+
+                when = self._tz.localize(when)
+
+                event = Event(session, when, 'committee:meeting',
+                              desc, location)
+                event.add_source(url)
+                self.save_event(event)
