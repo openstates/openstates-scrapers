eaf05883cf9503ccd06fe2e328fc588872f94c37
Michael Stephens <mstephens@sunlightfoundation.com>
2011-05-02 11:31:23 -0400

    KY: replace bill BeautifulSoup/html5lib with lxml

--- openstates/ky/bills.py
+++ openstates/ky/bills.py
@@ -1,12 +1,11 @@
 import re
-import datetime as dt
+import datetime
 
-from BeautifulSoup import BeautifulSoup
-import html5lib
-
-from billy.scrape import NoDataForPeriod
 from billy.scrape.bills import BillScraper, Bill
 from openstates.ky import metadata
+
+import lxml.html
+
 
 def chamber_abbr(chamber):
     if chamber == 'upper':
@@ -15,77 +14,69 @@ def chamber_abbr(chamber):
         return 'H'
 
 
-def bill_abbr(chamber):
-    if chamber == 'upper':
-        return 'SB'
-    else:
-        return 'HB'
-
-
 def session_url(session):
     if session.endswith('Special Session'):
         return "http://www.lrc.ky.gov/record/%s/" % (session[2:4] + 'SS')
     else:
         return "http://www.lrc.ky.gov/record/%s/" % (session[2:4] + 'RS')
 
+
 class KYBillScraper(BillScraper):
-
     state = 'ky'
-
-    soup_parser = html5lib.HTMLParser(
-        tree=html5lib.treebuilders.getTreeBuilder('beautifulsoup')).parse
 
     def scrape(self, chamber, year):
         self.scrape_session(chamber, year)
-        for sub in metadata['session_details'][year]['sub_sessions']:
+        for sub in metadata['session_details'][year].get('sub_sessions', []):
             self.scrape_session(chamber, sub)
 
     def scrape_session(self, chamber, session):
-        bill_list_url = session_url(session) + "bills_%s.htm" % (
+        url = session_url(session) + "bills_%s.htm" % (
             chamber_abbr(chamber))
 
-        with self.urlopen(bill_list_url) as bill_list:
-            bill_list = BeautifulSoup(bill_list)
-            bill_re = "%s\d{1,4}.htm" % bill_abbr(chamber)
-            bill_links = bill_list.findAll(href=re.compile(bill_re))
+        bill_abbr = {'upper': 'SB', 'lower': 'HB'}[chamber]
 
-            for bill_link in bill_links:
-                bill_id = bill_link['href'].replace('.htm', '')
-                bill_info_url = session_url(session) + bill_link['href']
-                self.parse_bill(chamber, session, bill_id, bill_info_url)
+        with self.urlopen(url) as page:
+            page = lxml.html.fromstring(page)
+            page.make_links_absolute(url)
 
-    def parse_bill(self, chamber, session, bill_id, bill_info_url):
-        with self.urlopen(bill_info_url) as bill_info_data:
-            bill_info = self.soup_parser(bill_info_data)
-            version_url = '%s/bill.doc' % bill_id
-            version_link = bill_info.find(href=version_url)
+            for link in page.xpath("//a"):
+                if re.search(r"%s\d{1,4}\.htm" % bill_abbr,
+                             link.attrib.get('href', '')):
+                    if link.text.startswith(bill_abbr):
+                        bill_id = link.text.replace(' ', '')
+                    else:
+                        bill_id = "%s%s" % (bill_abbr, link.text)
 
-            if not version_link:
-                # This bill was withdrawn
-                return
+                    self.parse_bill(chamber, session, bill_id,
+                                    link.attrib['href'])
 
-            bill_title = version_link.findNext('p').contents[0].strip()
+    def parse_bill(self, chamber, session, bill_id, url):
+        with self.urlopen(url) as page:
+            page = lxml.html.fromstring(page)
+            page.make_links_absolute(url)
 
-            bill = Bill(session, chamber, bill_id, bill_title)
+            version_link = page.xpath(
+                "//a[contains(@href, '%s/bill.doc')]" % bill_id)[0]
+            title = version_link.xpath("string(following-sibling::p[1])")
+            bill = Bill(session, chamber, bill_id, title)
+            bill.add_source(url)
+
             bill.add_version("Most Recent Version",
-                             session_url(session) + version_url)
-            bill.add_source(bill_info_url)
+                             version_link.attrib['href'])
 
-            sponsor_links = bill_info.findAll(href=re.compile(
-                    'legislator/[SH]\d+\.htm'))
+            for link in page.xpath("//a[contains(@href, 'legislator/')]"):
+                bill.add_sponsor('primary', link.text.strip())
 
-            for sponsor_link in sponsor_links:
-                bill.add_sponsor('primary', sponsor_link.contents[0].strip())
-
-            action_p = version_link.findAllNext('p')[-1]
-            for action in action_p.findAll(text=True):
-                action = action.strip()
+            action_p = version_link.xpath("following-sibling::p[2]")[0]
+            for line in action_p.xpath("string()").split("\n"):
+                action = line.strip()
                 if (not action or action == 'last action' or
                     'Prefiled' in action):
                     continue
 
                 action_date = action.split('-')[0]
-                action_date = dt.datetime.strptime(action_date, '%b %d')
+                action_date = datetime.datetime.strptime(
+                    action_date, '%b %d')
                 # Fix:
                 action_date = action_date.replace(
                     year=int('20' + session[2:4]))
@@ -101,11 +92,4 @@ class KYBillScraper(BillScraper):
 
                 bill.add_action(actor, action, action_date)
 
-            vote_link = bill_info.find(href=re.compile('.*/vote_history.pdf'))
-            if vote_link:
-                bill.add_document(
-                    'vote_history.pdf',
-                    bill_info_url.replace('.htm', '') + "/vote_history.pdf")
-
             self.save_bill(bill)
-
