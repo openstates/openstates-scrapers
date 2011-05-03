7e0e447b47cae773f1ae47022728d4eb3d8fb054
Michael Stephens <mstephens@sunlightfoundation.com>
2011-05-03 16:02:06 -0400

    AR: actually, this time

--- openstates/ar/bills.py
+++ openstates/ar/bills.py
@@ -140,7 +140,7 @@ class ARBillScraper(BillScraper):
             self.scrape_vote(bill, date, motion, link.attrib['href'])
 
     def scrape_vote(self, bill, date, motion, url):
-        page = lxml.urlopen(url)
+        page = self.urlopen(url)
 
         if 'not yet official' in page:
             # Sometimes they link to vote pages before they go live
