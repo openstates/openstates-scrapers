4681e13260daa07bbbdebdaf2b6bb40d7fa0e9b2
Tami Crawford <tami.crawford@gmail.com>
2011-05-08 17:15:39 -0700

    initial working version

--- openstates/sc/bills.py
+++ openstates/sc/bills.py
@@ -1,12 +1,17 @@
 import re
 import datetime
-import urllib2
+import contextlib
+
+from .utils import get_bill_detail_url, get_session_days_url, get_vote_history_url
+from .utils import removeNonAscii, sponsorsToList
 
 from billy.scrape import NoDataForPeriod, ScrapeError
 from billy.scrape.bills import BillScraper, Bill
 from billy.scrape.votes import Vote
 
 import lxml.html
+
+table_xpath = "/html/body/table/tbody/tr/td/div[@id='tablecontent']/table/tbody/tr/td[@class='content']/div[@id='votecontent']/div/div/table"
 
 # Extracts bill number and title from a string
 # Sample input "Something someting Bill 1: Bill Title - Something else "
@@ -30,7 +35,7 @@ def extract_title(string):
 #   Senator last names are separated by commas, except the last two 
 #   are separated by the word 'and'
 #   handles ' and '
-def corrected_sponsor(orig):
+def xxxxcorrected_sponsor(orig):
 
 	sponsors = []
 	parts = orig.split()
@@ -113,20 +118,245 @@ def extract_bill_data(data):
 	return ( bill_number, bill_name, sponlist, blurb, chamber )
 
 
-#
-# This page contains links for each day in the session
-# Follow Each link to get the bills introduced on that day.
-#   Note that both senate and house bills will be on the same
-#   page.
-#
-daily_url = "http://www.scstatehouse.gov/sintro/sintros.htm"
 
+#################################################################
+# main class:
 class SCBillScraper(BillScraper):
     state = 'sc'
 
+    @contextlib.contextmanager
+    def lxml_context(self,url):
+	body = self.urlopen(url)
+	body = unicode(self.urlopen(url), 'latin-1')
+	#body = body.replace('name=Expires', 'name="Expires"')
+	elem = lxml.html.fromstring(body)
+	#print "lxml_context() GOT ELEM\n"
+	try:
+		yield elem
+	except Exception as error:
+		print 'show error ', url , ' error ', error
+		#self.show_error(url,body)	
+		raise
+
+
+    #################################################################
+    # Return a list of bills found on this page
+    def scrape_vote_page(self, chamber, page):
+	self.log('scraping bill vote page for chamber %s' % chamber)
+	bills_on_this_page = []
+	bills_seen = set()
+
+	tables = page.xpath(table_xpath)
+	if len(tables) != 1:
+		self.warn( 'error, expecting one table, aborting' )
+		return
+
+	rows = tables[0].xpath("//tr")
+	numbills, dupcount = 0, 0
+	session = "119"
+
+	for r in rows:
+	  try:
+		item = {}
+		alltd = r.xpath("td");
+		brows = r.xpath("td[1]/a[@target='_blank']/text()")
+		if len(brows) <= 0:
+			continue
+
+		bname = r.xpath("td[2]/a[@target='_blank']/text()")
+		if len(bname) == 0:
+			continue
+
+		btitle = r.xpath("td[3]/text()")
+		btitle1 = r.xpath("string(td[3])")
+		
+		title = btitle
+		if len(btitle) == 0:
+			title = btitle1
+		else:
+			title = " ".join(btitle)
+
+		if title.startswith("(") and title.endswith(")"):
+			title = title[1:-1]
+
+		bdate = r.xpath("td[4]/text()")
+		bcandidate = r.xpath("td[5]/text()")
+
+		bayes = r.xpath("td[6]/text()")
+		bnayes = r.xpath("td[7]/text()")
+		bnv = r.xpath("td[8]/text()")
+		excabs = r.xpath("td[9]/text()")
+		abstain = r.xpath("td[10]/text()")
+		total = r.xpath("td[11]/text()")
+		result = r.xpath("td[5]/text()")
+
+		bill_id = " ".join(bname)
+		if bill_id in bills_seen:
+			dupcount = dupcount + 1
+		else:
+			bills_seen.add( bill_id )
+			numbills = numbills + 1
+
+			bill = Bill(session,chamber, bill_id, title)
+			bills_on_this_page.append(bill)
+			bills_seen.add( bill_id )
+			self.debug("   votepage: found bill |%s| |%s|", bill_id, title)
+	  except UnicodeEncodeError:
+		self.error("scrape vote page : unicode error")
+
+	self.log( "THERE ARE %d bills: %d dups " %(numbills,dupcount)) 
+	return bills_on_this_page
+
+
+    ####################################################
+    # scrape static bill html pages, urls are like 
+    # http://scstatehouse.gov/sess119_2011-2012/bills/%d.htm
+    def scrape_static_bill(self, billurl, session, data, summary_file):
+	(bill_id,title,sponsors,blurb,chamber) = extract_bill_data(data)
+	self.log("     %s:%s:%s:%s:%s" % (bill_id,title,chamber,sponsors,blurb))
+	print "     %s:%s:%s:%s:%s" % (bill_id,title,chamber,sponsors,blurb)
+	summary_file.write( "%s:%s:%s\n    %s\n" % (bill_id,title,chamber,sponsors))
+	#summary_file.write( "%s:%s:%s:%s" % (bill_id,title,sponsors,blurb))
+        bill = Bill(session, chamber, bill_id, title)
+	bill.add_source(billurl)
+
+	for sponsor in sponsors:
+		bill.add_sponsor("sponsor", sponsor )
+	return bill
+
+    ###################################################
+    def scrape_session_days(self, url, page):
+	"""The session days page contains a list of 
+	bills by introduction date.
+
+	Find links which are of the form YYYYMMDD.htm (eg. 20110428.htm)
+	Returns a list of these pages
+	"""
+
+	page = lxml.html.fromstring(page)
+	self.log("scrape_session_days: getting list of day pages: [%s]" % url )
+	page.make_links_absolute(url)
+
+	# All the links have 'contentlink' class
+	for link in page.find_class('contentlink'):
+		self.debug( "   Bills introduced on %s are at %s" % ( str(link.text),str(link.attrib['href']) ))
+	return [str(link.attrib['href']) for link in page.find_class('contentlink')]
+
+
+    ###################################################
+    def extract_bill_info(self, session,chamber,d):
+	"""Extracts bill info, which is the bill id,
+	the sponsors, and a paragraph describing the bill (a shorter
+	summary suitable for a title may be available 
+	elsewhere, but not here).
+
+	Returns: a Bill with bill id, sponsor, and title set."""
+
+	regexp = re.compile( "/cgi-bin/web_bh10.exe" )
+	bregexp = re.compile( "bill1=(\d+)&" )
+
+	bill_name = None
+	sponsors_str = None
+	summary = None
+	the_action = None
+
+	start = d.index("--")
+	stop = d.index(":",start)
+
+	d1 = d
+	after_sponsor = d
+	splist = []
+
+	maybe_actions = ""
+	#print "   sp (%d,%d)" % (start , stop)
+	if start >= 0 and stop > start:
+		bn = d[:start]
+		start += 2  # skip the two dashes
+		bill_name = bregexp.search(bn).groups()[0]
+		#print 'bill_name ', bill_name
+		self.debug( "\nbill_name |%s|" % bill_name )
+
+		sponsors_str = d[start:stop]
+		self.debug( "\nsponsors_str |%s|" %  sponsors_str)
+		splist = sponsorsToList(sponsors_str)
+		self.debug( "\nafter cleansing |%s|\n\n" %  ("|".join(splist)))
+
+		after_sponsor = d[stop+1:]
+
+	summary = d1 
+	d1_end = after_sponsor.find("<br>",stop)
+	if d1_end > 0:
+		summary = d1[stop:d1_end]
+
+		d1 = d1[:d1_end]
+		maybe_actions = d
+		#print "AAAAA WHATEVER ", maybe_actions
+		act = maybe_actions.find("<center>")
+		#print "    cter?  ", act
+		if act != -1:
+			end_act = maybe_actions.index("</center>", act)
+			act += len("<center>")
+			maybe_actions = maybe_actions[act:end_act]
+			#print "Action: ", maybe_actions
+			the_action = maybe_actions
+
+
+	try:
+		bs = summary.decode('utf8')
+	except:
+		self.log("scrubbing %s summary, originally %s" % (bill_name,summary) )
+		summary = removeNonAscii(summary)
+
+	#print "==> [%s] Sponsors [%s] Summary [%s]\n\n" % (bill_name, sponsors_str, summary)
+	
+	if chamber == "lower":
+		bill_prefix  = "H "
+		bill_id = "H %s" % bill_name
+	else:
+		bill_prefix  = "S "
+		bill_id = "S %s" % bill_name
+
+	bill = Bill(session,chamber,bill_id,summary)
+	for sponsor in splist:
+		bill.add_sponsor("primary", sponsor )
+
+	#TEMPORARY TAMI
+	#if the_action != None:
+	#	bill.add_action(chamber, the_action, 'date')
+	return bill
+
+    ###################################################
+    def scrape_day(self, session,chamber,data):
+	"""Extract bill info from the daily summary
+	page.  Splits the page into the different
+	bill sections, extracts the data,
+	and returns list containing the parsed bills.
+	"""
+
+	# throw away everything before <body>
+	start = data.index("<body>")
+	stop = data.index("</body>",start)
+
+	if stop >= 0 and stop > start:
+	  all = re.compile( "/cgi-bin/web_bh10.exe" ).split(data[start:stop])
+	  #print "number of bills page = %d" % (len(all)-1)
+	  self.log( "number of bills page = %d" % (len(all)-1 ))
+
+	  # skip first one, because it's before the pattern
+	  return [self.extract_bill_info(session,chamber,segment) 
+		for segment in all[1:]]
+
+	print 'bad format'
+	raise
+
+    ####################################################
+    # 1. Get list of days with information by parsing daily url
+    #    which will have one link for each day in the session.
+    # 2. Scan each of the days and collect a list of bill numbers.
+    # 3. Get Bill info.
     ####################################################
     def scrape(self, chamber, session):
-	print '=== South Carolina SCRAPING session %s chamber %s\n' % (session, chamber)
+	self.log( 'scrape(session %s,chamber %s)' % (session, chamber))
 
         if session != '119':
             raise NoDataForPeriod(session)
@@ -134,47 +364,97 @@ class SCBillScraper(BillScraper):
         #if session != '2011-2012':
         #    raise NoDataForPeriod(session)
 
-        if chamber == 'lower':
-            bill_abbr = "H."
-        else:
-            bill_abbr = "S."
-            return
-		
+        #if chamber == 'lower':
+        #    bill_abbr = "H."
+        #else:
+        #    bill_abbr = "S."
+	#    #print 'Not processing senate'
+        #    #return
 
-	# the first page contains a list of bills by introduction date
-	# find links which are of the form YYYYMMDD.htm (eg. 20110428.htm)
-	# then process each of these pages
-        url = "http://www.scstatehouse.gov/sintro/sintros.htm"
+        sessions_url = get_session_days_url(chamber) 
 
-	summary_file = open("summary.txt","w")
-	print "BillNumber:Title:Sponsors:Blurb" 
-	for r in range(1,3):
-		#billurl = "http://scstatehouse.gov/cgi-bin/web_bh10.exe?bill1=%d&session=119" % r
-		billurl = "http://scstatehouse.gov/sess119_2011-2012/bills/%d.htm" % r
+	session_days = []
+	with self.urlopen( sessions_url ) as page:
+		session_days = self.scrape_session_days(sessions_url,page)
+
+	self.log( "Session days: %d" % len(session_days) )
+
+	bills_with_votes = []
+	with self.lxml_context( get_vote_history_url(chamber) ) as page:
+		bills_with_votes = self.scrape_vote_page(chamber,page)
+
+	self.log( "Bills with votes: %d" % len(bills_with_votes))
+	
+	bill_ids_with_votes = [b['bill_id'] for b in bills_with_votes]
+	bills_with_summary = set(bill_ids_with_votes)
+				
+
+	# now go through each session day and extract the bill summaries
+	# if the vote page has a bill summary, use that for the title
+	# otherwise, use the longer paragraph on the day page
+	#
+	# keep track of which bills have been processed, and get bill details only once
+	# (each bill will appear on the day page whenever it had an action)
+	numdays = 0
+	bills_processed = set()
+	billdict = dict()
+	for b in bills_with_votes:
+		billdict[ b['bill_id'] ] = b
+
+	bills_to_save = []
+	self.debug( "INITIALLY bill procssed : %d", len(bills_processed))
+	for dayurl in session_days:
+		if numdays < 1:
+			self.debug( "processing day %s" % dayurl )
+			with self.urlopen( dayurl ) as data:
+				daybills = self.scrape_day(session,chamber,data)
+				self.debug( "  #bills on this day %d" % len(daybills))
+				for b in daybills:
+					bill_id = b['bill_id']
+					if bill_id in bills_processed: 
+						self.debug("   already processed %s" % bill_id )
+						continue
+
+					self.debug( "      processing bill .... %s" % bill_id )
+					if bill_id in bills_with_summary:
+						#print "*** HAS SUMMARY ", bill_id, ' ', b['title']
+						ab = billdict[bill_id]
+						#print "*** ab ", ab
+						self.debug( "  changing title to %s" % ab['title'])
+						b['description'] = b['title']
+						b['title'] = ab['title']
+
+					bills_to_save.append(b)
+					bills_processed.add( b['bill_id']  )
+
+				#print "bill processed : ", len(bills_processed)
+			
+			numdays += 1
+
+	self.log( "Total bills processed: %d : " % len(bills_processed) )
+	self.log( "Bills processed: %s" % ("|".join(bills_processed)) )
+	self.log( "Saving %d bills" % len(bills_to_save) )
+
+	for bill in bills_to_save:
 		try:
-        		with self.urlopen(billurl) as data:
-				bill = scrape_static_html_bill_page(data,summary_file):
-            			self.save_bill(bill)
-		except IOError:
-			print 'Failed ', billurl
-	summary_file.close()
+			self.save_bill(bill)
+		except:
+			print "failed to save bill: ", bill
+			self.error( "failed to save bill: %s" % str(bill))
+			raise
 
-    	####################################################
-	# scrape static bill html pages, urls are like 
-	# http://scstatehouse.gov/sess119_2011-2012/bills/%d.htm
-	def scrape_static_html_bill_page(data,summary_file):
-		(bill_id,title,sponsors,blurb,chamber) = extract_bill_data(data)
-		print "%s:%s:%s:%s:%s" % (bill_id,title,chamber,sponsors,blurb)
-		summary_file.write( "%s:%s:%s\n    %s\n" % (bill_id,title,chamber,sponsors))
-		#summary_file.write( "%s:%s:%s:%s" % (bill_id,title,sponsors,blurb))
-		save_file = open("bill_%s.html" %(bill_id),"w")
-		save_file.write(data)
-		save_file.close()
 
-            	bill = Bill(session, chamber, bill_id, title)
-		bill.add_source(billurl)
 
-		for sponsor in sponsors:
-			bill.add_sponsor("sponsor", sponsor )
+# example title: <title>2011-2012 Bill 1: S.C. Election Reform Act - South Carolina Legislature Online</title>
+#########################################################################
+def scrape_bill_name(page):
+	#log('scraping bill name')
 
-		return bill
+	title = page.xpath("/html/head/title/text()")[0].split(":",1)[1].split("-",1)[0]
+	billname = page.xpath("/html/body/p/b")[0].text
+	if billname.startswith("S"):
+		chamber = "upper"
+	else:
+		chamber = "lower"
+	return (title,billname,chamber)
+
--- openstates/sc/parse_votes_page.py
+++ openstates/sc/parse_votes_page.py
@@ -1,10 +1,6 @@
 import re
 import datetime
 
-import sys
-print 'sys ', sys
-sys.path.append("../..")
-
 from billy.scrape import NoDataForPeriod, ScrapeError
 from billy.scrape.bills import BillScraper, Bill
 from billy.scrape.votes import Vote
@@ -16,15 +12,12 @@ house_url = "http://scstatehouse.gov/php/votehistory.php?chamber=H"
 
 table_xpath = "/html/body/table/tbody/tr/td/div[@id='tablecontent']/table/tbody/tr/td[@class='content']/div[@id='votecontent']/div/div/table"
 
-# for testing
-senate_filename = "senate_votehistory.html"
-house_filename = "house_votehistory.html"
-
-
 # example title: <title>2011-2012 Bill 1: S.C. Election Reform Act - South Carolina Legislature Online</title>
 
 #########################################################################
-def scrape_bill_name_chamber(page):
+def scrape_bill_name(page):
+	#log('scraping bill name')
+
 	title = page.xpath("/html/head/title/text()")[0].split(":",1)[1].split("-",1)[0]
 	billname = page.xpath("/html/body/p/b")[0].text
 	if billname.startswith("S"):
@@ -34,129 +27,117 @@ def scrape_bill_name_chamber(page):
 	return (title,billname,chamber)
 
 
+#########################################################################
+# Return a list of bills found on this page
 def scrape_vote_page(chamber, page):
+	#log('scraping bill vote page for chamber ', chamber)
+	bills_on_this_page = []
+	bills_seen = set()
 	if chamber == "S":
 		url = senate_url
 	else:
 		url = house_url
 
-	fname = ("%s_summary_bills.txt") % chamber
-	summary = open(fname, "w")
 	tables = page.xpath(table_xpath)
+	if len(tables) != 1:
+		print 'error, expecting one table, aborting'
+		return
 
-	print "num tables ", len(tables)
-	tbl = tables[0]
-	print "1 GOT TABLE ", tbl
-	tnum = 1
-	numbills = 0
-	dupcount = 0
-	billdict = {}
-	for table in tables:
-		rows = table.xpath("//tr")
+	rows = tables[0].xpath("//tr")
+	numbills, dupcount = 0, 0
+	session = "119"
 
-		print 'table ', tnum, ' numrows ', len(rows)
-		#or rr in brows:
-		#print "rr ", rr, " ", rr.text 
-		rownum = 1
-		for r in rows:
-		  try:
-			item = {}
-			alltd = r.xpath("td");
-			brows = r.xpath("td[1]/a[@target='_blank']/text()")
-			bname = r.xpath("td[2]/a[@target='_blank']/text()")
-			btitle = r.xpath("td[3]/text()")
-			btitle1 = r.xpath("string(td[3])")
+	for r in rows:
+	  try:
+		item = {}
+		alltd = r.xpath("td");
+		brows = r.xpath("td[1]/a[@target='_blank']/text()")
+		if len(brows) <= 0:
+			continue
+
+		bname = r.xpath("td[2]/a[@target='_blank']/text()")
+		if len(bname) == 0:
+			continue
+
+		btitle = r.xpath("td[3]/text()")
+		btitle1 = r.xpath("string(td[3])")
 		
-			if len(bname) == 0:
-				continue
+		title = btitle
+		if len(btitle) == 0:
+			title = btitle1
 
-			title = btitle
-			if len(btitle) == 0:
-				title = btitle1
+		bdate = r.xpath("td[4]/text()")
+		bcandidate = r.xpath("td[5]/text()")
 
-			bdate = r.xpath("td[4]/text()")
-			bcandidate = r.xpath("td[5]/text()")
+		bayes = r.xpath("td[6]/text()")
+		bnayes = r.xpath("td[7]/text()")
+		bnv = r.xpath("td[8]/text()")
+		excabs = r.xpath("td[9]/text()")
+		abstain = r.xpath("td[10]/text()")
+		total = r.xpath("td[11]/text()")
+		result = r.xpath("td[5]/text()")
 
-			bayes = r.xpath("td[6]/text()")
-			bnayes = r.xpath("td[7]/text()")
-			bnv = r.xpath("td[8]/text()")
-			excabs = r.xpath("td[9]/text()")
-			abstain = r.xpath("td[10]/text()")
-			total = r.xpath("td[11]/text()")
-			result = r.xpath("td[5]/text()")
+		bill_id = " ".join(bname)
+		if bill_id in bills_seen:
+			dupcount = dupcount + 1
+		else:
+			bills_seen.add( bill_id )
+			numbills = numbills + 1
 
-			if len(brows) > 0:
-				bill_name = " ".join(bname)
-				#print 'row ', rownum, ' num cols ', len(alltd)
-				#print '   ', title, ' nm ', bname , ' ', brows
+			bill = Bill(session,chamber, bill_id, title)
+			bills_on_this_page.append(bill)
+			bills_seen.add( bill_id )
+	  except UnicodeEncodeError:
+		print "unicode error"
 
-				item["title"] = " ".join(title)
-				item["name"] = " ".join(bname)
-				item["ayes"] = " ".join(bayes)
-				item["nayes"] = " ".join(bnayes)
-				item["nv"] = " ".join(bnv)
-				item["candidate"] = " ".join(bcandidate)
-				item["date"] = " ".join(bdate)
+	#print "THERE ARE #bills: " ,  numbills, "  dups= " , dupcount 
+	return bills_on_this_page
 
-				try:
-					if billdict[bill_name] != None:
-						print "**** DUPLICATE: ", bill_name
-					billdict[bill_name] = item
-					dupcount = dupcount + 1
-				except KeyError:
-					numbills = numbills + 1
-					billdict[bill_name] = item
 
+################################################################
+if __name__ == '__main__':
+	import sys
+	sys.path.append("../..")
+
+	def go(filename,chamber):
+		"""Used as part of testing.  Processes a page stored in a file.
+		Extracts the bills.  Writes the bills to a file name
+		based on the chamber. eg. S_chamber_bills.txt or H_chamber_bills.txt.
+		"""
+
+		# Scrape the data 
+		try:
+			fsock = open(filename, "r")
+			page = lxml.html.parse(fsock)
+			bills = scrape_vote_page(chamber,page)
+			print '  read file ', filename, ' chamber ', chamber, ' number of bills ', len(bills)
+			fsock.close()
+		except IOError:
+			print 'Got IOError, filename = ', filename
 		
-				#print '   ', brows, ' ', title, ' nm ', bname, ' btitle ', btitle , ' b1 ', btitle1
-
-				#print '   date ', bdate , ' candidate ', bcandidate 
-				#print '   ayes ', bayes, ' nayes ', bnayes, ' nv ', bnv , ' excabs ', excabs, ' abstain ', abstain , ' total', total,  ' result ', result 
-				#print '   item ', item 
-				#print ' '
-				summary.write("%s,%s,%s,%s\n" % (bname, title, bdate, bcandidate ))
-			cols = r.xpath("string(td)");
-
-		  except UnicodeEncodeError:
-				print "unicode error"
-
-		  rownum = rownum + 1
-		tnum = tnum + 1
+		# Save data to file 
+		try:
+			ofilename = "%s_chamber_bills.txt" % chamber
+			print '  writing output file: ', ofilename, ' #bills ', len(bills) 
+			out = open(ofilename, "w")
+			for bill in bills:
+				out.write("%s,%s\n" % (bill['bill_id'], bill['title']))
+			out.close()
+			#output_bills_file(bills,ofilename);
+		except IOError:
+			print 'Got IOError, filename = ', ofilename
 
 
-	summary.close()
+	# for testing
+	senate_filename = "senate_votehistory.html"
+	senate_filename = "tempfile.txt"
+	house_filename = "house_votehistory.html"
 
-	print "THERE ARE " , len(billdict) , " different bills"
-	print "  numbills = " , numbills 
-	print "  dupcount = " , dupcount 
-
-	fname = "%s_summary2.txt" % chamber
-	summary2 = open(fname, "w")
-	for k in billdict.keys():
-		v = billdict[k]
-		#print "%s,%s" % (v['name'],v['title'])
-		summary2.write("%s,%s\n" % (v['name'],v['title']))
-	summary2.close()
-
-
-
-try:
-	fsock = open(senate_filename, "r")
-	print "file opened: ", senate_filename
-	page = lxml.html.parse(fsock)
-
-	scrape_vote_page("S",page)
-	fsock.close()
-
-
-	fsock = open(house_filename, "r")
-	print "file opened: ", house_filename
-	page = lxml.html.parse(fsock)
-
-	scrape_vote_page("H",page)
-	fsock.close()
-
-except IOError:
-	print 'Got IOError, reading ', filename
-
+	try:
+		print 'Process senate: ', senate_filename
+		go(senate_filename,"S")
+		#print 'Process house: ', house_filename
+		#go(house_filename,"H")
+	except IOError:
+		print 'Got IOError'
 
