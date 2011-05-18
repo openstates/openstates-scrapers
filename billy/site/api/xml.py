adf35c0a3938c8f1c057368720725b4fce349ad1
Michael Stephens <mstephens@sunlightfoundation.com>
2011-05-18 12:09:57 -0400

    replace None committee_id with empty string

--- billy/site/api/xml.py
+++ billy/site/api/xml.py
@@ -165,7 +165,7 @@ def render_full_legislator(leg):
         if r['type'] == 'committee member':
             children.append(E.committee(
                 r['committee'],
-                id=r.get('committee_id', '')))
+                id=r.get('committee_id', '') or ''))
 
         for optional_attr in ('party', 'start_date', 'end_date',
                               'chamber'):
