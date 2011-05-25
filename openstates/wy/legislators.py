d7f0d58e483f51daa1b153e9a9dc7f2c7defd933
Matt Grayson <matt.grayson@speakcreative.com>
2011-05-25 13:30:04 -0500

    Rejiggered WY metadata

--- openstates/wy/legislators.py
+++ openstates/wy/legislators.py
@@ -7,5 +7,12 @@ class WYLegislatorScraper(LegislatorScraper):
     state = 'wy'
 
     def scrape(self, chamber, term):
+        years = []
+        for t in self.metadata['terms']:
+            if term == t['name']:
+                years.append(t['start_year'])
+                years.append(t['end_year'])
+                break
         print chamber
         print term
+        print years
