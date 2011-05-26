65280b855ef77287786f28d0153eda9c0de000fb
Matt Grayson <matt.grayson@speakcreative.com>
2011-05-26 13:22:29 -0500

    Cleaned up Wy legislator scraping

--- openstates/wy/legislators.py
+++ openstates/wy/legislators.py
@@ -2,85 +2,71 @@ from billy.scrape import ScrapeError, NoDataForPeriod
 from billy.scrape.legislators import LegislatorScraper, Legislator
 
 import lxml.html
-class WYLegislatorScraper(LegislatorScraper):
-    state = 'wy'
+import re
 
+class WYLegislatorScraper(LegislatorScraper):
+    state = "wy"
+
+    members = {}
     urls = {
-            'old': 'http://legisweb.state.wy.us/%s/members/%s.htm',
-            'new': 'http://legisweb.state.wy.us/LegbyYear/LegislatorList.aspx?House=%s&Year=%s&Number=%s'
+            "old": "http://legisweb.state.wy.us/%s/members/%s.htm",
+            "new": "http://legisweb.state.wy.us/LegbyYear/LegislatorList.aspx?House=%s&Year=%s&Number=%s"
     }
 
     def scrape(self, chamber, term):
-        abbr = {'upper': 'S', 'lower': 'H'}
+        abbr = {"upper": "S", "lower": "H"}
         years = []
 
         # Each term spans two years, so we need to scrape the 
         # members from both years and eliminate dupes
-        for t in self.metadata['terms']:
-            if term == t['name']:
-                years.append(t['start_year'])
-                years.append(t['end_year'])
+        for t in self.metadata["terms"]:
+            if term == t["name"]:
+                years.append(t["start_year"])
+                years.append(t["end_year"])
                 break
 
-        members = {}
         for year in years: 
             if(year > 2005):
-                chamber_indication = 'H' if chamber == 'lower' else 'S'
-                url = self.urls['new'] % (chamber_indication, year, term)
-                members = self.scrape_new_style(url, members)
+                chamber_indication = "H" if chamber == "lower" else "S"
+                url = self.urls["new"] % (chamber_indication, year, term)
+                self.scrape_members(url, 2)
             else:
-                chamber_indication = 'rep' if chamber == 'lower' else 'sen'
-                url = self.urls['old'] % (year, chamber_indication)
-                members = self.scrape_old_style(url, members)
+                chamber_indication = "rep" if chamber == "lower" else "sen"
+                url = self.urls["old"] % (year, chamber_indication)
+                self.scrape_members(url, 1)
 
-        for m in members:
+        for m in self.members:
             self.log(m)
-            m = members[m]
-            leg = Legislator(term, chamber, m['district'], m['name'], party=m['party'])
-            leg.add_source(m['source'])
+            m = self.members[m]
+            leg = Legislator(term, chamber, m["district"], m["name"], party=m["party"])
+            leg.add_source(m["source"])
             self.save_legislator(leg)
 
 
-    def scrape_new_style(self, url, members):
+    def scrape_members(self, url, version):
         """
         Scrapes legislator information from pages created since 2006
         """
         self.log(url)
+        self.log(version)
         with self.urlopen(url) as page:
             page = lxml.html.fromstring(page)
 
-            for row in page.xpath('//table[contains(@id,"Members")]/tr')[1:]:
-                tds = row.xpath('.//td')
+            row_path = '//table[contains(@id,"Members")]/tr' if version == 2 else "//table/tr"
+            for row in page.xpath(row_path)[1:]:
+                td_path = ".//td" if version == 2 else ".//td/font"
+                tds = row.xpath(td_path)
                 if(len(tds) < 4):
                     continue
-                name = tds[0].text_content().strip()
+                name = tds[0].text_content().replace("\n"," ").replace("\r"," ")
+                name = re.sub("\s+"," ", name)
+                # Special case from http://legisweb.state.wy.us/2003/members/sen.htm; need better fix
+                name = re.sub("Resigned - \d+/\d+", "", name)
+                name = name.strip()
+
                 party = tds[1].text_content().strip()
                 district = tds[2].text_content().strip()
                 # Only keep if we don't already have member
-                if(not members.has_key(name)):
+                if(not self.members.has_key(name)):
                     self.log(name)
-                    members[name] = { 'name': name, 'party': party, 'district': district, 'source': url }
-
-            return members
-
-    def scrape_old_style(self, url, members):
-        """
-        Scrapes legislator information from pages created prior to 2005
-        """
-        self.log(url)
-        with self.urlopen(url) as page:
-            page = lxml.html.fromstring(page)
-
-            for row in page.xpath('//table/tr')[1:]:
-                tds = row.xpath('.//td')
-                if(len(tds) < 4):
-                    continue
-                name = tds[0].text_content().strip()
-                party = tds[1].text_content().strip()
-                district = tds[2].text_content().strip()
-                # Only keep if we don't already have member
-                if(not members.has_key(name)):
-                    self.log(name)
-                    members[name] = { 'name': name, 'party': party, 'district': district, 'source': url }
-
-            return members
+                    self.members[name] = { "name": name, "party": party, "district": district, "source": url }
