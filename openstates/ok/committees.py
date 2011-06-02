a2a23f6c62d70c6f9a19f1926c40f1b8d9e45195
Michael Stephens <mstephens@sunlightfoundation.com>
2011-06-02 11:16:22 -0400

    OK: lower committees

--- openstates/ok/committees.py
+++ openstates/ok/committees.py
@@ -18,6 +18,41 @@ class OKCommitteeScraper(CommitteeScraper):
         elif chamber == "lower":
             self.scrape_lower()
 
+    def scrape_lower(self):
+        url = "http://www.okhouse.gov/Committees/Default.aspx"
+        page = lxml.html.fromstring(self.urlopen(url))
+        page.make_links_absolute(url)
+
+        for link in page.xpath("//a[contains(@href, 'Members')]"):
+            name = link.xpath("string()").strip()
+
+            if 'Members' in name or 'Conference' in name or 'Joint' in name:
+                continue
+
+            self.scrape_lower_committee(name, link.attrib['href'])
+
+    def scrape_lower_committee(self, name, url):
+        page = lxml.html.fromstring(self.urlopen(url))
+        page.make_links_absolute(url)
+
+        comm = Committee('lower', name)
+        comm.add_source(url)
+
+        for link in page.xpath("//a[contains(@href, 'District')]"):
+            member = link.xpath('string()').strip()
+            member = re.sub(r'\s+', ' ', member)
+
+            if not member:
+                continue
+
+            match = re.match(r'((Vice )?Chair)?Rep\. ([^\(]+)', member)
+            member = match.group(3).strip()
+            role = match.group(1) or 'member'
+
+            comm.add_member(member, role)
+
+        self.save_committee(comm)
+
     def scrape_upper(self):
         url = "http://www.oksenate.gov/Committees/standingcommittees.htm"
         page = lxml.html.fromstring(self.urlopen(url))
