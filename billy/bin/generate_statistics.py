6b03e64acf0675d123ebdb86db8e6361d6daf1d9
James Turk <james.p.turk@gmail.com>
2011-06-09 18:04:18 -0400

    emit either way

--- billy/bin/generate_statistics.py
+++ billy/bin/generate_statistics.py
@@ -28,60 +28,60 @@ def generate_statistics():
         }
 
         // only do remaining calculations for current term
-        if(!this['_current_term']) {
-             return val;
-        }
+        if(this['_current_term']) {
 
-        val['actions_this_term'] = val['actions'];
+            val['actions_this_term'] = val['actions'];
 
-        // count how many actions are categorized & introduced
-        for(var i=0; i < this.actions.length; ++i) {
-            if(this.actions[i]["type"] != "other") {
-                val['categorized']++;
+            // count how many actions are categorized & introduced
+            for(var i=0; i < this.actions.length; ++i) {
+                if(this.actions[i]["type"] != "other") {
+                    val['categorized']++;
+                }
+                for(var j=0; j < this.actions[i]["type"].length; ++j) {
+                    if(this.actions[i]["type"][j] == "bill:introduced") {
+                        val['introduced'] = 1;
+                    }
+                }
             }
-            for(var j=0; j < this.actions[i]["type"].length; ++j) {
-                if(this.actions[i]["type"][j] == "bill:introduced") {
-                    val['introduced'] = 1;
+
+            // check sponsor leg_ids
+            val['sponsors'] = this.sponsors.length;
+            val['idd_sponsors'] = 0;
+
+            for(var i=0; i < this.sponsors.length; ++i) {
+                 if(this.sponsors[i]['leg_id']) {
+                     val['idd_sponsors'] += 1;
+                 }
+            }
+
+            // go through yes/no/other votes and count voter leg_ids
+            for(var i=0; i < this.votes.length; ++i) {
+                for(var j=0; j < this.votes[i]["yes_votes"].length; ++j) {
+                    val['voters'] += 1;
+                    if(this.votes[i]["yes_votes"][j]["leg_id"]) {
+                        val['idd_voters'] += 1;
+                    }
+                }
+            }
+            for(var i=0; i < this.votes.length; ++i) {
+                for(var j=0; j < this.votes[i]["no_votes"].length; ++j) {
+                    val['voters'] += 1;
+                    if(this.votes[i]["no_votes"][j]["leg_id"]) {
+                        val['idd_voters'] += 1;
+                    }
+                }
+            }
+            for(var i=0; i < this.votes.length; ++i) {
+                for(var j=0; j < this.votes[i]["other_votes"].length; ++j) {
+                    val['voters'] += 1;
+                    if(this.votes[i]["other_votes"][j]["leg_id"]) {
+                        val['idd_voters'] += 1;
+                    }
                 }
             }
         }
 
-        // check sponsor leg_ids
-        val['sponsors'] = this.sponsors.length;
-        val['idd_sponsors'] = 0;
-
-        for(var i=0; i < this.sponsors.length; ++i) {
-             if(this.sponsors[i]['leg_id']) {
-                 val['idd_sponsors'] += 1;
-             }
-        }
-
-        // go through yes/no/other votes and count voter leg_ids
-        for(var i=0; i < this.votes.length; ++i) {
-            for(var j=0; j < this.votes[i]["yes_votes"].length; ++j) {
-                val['voters'] += 1;
-                if(this.votes[i]["yes_votes"][j]["leg_id"]) {
-                    val['idd_voters'] += 1;
-                }
-            }
-        }
-        for(var i=0; i < this.votes.length; ++i) {
-            for(var j=0; j < this.votes[i]["no_votes"].length; ++j) {
-                val['voters'] += 1;
-                if(this.votes[i]["no_votes"][j]["leg_id"]) {
-                    val['idd_voters'] += 1;
-                }
-            }
-        }
-        for(var i=0; i < this.votes.length; ++i) {
-            for(var j=0; j < this.votes[i]["other_votes"].length; ++j) {
-                val['voters'] += 1;
-                if(this.votes[i]["other_votes"][j]["leg_id"]) {
-                    val['idd_voters'] += 1;
-                }
-            }
-        }
-
+        // emit either way
         emit(this[this['level']], val);
         emit('total', val);
     }""")
