571f1a5a84b4e87fe7b864467ecf517c8d6ee166
Michael Stephens <mstephens@sunlightfoundation.com>
2011-06-14 11:25:16 -0400

    IA: metadata & legislator scraper

---
+++ openstates/ia/__init__.py
@@ -0,0 +1,19 @@
+metadata = dict(
+    name='Iowa',
+    abbreviation='ia',
+    legislature_name='Iowa General Assembly',
+    upper_chamber_name='Senate',
+    lower_chamber_name='House of Representatives',
+    upper_chamber_title='Senator',
+    lower_chamber_title='Representative',
+    upper_chamber_term=4,
+    lower_chamber_term=2,
+    terms=[
+        {
+            'name': '2011-2012',
+            'start_year': 2011,
+            'end_year': 2012,
+            'sessions': ['2011-2012'],
+        },
+    ],
+)
