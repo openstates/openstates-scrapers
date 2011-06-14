06353f583ed7bedd05b30fdc7ce6330a8d44fdf2
Michael Stephens <mstephens@sunlightfoundation.com>
2011-06-14 11:25:16 -0400

    IA: metadata & legislator scraper

---
+++ openstates/ia/legislators.py
@@ -0,0 +1,36 @@
+import re
+
+from billy.scrape import NoDataForPeriod
+from billy.scrape.legislators import LegislatorScraper, Legislator
+
+import lxml.html
+
+
+class IALegislatorScraper(LegislatorScraper):
+    state = 'ia'
+
+    def scrape(self, chamber, term):
+        if term != '2011-2012':
+            raise NoDataForPeriod(term)
+
+        if chamber == 'upper':
+            chamber_name = 'senate'
+        else:
+            chamber_name = 'house'
+
+        url = "http://www.legis.iowa.gov/Legislators/%s.aspx" % chamber_name
+        page = lxml.html.fromstring(self.urlopen(url))
+
+        for link in page.xpath("//a[contains(@href, 'legislator.aspx')]"):
+            name = link.text.strip()
+            district = link.xpath("string(../../td[2])")
+            party = link.xpath("string(../../td[3])")
+            email = link.xpath("string(../../td[5])")
+
+            if party == 'Democrat':
+                party = 'Democratic'
+
+            leg = Legislator(term, chamber, district, name, party=party,
+                             email_address=email)
+            leg.add_source(url)
+            self.save_legislator(leg)
