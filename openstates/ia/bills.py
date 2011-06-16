a00ab7f97387cc5b50060072a02812a9b1e5ea9a
Michael Stephens <mstephens@sunlightfoundation.com>
2011-06-16 10:58:14 -0400

    IA: no history

--- openstates/ia/bills.py
+++ openstates/ia/bills.py
@@ -51,6 +51,8 @@ class IABillScraper(BillScraper):
             return
 
         page = lxml.html.fromstring(self.urlopen(hist_url))
+        if "No history is recorded at this time." in page:
+            return
         page.make_links_absolute(hist_url)
 
         title = page.xpath("string(//table[2]/tr[4])").strip()
