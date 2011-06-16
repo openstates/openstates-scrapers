bb22244330975071385a8e83ac92f9c198ccc1c2
James Turk <james.p.turk@gmail.com>
2011-06-16 15:56:43 -0400

    MA: fix legislators party/index

--- openstates/ma/legislators.py
+++ openstates/ma/legislators.py
@@ -49,17 +49,17 @@ class MALegislatorScraper(LegislatorScraper):
 
         url = "http://www.malegislature.gov/People/%s" % chamber_type
         with self.urlopen(url) as page:
-            root = lxml.html.fromstring(page)
+            doc = lxml.html.fromstring(page)
+            doc.make_links_absolute("http://www.malegislature.gov")
 
-            for member_url in root.xpath('//div[@class="container"]/a/@href'):
-                member_url = "http://www.malegislature.gov"+member_url
+            for member_url in doc.xpath('//td[@class="nameCol firstCol"]/a/@href'):
                 self.scrape_member(chamber, term, member_url)
 
     def scrape_member(self, chamber, term, member_url):
         with self.urlopen(member_url) as page:
             root = lxml.html.fromstring(page)
+            root.make_links_absolute(member_url)
 
-        root.make_links_absolute(member_url)
         photo_url = root.xpath('//div[@class="bioPicContainer"]/img/@src')[0]
         full_name = root.xpath('//div[@class="bioPicContainer"]/img/@alt')[0]
 
@@ -68,23 +68,11 @@ class MALegislatorScraper(LegislatorScraper):
             district = district[0].text.strip()
             district = clean_district(district)
 
-        party = root.xpath('//div[@class="bioDescription"]/div')[0].text.strip().split(',')[0]
-        if party == 'Democrat':
-            party = 'Democratic'
-        elif party == 'Republican':
-            party = 'Republican'
+        party = root.xpath('//span[@class="legislatorAffiliation"]/text()')[0]
+        party = 'Democratic' if party == '(D)' else 'Republican'
 
         leg = Legislator(term, chamber, district, full_name, party=party,
                          photo_url=photo_url)
         leg.add_source(member_url)
-
-        comm_div = root.xpath('//div[@id="Column5"]//div[@class="widgetContent"]')
-        if len(comm_div):
-            comm_div = comm_div[0]
-            for li in comm_div.xpath('ul/li'):
-                # Page shows no roll information for members.
-                role = li.xpath('text()')[0].strip().strip(',') or 'member'
-                comm = li.xpath('a/text()')[0]
-                leg.add_role(role, term, chamber=chamber, committee=comm)
 
         self.save_legislator(leg)
