695da62921104325a4763461612f0f75d54d2345
James Turk <james.p.turk@gmail.com>
2011-06-20 11:57:24 -0400

    MA: committees update

--- openstates/ma/committees.py
+++ openstates/ma/committees.py
@@ -21,23 +21,25 @@ class MACommitteeScraper(CommitteeScraper):
                 doc = lxml.html.fromstring(html)
                 doc.make_links_absolute('http://www.malegislature.gov')
 
-                for com_url in doc.xpath('//div[@class="widgetContent"]/ul/li/a/@href'):
+                for com_url in doc.xpath('//ul[@class="committeeList"]/li/a/@href'):
                     self.scrape_committee(chamber, com_url)
 
     def scrape_committee(self, chamber, url):
         with self.urlopen(url) as html:
             doc = lxml.html.fromstring(html)
 
-            name = doc.xpath('//h3/text()')[0]
+            name = doc.xpath('//span[@class="committeeShortName"]/text()')[0]
             com = Committee(chamber, name)
             com.add_source(url)
 
-            for describe_div in doc.xpath('//div[@class="describe"]'):
-                role = describe_div.xpath('.//b/text()')[-1]
-                name = describe_div.xpath('.//u/a/text()')[0]
-                com.add_member(name, role)
+            # get both titles and names, order is consistent
+            titles = doc.xpath('//p[@class="rankingMemberTitle"]/text()')
+            names = doc.xpath('//p[@class="rankingMemberName"]/a/text()')
 
-            for member in doc.xpath('//div[@class="membersGallyList"]/ul/li/a/text()'):
+            for title, name in zip(titles, names):
+                com.add_member(name, title)
+
+            for member in doc.xpath('//div[@class="committeeRegularMembers"]//a/text()'):
                 com.add_member(member)
 
             self.save_committee(com)
