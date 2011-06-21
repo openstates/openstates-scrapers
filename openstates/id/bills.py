98638ac0304b4e96ea764de06888a1f7116172a7
Michael Stephens <mstephens@sunlightfoundation.com>
2011-06-21 16:18:05 -0400

    ID: votes

--- openstates/id/bills.py
+++ openstates/id/bills.py
@@ -2,8 +2,17 @@ import re
 import datetime
 
 from billy.scrape.bills import BillScraper, Bill
+from billy.scrape.votes import Vote
 
 import lxml.html
+
+
+def split_names(text):
+    text = text.replace(u'\xa0', ' ')
+    for name in text.split(' -- ')[1].split(','):
+        name = name.strip()
+        if name and name != 'None':
+            yield name
 
 
 class IDBillScraper(BillScraper):
@@ -42,19 +51,22 @@ class IDBillScraper(BillScraper):
 
         act_table = page.xpath("//table")[5]
         actor = bill['chamber']
+        prev_date = None
         for tr in act_table.xpath("tr"):
             if len(tr.xpath("td")) < 4:
                 continue
 
             date = tr.xpath("string(td[2])").strip()
             if not date:
-                continue
+                date = prev_date
+            prev_date = date
             date = datetime.datetime.strptime(date + "/" + bill['session'],
                                               "%m/%d/%Y").date()
 
             action = tr.xpath("string(td[3])").strip().replace(u'\xa0', ' ')
 
             if re.match(r"3rd\s+rdg\s+-\s+(PASSED|FAILED)\s+-", action):
+                self.scrape_vote(bill, actor, date, tr.xpath("td[3]")[0])
                 action = tr.xpath("td[3]")[0].text
                 action += tr.xpath("string(td[3]/span[1])")
                 action += ". " + tr.xpath("td[3]/br")[-1].tail
@@ -69,3 +81,25 @@ class IDBillScraper(BillScraper):
             elif 'to Senate' in action:
                 actor = 'upper'
 
+    def scrape_vote(self, bill, chamber, date, td):
+        motion = td.text
+        result = td.xpath("string(span[1])").strip()
+        passed = result.split()[0] == "PASSED"
+        yes, no, other = [
+            int(g) for g in re.search(r'(\d+)-(\d+)-(\d+)$', result).groups()]
+
+        vote = Vote(chamber, date, motion, passed, yes, no, other)
+
+        for name in split_names(td.xpath("span[. = 'AYES']")[0].tail):
+            vote.yes(name)
+        for name in split_names(td.xpath("span[. = 'NAYS']")[0].tail):
+            vote.no(name)
+        for name in split_names(td.xpath(
+            "span[contains(., 'Absent')]")[0].tail):
+            vote.other(name)
+
+        assert len(vote['yes_votes']) == vote['yes_count']
+        assert len(vote['no_votes']) == vote['no_count']
+        assert len(vote['other_votes']) == vote['other_count']
+
+        bill.add_vote(vote)
