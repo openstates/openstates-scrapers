11f472f75560f751f9ab35aef541cbb89bb47a9a
Anner J. Bonilla <annerajb@annerajb-server.(none)>
2011-06-30 19:23:36 -0400

    Modified Bills so it reads Co-Authors successfully Changed some Action Categorys so they dont show up as other for example bill:passed

--- openstates/pr/bills.py
+++ openstates/pr/bills.py
@@ -14,15 +14,50 @@ class NoSuchBill(Exception):
 
 _classifiers = (
     ('Radicado', 'bill:introduced'),
+    #votes are here
+    (u'Aprobado por Cámara en Votación Final', 'bill:passed'),
+    (u'Aprobado por el Senado en Votación', 'bill:passed'),
     ('Aparece en Primera Lectura', 'bill:reading:1'),
+    #sent is not the same as received
+    ('Enviado al Gobernador', 'governor:received'),
+    ('Veto', 'governor:vetoed'),
+    #comissions give a report but sometimes they dont do any amendments and live them as they are.
+    #i am not checking if they did or not. but it be easy just read the end and if it dosnt have amendments it should say 'sin enmiendas'
+    ('1er Informe','amendment:amended'),
+    ('2do Informe','amendment:amended'),
+    ('Aprobado con enmiendas','amendment:passed'),
     (u'Remitido a Comisión', 'committee:referred'),
     (u'Referido a Comisión', 'committee:referred'),
-    ('Enviado al Gobernador', 'governor:received'),
-    ('Aprobado por Cámara', 'bill:passed'),
-    ('Aprobado por el Senado', 'bill:passed'),
-    ('Veto', 'governor:vetoed'),
 )
-
+'''    
+    , 'bill:introduced'
+    , 'bill:passed'
+    , 'bill:failed'
+    , 'bill:withdrawn'
+    , 'bill:substituted'
+    , 'bill:filed'
+    , 'bill:veto_override:passed'
+    , 'bill:veto_override:failed'
+    , 'governor:received'
+    , 'governor:signed'
+    , 'governor:vetoed'
+    , 'governor:vetoed:line-item'
+    , 'amendment:introduced'
+    , 'amendment:passed'
+    , 'amendment:failed'
+    , 'amendment:tabled'
+    , 'amendment:amended'
+    , 'amendment:withdrawn'
+    , 'committee:referred'
+    , 'committee:failed'
+    , 'committee:passed'
+    , 'committee:passed:favorable'
+    , 'committee:passed:unfavorable'
+    , 'bill:reading:1'
+    , 'bill:reading:2'
+    , 'bill:reading:3'
+    , 'other'
+'''
 class PRBillScraper(BillScraper):
     state = 'pr'
 
@@ -60,7 +95,14 @@ class PRBillScraper(BillScraper):
             bill = Bill(session, chamber, bill_id, title[0], type=bill_type)
             author = doc.xpath(u'//td/b[contains(text(),"Autor")]/../text()')[0]
             bill.add_sponsor('primary', author.strip())
+          
+            co_authors = doc.xpath(u'//td/b[contains(text(),"Co-autor")]/../text()')
+            if len(co_authors) !=0:
+                for co_author in co_authors[1].split(','):
+                    bill.add_sponsor('cosponsor',co_author.strip());
+                    #print co_author.strip();
 
+            
             action_table = doc.xpath('//table')[-1]
             for row in action_table[1:]:
                 tds = row.xpath('td')
@@ -74,11 +116,16 @@ class PRBillScraper(BillScraper):
                                                   "%m/%d/%Y")
                 
                 action = tds[1].text_content().strip()
+                #parse the text to see if it's a new version or a unrelated document
+                #if has - let's *shrug* assume it's a vote document        
+ #               version_txt = ['Radicado','Enmiendas','Entirillado',
+#                if u'Votacion  ' != :
+                    #regular document
                 #get url of action
-                action_document =  tds[1].xpath('a[text()=\''+ action+'\']/@href')
+                #action_document =  tds[1].xpath('a[text()=\''+ action+'\']/@href')
                 #check it has a url and is not just text
-                if len(action_document) != 0:
-                    bill.add_document(action,action_document[0]);
+                #if len(action_document) != 0:
+                    #bill.add_document(action,action_document[0]);
                 
                 
                 for pattern, atype in _classifiers:
@@ -87,10 +134,11 @@ class PRBillScraper(BillScraper):
                 else:
                     atype = 'other'
                 bill.add_action(chamber, action, date, type=atype)
-
+                #if atype == 'bill:passed':
+                    #print 'voto'
                 # also has an associated version
-                if tds[1].xpath('a'):
-                    bill.add_version(action, tds[1].xpath('a/@href')[0])
+                #if tds[1].xpath('a'):
+                    #bill.add_version(action, tds[1].xpath('a/@href')[0])
 
             bill.add_source(url)
             self.save_bill(bill)
