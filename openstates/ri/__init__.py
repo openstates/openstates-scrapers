b96af6e19e14e430737aa20a251d718e4ff34d0a
Joshua Ballanco <joshua.ballanco@patch.com>
2011-07-14 15:05:29 -0400

    State and current term metadata

--- openstates/ri/__init__.py
+++ openstates/ri/__init__.py
@@ -1,1 +1,18 @@
-# Rhode Island Metadata goes here...
+metadata = dict(
+    name='Rhode Island and Providence Plantations',
+    abbreviation='ri',
+    legislature_name='Rhode Island General Assembly',
+    upper_chamber_name='Senate',
+    lower_chamber_name='House of Representatives',
+    upper_chamber_title='Senator',
+    lower_chamber_title='Representative',
+    upper_chamber_term=2,
+    lower_chamber_term=2,
+    terms=[{'name': '2011',
+            'start_year': 2011,
+            'end_year': 2011,
+            'sessions': ['2011']}
+            ],
+    session_details={'2011': {'type': 'primary'}
+                     },
+)
