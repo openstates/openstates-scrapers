fe8aaf780d35ed0d009b621ec906afa668c0e8ba
James Turk <james.p.turk@gmail.com>
2011-08-04 16:20:17 -0400

    prune_committees command

---
+++ billy/misc/prune_committees.py
@@ -0,0 +1,36 @@
+import argparse
+import datetime
+
+from billy import db
+from billy.conf import settings, base_arg_parser
+
+def prune_committees(state, remove=False):
+    empty_list = []
+    old_list = []
+    empty_and_old = []
+    for com in db.committees.find({'state': state}):
+        empty = len(com['members']) == 0
+        old = (com['updated_at'] + datetime.timedelta(days=30) < 
+               datetime.datetime.utcnow())
+        if empty:
+            empty_list.append(com)
+        if old:
+            old_list.append(com)
+        if empty and old:
+            empty_and_old.append(com)
+
+    print '  %s empty' % len(empty)
+    print '  %s not updated in last month' % len(old)
+    print '  %s empty & old' % len(empty_and_old)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='validate API results',
+                                     parents=[base_arg_parser])
+    parser.add_argument('states', nargs='+', help='states to validate')
+    args = parser.parse_args()
+    settings.update(args)
+
+    for state in args.states:
+        print "Pruning committees for %s" % state
+        validate_api(state, args.schema_dir)
