e201d2f722ddc1caa2969084244225bf2e85ddb5
James Turk <james.p.turk@gmail.com>
2011-08-04 16:44:25 -0400

    prune_committees misc command

--- billy/misc/prune_committees.py
+++ billy/misc/prune_committees.py
@@ -5,32 +5,38 @@ from billy import db
 from billy.conf import settings, base_arg_parser
 
 def prune_committees(state, remove=False):
-    empty_list = []
-    old_list = []
     empty_and_old = []
     for com in db.committees.find({'state': state}):
         empty = len(com['members']) == 0
         old = (com['updated_at'] + datetime.timedelta(days=30) < 
                datetime.datetime.utcnow())
-        if empty:
-            empty_list.append(com)
-        if old:
-            old_list.append(com)
         if empty and old:
             empty_and_old.append(com)
 
-    print '  %s empty' % len(empty)
-    print '  %s not updated in last month' % len(old)
-    print '  %s empty & old' % len(empty_and_old)
+    print '  %s to prune' % len(empty_and_old)
+    if remove:
+        for c in empty_and_old:
+            db.committees.remove(c['_id'])
+        print 'deleted'
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='prune empty committees',
                                      parents=[base_arg_parser])
-    parser.add_argument('states', nargs='+', help='states to validate')
+    parser.add_argument('states', nargs='*', help='states to validate')
+    parser.add_argument('--all', action='store_true', default=False,
+                        help='prune all states')
+    parser.add_argument('--delete', action='store_true', default=False,
+                        help='actually delete committees')
     args = parser.parse_args()
     settings.update(args)
 
+    if args.all:
+        args.states = db.metadata.find().distinct('_id')
+
+    if not args.states:
+        print "Must specify at least one state or --all"
+
     for state in args.states:
         print "Pruning committees for %s" % state
-        prune_committees(state)
+        prune_committees(state, args.delete)
