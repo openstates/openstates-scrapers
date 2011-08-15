f96af269439b5e1fe7806eda712081a80048ffb7
Dan Schneiderman <schneiderman.daniel@gmail.com>
2011-08-15 11:00:04 -0400

    Started Nebraska. Added basic info to __init__.py.

---
+++ openstates/ne/__init__.py
@@ -0,0 +1,21 @@
+import datetime
+
+metadata = dict(
+    name='Nebraska',
+    abbreviation='ne',
+    legislature_name='Nebraska Legislature',
+#   lower_chamber_name='n/a',
+    upper_chamber_name='The Univameral',
+#   lower_chamber_title='n/a',
+    upper_chamber_title='Senator',
+#   lower_chamber_term=2,
+    upper_chamber_term=2,
+    terms=[
+        {'name': '2011-2012', 'sessions': ['102'],
+        'start_year'; 2011, 'end_year': 2012},
+    ],
+    session_details={
+        '102': {'display_name': '102nd Legislature',}
+    },
+    feature_flags=[],
+)
