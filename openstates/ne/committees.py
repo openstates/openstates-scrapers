a164d1011aa781b8f2f9ba6dbc22a490388c7490
Dan Schneiderman <schneiderman.daniel@gmail.com>
2011-08-18 19:18:01 -0400

    Finished standing committees. Other comms started.

--- openstates/ne/committees.py
+++ openstates/ne/committees.py
@@ -9,10 +9,29 @@ class NECommitteeScraper(CommitteeScraper):
     def scrape(self, chamber, term):
         self.validate_term(term, latest_only=True)
 
-        self.standing_comm()
-   
-    def select_comm(self):
-        return "please hold.\n"
+        #self.standing_comm()
+        self.select_special_comm()
+
+    def select_special_comm(self):
+        main_url = 'http://www.nebraskalegislature.gov/committees/select-committees.php'
+        with self.urlopen(main_url) as page:
+            page = lxml.html.fromstring(page)
+
+            comm_names = page.xpath('/html/body/div[@id="wrapper"]/div[@id="content"]/div[@id="content_text"]/div[@class="content_box_container"]/div[@class="content_box"]/h2/a')
+            for x in range(len(comm_names)):
+                print comm_names[x].text
+
+                #senators = page.xpath('/ul[@class="nobullet"]/li/a')
+                #print senators
+            #print "\n"
+            for comm_links in page.xpath('/html/body/div[@id="wrapper"]/div[@id="content"]/div[@id="content_text"]/div[@class="content_box_container"]/div[@class="content_box"]/h2'):
+                comm_name = comm_links.text
+                if comm_name != None:
+                    print comm_name + '\n'
+                    senator = comm_links.xpath('/ul[@class="nobullet"]/li/a').text
+                    print senator
+
+
 
     def standing_comm(self):
        main_url = 'http://www.nebraskalegislature.gov/committees/standing-committees.php'
@@ -30,7 +49,7 @@ class NECommitteeScraper(CommitteeScraper):
                    comm_name = ''
                    for x in range(len(name)):
                        comm_name += name[x] + ' '
-
+                   comm_name = comm_name[0: -1]
                    committee = Committee('upper', comm_name)
 
                    for senators in detail_page.xpath('/html/body[@class="home blog"]/div[@id="page"]/div[@id="sidebar"]/ul[1]/li[1]/ul/li/a'):
@@ -42,4 +61,6 @@ class NECommitteeScraper(CommitteeScraper):
                             role = 'member'
                             senator = senator[6:-1]
                        committee.add_member(senator, role)
-                       self.save_committee(committee)
+                   committee.add_source(main_url)
+                   committee.add_source(detail_link)
+                   self.save_committee(committee)
