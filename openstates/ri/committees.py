96c74331e06f03b3c4199204aa8e5f4d798d9d70
James Turk <james.p.turk@gmail.com>
2011-09-12 14:49:47 -0400

    RI: remove greedy try-excepts

--- openstates/ri/committees.py
+++ openstates/ri/committees.py
@@ -23,32 +23,26 @@ class RICommitteeScraper(CommitteeScraper):
     def scrape_comm_list(self, ctype):
         url = 'http://www.rilin.state.ri.us/Sitemap.html'
         self.log("looking for "+ctype)
-        try:
-            with self.urlopen(url) as page:
-                root = lxml.html.fromstring(page)
-                return root.xpath("//a[contains(@href,'"+ctype+"')]")
-        except:
-            pass
+        with self.urlopen(url) as page:
+            root = lxml.html.fromstring(page)
+            return root.xpath("//a[contains(@href,'"+ctype+"')]")
 
     def add_members(self,comm,url):
-        try:
-            with self.urlopen(url) as page:
-                self.log(comm)
-                root = lxml.html.fromstring(page)
-                # The first <tr> in the table of members
-                membertable=root.xpath('//p[@class="style28"]')[0].getparent().getparent().getnext()
-                while membertable is not None and membertable.tag == 'tr':
-                    flds=membertable.xpath('td//text()')
-                    mname = flds[0]
-                    role = flds[1][2:]
-                    idx = mname.find("Senator ")
-                    if idx == -1: continue
-                    membername=mname[idx+8:]
-                    self.log("name "+membername+" role "+role)
-                    comm.add_member(membername, role)
-                    membertable = membertable.getnext()
-        except:
-            pass
+        with self.urlopen(url) as page:
+            self.log(comm)
+            root = lxml.html.fromstring(page)
+            # The first <tr> in the table of members
+            membertable=root.xpath('//p[@class="style28"]')[0].getparent().getparent().getnext()
+            while membertable is not None and membertable.tag == 'tr':
+                flds=membertable.xpath('td//text()')
+                mname = flds[0]
+                role = flds[1][2:]
+                idx = mname.find("Senator ")
+                if idx == -1: continue
+                membername=mname[idx+8:]
+                self.log("name "+membername+" role "+role)
+                comm.add_member(membername, role)
+                membertable = membertable.getnext()
 
     def scrape_reps_comm(self):
         base = 'http://www.rilin.state.ri.us'
