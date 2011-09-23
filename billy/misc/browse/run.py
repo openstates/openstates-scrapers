8bc0517b99f5b9b3eb341ec0785d18898e69d034
James Turk <james.p.turk@gmail.com>
2011-09-22 21:29:34 -0400

    spin off billy

--- billy/misc/browse/run.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-if __name__ == '__main__':
-    import webbrowser
-    import os
-
-    path = "file:///" + os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                                     "browse.html"))
-    webbrowser.get("firefox").open(path, new=2)
--- billy/site/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-    (r'^api/', include('billy.site.api.urls')),
-    (r'^browse/', include('billy.site.browse.urls')),
-)
