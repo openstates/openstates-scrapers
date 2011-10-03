e563e998a24a53a9f7c4ab5984757849081dfb17
Dan Schneiderman <schneiderman.daniel@gmail.com>
2011-10-03 08:56:59 -0400

    ND: Set up connection for pages.

--- openstates/nd/committees.py
+++ openstates/nd/committees.py
@@ -8,13 +8,19 @@ class NECommitteeScraper(CommitteeScraper):
     def scrape(self, chamber, term):
         self.validate_term(term, latest_only=True)
 
+        #testing for chamber
         if chamber == 'upper':
             url_chamber_name = 'senate'
         else:
             url_chamber_name = 'house'
 
-        
+        #testing for starting year
+        if int(term) == 62:
+            start_year = 2011
+
         committee_types = ["standing-comm", "pro-comm"]
         for committee in committee_types:
-           url = "http://www.legis.nd.gov/assembly/%s/%s/%s/" % (term, url_chamber_name, committee)
-           print url
+           url = "http://www.legis.nd.gov/assembly/%s-%s/%s/%s/" % (term, start_year, url_chamber_name, committee)
+           
+           with self.urlopen(url) as page:
+               page = lxml.html.fromstring(page)
