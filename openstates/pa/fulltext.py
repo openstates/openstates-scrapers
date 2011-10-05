4bca97ffc942b3fa6ac78a94f644097b3c117aff
James Turk <james.p.turk@gmail.com>
2011-10-05 13:42:47 -0400

    PA: extract_text

--- openstates/pa/fulltext.py
+++ openstates/pa/fulltext.py
@@ -4,7 +4,7 @@ import lxml.html
 def collapse_spaces(text):
     return re.sub('\s+', ' ', text)
 
-def pa_handler(filedata, metadata):
+def extract_text(filedata, metadata):
     doc = lxml.html.fromstring(filedata)
     # another state where the second column of the table is the good stuff
     text = ' '.join(x.text_content() for x in doc.xpath('//tr/td[2]'))
