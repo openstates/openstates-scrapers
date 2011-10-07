728ea9182c0310fd49dfd88d084f05e2d5d742dc
James Turk <james.p.turk@gmail.com>
2011-10-07 15:24:37 -0400

    SC: version url fix

--- openstates/sc/bills.py
+++ openstates/sc/bills.py
@@ -483,7 +483,7 @@ class SCBillScraper(BillScraper):
         version_url = doc.xpath('//a[text()="View full text"]/@href')[0]
         version_html = self.urlopen(version_url)
         version_doc = lxml.html.fromstring(version_html)
-        version_doc.make_links_absolute(bill_detail_url)
+        version_doc.make_links_absolute(version_url)
         for version in version_doc.xpath('//a[contains(@href, "/prever/")]'):
             bill.add_version(version.text, version.get('href'))
 
