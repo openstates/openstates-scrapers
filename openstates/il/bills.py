7343b614ae0769ff2e606e95d4c0532b4ae0a847
Joe Germuska <joe@germuska.com>
2011-11-18 15:38:47 -0600

    multiple changes (bad form): work on refining action categorization, including documenting common 'other' types; also, move add_source calls to slighly clearer spots (and so that the billstatus url is always first)

--- openstates/il/bills.py
+++ openstates/il/bills.py
@@ -22,6 +22,10 @@ SPONSOR_PATTERN = re.compile("^(Added |Removed )?(.+Sponsor) (Rep|Sen). (.+)$")
 
 VERSION_TYPES = ('Introduced', 'Engrossed', 'Enrolled', 'Re-Enrolled')
 FULLTEXT_DOCUMENT_TYPES = ('Public Act', "Governor's Message", )
+# not as common, but maybe should just be added to FULLTEXT_DOCUMENT_TYPES?
+# Amendatory Veto Motion \d{3}
+# Conference Committee Report \d{3}
+
 DOC_TYPES = {
     'B': 'bill',
     'R': 'resolution',
@@ -29,34 +33,76 @@ DOC_TYPES = {
     'JRCA': 'constitutional amendment',
 }
 
-_action_classifiers = (
-    ('Amendment No. \d+ Filed', 'amendment:introduced'),
-    ('Amendment No. \d+ Tabled', 'amendment:failed'),
-    ('Amendment No. \d+ Adopted', 'amendment:passed'),
-    ('(Pref|F)iled with', 'bill:filed'),
-    ('Arrived? in', 'bill:introduced'),
-    ('First Reading', 'bill:reading:1'),
-    ('(Recalled to )?Second Reading', 'bill:reading:2'),
-    ('(Re-r|R)eferred to', 'committee:referred'),
-    ('(Re-a|A)ssigned to', 'committee:referred'),
-    ('Sent to the Governor', 'governor:received'),
-    ('Governor Approved', 'governor:signed'),
-    ('Governor Vetoed', 'governor:vetoed'),
-    ('Governor Item', 'governor:vetoed:line-item'),
-    ('Governor Amendatory Veto', 'governor:vetoed'),
-    ('Do Pass', 'committee:passed'),
-    ('Recommends Be Adopted', 'committee:passed:favorable'),
-    ('Third Reading .+? Passed', ['bill:reading:3', 'bill:passed']),
-    ('Third Reading .+? Lost', ['bill:reading:3', 'bill:failed']),
-    ('Resolution Adopted', 'bill:passed'),
-    ('Resolution Lost', 'bill:failed'),
-    ('Tabled', 'bill:withdrawn'),
+_action_classifiers = ( # see http://openstates.org/categorization/
+    (re.compile(r'Amendment No. \d+ Filed'), 'amendment:introduced'),
+    (re.compile(r'Amendment No. \d+ Tabled'), 'amendment:failed'),
+    (re.compile(r'Amendment No. \d+ Adopted'), 'amendment:passed'),
+    (re.compile(r'(Pref|F)iled with'), 'bill:filed'),
+    (re.compile(r'Arrived? in'), 'bill:introduced'),
+    (re.compile(r'First Reading'), 'bill:reading:1'),
+    (re.compile(r'(Recalled to )?Second Reading'), 'bill:reading:2'),
+    (re.compile(r'(Re-r|R)eferred to'), 'committee:referred'),
+    (re.compile(r'(Re-a|A)ssigned to'), 'committee:referred'),
+    (re.compile(r'Sent to the Governor'), 'governor:received'),
+    (re.compile(r'Governor Approved'), 'governor:signed'),
+    (re.compile(r'Governor Vetoed'), 'governor:vetoed'),
+    (re.compile(r'Governor Item'), 'governor:vetoed:line-item'),
+    (re.compile(r'Governor Amendatory Veto'), 'governor:vetoed'),
+    (re.compile(r'Do Pass'), 'committee:passed'),
+    (re.compile(r'Recommends Be Adopted'), 'committee:passed:favorable'),
+    (re.compile(r'Be Adopted'), 'committee:passed:favorable'),
+    (re.compile(r'Third Reading .+? Passed'), ['bill:reading:3', 'bill:passed']),
+    (re.compile(r'Third Reading .+? Lost'), ['bill:reading:3', 'bill:failed']),
+    (re.compile(r'Third Reading'), 'bill:reading:3'),
+    (re.compile(r'Resolution Adopted'), 'bill:passed'),
+    (re.compile(r'Resolution Lost'), 'bill:failed'),
+    (re.compile(r'Session Sine Die',), 'bill:failed'),
+    (re.compile(r'Tabled'), 'bill:withdrawn'),
 )
 
+OTHER_FREQUENT_ACTION_PATTERNS_WHICH_ARE_CURRENTLY_UNCLASSIFIED = [
+    r'Accept Amendatory Veto - (House|Senate) (Passed|Lost) \d+-\d+\d+.?',
+    r'Amendatory Veto Motion - (.+)',
+    r'Balanced Budget Note (.+)',
+    r'Effective Date(\s+.+ \d{4})?(;.+)?',
+    r'To .*Subcommittee',
+    r'Note Requested',
+    r'Note Filed',
+    r'^Public Act',
+    r'Appeal Ruling of Chair',
+    r'Added .*Sponsor',
+    r'Remove(d)? .*Sponsor',
+    r'Sponsor Removed',
+    r'Sponsor Changed',
+    r'^Chief .*Sponsor',
+    r'^Co-Sponsor',
+    r'Deadline Extended.+9\(b\)',
+    r'Amendment.+Approved for Consideration',
+    r'Approved for Consideration',
+    r'Amendment.+Do Adopt',
+    r'Amendment.+Concurs',
+    r'Amendment.+Lost',
+    r'Amendment.+Withdrawn',
+    r'Amendment.+Motion.+Concur',
+    r'Amendment.+Motion.+Table',
+    r'Amendment.+Rules Refers',
+    r'Amendment.+Motion to Concur Recommends be Adopted',
+    r'Amendment.+Assignments Refers',
+    r'Amendment.+Assignments Refers',
+    r'Amendment.+Held',
+    r'Motion.+Suspend Rule 25',
+    r'Motion.+Reconsider Vote',
+    r'Placed on Calendar',
+    r'Amendment.+Postponed - (?P<committee>.+)',
+    r'Postponed - (?P<committee>.+)',
+    r"Secretary's Desk",
+    r'Rule 2-10 Committee Deadline Established',
+    r'^Held in (?P<committee>.+)'
+]
 
 def _categorize_action(action):
     for pattern, atype in _action_classifiers:
-        if re.findall(pattern, action):
+        if pattern.findall(action):
             return atype
     return 'other'
 
@@ -108,6 +154,7 @@ class ILBillScraper(BillScraper):
         bill = Bill(session, chamber, bill_id, title, type=bill_type,
                     synopsis=synopsis)
 
+        bill.add_source(url)
         # sponsors
         for sponsor in doc.xpath('//a[@class="content"]/text()'):
             bill.add_sponsor('cosponsor', sponsor)
@@ -136,9 +183,7 @@ class ILBillScraper(BillScraper):
         if len(doc.xpath('//a[contains(@href, "votehistory")]')) > 1:
             votes_url = doc.xpath('//a[text()="Votes"]/@href')[0]
             self.scrape_votes(bill, votes_url)
-            bill.add_source(votes_url)
 
-        bill.add_source(url)
         self.save_bill(bill)
 
 
@@ -164,21 +209,21 @@ class ILBillScraper(BillScraper):
         html = self.urlopen(votes_url)
         doc = lxml.html.fromstring(html)
         doc.make_links_absolute(votes_url)
-
+        
         EXPECTED_VOTE_CODES = ['Y','N','E','NV','A','P','-']
-
+        
         # vote indicator, a few spaces, a name, newline or multiple spaces
         VOTE_RE = re.compile('(Y|N|E|NV|A|P|-)\s{2,5}(\w.+?)(?:\n|\s{2})')
-
+        
         for link in doc.xpath('//a[contains(@href, "votehistory")]'):
-
+            
             pieces = link.text.split(' - ')
             date = pieces[-1]
             if len(pieces) == 3:
                 motion = pieces[1]
             else:
                 motion = 'Third Reading'
-
+                
             chamber = link.xpath('../following-sibling::td/text()')[0]
             if chamber == 'HOUSE':
                 chamber = 'lower'
@@ -186,16 +231,16 @@ class ILBillScraper(BillScraper):
                 chamber = 'upper'
             else:
                 self.warning('unknown chamber %s' % chamber)
-
+                
             date = datetime.datetime.strptime(date, "%A, %B %d, %Y")
-
+            
             # download the file
             fname, resp = self.urlretrieve(link.get('href'))
             pdflines = convert_pdf(fname, 'text').splitlines()
             os.remove(fname)
-
+            
             vote = Vote(chamber, date, motion.strip(), False, 0, 0, 0)
-
+            
             for line in pdflines:
                 for match in VOTE_RE.findall(line):
                     vcode, name = match
@@ -205,12 +250,15 @@ class ILBillScraper(BillScraper):
                         vote.no(name)
                     else:
                         vote.other(name)
-
+            
             # fake the counts
             vote['yes_count'] = len(vote['yes_votes'])
             vote['no_count'] = len(vote['no_votes'])
             vote['other_count'] = len(vote['other_votes'])
             vote['passed'] = vote['yes_count'] > vote['no_count']
             vote.add_source(link.get('href'))
-
+            
             bill.add_vote(vote)
+        
+        bill.add_source(votes_url)
+        
