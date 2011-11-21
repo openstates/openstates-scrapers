ec6a48dc9a5541dddfe9ded6dd700558aa880127
Dan Schneiderman <schneiderman.daniel@gmail.com>
2011-11-21 13:04:08 -0500

    ND: Standing committee finished. Comm names for Procedural done.

--- openstates/nd/committees.py
+++ openstates/nd/committees.py
@@ -27,16 +27,60 @@ class NECommitteeScraper(CommitteeScraper):
                page = lxml.html.fromstring(page)
 
                if committee == 'standing-comm':
-                   self.scrapeStanding(chamber, page)
+                   self.scrapeStanding(chamber, page, url)
+                   test = True
                else:
-                   self.scrapeProcedural(chamber, page)
+                   #self.scrapeProcedural(chamber, page, url)
+                   test = True
 
-    def scrapeStanding(self, chamber, page):
-        for comm_names in page.xpath('//div[@class="content"][1]/p/a/span'):
-            name = re.sub('[^A-Za-z0-9]+', ' ', comm_names.text)
-            print name
+    def scrapeStanding(self, chamber, page, url):
+        comm_count = 1
+        for comm_names in page.xpath('//div[@class="content"][1]/p//span'):
+            name = re.sub('[^A-Za-z0-9]+', ' ', comm_names.text).replace(' ', '')
+            comm = Committee(chamber, name)
 
-    def scrapeProcedural(self, chamber, page):
+            member_count = 1
+            members_path = '//div[@class="content"][1]/table[@class="p"][%s]//tr/td[2]' % (str(comm_count))
+            for members in comm_names.xpath(members_path):
+                memberName = members.xpath('a')[0].text
+                if memberName == None: #special case for Randy Boehning under Goverment and Vetran Affairs in House
+                    memberName = members.xpath('a')[1].text
+                memberName = re.sub('[^A-Za-z0-9]+', ' ', memberName)
+
+                #role
+                role_path = '//div[@class="content"][1]/table[@class="p"][%s]//tr[%s]/td[2]/a' % (comm_count, member_count)
+                role_text = page.xpath(role_path)[0].tail
+                if role_text != None:
+                    if "Vice" in role_text:
+                        role = "Vice-Chairman"
+                    elif "Chairman" in role_text:
+                        role = "Chairman"
+                    else:
+                        role = "Member"
+                    comm.add_member(memberName, role) 
+                else:
+                    if member_count == 1:
+                        role = "Chairman"
+                    elif member_count == 2:
+                        role = "Vice-Chairman"
+                    else:
+                        role = "Member"
+                    comm.add_member(memberName, role)
+                member_count += 1
+            comm.add_source(url)
+            self.save_committee(comm)
+            comm_count += 1
+
+    def scrapeProcedural(self, chamber, page, url):
+        comm_count = 1
         for comm_names in page.xpath('//div[@class="content"][1]/p/a'):
-            name = re.sub('[^A-Za-z0-9]+', ' ', comm_names.text)
-            print name
+            name = re.sub('[^A-Za-z0-9]+', ' ', comm_names.text).replace(' ', '')
+            comm = Committee(chamber, page)
+            
+            print '\n\n' + name + '\n'
+            members_path = '//div[@class="content"][1]/table[@class="p"][%s]//tr/td[2]/a' % (str(comm_count))
+            print members_path
+            #for members in comm_names.xpath(members_path):
+            #    member = members.text
+            #    member = re.sub('[^A-Za-z0-9]+', ' ', member)
+            #    #print member
