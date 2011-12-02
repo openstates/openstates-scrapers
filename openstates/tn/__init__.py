39df24a710736cc505ca3c21e012a1b4b699ae1c
Dan Schneiderman <schneiderman.daniel@gmail.com>
2011-12-02 14:54:36 -0500

    TN: Updated __init__ file to new format.

--- openstates/tn/__init__.py
+++ openstates/tn/__init__.py
@@ -1,56 +1,18 @@
-metadata = {
-    'name': 'Tennessee',
-    'abbreviation': 'tn',
-    'legislature_name': 'Tennessee General Assembly',
-    'lower_chamber_name': 'House',
-    'upper_chamber_name': 'Senate',
-    'lower_chamber_title': 'Represenative',
-    'upper_chamber_title': 'Senator',
-    'lower_chamber_term': 2,
-    'upper_chamber_term': 4,
-    'terms':[
-        {
-            'name': '99th General Assembly', 
-            'start_year': 1995, 
-            'end_year': 1996, 
-            'sessions': ['99th Regular Session', '99th Special Session']
-        },
-        {
-            'name': '100th General Assembly', 
-            'start_year': 1997, 
-            'end_year': 1998, 
-            'sessions': ['100th Regular Session',]
-        },
-        {
-            'name': '101st General Assembly', 
-            'start_year': 1999, 
-            'end_year': 2000, 
-            'sessions': ['101st Regular Session', '101st, 1st Special Session', '101st, 2nd Special Session']
-        },
-        {
-            'name': '102nd General Assembly', 
-            'start_year': 2001, 
-            'end_year': 2002, 
-            'sessions': ['102nd Regular Session',]
-        },
-        {
-            'name': '103rd General Assembly', 
-            'start_year': 2003, 
-            'end_year': 2004, 
-            'sessions': ['103rd Regular Session',]
-        },
-        {
-            'name': '104th General Assembly', 
-            'start_year': 2005, 
-            'end_year': 2006, 
-            'sessions': ['104th Regular Session', '104th Special Session']
-        },
-        {
-            'name': '105th General Assembly', 
-            'start_year': 2007, 
-            'end_year': 2008, 
-            'sessions': ['105th Regular Session',]
-        },
+import datetime
+
+#start date of each session is the first tuesday in January after new years
+
+metadata = dict(
+    name = 'Tennessee',
+    abbreviation = 'tn',
+    legislature_name = 'Tennessee General Assembly',
+    lower_chamber_name = 'House',
+    upper_chamber_name = 'Senate',
+    lower_chamber_title = 'Represenative',
+    upper_chamber_title = 'Senator',
+    lower_chamber_term = 2,
+    upper_chamber_term = 4,
+    terms = [
         {
             'name': '106th General Assembly', 
             'start_year': 2009, 
@@ -61,7 +23,24 @@ metadata = {
             'name': '107th General Assembly', 
             'start_year': 2010, 
             'end_year': 2011, 
-            'sessions': ['107th Regular Session',]
+            'sessions': ['107th Regular Session']
         }       
-    ]
-}
+    ],
+    session_details={
+        '107th Regular Session': {
+            'start_date': datetime.date(2011, 1, 11),
+            'end_date': datetime.date(2012, 1, 10),
+            'number': 107,
+            'type': 'primary' },
+        '106th Regular Session': {
+            'start_date': None,
+            'end_date': None,
+            'number': 106,
+            'type': 'primary'},
+        '106th Special Session': {
+            'start_date': None,
+            'end_date': None,
+            'number': 106,
+            'type': 'special'},
+    }
+)
