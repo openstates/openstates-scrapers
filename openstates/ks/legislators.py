febe234ac05d6efc844f9bb259adcec417dd865f
James Turk <james.p.turk@gmail.com>
2011-12-06 02:11:03 -0500

    KS: fix legislators

--- openstates/ks/legislators.py
+++ openstates/ks/legislators.py
@@ -1,4 +1,3 @@
-
 from billy.scrape.legislators import LegislatorScraper, Legislator
 
 import lxml.html
@@ -6,51 +5,51 @@ import re
 
 ksleg = 'http://www.kslegislature.org'
 legislator_list_url = '%s/li/b2011_12/year1/members/' % ksleg
-legislator_name_pattern = re.compile('(Representative|Senator) ([A-Za-z]+) ([A-Za-z]+)')
+legislator_name_pattern = re.compile('(Representative|Senator) (.*)')
 legislator_line_pattern = re.compile('Party: ([A-Za-z]+).*First Term: ([0-9]+)')
 
 class KSLegislatorScraper(LegislatorScraper):
     state = 'ks'
 
     def scrape(self, chamber, term):
+        if chamber == 'lower':
+            legislator_list_url = 'http://www.kslegislature.org/li/chamber/house/roster/'
+        else:
+            legislator_list_url = 'http://www.kslegislature.org/li/chamber/senate/roster/'
+
         with self.urlopen(legislator_list_url) as legislator_list_page:
-            legislator_list_page = lxml.html.fromstring(legislator_list_page)
+            doc = lxml.html.fromstring(legislator_list_page)
+            doc.make_links_absolute(legislator_list_url)
 
-            legislator_main = legislator_list_page.xpath("//div[@id='container']/div[@id='wrapper']/div[@id='main_content']/div[@id='main']")
+            rows = doc.xpath('//table/tr')[1:]
+            for row in rows:
+                name, district, phone, email = row.getchildren()
+                url = name.xpath('a')[0].get('href')
+                district = district.text_content().strip()
+                phone = phone.text_content().strip()
+                email = email.text_content().strip()
+                self.scrape_legislator(chamber, term, url, district, phone,
+                                       email)
 
-            if chamber == 'upper':
-                table = legislator_main[0].xpath('div')[1]
-            else:
-                table = legislator_main[0].xpath('div')[0]
-
-            tabs = table.xpath("div/ul")
-            for tab in tabs:
-                rows = tab.xpath('li')
-                for row in rows:
-                    name = row.xpath('a')[0].text_content()
-                    url = row.xpath('a')[0].get('href')
-                    self.scrape_legislator(chamber, term, name, url)
-
-    def scrape_legislator(self, chamber, term, name, url):
-        with self.urlopen(ksleg + url) as legislator_page:
+    def scrape_legislator(self, chamber, term, url, district, phone, email):
+        with self.urlopen(url) as legislator_page:
             legislator_page = lxml.html.fromstring(legislator_page)
 
-            main = legislator_page.xpath("/html/body/div[@id='container']/div[@id='wrapper']/div[@id='main_content']/div[@id='main']")[0]
+            main = legislator_page.xpath("//div[@id='main']")[0]
             name = main.xpath('h1')[0].text_content()
-            district = main.xpath('h2')[0].text_content()
             info = main.xpath('h3')[0].text_content()
 
-            match = legislator_name_pattern.match(name)
-            if match:
-                first_name = match.group(2)
-                last_name = match.group(3)
+            full_name = legislator_name_pattern.match(name).group(2).split(' - ')[0]
 
-                match = legislator_line_pattern.match(info)
-                if match:
-                    party = match.group(1)
+            party = legislator_line_pattern.match(info).group(1)
+            if party == 'Democrat':
+                party = 'Democratic'
 
-                    legislator = Legislator(term, chamber, district, "%s %s" % (first_name, last_name), first_name, last_name, '', party)
-                    legislator.add_source(ksleg + url)
+            photo = legislator_page.xpath('//img[@class="profile-picture"]/@src')[0]
 
-                    self.save_legislator(legislator)
+            legislator = Legislator(term, chamber, district, full_name,
+                                    party=party, office_phone=phone,
+                                    email=email, url=url, photo_url=photo)
+            legislator.add_source(url)
 
+            self.save_legislator(legislator)
