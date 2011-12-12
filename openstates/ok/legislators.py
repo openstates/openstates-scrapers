09f0f475b81f7fc1b37faecc0bde58fa9dd62c17
James Turk <james.p.turk@gmail.com>
2011-12-12 15:19:39 -0500

    OK: photo and legislator url

--- openstates/ok/legislators.py
+++ openstates/ok/legislators.py
@@ -33,8 +33,15 @@ class OKLegislatorScraper(LegislatorScraper):
             elif party == 'D':
                 party = 'Democratic'
 
-            leg = Legislator(term, 'lower', district, name, party=party)
+            leg_url = 'http://www.okhouse.gov/District.aspx?District=' + district
+            leg_doc = lxml.html.fromstring(self.urlopen(leg_url))
+            leg_doc.make_links_absolute(leg_url)
+            photo_url = leg_doc.xpath('//a[contains(@href, "HiRes")]/@href')[0]
+
+            leg = Legislator(term, 'lower', district, name, party=party,
+                             photo_url=photo_url, url=leg_url)
             leg.add_source(url)
+            leg.add_source(leg_url)
             self.save_legislator(leg)
 
     def scrape_upper(self, term):
