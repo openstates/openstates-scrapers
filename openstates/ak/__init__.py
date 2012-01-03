a898c4e6c3f852c91420071f5e087b1ac9386b6b
James Turk <james.p.turk@gmail.com>
2012-01-03 13:55:21 -0500

    AK: sessions in metadata

--- openstates/ak/__init__.py
+++ openstates/ak/__init__.py
@@ -15,9 +15,19 @@ metadata = dict(
              start_year=2011, end_year=2012),
     ],
     session_details={
-        '26': {'display_name': '26th Legislature'},
-        '27': {'display_name': '27th Legislature'},
+        '26': {'display_name': '26th Legislature',
+               '_scraped_name': 'The 26th Legislature (2009-2010)'},
+        '27': {'display_name': '27th Legislature',
+               '_scraped_name': 'The 27th Legislature (2011-2012)'},
     },
+    _ignored_scraped_sessions=['The 25th Legislature (2007-2008)',
+                               'The 24th Legislature (2005-2006)',
+                               'The 23rd Legislature (2003-2004)',
+                               'The 22nd Legislature (2001-2002)',
+                               'The 21st Legislature (1999-2000)',
+                               'The 20th Legislature (1997-1998)',
+                               'The 19th Legislature (1995-1996)',
+                               'The 18th Legislature (1993-1994)'],
     feature_flags=['subjects'],
 )
 
