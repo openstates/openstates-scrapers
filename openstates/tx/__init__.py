269b6ad3a7b048d8f8f6988df3d82f17f1cc32df
James Turk <james.p.turk@gmail.com>
2012-01-04 13:55:47 -0500

    TX: sessions

--- openstates/tx/__init__.py
+++ openstates/tx/__init__.py
@@ -24,21 +24,35 @@ metadata = dict(
                'end_date': datetime.date(2009, 6, 1),
                'type': 'primary',
                'display_name': '81st Legislature',
+                '_scraped_name': '81(R) - 2009',
               },
         '811': {'start_date': datetime.date(2009, 7, 1),
                 'end_date': datetime.date(2009, 7, 10),
                 'type': 'special',
                 'display_name': '81st Legislature, 1st Called Session',
+                '_scraped_name': '81(1) - 2009',
                },
         '82': {'start_date': datetime.date(2011, 1, 11),
                'type': 'primary',
                'display_name': '82nd Legislature',
+               '_scraped_name': '82(R) - 2011',
               },
         '821': {'type': 'special',
-                'display_name': '82nd Legislature, 1st Called Session'
+                'display_name': '82nd Legislature, 1st Called Session',
+                '_scraped_name': '82(1) - 2011',
                }
     },
     feature_flags=['events', 'subjects'],
+    _ignored_scraped_sessions=['80(R) - 2007', '79(3) - 2006', '79(2) - 2005',
+                               '79(1) - 2005', '79(R) - 2005', '78(4) - 2004',
+                               '78(3) - 2003', '78(2) - 2003', '78(1) - 2003',
+                               '78(R) - 2003', '77(R) - 2001', '76(R) - 1999',
+                               '75(R) - 1997', '74(R) - 1995', '73(R) - 1993',
+                               '72(4) - 1992', '72(3) - 1992', '72(2) - 1991',
+                               '72(1) - 1991', '72(R) - 1991', '71(6) - 1990',
+                               '71(5) - 1990', '71(4) - 1990', '71(3) - 1990',
+                               '71(2) - 1989', '71(1) - 1989', '71(R) - 1989']
+
 )
 
 def session_list():
