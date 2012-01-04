86966e8320d9934d927094af288de18ba2041b87
James Turk <james.p.turk@gmail.com>
2012-01-04 14:08:12 -0500

    VT: sessions

--- openstates/vt/__init__.py
+++ openstates/vt/__init__.py
@@ -19,12 +19,20 @@ metadata = dict(
            ],
     session_details={'2009-2010': {'type': 'primary',
                                    'display_name': '2009-2010 Regular Session',
+                                   '_scraped_name': '2009-2010 Session',
                                   },
                      '2011-2012': {'type': 'primary',
                                    'display_name': '2011-2012 Regular Session',
+                                   '_scraped_name': '2011-2012 Session',
                                   },
                      },
     feature_flags=[],
+    _ignored_scraped_sessions=['2009 Special Session', '2007-2008 Session',
+                               '2005-2006 Session', '2005 Special Session',
+                               '2003-2004 Session', '2001-2002 Session',
+                               '1999-2000 Session', '1997-1998 Session',
+                               '1995-1996 Session', '1993-1994 Session']
+
 )
 
 def session_list():
