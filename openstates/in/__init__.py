530e9bafd289fe4692c38f70a0c8e770220ef3dd
James Turk <james.p.turk@gmail.com>
2012-01-05 15:41:41 -0500

    IN: sessions

--- openstates/in/__init__.py
+++ openstates/in/__init__.py
@@ -12,14 +12,34 @@ metadata = dict(
     lower_chamber_term=2,
     terms=[
         {'name': '2011-2012', 'start_year': 2011,
-         'end_year': 2012, 'sessions': ['2011']},
+         'end_year': 2012, 'sessions': ['2011', '2012']},
         ],
     session_details={
         '2011': {'start_date': datetime.date(2011, 1, 5),
                  'display_name': '2011 Regular Session',
+                 '_scraped_name': '2011 Regular Session',
                 },
+        '2012': {'display_name': '2012 Regular Session',
+                 '_scraped_name': '2012 Regular Session',},
         },
     feature_flags=['subjects'],
+    _ignored_scraped_sessions=[
+        '2010 Regular Session',
+        '2009 Special Session',
+        '2009 Regular Session',
+        '2008 Regular Session',
+        '2007 Regular Session',
+        '2006 Regular Session',
+        '2005 Regular Session',
+        '2004 Regular Session',
+        '2003 Regular Session',
+        '2002 Special Session',
+        '2002 Regular Session',
+        '2001 Regular Session',
+        '2000 Regular Session',
+        '1999 Regular Session',
+        '1998 Regular Session',
+        '1997 Regular Session']
 )
 
 def session_list():
