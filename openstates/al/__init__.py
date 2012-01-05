631254c37a95eb0877ea1506b4516a7db5687c4f
James Turk <james.p.turk@gmail.com>
2012-01-05 15:21:31 -0500

    AL: sessions, including 2012

--- openstates/al/__init__.py
+++ openstates/al/__init__.py
@@ -1,8 +1,5 @@
 import datetime
 
-# TODO: this state isn't live and so we're letting it get away with violations
-# of https://github.com/sunlightlabs/openstates/wiki/Session-Naming-Style-Guide
-
 metadata =  {
     'name': 'Alabama',
     'abbreviation': 'al',
@@ -25,7 +22,7 @@ metadata =  {
         # 'start_year': 2009,
         # 'end_year': 2010},
         {'name': '2011-2014',
-         'sessions': ['2011rs',],
+         'sessions': ['2011rs','2012rs'],
          'start_year': 2011,
          'end_year': 2014,
         }
@@ -75,26 +72,49 @@ metadata =  {
         #    'internal_id': 1051,
         #},
         '2011rs': {
-            'name': '2011 Regular Session',
+            'display_name': '2011 Regular Session',
             'internal_id': '1058',
             'type': 'primary',
+            '_scraped_name': 'Regular Session 2011',
         },
         '2012rs': {
-            'name': '2012 Regular Session',
+            'display_name': '2012 Regular Session',
             'internal_id': '1059',
             'type': 'primary',
+            '_scraped_name': 'Regular Session 2012',
         },
-    }
+    },
+    '_ignored_scraped_sessions': [
+        'First Special Session 2010',
+        'First Special Session 2009',
+        'Regular Session 2010',
+        'Regular Session 2009',
+        'First Special Session 2008',
+        'Regular Session 2008',
+        'First Special Session 2007',
+        'Regular Session 2007',
+        'Organizational Session 2007',
+        'Regular Session 2006',
+        'First Special Session 2005',
+        'Regular Session 2005',
+        'First Special Session 2004',
+        'Regular Session 2004',
+        'Second Special Session 2003',
+        'First Special Session 2003',
+        'Regular Session 2003',
+        'Organizational Session 2003',
+        'Regular Session 2002',
+        'Fourth Special Session 2001',
+        'Third Special Session 2001',
+        'Second Special Session 2001',
+        'First Special Session 2001',
+        'Regular Session 2001',
+        'Regular Session 2000',
+        'Organizational Session 2011']
 }
 
 def session_list():
-    import re
     from billy.scrape.utils import url_xpath
-    pblocks = url_xpath('http://www.legislature.state.al.us/',
-        '//div/p/text()' )
-    sessions = []
-    for pblock in pblocks:
-        pb_sessions = re.findall('2[0-9][0-9][0-9]\ Regular\ Session', pblock)
-        for session in pb_sessions:
-            sessions.append( session )
-    return sessions
+    sessions = url_xpath('http://alisondb.legislature.state.al.us/acas/ACTIONSessionResultsMac.asp',
+        '//option/text()' )
+    return [s.strip() for s in sessions]
