7168426bf1cfa6b9456632c688604f797b00da25
James Turk <james.p.turk@gmail.com>
2012-01-06 17:21:10 -0500

    WY: allow bills w/o digest

--- openstates/ct/__init__.py
+++ openstates/ct/__init__.py
@@ -10,13 +10,17 @@ metadata = dict(
     lower_chamber_term=2,
     terms=[
         {'name': '2011-2012',
-         'sessions': ['2011'],
+         'sessions': ['2011', '2012'],
          'start_year': 2011, 'end_year': 2012},
     ],
     session_details={
         '2011': {
             'display_name': '2011 Regular Session',
             '_scraped_name': '2011',
+        },
+        '2012': {
+            'display_name': '2012 Regular Session',
+            '_scraped_name': '2012',
         }
     },
     feature_flags=['subjects'],
