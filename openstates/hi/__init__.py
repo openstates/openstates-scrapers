f3ef99bcc1995133f29664c2e255e76585ac444d
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-01-11 14:31:03 -0500

    HI: Cleaning up the __init__ a skitch

--- openstates/hi/__init__.py
+++ openstates/hi/__init__.py
@@ -9,20 +9,27 @@ metadata = dict(
     upper_chamber_term=4,
     lower_chamber_term=2,
     terms = [
-        {'name': '2011-2012',
-         'sessions': ['2011 Regular Session',
-                     ],
-         'start_year': 2011, 'end_year': 2012},
+        {
+            'name': '2011-2012',
+            'sessions': [
+                '2011 Regular Session',
+            ],
+            'start_year' : 2011,
+            'end_year'   : 2012
+        },
      ],
     session_details={
-        '2011 Regular Session': {
+        '2011 Regular Session' : {
             'display_name'  : '2011 Regular Session',
             '_scraped_name' : '2011'
         },
     },
     feature_flags=['subjects'],
-    _ignored_scraped_sessions = ['2010', '2009', '2008', '2007', '2006',
-        '2005', '2004', '2003', '2002', '2001', '2000', '1999']    
+    _ignored_scraped_sessions = [
+        '2010', '2009', '2008', '2007', '2006',
+        '2005', '2004', '2003', '2002', '2001',
+        '2000', '1999'
+    ]
 )
 
 def session_list():
