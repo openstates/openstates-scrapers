026d61fc904bab2f7afaffb0a9add624b808af15
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-01-13 13:36:59 -0500

    HI: Small fix to the URL

--- openstates/hi/bills.py
+++ openstates/hi/bills.py
@@ -6,12 +6,12 @@ from urlparse import urlparse
 from billy.scrape.bills import BillScraper, Bill
 from billy.scrape.votes import Vote
 
-HI_URL_BASE = "http://capitol.hawaii.gov/"
+HI_URL_BASE = "http://capitol.hawaii.gov"
 
 
 def create_bill_report_url( chamber, year ):
     cname = { "upper" : "s", "lower" : "h" }[chamber]
-    return HI_URL_BASE + "report.aspx?type=intro" + cname + "b&year=" + year
+    return HI_URL_BASE + "/report.aspx?type=intro" + cname + "b&year=" + year
 
 class HIBillScraper(BillScraper):
     
