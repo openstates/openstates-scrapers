0470c06af3faf2b54babe6d464e5d3cfe915a09e
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-01-13 11:59:23 -0500

    HI: Adding in some bill scraping voodoo

--- openstates/hi/bills.py
+++ openstates/hi/bills.py
@@ -17,7 +17,14 @@ class HIBillScraper(BillScraper):
     
     state = 'hi'
 
+    def scrape_report_page(self, url):
+        with self.urlopen(url) as list_html: 
+            list_page = lxml.html.fromstring(list_html)
+            for bill in list_page.xpath("//a[@class='report']"):
+                print bill.attrib['href']
+
     def scrape(self, chamber, session):
         session_urlslug = \
             self.metadata['session_details'][session]['_scraped_name']
-        print create_bill_report_url( chamber, session_urlslug )
+        self.scrape_report_page( \
+            create_bill_report_url( chamber, session_urlslug ) )
