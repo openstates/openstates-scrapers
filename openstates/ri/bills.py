011665f1bc7d2a3e2a2ea35f162f9f1a366af012
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-01-27 13:33:40 -0500

    RI tweeks

--- openstates/ri/bills.py
+++ openstates/ri/bills.py
@@ -1,18 +1,78 @@
 import datetime as dt
 import lxml.html
+import urllib
 
 from billy.scrape.bills import BillScraper, Bill
 from billy.scrape.utils import url_xpath
 
-def get_postable_subjects():
-    return url_xpath( "http://status.rilin.state.ri.us/",
-        "//select[@id='rilinContent_cbCategory']" )[0].xpath("./*")
+subjects      = None
+bill_subjects = None
 
-subjects = { o.text : o.attrib['value'] for o in get_postable_subjects() }
-subjects.pop(None)
+def get_postable_subjects():
+    global subjects
+    if subjects == None:
+        subs = url_xpath( "http://status.rilin.state.ri.us/",
+            "//select[@id='rilinContent_cbCategory']" )[0].xpath("./*")
+        subjects = { o.text : o.attrib['value'] for o in subs }
+        subjects.pop(None)
+    return subjects
+
+def get_default_headers( page ):
+    headers = {}
+    for el in url_xpath( page, "//*[@name]" ):
+        name = el.attrib['name']
+        value = ""
+        try:
+            value = el.attrib['value']
+        except KeyError:
+            value = el.text
+        headers[name] = value or ""
+    return headers
+
+SEARCH_URL = "http://status.rilin.state.ri.us/"
 
 class RIBillScraper(BillScraper):
     state = 'ri'
 
+    def parse_results_page( self, page ):
+        blocks  = []
+        current = []
+
+        p = lxml.html.fromstring(page)
+        nodes = p.xpath("//span[@id='lblBills']/*")
+        for node in nodes:
+            if node.tag == "br":
+                if len(current) > 0:
+                    current = []
+                    blocks.append(current)
+            else:
+                current.append(node)
+        return blocks
+
+    def get_subject_bill_dict(self):
+        global bill_subjects
+        if bill_subjects == None:
+            subjects = get_postable_subjects()
+            for subject in subjects:
+                default_headers = get_default_headers( SEARCH_URL )
+
+                default_headers['ctl00$rilinContent$cbCategory'] = \
+                    subjects[subject]
+
+                default_headers['ctl00$rilinContent$cbYear'] = \
+                    "2012" # XXX: Fixme
+
+                #for header in default_headers:
+                #    print header, default_headers[header]
+
+                headers = urllib.urlencode( default_headers )
+                blocks = self.parse_results_page(self.urlopen( SEARCH_URL,
+                    method="POST", body=headers))
+                for block in blocks:
+                    for div in block:
+                        print div.text_content()
+
+        return bill_subjects
+
     def scrape(self, chamber, session):
-        print subjects
+        print self.get_subject_bill_dict()
