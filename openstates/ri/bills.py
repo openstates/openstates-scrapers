35924915110bb31c336223880e452a75d2c4ae20
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-01-27 11:45:21 -0500

    Grabbing subjects to post with

--- openstates/ri/bills.py
+++ openstates/ri/bills.py
@@ -2,9 +2,17 @@ import datetime as dt
 import lxml.html
 
 from billy.scrape.bills import BillScraper, Bill
+from billy.scrape.utils import url_xpath
+
+def get_postable_subjects():
+    return url_xpath( "http://status.rilin.state.ri.us/",
+        "//select[@id='rilinContent_cbCategory']" )[0].xpath("./*")
+
+subjects = { o.text : o.attrib['value'] for o in get_postable_subjects() }
+subjects.pop(None)
 
 class RIBillScraper(BillScraper):
     state = 'ri'
 
     def scrape(self, chamber, session):
-        print chamber, session
+        print subjects
