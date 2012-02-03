35e20b3f51a21c9ca9ed12c802988eca6496c733
Dan Schneiderman <schneiderman.daniel@gmail.com>
2012-02-03 14:44:43 -0500

    NE: Broke up main function into a two functions to seperate scrapping the bill page.

--- openstates/ne/bills.py
+++ openstates/ne/bills.py
@@ -16,95 +16,102 @@ class NEBillScraper(BillScraper):
             for docs in page.xpath('/html/body/div[@id="wrapper"]/div[@id="content"]/div[@id="content_text"]/div[@class="cal_content_full"]/table[@id="bill_results"]/tr/td[1]/a'):
                 bill_link = docs.attrib['href']
                 bill_link = 'http://nebraskalegislature.gov/' + bill_link
-                with self.urlopen(bill_link) as bill_page:
-                    bill_page = lxml.html.fromstring(bill_page)
+                
+                #scrapes info from bill page
+                self.bill_info(bill_link, session, chamber, main_url)
 
-                    #basic info
-                    long_title = bill_page.xpath('/html/body/div[@id="wrapper"]/div[@id="content"]/div[@id="content_text"]/h2')[0].text.split()
-                    bill_id = long_title[0]
-                    title = ''
-                    for x in range(2, len(long_title)):
-                        title += long_title[x] + ' '
-                    title = title[0:-1]
-                    #bill = Bill(session, chamber, bill_id, title)
+    #Scrapes info from the bill page
+    def bill_info(self, bill_link, session, chamber, main_url):
+                
+        with self.urlopen(bill_link) as bill_page:
+            bill_page = lxml.html.fromstring(bill_page)
 
-                    #bill_type
-                    if 'LR' in bill_id:
-                        bill_type = 'resolution'
+            #basic info
+            long_title = bill_page.xpath('/html/body/div[@id="wrapper"]/div[@id="content"]/div[@id="content_text"]/h2')[0].text.split()
+            bill_id = long_title[0]
+            title = ''
+            for x in range(2, len(long_title)):
+                title += long_title[x] + ' '
+            title = title[0:-1]
+            #bill = Bill(session, chamber, bill_id, title)
+
+            #bill_type
+            if 'LR' in bill_id:
+                bill_type = 'resolution'
+            else:
+                bill_type = 'bill'
+
+            bill = Bill(session, chamber, bill_id, title, type = bill_type)
+                    
+            #sources
+            bill.add_source(main_url)
+            bill.add_source(bill_link)
+                    
+            #Sponsor
+            introduced_by = bill_page.xpath('/html/body/div[@id="wrapper"]/div[@id="content"]/div[@id="content_text"]/div[2]/table/tr[2]/td[1]/a[1]')[0].text
+            bill.add_sponsor('primary', introduced_by)
+
+            #actions
+            for actions in bill_page.xpath('/html/body/div[@id="wrapper"]/div[@id="content"]/div[@id="content_text"]/div[3]/table/tr[1]/td[1]/table/tr'):
+                date = actions[0].text
+                if 'Date' not in date:
+                    date = datetime.strptime(date, '%b %d, %Y')
+                    action = actions[1].text
+
+                    if '-' in action:
+                        vote_info = action.split()[-1].split('-')
+                        yes_count = int(vote_info[0])
+                        no_count = int(vote_info[1])
+                        abstention_count = int(vote_info[2])
+                        if yes_count > no_count:
+                            passed = True
+                        else:
+                            passed = False
+                        vote = Vote(chamber, date, action, passed, yes_count, no_count, abstention_count)
+                        vote.add_source(bill_link)
+                        bill.add_vote(vote)
+
+                    if 'Governor' in action:
+                        actor = 'Governor'
+                    elif 'Speaker' in action:
+                        actor = 'Speaker'
                     else:
-                        bill_type = 'bill'
+                        actor = chamber
 
-                    bill = Bill(session, chamber, bill_id, title, type = bill_type)
+                    action_type = self.action_types(action)
+                    bill.add_action(actor, action, date, action_type)
                     
-                    #sources
-                    bill.add_source(main_url)
-                    bill.add_source(bill_link)
-                    
-                    #Sponsor
-                    introduced_by = bill_page.xpath('/html/body/div[@id="wrapper"]/div[@id="content"]/div[@id="content_text"]/div[2]/table/tr[2]/td[1]/a[1]')[0].text
-                    bill.add_sponsor('primary', introduced_by)
-
-                    #actions
-                    for actions in bill_page.xpath('/html/body/div[@id="wrapper"]/div[@id="content"]/div[@id="content_text"]/div[3]/table/tr[1]/td[1]/table/tr'):
-                        date = actions[0].text
-                        if 'Date' not in date:
-                            date = datetime.strptime(date, '%b %d, %Y')
-                            action = actions[1].text
-
-                            if '-' in action:
-                                vote_info = action.split()[-1].split('-')
-                                yes_count = int(vote_info[0])
-                                no_count = int(vote_info[1])
-                                abstention_count = int(vote_info[2])
-                                if yes_count > no_count:
-                                    passed = True
-                                else:
-                                    passed = False
-                                vote = Vote(chamber, date, action, passed, yes_count, no_count, abstention_count)
-                                vote.add_source(bill_link)
-                                bill.add_vote(vote)
-
-                            if 'Governor' in action:
-                                actor = 'Governor'
-                            elif 'Speaker' in action:
-                                actor = 'Speaker'
-                            else:
-                                actor = chamber
-
-                            action_type = self.action_types(action)
-                            bill.add_action(actor, action, date, action_type)
-                    
-                    #versions
-                    for versions in bill_page.xpath('/html/body/div[@id="wrapper"]/div[@id="content"]/div[@id="content_text"]/div[2]/table/tr[2]/td[2]/a'):
-                        version_url = versions.attrib['href']
-                        version_url = 'http://nebraskalegislature.gov/' + version_url[3:len(version_url)]
-                        version_name = versions.text
-                        bill.add_version(version_name, version_url)
+            #versions
+            for versions in bill_page.xpath('/html/body/div[@id="wrapper"]/div[@id="content"]/div[@id="content_text"]/div[2]/table/tr[2]/td[2]/a'):
+                version_url = versions.attrib['href']
+                version_url = 'http://nebraskalegislature.gov/' + version_url[3:len(version_url)]
+                version_name = versions.text
+                bill.add_version(version_name, version_url)
 
                         
-                    #documents
-                    #additional_info
-                    for additional_info in bill_page.xpath('/html/body/div[@id="wrapper"]/div[@id="content"]/div[@id="content_text"]/div[2]/table/tr[2]/td/a'):
-                        document_name = additional_info.text
-                        document_url = additional_info.attrib['href']
-                        document_url = 'http://nebraskalegislature.gov/' + document_url[3:len(document_url)]
-                        if '.pdf' in document_url:
-                            bill.add_document(document_name, document_url)
+            #documents
+            #additional_info
+            for additional_info in bill_page.xpath('/html/body/div[@id="wrapper"]/div[@id="content"]/div[@id="content_text"]/div[2]/table/tr[2]/td/a'):
+                document_name = additional_info.text
+                document_url = additional_info.attrib['href']
+                document_url = 'http://nebraskalegislature.gov/' + document_url[3:len(document_url)]
+                if '.pdf' in document_url:
+                    bill.add_document(document_name, document_url)
 
-                    #amendments
-                    for admendments in bill_page.xpath('/html/body/div[@id="wrapper"]/div[@id="content"]/div[@id="content_text"]/div[3]/table/tr[1]/td[2]/table/tr/td/a'):
-                        admendment_name = admendments.text
-                        admendment_url = admendments.attrib['href']
-                        admendment_url = 'http://nebraskalegislature.gov/' + admendment_url[3:len(admendment_url)]
-                        bill.add_document(admendment_name, admendment_url)
+            #amendments
+            for admendments in bill_page.xpath('/html/body/div[@id="wrapper"]/div[@id="content"]/div[@id="content_text"]/div[3]/table/tr[1]/td[2]/table/tr/td/a'):
+                admendment_name = admendments.text
+                admendment_url = admendments.attrib['href']
+                admendment_url = 'http://nebraskalegislature.gov/' + admendment_url[3:len(admendment_url)]
+                bill.add_document(admendment_name, admendment_url)
 
-                    #related transcripts
-                    for transcripts in bill_page.xpath('/html/body/div[@id="wrapper"]/div[@id="content"]/div[@id="content_text"]/div[3]/table/tr[2]/td[2]/a'):
-                        transcript_name = transcripts.text
-                        transcript_url = transcripts.attrib['href']
-                        bill.add_document(transcript_name, transcript_url)
+            #related transcripts
+            for transcripts in bill_page.xpath('/html/body/div[@id="wrapper"]/div[@id="content"]/div[@id="content_text"]/div[3]/table/tr[2]/td[2]/a'):
+                transcript_name = transcripts.text
+                transcript_url = transcripts.attrib['href']
+                bill.add_document(transcript_name, transcript_url)
 
-                    self.save_bill(bill)
+            self.save_bill(bill)
 
 
     #Setting action types
