19aea6c13d34910cc151bfa9ca38ec9bdc9a44b2
Dan Schneiderman <schneiderman.daniel@gmail.com>
2012-02-03 14:58:09 -0500

    NE: Cleaned up code.

--- openstates/ne/bills.py
+++ openstates/ne/bills.py
@@ -13,7 +13,7 @@ class NEBillScraper(BillScraper):
         with self.urlopen(main_url) as page:
             page = lxml.html.fromstring(page)
 
-            for docs in page.xpath('/html/body/div[@id="wrapper"]/div[@id="content"]/div[@id="content_text"]/div[@class="cal_content_full"]/table[@id="bill_results"]/tr/td[1]/a'):
+            for docs in page.xpath('//div[@class="cal_content_full"]/table[@id="bill_results"]/tr/td[1]/a'):
                 bill_link = docs.attrib['href']
                 bill_link = 'http://nebraskalegislature.gov/' + bill_link
                 
@@ -27,19 +27,15 @@ class NEBillScraper(BillScraper):
             bill_page = lxml.html.fromstring(bill_page)
 
             #basic info
-            long_title = bill_page.xpath('/html/body/div[@id="wrapper"]/div[@id="content"]/div[@id="content_text"]/h2')[0].text.split()
+            long_title = bill_page.xpath('//div[@id="content_text"]/h2')[0].text.split()
             bill_id = long_title[0]
             title = ''
             for x in range(2, len(long_title)):
                 title += long_title[x] + ' '
             title = title[0:-1]
-            #bill = Bill(session, chamber, bill_id, title)
 
             #bill_type
-            if 'LR' in bill_id:
-                bill_type = 'resolution'
-            else:
-                bill_type = 'bill'
+            bill_type = 'resolution' if 'LR' in bill_id else 'bill'            
 
             bill = Bill(session, chamber, bill_id, title, type = bill_type)
                     
@@ -48,11 +44,11 @@ class NEBillScraper(BillScraper):
             bill.add_source(bill_link)
                     
             #Sponsor
-            introduced_by = bill_page.xpath('/html/body/div[@id="wrapper"]/div[@id="content"]/div[@id="content_text"]/div[2]/table/tr[2]/td[1]/a[1]')[0].text
+            introduced_by = bill_page.xpath('//div[@id="content_text"]/div[2]/table/tr[2]/td[1]/a[1]')[0].text
             bill.add_sponsor('primary', introduced_by)
 
             #actions
-            for actions in bill_page.xpath('/html/body/div[@id="wrapper"]/div[@id="content"]/div[@id="content_text"]/div[3]/table/tr[1]/td[1]/table/tr'):
+            for actions in bill_page.xpath('//div[@id="content_text"]/div[3]/table/tr[1]/td[1]/table/tr'):
                 date = actions[0].text
                 if 'Date' not in date:
                     date = datetime.strptime(date, '%b %d, %Y')
@@ -82,7 +78,7 @@ class NEBillScraper(BillScraper):
                     bill.add_action(actor, action, date, action_type)
                     
             #versions
-            for versions in bill_page.xpath('/html/body/div[@id="wrapper"]/div[@id="content"]/div[@id="content_text"]/div[2]/table/tr[2]/td[2]/a'):
+            for versions in bill_page.xpath('//div[@id="content_text"]/div[2]/table/tr[2]/td[2]/a'):
                 version_url = versions.attrib['href']
                 version_url = 'http://nebraskalegislature.gov/' + version_url[3:len(version_url)]
                 version_name = versions.text
@@ -91,7 +87,7 @@ class NEBillScraper(BillScraper):
                         
             #documents
             #additional_info
-            for additional_info in bill_page.xpath('/html/body/div[@id="wrapper"]/div[@id="content"]/div[@id="content_text"]/div[2]/table/tr[2]/td/a'):
+            for additional_info in bill_page.xpath('//div[@id="content_text"]/div[2]/table/tr[2]/td/a'):
                 document_name = additional_info.text
                 document_url = additional_info.attrib['href']
                 document_url = 'http://nebraskalegislature.gov/' + document_url[3:len(document_url)]
@@ -99,14 +95,14 @@ class NEBillScraper(BillScraper):
                     bill.add_document(document_name, document_url)
 
             #amendments
-            for admendments in bill_page.xpath('/html/body/div[@id="wrapper"]/div[@id="content"]/div[@id="content_text"]/div[3]/table/tr[1]/td[2]/table/tr/td/a'):
+            for admendments in bill_page.xpath('//div[@id="content_text"]/div[3]/table/tr[1]/td[2]/table/tr/td/a'):
                 admendment_name = admendments.text
                 admendment_url = admendments.attrib['href']
                 admendment_url = 'http://nebraskalegislature.gov/' + admendment_url[3:len(admendment_url)]
                 bill.add_document(admendment_name, admendment_url)
 
             #related transcripts
-            for transcripts in bill_page.xpath('/html/body/div[@id="wrapper"]/div[@id="content"]/div[@id="content_text"]/div[3]/table/tr[2]/td[2]/a'):
+            for transcripts in bill_page.xpath('//div[@id="content_text"]/div[3]/table/tr[2]/td[2]/a'):
                 transcript_name = transcripts.text
                 transcript_url = transcripts.attrib['href']
                 bill.add_document(transcript_name, transcript_url)
