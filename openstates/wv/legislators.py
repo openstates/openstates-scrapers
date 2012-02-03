698ddd2d60516fbf7757943dbdc71725462effc1
James Turk <james.p.turk@gmail.com>
2012-02-03 17:04:47 -0500

    WV: skip VACANT

--- openstates/wv/legislators.py
+++ openstates/wv/legislators.py
@@ -31,7 +31,7 @@ class WVLegislatorScraper(LegislatorScraper):
             leg_url = urlescape(link.attrib['href'])
 
             if name in ['Members', 'Senate Members', 'House Members',
-                        'Vacancy']:
+                        'Vacancy', 'VACANT']:
                 continue
 
             self.scrape_legislator(chamber, term, name, leg_url)
