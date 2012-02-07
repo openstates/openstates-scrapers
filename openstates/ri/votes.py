c78615a95271920bc082d5354b62c1ae2516f6d4
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-02-07 12:08:12 -0500

    RI: Tweeking the vote scraper

--- openstates/ri/votes.py
+++ openstates/ri/votes.py
@@ -1,6 +1,8 @@
 from billy.scrape.votes import VoteScraper, Vote
 from billy.scrape.utils import url_xpath
 
+import datetime as dt
+
 import urllib
 import lxml
 import re
@@ -17,7 +19,7 @@ class RIVoteScraper(VoteScraper):
         return ret
 
     def get_votes(self, url):
-        ret = []
+        ret = {}
         with self.urlopen(url) as html:
             p = lxml.html.fromstring(html)
             tables = \
@@ -29,7 +31,35 @@ class RIVoteScraper(VoteScraper):
             inf = metainf.xpath("./tr/td/pre")[0]
             headers = [ br.tail for br in inf.xpath("./*") ]
 
-            bill_s_n_no = r"(?P<year>[0-9]{4})(?P<chamber>[SH])\s*(?P<bill>[0-9]{4})"
+            dateinf = metainf.xpath("./tr/td")[3]
+            date = dateinf.text
+            time = dateinf.xpath("./*")[0].tail
+
+            vote_digest = metainf.xpath("./tr/td[@colspan='3']")
+            digest = vote_digest[2].text_content()
+            dig = []
+            for d in digest.split("\n"):
+                lis = d.strip().split("-")
+                for l in lis:
+                    if l != None and l != "":
+                        dig.append(l.strip())
+            digest = dig
+
+            il = iter( digest )
+            d = dict(zip(il, il))
+            vote_count = d
+
+            time_string = "%s %s" % ( time, date )
+
+            fmt_string = "%I:%M:%S %p %A, %B %d, %Y"
+            # 4:31:14 PM TUESDAY, JANUARY 17, 2012
+            date_time = dt.datetime.strptime( time_string, fmt_string )
+
+            bill_s_n_no = r"(?P<year>[0-9]{2,4})(?P<chamber>[SH])\s*(?P<bill>[0-9]{4})"
+            # This is technically wrong, but it's close enough to be fine.
+            # something like "123S  3023" is technically valid, even though it's
+            # silly
+
             bill_metainf = None
             for h in headers:
                 inf = re.search( bill_s_n_no, h )
@@ -52,16 +82,24 @@ class RIVoteScraper(VoteScraper):
                             "vote" : vote
                         })
                 if len(votes) > 0:
-                    ret.append( votes )
+                    bid = bill_metainf['bill']
+                    ret[bid] = {
+                        "votes" : votes,
+                        "meta"  : bill_metainf,
+                        "time"  : date_time,
+                        "count" : vote_count
+                    }
         return ret
 
     def parse_vote_page(self, page, context_url):
+        ret = []
         p = lxml.html.fromstring(page)
         votes = p.xpath( "//center/div[@class='vote']" )
         for vote in votes:
             votes = self.get_votes( context_url + "/" +
                             vote.xpath("./a")[0].attrib["href"] )
-            print votes
+            ret.append(votes)
+        return ret
 
     def post_to(self, url, vote):
         headers = {
@@ -79,4 +117,9 @@ class RIVoteScraper(VoteScraper):
         action = "%s/%s" % ( url, "votes.asp" )
         dates = self.get_dates( url )
         for date in dates:
-            self.parse_vote_page( self.post_to( action, date ), url )
+            votes = self.parse_vote_page( self.post_to( action, date ), url )
+            for vote_dict in votes:
+                for vote in vote_dict:
+                    print vote
+                    #v = Vote( chamber, vote['time'] , "", # XXX: FIXME
+                    #         True, 0, 0, 0)
