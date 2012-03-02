c182b9c38a960c1d4f00dfc554b5c5f2452d0a53
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-03-02 16:08:33 -0500

    MO: Small tweeks & new session

--- openstates/mo/__init__.py
+++ openstates/mo/__init__.py
@@ -12,10 +12,12 @@ metadata = dict(
     upper_chamber_term=4,
     terms=[
         {'name': '2011-2012',
-         'sessions': ['2011'],
+         'sessions': ['2011', '2012'],
          'start_year': 2011, 'end_year': 2012},
         ],
     session_details={
+        '2012': {'start_date': datetime.date(2012,1,26), 'type': 'primary',
+                 'display_name': '2012 Regular Session'},
         '2011': {'start_date': datetime.date(2011,1,26), 'type': 'primary',
                  'display_name': '2011 Regular Session'},
     },
@@ -44,5 +46,5 @@ metadata = dict(
 def session_list():
     from billy.scrape.utils import url_xpath
     return url_xpath('http://www.senate.mo.gov/pastsessions.htm',
-        "//div[@id='list']/li/a/text()") 
+        "//div[@id='list']/li/a/text()")
 
