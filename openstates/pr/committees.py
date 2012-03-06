6229f41a1505f20f574bb339622ba86404a27dfa
annerajb-server <annerajb-server@annerajbserver-server.(none)>
2012-03-05 20:23:23 -0400

    tab to space

--- openstates/pr/committees.py
+++ openstates/pr/committees.py
@@ -50,18 +50,18 @@ class PRCommitteeScraper(CommitteeScraper):
                         line_text = line.text.replace(u'–','-')
                         name_split = line_text.split(u'-',1)
                         title = 'member'
-#			print name_split
+#           print name_split
                         if len(name_split) >= 2:
-			    name_split[1] = name_split[1].strip()
-			    
+                name_split[1] = name_split[1].strip()
+                
                             if name_split[1] == 'Presidenta' or name_split[1] == 'Presidente':
                                 title = 'chairman'
                             elif name_split[1] == 'Vicepresidente' or name_split[1] == 'Vicepresidenta':
                                 title = 'vicechairman'
                             elif name_split[1] == 'Secretaria' or name_split[1] == 'Secretario':
                                 title = 'secretary'
-#			if title != 'member':
-#			    print name_split[0]
+#           if title != 'member':
+#               print name_split[0]
                         if name_split[0] != 'VACANTE':
                             comm.add_member(name_split[0].replace('Hon.',''),title)
             self.save_committee(comm)
