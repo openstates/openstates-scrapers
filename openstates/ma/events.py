0a5761a47e9591bc01b99c200350fa05ef4f1431
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-04-10 15:08:50 -0400

    MA: Events: Big changes to get events up to standard.

--- openstates/ma/events.py
+++ openstates/ma/events.py
@@ -1,5 +1,5 @@
 import re
-import datetime
+import datetime as dt
 
 from billy.scrape import NoDataForPeriod
 from billy.scrape.events import Event, EventScraper
@@ -7,72 +7,54 @@ from billy.scrape.events import Event, EventScraper
 import pytz
 import lxml.html
 
+urls = "http://www.malegislature.gov/Events/%s"
+pages = {
+    "lower" : [ urls % "SenateSessions" ],
+    "upper" : [ urls % "HouseSessions" ],
+    "other" : [ urls % "JointSessions",
+                urls % "Hearings", urls % "SpecialEvents" ]
+}
 
 class MAEventScraper(EventScraper):
     state = 'ma'
 
     _tz = pytz.timezone('US/Eastern')
 
+    def lxmlize(self, url):
+        with self.urlopen(url) as page:
+            page = lxml.html.fromstring(page)
+        page.make_links_absolute(url)
+        return page
+
+    def parse_row(self, row):
+        dates = row.xpath("./td[@class='dateCell']")
+        for date in dates:
+            # alright, so we *may* not get a date, in which case the date
+            # is the same as the last event.
+            cal_date = date.xpath("./span[@class='calendarMonth']")[0]
+            cal_day = date.xpath("./span[@class='calendarDay']")[0]
+            self.last_month = cal_date.text_content()
+            self.last_day = cal_day.text_content()
+        time = row.xpath("./td[@class='timeCell']")
+        if not time:
+            return  # Nada.
+        time = time[0]
+        time = time.text.strip()
+        dt_string = "%s %s %s %s" % (
+            self.last_month,
+            self.last_day,
+            self.year,
+            time
+        )
+        fmt = "%b %d %Y %I:%M %p"
+        when = dt.datetime.strptime(dt_string, fmt)
+        print when
+
     def scrape(self, chamber, session):
-        if session != '187th':
-            raise NoDataForPeriod(session)
-
-        #Currently this code only supports 2011
-        year = '2011'
-        
-        #Search by event type. [post attribute, name]
-        event_type_list = [('Joint','joint:session'),
-                           ('House','house:session'),
-                           ('Senate','senate:session'),
-                           ('Hearings','committee:hearing'),
-                           ('SpecialEvents','special'),
-                           ('Redistricting','redistricting')]
-        
-        for event_type in event_type_list:
-            url = "http://www.malegislature.gov/Events/Search"
-            input_string = "Input.%s=true&" % event_type[0]
-            post_data = (input_string +
-                         "Input.StartDate=1%2F1%2F2011&"
-                         "Input.EndDate=12%2F31%2F2011")
-
-            with self.urlopen(url, 'POST', post_data) as page:
-                page = lxml.html.fromstring(page)
-                page.make_links_absolute(url)
-                
-                path = "//div[@id='eventsTable']/table//tr"
-                for row in page.xpath(path)[2:]:
-                    cells = row.xpath('./td')
-                    
-                    #td class='dateCell'
-                    if len(cells) == 7:
-                        month = cells[0].xpath('string(span[2])')
-                        day = cells[0].xpath('string(span[3])')
-                        del cells[0]
-                        
-                    #td class='timeCell'
-                    time = cells[0].text.strip()
-                    when = datetime.datetime.strptime(
-                            month+' '+day+' '+time+' '+year,
-                                                      "%b %d %I:%M %p %Y")
-                    when = self._tz.localize(when)
-
-                    #td class='eventCell'
-                    description = cells[1].xpath('string(./a)').strip()
-                    link = cells[1].xpath(
-                            'string(./a/attribute::href)').strip()
-                    
-                    #td class='locationCell'
-                    where_name = cells[3].xpath('string(./a)').strip()
-                    loc = cells[3].xpath(
-                            'string(./a/attribute::href)').strip()
-                    try:
-                        where_address = re.search('daddr=(.*)', loc).group(1)
-                    except:
-                        where_address = ""
-                    where = where_name + ", " + where_address
-                    
-                    event = Event(session, when, event_type[1],
-                                  description, location=where)
-                    event.add_source(url)
-                    event['link'] = link
-                    self.save_event(event)
+        scrape_list = pages[chamber]
+        self.year = dt.datetime.now().year
+        for site in scrape_list:
+            page = self.lxmlize(site)
+            rows = page.xpath("//tbody[not(contains(@id, 'noDates'))]/tr[contains(@class, 'dataRow')]")
+            for row in rows:
+                self.parse_row(row)
