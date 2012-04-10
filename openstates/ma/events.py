eeda4991f0afa0250244b690f7b29e0aba4003d9
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-04-10 17:24:37 -0400

    MA: Events: Adding in s'more stuff

--- openstates/ma/events.py
+++ openstates/ma/events.py
@@ -26,7 +26,40 @@ class MAEventScraper(EventScraper):
         page.make_links_absolute(url)
         return page
 
-    def parse_row(self, row):
+    def add_agenda(self, event, url):
+        event.add_source(url)
+        page = self.lxmlize(url)
+        description = page.xpath("//div[@id='eventData']/p")
+        if len(description) > 0:
+            description = description[0].text_content().strip()
+            if description != "":
+                event['description'] = description
+
+        trs = page.xpath("//tbody/tr")
+        for tr in trs:
+            # Alright. Let's snag some stuff.
+            cells = {
+                "num" : "agendaItemNum",
+                "bill_id": "agendaBillNum",
+                "title": "agendaBillTitle",
+                "spons": "agendaBillSponsor"
+            }
+            metainf = {}
+            for cell in cells:
+                metainf[cell] = tr.xpath(".//td[@class='" + cells[cell] + "']")
+            if metainf['bill_id'] == []:
+                return
+            kwargs = { "type" : "consideration" }
+            # Alright. We can assume we have at least the bill ID.
+            bill_id = metainf['bill_id'][0].text_content().strip()
+            if cells['title'] != []:
+                kwargs['description'] = metainf['title'][0].text_content(
+                    ).strip()
+            # XXX: Add sponsors.
+            event.add_related_bill(bill_id, **kwargs)
+
+
+    def parse_row(self, row, session, chamber):
         dates = row.xpath("./td[@class='dateCell']")
         for date in dates:
             # alright, so we *may* not get a date, in which case the date
@@ -48,7 +81,35 @@ class MAEventScraper(EventScraper):
         )
         fmt = "%b %d %Y %I:%M %p"
         when = dt.datetime.strptime(dt_string, fmt)
-        print when
+        cells = {
+            "event": "eventCell",
+            "status": "statusCell",
+            "location": "locationCell",
+            "transcript": "transcriptCell",
+            "video": "videoCell"
+        }
+        metainf = {}
+        for thing in cells:
+            mi = row.xpath("./td[@class='" + cells[thing] + "']")
+            if mi == []:
+                continue
+            metainf[thing] = mi[0]
+
+        if metainf['location'].xpath("./*") == []:
+            metainf['location'] = self.last_location
+        else:
+            self.last_location = metainf['location']
+
+        loc_url = metainf['location'].xpath(".//a")
+        loc_url = loc_url[0].attrib['href']
+        event = Event(session, when,
+            'committee:meeting', metainf['event'].text_content().strip(),
+            location=metainf['location'].text_content().strip(),
+            location_url=loc_url)
+        event.add_participant("host", metainf['event'].text_content().strip(),
+                              chamber=chamber)
+        self.add_agenda(event, metainf['event'].xpath(".//a")[0].attrib['href'])
+        return event
 
     def scrape(self, chamber, session):
         scrape_list = pages[chamber]
@@ -57,4 +118,7 @@ class MAEventScraper(EventScraper):
             page = self.lxmlize(site)
             rows = page.xpath("//tbody[not(contains(@id, 'noDates'))]/tr[contains(@class, 'dataRow')]")
             for row in rows:
-                self.parse_row(row)
+                event = self.parse_row(row, session, chamber)
+                if event:
+                    event.add_source(site)
+                    self.save_event(event)
