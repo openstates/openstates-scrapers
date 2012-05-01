189751cd63df0087d9421d541c1736d9eaf20650
James Turk <james.p.turk@gmail.com>
2012-05-01 16:04:28 -0400

    MS: extract_text

--- openstates/ms/__init__.py
+++ openstates/ms/__init__.py
@@ -1,3 +1,6 @@
+import lxml.html
+from billy.fulltext import oyster_text
+
 metadata = dict(
     name='Mississippi',
     abbreviation='ms',
@@ -90,8 +93,16 @@ def session_list():
     return url_xpath('http://billstatus.ls.state.ms.us/sessions.htm',
                      '//a/text()')
 
+@oyster_text
+def extract_text(oyster_doc, data):
+    doc = lxml.html.fromstring(data)
+    text = ' '.join(p.text_content() for p in
+                    doc.xpath('//h2/following-sibling::p'))
+    return text
+
 document_class = dict(
     AWS_PREFIX = 'documents/ms/',
     update_mins = 30*24*60,
+    extract_text = extract_text,
     onchanged = []
 )
