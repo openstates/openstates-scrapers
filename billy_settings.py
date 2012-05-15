d2699d39e488adbaf6697acfd01d86eb8add9a69
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-05-15 11:59:55 -0400

    billy_settings: I flipped this to test, commited it by mistake.

--- billy_settings.py
+++ billy_settings.py
@@ -10,7 +10,7 @@ MONGO_HOST = os.environ.get('OPENSTATES_MONGO_HOST', 'localhost')
 MONGO_PORT = os.environ.get('OPENSTATES_MONGO_PORT', 27017)
 MONGO_DATABASE = os.environ.get('OPENSTATES_MONGO_DATABASE', 'fiftystates')
 
-ENABLE_OYSTER = False
+ENABLE_OYSTER = True
 
 try:
     from billy_local import *
