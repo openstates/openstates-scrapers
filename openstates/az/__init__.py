70745821bcb8a43bfeae7d86bf481ece7dccce56
James Turk <james.p.turk@gmail.com>
2012-05-15 11:32:25 -0400

    elasticsearch push for 9 states

--- openstates/az/__init__.py
+++ openstates/az/__init__.py
@@ -544,5 +544,5 @@ document_class = dict(
     AWS_PREFIX = 'documents/az/',
     update_mins = None,
     extract_text = extract_text,
-    onchanged = []
+    onchanged = ['oyster.ext.elasticsearch.ElasticSearchPush']
 )
