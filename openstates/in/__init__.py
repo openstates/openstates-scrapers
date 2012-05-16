2dc93a5420121fa904b1d0eed497e1978ee73d6f
James Turk <james.p.turk@gmail.com>
2012-05-16 10:47:58 -0400

    more states with auto push into ElasticSearch

--- openstates/in/__init__.py
+++ openstates/in/__init__.py
@@ -59,5 +59,5 @@ document_class = dict(
     AWS_PREFIX = 'documents/in/',
     update_mins = None,
     extract_text = extract_text,
-    onchanged = []
+    onchanged = ['oyster.ext.elasticsearch.ElasticSearchPush']
 )
