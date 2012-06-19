9010ce8ca8513108108aead5b889eb822205a009
Thom Neale <twneale@gmail.com>
2012-06-19 14:41:30 -0400

    add request context processes; lets you refer to request in templates

--- site/openstates_site/settings.py
+++ site/openstates_site/settings.py
@@ -51,6 +51,10 @@ TEMPLATE_LOADERS = (
     'django.template.loaders.app_directories.Loader',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.request',
+)
+
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'locksmith.mongoauth.middleware.APIKeyMiddleware',
