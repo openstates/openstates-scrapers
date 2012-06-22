1dabc39376fb3437bca619a20bc560f3d9137536
James Turk <james.p.turk@gmail.com>
2012-06-22 10:44:55 -0400

    UT: fix for S4

--- openstates/ut/__init__.py
+++ openstates/ut/__init__.py
@@ -14,7 +14,7 @@ metadata = dict(
     upper_chamber_term=4,
     terms=[
         dict(name='2011-2012', sessions=['2011', '2011S1', '2011S2', '2011S3',
-                                         '2012'],
+                                         '2012', '2012S4'],
              start_year=2011, end_year=2012),
         ],
     session_details={
