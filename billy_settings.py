5cc06fabecaa66457eb4264eaf93fb4c20cd138a
James Turk <james.p.turk@gmail.com>
2012-07-17 11:23:15 -0400

    change default settings

--- billy_settings.py
+++ billy_settings.py
@@ -3,11 +3,9 @@ import os
 from os.path import abspath, dirname, join
 
 SCRAPER_PATHS=[os.path.join(os.getcwd(), 'openstates')]
-MONGO_HOST = os.environ.get('OPENSTATES_MONGO_HOST', 'localhost')
-MONGO_PORT = os.environ.get('OPENSTATES_MONGO_PORT', 27017)
-MONGO_DATABASE = os.environ.get('OPENSTATES_MONGO_DATABASE', 'fiftystates')
-
-ENABLE_OYSTER = True
+MONGO_HOST = 'localhost'
+MONGO_PORT = 27017
+MONGO_DATABASE = 'fiftystates'
 
 try:
     from billy_local import *
