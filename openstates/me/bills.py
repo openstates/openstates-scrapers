1cbc4ddce2a0d025eda75182488e51803218a003
Thom Neale <twneale@gmail.com>
2012-07-18 12:35:44 -0400

    ME: properly extract action text

--- openstates/me/bills.py
+++ openstates/me/bills.py
@@ -1,14 +1,45 @@
 import datetime
 import re
 from operator import methodcaller
-
-from .utils import chamber_name
-from billy.scrape.bills import BillScraper, Bill
-from billy.scrape.votes import VoteScraper, Vote
+import htmlentitydefs
 
 import lxml.html
 
+from billy.scrape.bills import BillScraper, Bill
+from billy.scrape.votes import Vote
+
+
 strip = methodcaller('strip')
+
+
+def unescape(text):
+    '''Removes HTML or XML character references and entities from a text string.
+
+    @param text The HTML (or XML) source text.
+    @return The plain text, as a Unicode string, if necessary.
+
+    Source: http://effbot.org/zone/re-sub.htm#unescape-html'''
+
+    def fixup(m):
+        text = m.group(0)
+        if text[:2] == "&#":
+            # character reference
+            try:
+                if text[:3] == "&#x":
+                    return unichr(int(text[3:-1], 16))
+                else:
+                    return unichr(int(text[2:-1]))
+            except ValueError:
+                pass
+        else:
+            # named entity
+            try:
+                text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
+            except KeyError:
+                pass
+        return text  # leave as is
+    return re.sub("&#?\w+;", fixup, text)
+
 
 def classify_action(action):
     # TODO: this likely needs to be retuned after more happens
@@ -18,6 +49,7 @@ def classify_action(action):
         return 'bill:passed'
     else:
         return 'other'
+
 
 class MEBillScraper(BillScraper):
     state = 'me'
@@ -231,7 +263,21 @@ class MEBillScraper(BillScraper):
                 elif chamber == 'House':
                     chamber = 'lower'
 
-                action = row.xpath("string(td[3])").strip()
+                # The action text contains html, and .text_content
+                # does't quite work here, hence these horrific hacks:
+                action = lxml.html.tostring(row.xpath('td')[2])
+
+                # Kill html.
+                action = re.sub(r'[<].+?[>]', ' ', action)
+
+                # Squash whitespace.
+                action = re.sub(r'\s+', ' ', action)
+
+                # Fix period spacing.
+                action = re.sub(r'\s+\.', '.', action)
+
+                action = unescape(action).strip()
+
                 if action == 'Unfinished Business' or not action:
                     continue
 
