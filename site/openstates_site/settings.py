a12479f8c1c4388051b7c1aadb55a769344abb66
James Turk <james.p.turk@gmail.com>
2012-07-20 14:15:38 -0400

    OH, ID, HI, AZ to staging

--- site/openstates_site/settings.py
+++ site/openstates_site/settings.py
@@ -128,7 +128,7 @@ ACTIVE_STATES = [u'co', u'de', u'ny', u'in', u'tn',
                  u'pr', u'ri', u'sc', u'sd', u'tx',
                  u'ut', u'vt', u'wa', u'wi', u'wy',
                  u'ok', u'pa']
-ACTIVE_STATES = 'ca il la md mn nh nj nc tx dc wi'.split()
+ACTIVE_STATES = 'ca il la md mn nh nj nc tx dc wi oh id hi az'.split()
 
 ENABLE_ELASTICSEARCH = True
 
