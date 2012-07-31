1afc1cfd0ed4844283e4852233116c5d50f71576
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-07-31 17:40:35 -0400

    Adding in the right middleware

--- site/openstates_site/settings.py
+++ site/openstates_site/settings.py
@@ -61,9 +61,9 @@ MIDDLEWARE_CLASSES = (
     'locksmith.mongoauth.middleware.APIKeyMiddleware',
     'billy.web.public.middleware.LimitStatesMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    #'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
