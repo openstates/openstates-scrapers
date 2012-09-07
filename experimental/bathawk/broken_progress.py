be0256e437a9f5313051e81e657585d77e74f1cb
Thom Neale <twneale@gmail.com>
2012-09-07 15:12:57 -0400

    bathawk: broken progress script can report on all states

--- experimental/bathawk/broken_progress.py
+++ experimental/bathawk/broken_progress.py
@@ -4,7 +4,7 @@ import operator
 from billy import db
 
 
-def main(sciptname, abbr, session=None):
+def main(abbr, session=None):
 
     spec = {
         'state': abbr,
@@ -19,24 +19,59 @@ def main(sciptname, abbr, session=None):
         spec['session'] = session
 
     bills = db.bills.find(spec)
-    print 'Found', bills.count(), 'offending bills'
+    yield bills.count()
     bills = list(bills)
-
+    yield bills
     if not session:
         grouper = operator.itemgetter('session')
         for session, _bills in itertools.groupby(bills, key=grouper):
-            print session, '-', len(list(_bills)), 'offendings bills'
+            yield session, len(list(_bills))
 
-    raw_input("Press enter to view the offending bills: ")
-
-    for bill in bills:
-        url = 'http://localhost:8000/{state}/bills/{session}/{bill_id}/'.format(**bill)
-        print bill['bill_id'], url
-        print bill['action_dates']
-        webbrowser.open(url)
-        import pdb;pdb.set_trace()
 
 
 if __name__ == '__main__':
     import sys
-    main(*sys.argv)
+
+    from clint.textui import puts, indent
+    from clint.textui.colored import green, yellow, red
+
+    if not sys.argv[1:]:
+        for meta in db.metadata.find():
+            abbr = meta['_id']
+            data = main(abbr)
+            bogus = data.next()
+            bills = data.next()
+            if not bogus:
+                puts(green('%s: ok!' % abbr))
+                continue
+            msg = '%s: Found %s bills with progress gaps...'
+            puts(msg % (green(abbr), red(str(bogus))))
+            with indent(2):
+                for session, total in data:
+                    vals = (yellow(session), red(total))
+                    puts('|-%s: %s' % vals)
+
+    else:
+        abbr = sys.argv[1]
+        data = main(abbr)
+        bogus = data.next()
+        bills = data.next()
+        if not bogus:
+            puts(green('%s: ok!' % abbr))
+            sys.exit(1)
+        msg = '%s: Found %s bills with progress gaps...'
+        puts(msg % (green(abbr), red(str(bogus))))
+        with indent(2):
+            for session, total in data:
+                vals = (yellow(session), red(total))
+                puts('|-%s: %s' % vals)
+
+        raw_input("Press enter to view the offending bills: ")
+
+        import pdb;pdb.set_trace()
+        for bill in bills:
+            url = 'http://localhost:8000/{state}/bills/{session}/{bill_id}/'.format(**bill)
+            print bill['bill_id'], url
+            print bill['action_dates']
+            webbrowser.open(url)
+            import pdb;pdb.set_trace()
