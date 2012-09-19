ddcc59a2595d1d3c6b194b14021f8dc4295b853e
Thom Neale <twneale@gmail.com>
2012-09-19 09:33:11 -0400

    NY: add assembly votes; fix action chamber

--- openstates/ny/bills.py
+++ openstates/ny/bills.py
@@ -1,5 +1,6 @@
 import re
 import datetime
+from itertools import islice
 
 from billy.scrape.bills import BillScraper, Bill
 from billy.scrape.votes import Vote
@@ -28,79 +29,79 @@ class NYBillScraper(BillScraper):
                        "?search=otype:bill&searchType=&format=xml"
                        "&pageIdx=%d" % index)
 
-                with self.urlopen(url) as page:
-                    page = lxml.etree.fromstring(page.bytes)
-                    if not page.getchildren():
-                        # If the result response is empty, we've hit the end of
-                        # the data. Quit.
-                        break
+                page = self.urlopen(url)
+                page = lxml.etree.fromstring(page.bytes)
+                if not page.getchildren():
+                    # If the result response is empty, we've hit the end of
+                    # the data. Quit.
+                    break
 
-                    for result in page.xpath("//result[@type = 'bill']"):
+                for result in page.xpath("//result[@type = 'bill']"):
 
-                        bill_id = result.attrib['id'].split('-')[0]
+                    bill_id = result.attrib['id'].split('-')[0]
 
-                        # Parse the bill_id into beginning letter, number
-                        # and any trailing letters indicating its an amendment.
-                        bill_id_rgx = r'(^[A-Z])(\d{,6})([A-Z]{,3})'
-                        bill_id_base = re.search(bill_id_rgx, bill_id)
-                        letter, number, is_amendment = bill_id_base.groups()
-                        if is_amendment:
+                    # Parse the bill_id into beginning letter, number
+                    # and any trailing letters indicating its an amendment.
+                    bill_id_rgx = r'(^[A-Z])(\d{,6})([A-Z]{,3})'
+                    bill_id_base = re.search(bill_id_rgx, bill_id)
+                    letter, number, is_amendment = bill_id_base.groups()
+                    if is_amendment:
 
-                            # If this bill is an amendment, use the last
-                            # nonamendment bill to add votes, documents,
-                            # etc.
-                            if previous_nonamendment_bill is not None:
-                                self.add_amendment_data(previous_nonamendment_bill, result)
+                        # If this bill is an amendment, use the last
+                        # nonamendment bill to add votes, documents,
+                        # etc.
+                        if previous_nonamendment_bill is not None:
+                            self.add_amendment_data(previous_nonamendment_bill, result)
 
-                            if bill_id in scraped_amendments:
-                                continue
-
-                        else:
-                            if previous_nonamendment_bill is not None:
-                                self.save_bill(previous_nonamendment_bill)
-                                previous_nonamendment_bill = None
-
-                        title = result.attrib['title'].strip()
-                        if title == '(no title)':
+                        if bill_id in scraped_amendments:
                             continue
 
-                        if "sponsor" not in result.attrib:
-                            continue
+                    else:
+                        if previous_nonamendment_bill is not None:
+                            self.save_bill(previous_nonamendment_bill)
+                            previous_nonamendment_bill = None
 
-                        primary_sponsor = result.attrib['sponsor']
-                        primary_sponsor = re.sub(
-                            r'\s+\(MS\)\s*$', '', primary_sponsor).strip()
+                    title = result.attrib['title'].strip()
+                    if title == '(no title)':
+                        continue
 
-                        bill_chamber, bill_type = {
-                            'S': ('upper', 'bill'),
-                            'R': ('upper', 'resolution'),
-                            'J': ('upper', 'legislative resolution'),
-                            'B': ('upper', 'concurrent resolution'),
-                            'A': ('lower', 'bill'),
-                            'E': ('lower', 'resolution'),
-                            'K': ('lower', 'legislative resolution'),
-                            'L': ('lower', 'joint resolution')}[bill_id[0]]
+                    if "sponsor" not in result.attrib:
+                        continue
 
-                        if chamber != bill_chamber:
-                            continue
+                    primary_sponsor = result.attrib['sponsor']
+                    primary_sponsor = re.sub(
+                        r'\s+\(MS\)\s*$', '', primary_sponsor).strip()
 
-                        bill = Bill(session, chamber, bill_id, title,
-                                    type=bill_type)
+                    bill_chamber, bill_type = {
+                        'S': ('upper', 'bill'),
+                        'R': ('upper', 'resolution'),
+                        'J': ('upper', 'legislative resolution'),
+                        'B': ('upper', 'concurrent resolution'),
+                        'A': ('lower', 'bill'),
+                        'E': ('lower', 'resolution'),
+                        'K': ('lower', 'legislative resolution'),
+                        'L': ('lower', 'joint resolution')}[bill_id[0]]
 
-                        bill.add_source(url)
+                    if chamber != bill_chamber:
+                        continue
 
-                        # Adding sponsors below.
-                        #bill.add_sponsor('primary', primary_sponsor)
+                    bill = Bill(session, chamber, bill_id, title,
+                                type=bill_type)
 
-                        bill_url = ("http://open.nysenate.gov/legislation/"
-                                    "bill/%s" % result.attrib['id'])
-                        self.scrape_bill(bill, bill_url)
-                        bill.add_source(bill_url)
+                    bill.add_source(url)
 
-                        if not is_amendment:
-                            previous_nonamendment_bill = bill
+                    # Adding sponsors below.
+                    #bill.add_sponsor('primary', primary_sponsor)
 
-                        index += 1
+                    bill_url = ("http://open.nysenate.gov/legislation/"
+                                "bill/%s" % result.attrib['id'])
+                    self.scrape_bill(bill, bill_url)
+                    bill.add_source(bill_url)
+
+                    if not is_amendment:
+                        previous_nonamendment_bill = bill
+
+                    index += 1
 
             except scrapelib.HTTPError as e:
                 code = e.response.status_code
@@ -162,8 +163,8 @@ class NYBillScraper(BillScraper):
                 actions.append((date, action))
 
             first = True
-            act_chamber = bill['chamber']
             for date, action in reversed(actions):
+                act_chamber = ('upper' if action.isupper() else 'lower')
                 atype = []
                 if first:
                     atype.append('bill:introduced')
@@ -193,12 +194,13 @@ class NYBillScraper(BillScraper):
                 if not atype:
                     atype = ['other']
 
-                bill.add_action(bill['chamber'], action, date,
+                bill.add_action(act_chamber, action, date,
                                 type=atype)
 
             self.scrape_versions(bill, page, url)
 
-            self.scrape_votes(bill, page)
+            self.scrape_senate_votes(bill, page)
+            self.scrape_assembly_votes(bill, page)
 
             subjects = []
             for link in page.xpath("//a[contains(@href, 'lawsection')]"):
@@ -206,7 +208,7 @@ class NYBillScraper(BillScraper):
 
             bill['subjects'] = subjects
 
-    def scrape_votes(self, bill, page):
+    def scrape_senate_votes(self, bill, page):
         for b in page.xpath("//div/b[starts-with(., 'VOTE: FLOOR VOTE:')]"):
             date = b.text.split('-')[1].strip()
             date = datetime.datetime.strptime(date, "%b %d, %Y").date()
@@ -258,6 +260,67 @@ class NYBillScraper(BillScraper):
 
             bill.add_vote(vote)
 
+    def scrape_assembly_votes(self, bill, page):
+
+        bill_id = bill['bill_id']
+        url = ('http://assembly.state.ny.us/leg/?'
+               'default_fld=&bn=%s&term=2011&Votes=Y')
+        html = self.urlopen(url % bill_id)
+        doc = lxml.html.fromstring(html)
+
+        pre = doc.xpath('//pre')[0].text_content()
+        no_votes = ('There are no votes for this bill in this '
+                    'legislative session.')
+        if pre == no_votes:
+            return
+
+        for table in doc.xpath('//table'):
+
+            date = table.xpath('caption/label[contains(., "DATE:")]')
+            date = date[0].itersiblings().next().text
+            date = datetime.datetime.strptime(date, '%m/%d/%Y')
+
+            votes = table.xpath('caption/span/label[contains(., "YEA/NAY:")]')
+            votes = votes[0].itersiblings().next().text
+            yes_count, no_count = map(int, votes.split('/'))
+
+            passed = yes_count > no_count
+            vote = Vote('lower', date, 'Floor Vote', passed, yes_count,
+                        no_count, other_count=0)
+
+            tds = table.xpath('tr/td/text()')
+            votes = iter(tds)
+            while True:
+                try:
+                    data = list(islice(votes, 2))
+                    name, vote_val = data
+                except (StopIteration, ValueError):
+                    # End of data. Stop.
+                    break
+                name = name.strip()
+                if vote_val.strip() == 'Y':
+                    vote.yes(name)
+                elif vote_val.strip() in ('N', 'NO'):
+                    vote.no(name)
+                else:
+                    vote.other(name)
+
+            # The page doesn't provide an other_count.
+            vote['other_count'] = len(vote['other_votes'])
+
+            print url % bill_id
+            if len(vote['yes_votes']) != vote['yes_count']:
+                print len(vote['yes_votes']), vote['yes_count']
+                import pdb;pdb.set_trace()
+            if len(vote['no_votes']) != vote['no_count']:
+                print len(vote['no_votes']), vote['no_count']
+                import pdb;pdb.set_trace()
+            if len(vote['other_votes']) != vote['other_count']:
+                print len(vote['other_votes']), vote['other_count']
+                import pdb;pdb.set_trace()
+
+            bill.add_vote(vote)
+
     def scrape_versions(self, bill, page, url):
         '''Note--this function also scrapes the companion bill_id, or
         'same-as' id.'''
@@ -291,4 +354,5 @@ class NYBillScraper(BillScraper):
             page = page.replace('\x00', '')
             page = lxml.html.fromstring(page)
             page.make_links_absolute(url)
-            self.scrape_votes(bill, page)
+            self.scrape_senate_votes(bill, page)
+            self.scrape_assembly_votes(bill, page)
