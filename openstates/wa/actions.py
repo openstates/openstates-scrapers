0cf3ae1e756f4d3523a46f6e067456b5c9e34a41
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-10-17 15:33:30 -0400

    WA: Actions update.

--- openstates/wa/actions.py
+++ openstates/wa/actions.py
@@ -7,11 +7,17 @@ _categorizer_rules = (
     Rule(r"(?i)first reading, referred to (?P<committees>.*)\.", 'bill:reading:1'),
     Rule(r"(?i)And refer to (?P<committees>.*)", 'committee:referred'),
     Rule(r"(?i).* substitute bill substituted.*", 'bill:substituted'),
-    Rule(r"(?i)chapter (((\d+),?)+) \d{4} laws."),  # XXX: Thom: Code stuff?
-    Rule(r"(?i)effective date \d{1,2}/\d{1,2}/\d{4}.*"),
+    Rule(r"(?i)chapter (((\d+),?)+) \d+ laws.( .+)?", "other"),  # XXX: Thom: Code stuff?
+    Rule(r"(?i)effective date \d{1,2}/\d{1,2}/\d{4}.*", "other"),
     Rule(r"(?i)(?P<committees>\w+) - majority; do pass with amendment\(s\) (but without amendments\(s\))?.*\.", "committee:passed:favorable", "committee:passed"),
-    Rule(r"(?i)Executive action taken in the (House|Senate) committee on (?P<committees>.*) (at)? .*\."),
-#    Rule(
+    Rule(r"(?i)Executive action taken in the (House|Senate) committee on (?P<committees>.*) (at)? .*\.", "other"),
+    Rule(r"(?i)(?P<committee_name>\w+) \- Majority; do pass .* \(Majority Report\)", 'bill:passed'),
+    Rule(r"(?i)Conference committee appointed.", "other"),
+    Rule(r"(?i)Conference committee report;", 'other'),
+    Rule(r"(?i).+ - Majority; \d+.+ substitute bill be substituted, do pass", 'bill:passed'),
+    Rule(r"(?i)Signed by (?P<signed_chamber>(Representatives|Senators)) (?P<members>.*)", "bill:passed"),
+#    Rule(r""),
+#    Rule(r""),
 )
 
 
