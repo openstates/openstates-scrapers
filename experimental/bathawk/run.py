3ad49fbedab0afef708e5720bfd82b9702a5c64c
Thom Neale <twneale@gmail.com>
2012-10-25 17:29:41 -0400

    huge code cleanup and simplification

--- experimental/bathawk/run.py
+++ experimental/bathawk/run.py
@@ -1,39 +1,13 @@
 import sys
 
-from clint.textui import puts, colored
-
-from batshell import Shell
+from bathawk import BathawkShell
 from actions import Actions
 
 
 def main(_, abbr):
-    # try:
-    #     patterns_module = __import__(abbr)
-    # except:
-    patterns_module = None
-    actions = Actions(abbr, patterns_module)
-    sys.ps1 = '(batshell) '
-    banner = colored.yellow('''
-      _==/           i     i           \==_
-     /XX/            |\___/|            \XX\\
-   /XXXX\            |XXXXX|            /XXXX\\
-  |XXXXXX\_         _XXXXXXX_         _/XXXXXX|
- XXXXXXXXXXXxxxxxxxXXXXXXXXXXXxxxxxxxXXXXXXXXXXX
-|XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX|
-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-|XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX|
- XXXXXX/^^^^"\XXXXXXXXXXXXXXXXXXXXX/^^^^^\XXXXXX
-  |XXX|       \XXX/^^\XXXXX/^^\XXX/       |XXX|
-    \XX\       \X/    \XXX/    \X/       /XX/
-       "\       "      \X/      "       /"
-''')
-    banner += colored.cyan('\n\nWelcome to bashell. '
-                           'Type h for a list of commands.')
-    shell = Shell(actions, abbr)
-    import readline
-    puts(banner)
+    actions = Actions(abbr)
+    shell = BathawkShell(actions, abbr)
     shell.interact(banner='')
 
 if __name__ == '__main__':
-    import sys
     main(*sys.argv)
