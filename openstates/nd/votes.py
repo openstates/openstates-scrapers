9a3bed7dfd0d678414b49a8897a58b83bb778478
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-10-30 12:07:50 -0400

    ND: Starting to collect votes via pdf scraping.

---
+++ openstates/nd/votes.py
@@ -0,0 +1,36 @@
+from billy.scrape.votes import VoteScraper, Vote
+from billy.scrape.utils import convert_pdf
+import datetime
+import subprocess
+import lxml
+import os
+import re
+
+
+PAGES = {
+    "upper":
+      "http://www.legis.nd.gov/assembly/62-2011/journals/senate-journal.html",
+    "lower":
+      "http://www.legis.nd.gov/assembly/62-2011/journals/house-journal.html"
+}
+
+
+class NDVoteScraper(VoteScraper):
+    state = 'nd'
+
+    def lxmlize(self, url):
+        with self.urlopen(url) as page:
+            page = lxml.html.fromstring(page)
+        page.make_links_absolute(url)
+        return page
+
+
+    def scrape(self, chamber, session):
+        if chamber not in PAGES:
+            return
+
+        url = PAGES[chamber]
+        page = self.lxmlize(url)
+        pdfs = page.xpath("//a[contains(@href, '.pdf')]")
+        for pdf in pdfs:
+            print pdf.attrib['href']
