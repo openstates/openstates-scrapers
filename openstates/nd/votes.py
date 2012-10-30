c16e7751cb5479ab81410f22fc56924132f337d8
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-10-30 16:15:39 -0400

    ND: More voting changes. Slow & steady

--- openstates/nd/votes.py
+++ openstates/nd/votes.py
@@ -14,6 +14,8 @@ PAGES = {
       "http://www.legis.nd.gov/assembly/62-2011/journals/house-journal.html"
 }
 
+fin_re = r"(?i).*(?P<bill_id>(S|H|J)(B|R|M) \d+).*(?P<passfail>(passed|lost)).*"
+date_re = r".*(?P<date>(MONDAY|TUESDAY|WEDNESDAY|THURSDAY|FRIDAY|SATURDAY|SUNDAY), .*\d{1,2},\s\d{4}).*"
 
 class NDVoteScraper(VoteScraper):
     state = 'nd'
@@ -33,4 +35,27 @@ class NDVoteScraper(VoteScraper):
         page = self.lxmlize(url)
         pdfs = page.xpath("//a[contains(@href, '.pdf')]")
         for pdf in pdfs:
-            print pdf.attrib['href']
+
+            bill_id = None
+            results = {}
+            in_vote = False
+            cur_date = None
+
+            pdf_url = pdf.attrib['href']
+            (path, response) = self.urlretrieve(pdf_url)
+            data = convert_pdf(path, type='text')
+            os.unlink(path)
+            lines = data.splitlines()
+            for line in lines:
+                date = re.findall(date_re, line)
+                if date != [] and not cur_date:
+                    print date
+                    date = date[0][0]
+                    cur_date = datetime.datetime.strptime(date, "%A, %B %d, %Y")
+
+                if line.strip() == "":
+                    continue
+
+                if line.strip() == 'ROLL CALL':
+                    in_vote = True
+                print cur_date, line
