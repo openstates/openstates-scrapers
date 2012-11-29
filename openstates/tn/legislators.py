50a49292fa2d5bc878349bc05951ac7976887888
Thom Neale <twneale@gmail.com>
2012-11-29 14:29:17 -0500

    TN: flak8e OCD

--- openstates/tn/legislators.py
+++ openstates/tn/legislators.py
@@ -1,10 +1,9 @@
-from billy.scrape import ScrapeError, NoDataForPeriod
 from billy.scrape.legislators import LegislatorScraper, Legislator
 import lxml.html
 
+
 class TNLegislatorScraper(LegislatorScraper):
     state = 'tn'
-
 
     def scrape(self, chamber, term):
         self.validate_term(term, latest_only=False)
@@ -21,7 +20,8 @@ class TNLegislatorScraper(LegislatorScraper):
             url_chamber_name = 'house'
             abbr = 'h'
         if term != self.metadata["terms"][-1]["sessions"][0]:
-            chamber_url = root_url + url_chamber_name + '/archives/'+term+'GA/Members/index.html'
+            chamber_url = root_url + url_chamber_name
+            chamber_url += '/archives/' + term + 'GA/Members/index.html'
         else:
             chamber_url = root_url + url_chamber_name + '/members/'
 
