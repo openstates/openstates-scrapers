1c04b05ccfa42dde97664f6542cc02ba1fb55509
James Turk <james.p.turk@gmail.com>
2012-12-14 16:25:49 -0500

    enable ct

--- site/openstates_site/settings.py
+++ site/openstates_site/settings.py
@@ -140,7 +140,7 @@ ACTIVE_STATES=sorted(['ca', 'il', 'la', 'md', 'mn',
                       'ar', 'co', 'in', 'ia', 'ky',
                       'ma', 'ms', 'ne', 'nm', 'or',
                       'pa', 'ri', 'tn', 'vt', 'va',
-                      'pr'
+                      'pr', 'ct'
                      ])
 
 LOGIN_REDIRECT_URL = '/'
