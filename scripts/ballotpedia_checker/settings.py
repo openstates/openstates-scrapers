7fdaadcbe7b27111079432962c77da0325100840
Thom Neale <twneale@gmail.com>
2013-01-02 10:16:42 -0500

    added example settings

--- scripts/ballotpedia_checker/settings.py
+++ scripts/ballotpedia_checker/settings.py
@@ -1,3 +1,7 @@
+CLIENT_ID = None  # Something like '123467893423.apps.googleusercontent.com'
+CLIENT_SECRET = None # Something like 'asdlfkalskdjhfaslkdjhflaksj'
+REDIRECT_URI = None # Something like 'urn:ietf:wg:oauth:2.0:oob'
+
 try:
     from local_settings import *
 except ImportError:
