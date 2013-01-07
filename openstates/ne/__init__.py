ae5fa85013cd9eb8c37ab599960aeceeaabdb1b8
James Turk <james.p.turk@gmail.com>
2013-01-07 15:04:03 -0500

    2013 metadata for NE and KY

--- openstates/ne/__init__.py
+++ openstates/ne/__init__.py
@@ -13,23 +13,31 @@ metadata = dict(
     terms=[
         {'name': '2011-2012', 'sessions': ['102', '102S1'],
         'start_year': 2011, 'end_year': 2012},
+        {'name': '2013-2014', 'sessions': ['103'],
+        'start_year': 2013, 'end_year': 2014},
     ],
     session_details={
         '102': {
             'start_date': datetime.date(2011, 1, 5),
             'end_date': datetime.date(2012, 4, 18),
             'display_name': '102nd Legislature',
-            '_scraped_name': '102nd Legislature 1st and Second Sessions',
+            '_scraped_name': '102nd Legislature 1st and 2nd Sessions',
                },
         '102S1': {
             'display_name': '102nd Legislature, 1st Special Session',
             '_scraped_name': '102nd Legislature 1st Special Session',
             'start_date': datetime.date(2011, 11, 1),
             'end_date': datetime.date(2011, 11, 22)
-                 }
+                 },
+        '103': {
+            'start_date': datetime.date(2013, 1, 8),
+            'end_date': datetime.date(2014, 5, 30),
+            'display_name': '103rd Legislature',
+            '_scraped_name': '103rd Legislature 1st and 2nd Sessions',
+               },
         },
     feature_flags=['influenceexplorer'],
-    _ignored_scraped_sessions=['101st Legislature 1st and Second Sessions',
+    _ignored_scraped_sessions=['101st Legislature 1st and 2nd Sessions',
                                '101st Legislature 1st Special Session',
                                '100th Legislature 1st and 2nd Sessions',
                                '100th Leg. First Special Session',
