3228c080baf61df6960a64f47d000b4bb00743f9
James Turk <james.p.turk@gmail.com>
2013-01-09 13:42:15 -0500

    KS: minor fixes

--- openstates/ks/legislators.py
+++ openstates/ks/legislators.py
@@ -18,61 +18,61 @@ class KSLegislatorScraper(LegislatorScraper):
         else:
             url = legislator_list_url % 'senate'
 
-        with self.urlopen(url) as legislator_list_page:
-            doc = lxml.html.fromstring(legislator_list_page)
-            doc.make_links_absolute(url)
+        html = self.urlopen(url)
+        doc = lxml.html.fromstring(html)
+        doc.make_links_absolute(url)
 
-            rows = doc.xpath('//table/tr')[1:]
-            for row in rows:
-                name, district, phone, email = row.getchildren()
-                url = name.xpath('a')[0].get('href')
-                district = district.text_content().strip()
-                phone = phone.text_content().strip()
-                email = email.text_content().strip()
-                self.scrape_legislator(chamber, term, url, district, phone,
-                                       email)
+        rows = doc.xpath('//table/tr')
+        for row in rows:
+            name, district, phone, email = row.getchildren()
+            url = name.xpath('a')[0].get('href')
+            district = district.text_content().strip()
+            phone = phone.text_content().strip()
+            email = email.text_content().strip()
+            self.scrape_legislator(chamber, term, url, district, phone,
+                                   email)
 
     def scrape_legislator(self, chamber, term, url, district, phone, email):
-        with self.urlopen(url) as legislator_page:
-            legislator_page = lxml.html.fromstring(legislator_page)
-            legislator_page.make_links_absolute(url)
+        html = self.urlopen(url)
+        legislator_page = lxml.html.fromstring(html)
+        legislator_page.make_links_absolute(url)
 
-            main = legislator_page.xpath("//div[@id='main']")[0]
-            name = main.xpath('h1')[0].text_content()
-            info = main.xpath('h3')[0].text_content()
+        main = legislator_page.xpath("//div[@id='main']")[0]
+        name = main.xpath('h1')[0].text_content()
+        info = main.xpath('h3')[0].text_content()
 
-            full_name = legislator_name_pattern.match(name).group(2).split(' - ')[0]
+        full_name = legislator_name_pattern.match(name).group(2).split(' - ')[0]
 
-            party = legislator_line_pattern.match(info).group(1)
-            if party == 'Democrat':
-                party = 'Democratic'
+        party = legislator_line_pattern.match(info).group(1)
+        if party == 'Democrat':
+            party = 'Democratic'
 
-            photo = legislator_page.xpath('//img[@class="profile-picture"]/@src')[0]
+        photo = legislator_page.xpath('//img[@class="profile-picture"]/@src')[0]
 
-            room = legislator_page.xpath('//p[contains(text(), "Room: ")]/text()')
-            if room:
-                room = room[0].split('Room: ')[1]
-                if room == 'DSOB':
-                    address = ('Kansas House of Representatives\n'
-                               'Docking State Office Building\n'
-                               '901 SW Harrison Street\n'
-                               'Topeka, KS 66612')
-                else:
-                    address = ('Room %s\n'
-                               'Kansas State Capitol Building\n'
-                               '300 SW 10th St.\n'
-                               'Topeka, KS 66612') % room
+        room = legislator_page.xpath('//p[contains(text(), "Room: ")]/text()')
+        if room:
+            room = room[0].split('Room: ')[1]
+            if room == 'DSOB':
+                address = ('Kansas House of Representatives\n'
+                           'Docking State Office Building\n'
+                           '901 SW Harrison Street\n'
+                           'Topeka, KS 66612')
             else:
-                address = None
+                address = ('Room %s\n'
+                           'Kansas State Capitol Building\n'
+                           '300 SW 10th St.\n'
+                           'Topeka, KS 66612') % room
+        else:
+            address = None
 
-            if not phone:
-                phone = None
+        if not phone:
+            phone = None
 
-            legislator = Legislator(term, chamber, district, full_name,
-                                    party=party, email=email, url=url,
-                                    photo_url=photo)
-            legislator.add_office('capitol', 'Capitol Office', phone=phone,
-                                  address=address)
-            legislator.add_source(url)
+        legislator = Legislator(term, chamber, district, full_name,
+                                party=party, email=email, url=url,
+                                photo_url=photo)
+        legislator.add_office('capitol', 'Capitol Office', phone=phone,
+                              address=address)
+        legislator.add_source(url)
 
-            self.save_legislator(legislator)
+        self.save_legislator(legislator)
