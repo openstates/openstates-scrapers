db0b097b751257adac8d6c4cad3a199911fdeb69
Paul Tagliamonte <paultag@sunlightfoundation.com>
2013-02-15 13:58:49 -0500

    Add filter skel

--- billy_settings.py
+++ billy_settings.py
@@ -25,6 +25,42 @@ PARTY_DETAILS = {
     'Republican/Democratic': {'plural_noun': 'Republican/Democratic'},   # VT
 }
 
+
+# LEGISLATOR_FILTERS = {
+#     "billy.importers.filters.single_space_filter": [
+#         "full_name",
+#         "first_name",
+#         "last_name",
+#         "middle_name",
+#     ],
+#     "billy.importers.filters.phone_filter": [
+#         "office_phone",
+#         "phone",
+#         "offices.phone",
+#         "offices.fax",
+#     ],
+#     "billy.importers.filters.email_filter": [
+#         "offices.email",
+#     ],
+# }
+# 
+# BILL_FILTERS = {
+#     "billy.importers.filters.single_space_filter": [
+#         "actions.action",
+#         "title",
+#     ]
+# }
+# 
+# EVENT_FILTERS = {
+#     "billy.importers.filters.single_space_filter": [
+#         "description",
+#         "participants.participant",
+#         "related_bills.bill_id",
+#         "related_bills.description",
+#     ]
+# }
+
+
 ENABLE_ELASTICSEARCH = True
 BOUNDARY_SERVICE_SETS = 'sldl,sldu'
 ENABLE_DOCUMENT_VIEW = {
