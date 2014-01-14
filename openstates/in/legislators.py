4ca6807dd18d0f577c0d984476260d4a9a8c3a8a
twneale <twneale@gmail.com>
2014-01-14 15:11:53 -0500

    IN: add addresses back in after switch to API

--- openstates/in/legislators.py
+++ openstates/in/legislators.py
@@ -70,6 +70,34 @@ class INLegislatorScraper(LegislatorScraper):
                     'member', term=term, chamber=chamber,
                     commmittee=comm['name'])
 
+            # Woooooo! Email addresses are guessable in IN/
+            tmpl = '{chamber[0]}{district}@igo.in.gov'
+            leg['email'] = tmpl.format(chamber=chamber, district=district)
+
+            # Add district generic IGA address, usually the only thing available.
+            tmpl = '''{title} {full_name}
+            200 W. Washington St.
+            Indianapolis, IN 46204'''
+            title = 'Senator' if chamber == 'upper' else 'Representative'
+            address = tmpl.format(title=title, full_name=leg['full_name'])
+            address = re.sub(r' +', ' ', address)
+
+            # Get the contact details.
+            deets_getter = 'get_contact_%s_%s' % (chamber, data['party'])
+            deets_getter = getattr(self, deets_getter)
+            deets = deets_getter(leg, leg_url)
+
+            # If email found, use that instead of guessed email.
+            if deets.get("email"):
+                leg['email'] = deets.pop("email")
+
+            office = dict(deets,
+                address=address, name='District Office',
+                type='district', fax=None)
+
+            import pprint
+            pprint.pprint(office)
+
             self.save_legislator(leg)
 
     def get_districts(self, chamber):
@@ -97,3 +125,58 @@ class INLegislatorScraper(LegislatorScraper):
             res[leg1_name] = dict(district=dist1, url=leg1_url)
             res[leg2_name] = dict(district=dist2, url=leg2_url)
         return res
+
+
+    def get_contact_upper_Republican(self, leg, leg_url):
+        '''Get contact info for Senate Republicans.
+        '''
+        deets = {}
+        html = self.urlopen(leg_url)
+        doc = lxml.html.fromstring(html)
+        phone = email = None
+        for el in doc.iterdescendants():
+            tail = (el.tail or '').strip()
+
+            if not email and tail.startswith('Email:'):
+                email = deets['email'] = tail.replace('Email: ', '').strip()
+
+            if not phone and tail.startswith('Phone:'):
+                phone = tail.replace('Phone: ', '').strip()
+                if ' or ' in phone:
+                    phone, _, _ = phone.partition(' or ')
+                deets['phone'] = phone
+        return deets
+
+    def get_contact_lower_Republican(self, leg, leg_url):
+        '''Get contact info for House Republicans.
+        '''
+        html = self.urlopen(leg_url)
+        doc = lxml.html.fromstring(html)
+        phones = []
+        for el in doc.iterdescendants():
+            tail = (el.tail or '').strip()
+            m = re.search(r'\(\d{3}\)\s+\d{3}-\d{4}', tail)
+            if m:
+                phones.append(m.group())
+        return dict(phone=', '.join(phones))
+
+    def get_contact_upper_Democratic(self, leg, leg_url):
+        '''Get contact info for Senate Dems.
+        '''
+        deets = {}
+        tmpl = ('http://www.in.gov/legislative/senate_democrats/'
+                'homepages/s%s/contactme.htm')
+        contact_url = tmpl % leg['roles'][0]['district']
+        html = self.urlopen(leg_url)
+        doc = lxml.html.fromstring(html)
+        for el in doc.iterdescendants():
+            tail = (el.tail or '').strip()
+            m = re.search(r'\(\d{3}\)\s+\d{3}-\d{4}', tail)
+            if m:
+                deets['phone'] = m.group()
+        return deets
+
+    def get_contact_lower_Democratic(self, leg, leg_url):
+        '''Get contact info for House Dems.
+        '''
+        return dict(phone="800-382-9842")
