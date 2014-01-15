2bc56d4b513666f4b42bb68915f6d36a76261886
twneale <twneale@gmail.com>
2014-01-15 16:25:55 -0500

    Added script to fix tn's bogus 109th session import

---
+++ scripts/tn_109fix/tnfix.py
@@ -0,0 +1,33 @@
+import sys
+import plop
+import pymongo
+
+
+def main(state):
+    db = pymongo.MongoClient().fiftystates
+    index = plop.Index()
+
+    spec = dict(state=state)
+    print('adding bills')
+    for bill in db.bills.find(spec):
+        index.add_object(bill)
+
+    print('adding legislators')
+    for obj in db.legislators.find(spec):
+        index.add_object(obj)
+
+    print('adding committees')
+    for obj in db.committees.find(spec):
+        index.add_object(bill)
+
+    print('adding votes')
+    for obj in db.committees.find(spec):
+        index.add_object(bill)
+
+	
+    import pdb; pdb.set_trace()
+
+
+if __name__ == "__main__":
+    main(*sys.argv[1:])
+
