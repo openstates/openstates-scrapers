eced12f492533f5e88dcf4d762e51e15fdc43637
Kevin Schaul <kevin.schaul@gmail.com>
2014-01-16 16:02:33 -0600

    Add House, Senate events to MN scraper
    Use US/Central timezone
    Add basic agenda support

--- openstates/mn/events.py
+++ openstates/mn/events.py
@@ -1,4 +1,5 @@
-import datetime as dt
+from datetime import datetime as datetime
+import re
 
 from billy.scrape import NoDataForPeriod
 from billy.scrape.events import Event, EventScraper
@@ -10,74 +11,175 @@ url = "http://www.leg.state.mn.us/calendarday.aspx?jday=all"
 
 class MNEventScraper(EventScraper):
     jurisdiction = 'mn'
-    _tz = pytz.timezone('US/Eastern')
+    central_timezone = pytz.timezone('US/Central')
+    date_formats = (
+        '%A, %B %d, %Y %I:%M %p',
+        '%A, %B %d'
+    )
+
+    def scrape(self, chamber, session):
+        self.session = session
+
+        page = self.lxmlize(url)
+
+        commission_meetings = page.xpath("//div[@class='Comm_item']")
+        self.scrape_meetings(commission_meetings, 'commission')
+
+        house_meetings = page.xpath("//div[@class='house_item']")
+        self.scrape_meetings(house_meetings, 'house')
+
+        senate_meetings = page.xpath("//div[@class='senate_item']")
+        self.scrape_meetings(senate_meetings, 'senate')
+
     def lxmlize(self, url):
         page = self.urlopen(url)
         page = lxml.html.fromstring(page)
         page.make_links_absolute(url)
         return page
 
-    def scrape(self, chamber, session):
-        if chamber != "other":
-            return None
-        page = self.lxmlize(url)
-        meetings = page.xpath("//div[@class='Comm_item']")
+    def scrape_meetings(self, meetings, group):
+        """
+        Scrape and save event data from a list of meetings.
+
+        Arguments:
+        meetings -- A list of lxml elements containing event information
+        group -- The type of meeting. The legislature site applies
+                 different formatting to events based on which group
+                 they correspond to.  `group` should be one of the
+                 following strings: 'house', 'senate', or 'commission'.
+
+        """
         for meeting in meetings:
-            metas = meeting.xpath(".//b")
-            ctty = meeting.xpath(".//a")[0]
-            ctty_name = ctty.text_content()
-            info = metas[1:]
-            datetime = metas[0]
-            metainf = {}
-            for meta in info:
-                header = meta.text_content().strip()
-                val = meta.tail
-                metainf[header] = val or ""
-            datetime = datetime.text_content().strip()
-            # Tuesday, June 05, 2012 9:00 AM
-            if "Canceled" in datetime:
-                continue
+            when = self.get_date(meeting)
+            description = self.get_description(meeting)
+            location = self.get_location(meeting)
 
-            formats = [
-               "%A, %B %d, %Y %I:%M %p",
-               "%A, %B %d, %Y"
-            ]
-            date_time = None
+            if when and description and location:
+                kwargs = {}
+                if group in self.metadata['chambers'].keys():
+                    kwargs['chamber'] = group
+                agenda = self.get_agenda(meeting)
+                if agenda:
+                    kwargs['agenda'] = agenda
 
-            for fmt in formats:
-                try:
-                    date_time = dt.datetime.strptime(
-                        datetime, fmt)
-                except ValueError:
-                    pass
+                # Event prototype is as follows:
+                # class Event(SourcedObject):
+                #    def __init__(self, session, when, type,
+                #                 description, location, end=None, **kwargs)
+                event = Event(self.session, when, 'committee:meeting',
+                        description, location, **kwargs)
+                event.add_source(url)
+                self.save_event(event)
 
-            if date_time is None:
-                continue
+    def get_date(self, meeting):
+        """
+        Get the date from a meeting lxml element.
 
-            event = Event(session,
-                          date_time,
-                          'committee:meeting',
-                          ctty_name,
-                          location=metainf['Room:'] or "State House",
-                          chamber=chamber
-                         )
-            event.add_source(url)
+        Arguments:
+        meeting -- A lxml element containing event information
 
-            if metainf['Chair:'] != "":
-                event.add_participant('chair', metainf['Chair:'], 'legislator',
-                                      chamber=chamber)
+        """
+        date_raw = meeting.xpath(".//b")
+        if len(date_raw) < 1:
+            return
 
-            chamber = "other"
-            chambers = {
-                "house": "lower",
-                "joint": "joint",
-                "senate": "upper",
-            }
-            for c in chambers:
-                if c in ctty_name.lower():
-                    chamber = chambers[c]
+        date_string = date_raw[0].text_content().strip()
 
-            event.add_participant('host', ctty_name, 'committee', chamber=chamber)
-            # add chair?
+        for date_format in self.date_formats:
+            try:
+                date = datetime.strptime(date_string, date_format)
+            except ValueError:
+                pass
 
-            self.save_event(event)
+        if 'date' in locals():
+            return self.central_timezone.localize(date)
+        return
+
+    def get_description(self, meeting, i=0):
+        """
+        Get the description from a meeting lxml element.
+
+        Because some events include a "House" or "Senate" `span`
+        before the `span` containing the description, a repetitive
+        search is necessary.
+
+        TODO Other events include a date span before the span
+        containing the description.
+
+        Arguments:
+        meeting -- A lxml element containing event information
+        i -- The index of `a`/`span` tags to look for.
+
+        """
+        description_raw = meeting.xpath(".//a")
+        if (len(description_raw) < 1 or
+                description_raw[0].text_content() == ''):
+            description_raw = meeting.xpath(".//span")
+        if len(description_raw) < (i + 1):
+            return
+
+        description = description_raw[i].text_content().strip()
+
+        if description == 'House' or description == 'Senate':
+            return self.get_description(meeting, i + 1)
+
+        return description
+
+    def get_location(self, meeting):
+        """
+        Get the location from a meeting lxml element.
+
+        Location information follows a `b` element containing the text
+        "Room:".
+
+        Arguments:
+        meeting -- A lxml element containing event information
+
+        """
+        return self.get_tail_of(meeting, '^Room:')
+
+    def get_agenda(self, meeting):
+        """
+        Get the agenda from a meeting lxml element.
+
+        Agenda information follows a `b` element containing the text
+        "Agenda:".
+
+        Arguments:
+        meeting -- A lxml element containing event information
+
+        """
+        return self.get_tail_of(meeting, '^Agenda:')
+
+    def get_tail_of(self, meeting, pattern_string):
+        """
+        Get the tail of a `b` element matching `pattern_string`, all
+        inside a `p` tag.
+
+        Surprisingly useful for the markup on the Minnesota
+        legislative events calendar page.
+
+        Arguments:
+        pattern_string -- A regular expression string to match
+                          against
+
+        """
+        pattern = re.compile(pattern_string)
+
+        p_tags = meeting.xpath(".//p")
+        if len(p_tags) < 1:
+            return
+
+        p_tag = p_tags[0]
+
+        for element in p_tag.iter():
+            if element.tag == 'b':
+                raw = element.text_content().strip()
+                r = pattern.search(raw)
+                if r:
+                    tail = element.tail.strip()
+                    if tail != '':
+                        return tail
+                    break
+        return
+
