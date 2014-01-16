fe113a602296ada7b9b6800f9cc3fd228304df5f
twneale <twneale@gmail.com>
2014-01-16 12:44:10 -0500

    Stringify session 108

--- scripts/tn_109fix/tn_script.py
+++ scripts/tn_109fix/tn_script.py
@@ -1,5 +1,6 @@
 import sys
 
+import pymongo
 from billy.core import db
 
 def main(state):
@@ -10,20 +11,20 @@ def main(state):
     bill_spec = dict(spec, session=108)
     for bill_109th in db.bills.find(bill_spec):
         print(bill_109th['bill_id'])
+        # print(bill_109th['session'])
         # Reset session and _term to 108.
         bill_109th.update(session='108', _term='108')
 
         try:
-            db.bills.save(bill_109th)
+            db.bills.save(bill_109th, w=1)
         except pymongo.errors.DuplicateKeyError:
             # This bill was duped, with the only different attr being
             # session value of 109. Merge the two bills by adding the
             # bad 109 bill id to the 108 bills' _all_ids.
 
             # First get the 108th session bill.
-            import pdb; pdb.set_trace()
             spec = dict(spec,
-                session=108,
+                session='108',
                 chamber=bill_109th['chamber'],
                 bill_id=bill_109th['bill_id'])
             bill_108th = db.bills.find_one(spec)
