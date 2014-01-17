2757e4d676a558a9d762931bac23db191287e2d5
twneale <twneale@gmail.com>
2014-01-17 16:12:00 -0500

    TN: Fix vote sessions

---
+++ scripts/tn_109fix/votefix.py
@@ -0,0 +1,31 @@
+import sys
+
+import pymongo
+
+try:
+    from billy.core import db
+except:
+    db = pymongo.MongoClient().fiftystates
+
+
+def main(state):
+
+    spec = dict(state=state)
+    for vote in db.votes.find(spec):
+        if vote['session'] in ('109', 108):
+            vote['session'] = '108'
+            print('fixing', vote['_id'])
+            db.votes.save(vote, w=1)
+
+
+
+if __name__ == "__main__":
+    main('tn')
+
+
+
+
+
+
+
+
