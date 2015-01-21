7fa8c3faabc98c14357e28f3e00be25584c59b7b
Miles Watkins <miles.w.watkins@gmail.com>
2015-01-21 17:42:51 -0500

    AK: Rewrote legislator scraper for new site style

--- openstates/ak/legislators.py
+++ openstates/ak/legislators.py
@@ -1,109 +1,90 @@
 import re
 
-from billy.scrape import NoDataForPeriod
 from billy.scrape.legislators import LegislatorScraper, Legislator
+from openstates.utils import LXMLMixin
 
 import lxml.html
 
 
-class AKLegislatorScraper(LegislatorScraper):
+class AKLegislatorScraper(LegislatorScraper, LXMLMixin):
     jurisdiction = 'ak'
     latest_only = True
 
     def scrape(self, chamber, term):
         if chamber == 'upper':
-            chamber_abbr = 'S'
             url = 'http://senate.legis.state.ak.us/'
-            search = 'senator'
         else:
-            chamber_abbr = 'H'
             url = 'http://house.legis.state.ak.us/'
-            search = 'rep'
+        page = self.lxmlize(url)
+        for link in page.xpath('//ul[@class="item lists"]/li/a/@href'):
+            self.scrape_legislator(chamber, term, link)
 
-        page = self.urlopen(url)
-        page = lxml.html.fromstring(page)
-        page.make_links_absolute(url)
+    def scrape_legislator(self, chamber, term, url):
+        page = self.lxmlize(url)
 
-        seen = set()
-        for link in page.xpath("//a[contains(@href, '%s')]" % search):
-            name = link.text_content()
-            # Members of the leadership are linked twice three times:
-            # one image link and two text links. Don't double/triple
-            # scrape them
-            if not name or link.attrib['href'] in seen:
-                continue
-            seen.add(link.attrib['href'])
+        (_title, name) = page.xpath(
+                '//div[@class="holder-legislator"]/h1/text()')
+        (photo_url, ) = page.xpath('//div[@class="bioleft"]/img/@src')
+        
+        bio = page.xpath('//div[@class="bioright"]/a/..//text()')
+        bio = {
+                x.split(':')[0].strip(): x.split(':')[1].strip()
+                for x in bio
+                if x.strip()
+                }
+        email = bio['Email']
+        district = bio['District']
+        party = bio['Party']
+        if party == "Democrat":
+            party = "Democratic"
 
-            self.scrape_legislator(chamber, term,
-                                   link.xpath('string()').strip(),
-                                   link.attrib['href'])
-
-    def scrape_address(self, leg, doc, id):
-        text = doc.xpath('//div[@id="{0}"]'.format(id))[0].text_content()
-        address = ''
-        for line in text.splitlines():
-            if line == 'Session Contact':
-                addr_type = 'capitol'
-                addr_name = 'Session Office'
-            elif line == 'Interim Contact':
-                addr_type = 'district'
-                addr_name = 'Interim Office'
-            elif line.startswith('Phone:'):
-                phone = line.split(': ')[-1]
-            elif line.startswith('Fax:'):
-                fax = line.split(': ')[-1]
-            elif line:
-                address += (line + '\n')
-
-        kwargs = {}
-        if fax:
-            kwargs['fax'] = fax
-        if phone:
-            kwargs['phone'] = phone
-
-        if address.strip() != ',':
-            leg.add_office(addr_type, addr_name, address=address.strip(),
-                           **kwargs)
-
-
-    def scrape_legislator(self, chamber, term, name, url):
-        page = self.urlopen(url)
-        page = lxml.html.fromstring(page)
-
-        name = re.sub(r'\s+', ' ', name)
-
-        info = page.xpath('string(//div[@id = "fullpage"])')
-
-        district = re.search(r'District: ([\w\d]+)', info)
-        if district is None:
-            maddr = page.xpath("//div[@id='fullpage']//a[contains(@href, 'mailto')]")
-            if maddr == []:
-                return   # Needed for http://senate.legis.state.ak.us/senator.php?id=cog ..
-            maddr = maddr[0]
-            district = maddr.getnext().tail
-            # This hack needed for http://house.legis.state.ak.us/rep.php?id=dru
-            # please remove as soon as this is alive.
-        else:
-            district = district.group(1)
-
-        party = re.search(r'Party: (.+)', info).group(1).strip()
-        email = re.search(r'Email: ([\w_]+@legis\.state\.ak\.us)',
-                          info)
-
-        if email is None:
-            email = re.search(r'Email: (.+@akleg\.gov)',
-                              info)
-
-        email = email.group(1)
-
-        # for consistency
-        if party == 'Democrat':
-            party = 'Democratic'
-
-        leg = Legislator(term, chamber, district, name, party=party,
-                         email=email, url=url)
-        self.scrape_address(leg, page, 'bioleft')
-        self.scrape_address(leg, page, 'bioright')
+        leg = Legislator(
+                term=term,
+                chamber=chamber,
+                district=district,
+                full_name=name,
+                party=party,
+                photo_url=photo_url
+                )
         leg.add_source(url)
+
+        capitol_office = [
+                x.strip()
+                for x in page.xpath('//div[@class="bioleft"]//text()')
+                if x.strip()
+                ]
+        assert capitol_office[0] == "Session Contact"
+        assert capitol_office[3].startswith("Phone:")
+        assert capitol_office[4].startswith("Fax:")
+        leg.add_office(
+                type='capitol',
+                name="Capitol Office",
+                address=capitol_office[1] + '\n' + capitol_office[2],
+                phone=capitol_office[3][len("Phone: "): ],
+                fax=capitol_office[4][len("Fax: "): ],
+                email=email
+                )
+
+        district_office = [
+                x.strip()
+                for x in page.xpath('//div[@class="bioright"][2]//text()')
+                if x.strip()
+                ]
+        # Some members don't have district offices listed, so skip them
+        if any("AK" in x for x in district_office):
+            assert district_office[0] == "Interim Contact"
+            assert district_office[3].startswith("Phone:")
+            assert district_office[4].startswith("Fax:")
+            leg.add_office(
+                    type='district',
+                    name="District Office",
+                    address=district_office[1] + '\n' + district_office[2],
+                    phone=district_office[3][len("Phone: "): ] if
+                            len(district_office[3]) > len("Phone:") else
+                            None,
+                    fax=district_office[4][len("Fax: "): ] if
+                            len(district_office[4]) > len("Fax:") else
+                            None
+                    )
 
         self.save_legislator(leg)
