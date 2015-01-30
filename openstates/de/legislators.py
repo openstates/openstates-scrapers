df71d902cd58cccd647be017bef2af952ee7560f
Rachel Shorey <rshorey@gmail.com>
2015-01-30 11:59:57 -0500

    more narrowly targeted fix to empty legislator rows

--- openstates/de/legislators.py
+++ openstates/de/legislators.py
@@ -34,7 +34,8 @@ class DELegislatorScraper(LegislatorScraper):
 
             bio_url = tr.xpath('descendant::a/@href')[0]
             name, _, district = map(text, tr.xpath("td"))
-            if name.strip() in ["","."]:
+            print name
+            if name.strip() == ".":
                 continue
             name = ' '.join(re_spaces.split(name))
 
