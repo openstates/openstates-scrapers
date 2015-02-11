b7408c88a96eb957937e6b03a53bfa7daf67f8ea
Miles Watkins <miles.w.watkins@gmail.com>
2015-02-11 17:31:58 -0500

    NE: Fixed version and document URLs

--- openstates/ne/bills.py
+++ openstates/ne/bills.py
@@ -1,9 +1,10 @@
 from billy.scrape.bills import BillScraper, Bill
+from openstates.utils import LXMLMixin
 from datetime import datetime
 import lxml.html
 import urllib
 
-class NEBillScraper(BillScraper):
+class NEBillScraper(BillScraper, LXMLMixin):
     jurisdiction = 'ne'
 
     def scrape(self, session, chambers):
@@ -15,8 +16,7 @@ class NEBillScraper(BillScraper):
 
     def scrape_year(self, session, year):
         main_url = 'http://nebraskalegislature.gov/bills/search_by_date.php?SessionDay=%s' % year
-        page = self.urlopen(main_url)
-        page = lxml.html.fromstring(page)
+        page = self.lxmlize(main_url)
 
         for docs in page.xpath('//div[@class="cal_content_full"]/table[@id="bill_results"]/tr/td[1]/a'):
             bill_abbr = docs.text
@@ -35,6 +35,7 @@ class NEBillScraper(BillScraper):
     def bill_info(self, bill_link, session, main_url, bill_page):
 
         bill_page = lxml.html.fromstring(bill_page)
+        bill_page.make_links_absolute(bill_link)
 
         #basic info
         try:
@@ -85,34 +86,22 @@ class NEBillScraper(BillScraper):
         bill['actions'].reverse()
 
         #versions
-        for versions in bill_page.xpath('//div[@id="content_text"]/div[2]/table/tr[2]/td[2]/a'):
-            version_url = versions.attrib['href']
-            version_url = 'http://nebraskalegislature.gov/' + version_url[3:len(version_url)]
-            version_name = versions.text
+        for version in bill_page.xpath('//div[@id="content_text"]/div[2]/table/tr[2]/td[2]/a'):
+            version_url = version.attrib['href']
+            version_name = version.text
             # replace Current w/ session number
             version_url = version_url.replace('Current', session)
             bill.add_version(version_name, version_url,
                              mimetype='application/pdf')
 
-
-        #documents
-        # this appear to be same as versions, dropped for now
-        #for additional_info in bill_page.xpath('//div[@id="content_text"]/div[2]/table/tr[2]/td/a'):
-        #    document_name = additional_info.text
-        #    document_url = additional_info.attrib['href']
-        #    document_url = 'http://nebraskalegislature.gov/' + document_url[3:len(document_url)]
-        #    if '.pdf' in document_url:
-        #        bill.add_document(document_name, document_url)
-
         #amendments
-        for admendments in bill_page.xpath('//div[@id="content_text"]/div[3]/table/tr[1]/td[2]/table/tr/td/a'):
-            admendment_name = admendments.text
-            admendment_url = admendments.attrib['href']
-            admendment_url = 'http://nebraskalegislature.gov/' + admendment_url[3:len(admendment_url)]
-            bill.add_document(admendment_name, admendment_url)
+        for amendment in bill_page.xpath('//h2[text()="Amendments"]/following-sibling::table[1]//a'):
+            amendment_name = amendment.text
+            amendment_url = amendment.attrib['href']
+            bill.add_document(amendment_name, amendment_url)
 
         #related transcripts
-        for transcripts in bill_page.xpath('//div[@id="content_text"]/div[3]/table/tr[2]/td[2]/a'):
+        for transcripts in bill_page.xpath('//h2[text()="Related Transcripts"]/following-sibling::table[1]//a'):
             transcript_name = transcripts.text
             transcript_url = transcripts.attrib['href']
             bill.add_document(transcript_name, transcript_url)
