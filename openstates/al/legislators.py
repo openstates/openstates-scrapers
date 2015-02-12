2d8164ec18e27ed9ccd720331add5e74861fd731
Rachel Shorey <rshorey@gmail.com>
2015-02-12 14:49:25 -0500

    senators scraping

--- openstates/al/legislators.py
+++ openstates/al/legislators.py
@@ -24,8 +24,7 @@ class ALLegislatorScraper(LegislatorScraper):
 
         #basically, here goes nothing.
 
-        party_dict = {'(D)': 'Democratic', '(R)': 'Republican', 
-                          '(I)': 'Independent'}
+        
 
         urls = {
             "upper":"http://www.legislature.state.al.us/aliswww/SenatorsPicture.aspx",
@@ -36,6 +35,8 @@ class ALLegislatorScraper(LegislatorScraper):
         if chamber == "lower":
             sponsor_ids = self.get_sponsor_ids()
             self.scrape_rep_info(urls["lower"],sponsor_ids,term)
+        else:
+            self.scrape_senate_info(urls["upper"], term)
 
 
 
@@ -58,6 +59,8 @@ class ALLegislatorScraper(LegislatorScraper):
         return name_to_sponsor
 
     def scrape_rep_info(self, url, sponsor_ids, term):
+
+        #get reps
         html = self.urlopen(url)
         page = lxml.html.fromstring(html)
         reps = page.xpath("//table[contains(@id,'HseMainContent_tabByName_TabPanel')]//tr")
@@ -72,7 +75,6 @@ class ALLegislatorScraper(LegislatorScraper):
             rep_name = rep_name.strip()
 
             #match rep to sponsor_id if possible
-
             ln,fn = rep_name.split(",")
             last_fi_key = "{ln} ({fi})".format(ln=ln.strip(), fi=fn.strip()[0])
             leg = Legislator(term,
@@ -108,22 +110,96 @@ class ALLegislatorScraper(LegislatorScraper):
             rep_page = lxml.html.fromstring(rep_html)
 
             leg["photo_url"] = rep_page.xpath("//input[contains(@id,'imgLEG')]/@src")[0]
-            rows = rep_page.xpath("//table[contains(@id,'TabCommittees')]//tr")
-            for row in rows:
-                tds = [r.text_content() for r in row.xpath(".//td")]
-                if len(tds) == 0:
-                    continue
-                role = "member"
-                comm_name = tds[1].lower().strip()
-                if tds[2].strip().lower() in ["chairperson", "ranking minority member", "vice chairperson"]:
-                    role = tds[2].strip().lower()
-                leg.add_role('committee member',
-                            term=term,
-                            chamber="lower",
-                            committee=comm_name,
-                            position=role)
+            self.add_committees(rep_page,leg,"lower",term)
             leg.add_source(rep_sponsor_url)
             self.save_legislator(leg)
 
 
+
+    def scrape_senate_info(self, url, term):
+
+        party_dict = {'(D)': 'Democratic', '(R)': 'Republican', 
+                          '(I)': 'Independent'}
+
+        base_senator_url = "http://www.legislature.state.al.us/aliswww/Senator.aspx?OID_SPONSOR={sponsor_id}&OID_PERSON={person_id}&SESSNAME="
+        html = self.urlopen(url)
+        page = lxml.html.fromstring(html)
+        senators = page.xpath("//input[contains(@id,'SenMainContent_Img')]") + page.xpath("//input[contains(@id,'SenMainContent_img')]")
+        for senator in senators:
+            person_id = senator.attrib["alt"]
+            sponsor_id = senator.attrib["longdesc"]
+            senator_link = base_senator_url.format(person_id = person_id, sponsor_id = sponsor_id)
+            sen_html = self.urlopen(senator_link)
+            sen_page = lxml.html.fromstring(sen_html)
+            photo_url = sen_page.xpath("//input[contains(@id,'imgLEG')]/@src")[0]
+            sen_name = sen_page.xpath("//span[@id = 'SenMainContent_lblMember']")[0].text_content().replace("SENATOR","").strip()
+            sen_trs = sen_page.xpath("//table[@id='SenMainContent_TabSenator_TabPanel1_gvLEG']//tr")
+            district_trs = sen_page.xpath("//table[@id='SenMainContent_TabSenator_TabBIO_gvBIO']//tr")
+            info_dict = {}
+            for tr in sen_trs:
+                key,value = tr.xpath(".//td")
+                info_dict[key.text_content().replace(":","").lower().strip()] = value.text_content().strip()
+            for tr in district_trs:
+                key,value = tr.xpath(".//td")
+                info_dict[key.text_content().replace(":","").lower().strip()+" district"] = value.text_content().strip()
+
+            leg = Legislator(term,
+                            "upper",
+                            info_dict["district"].replace("Senate District","").strip(),
+                            sen_name,
+                            party = party_dict[info_dict["affiliation"].strip()]
+                            )
+
+            address_parts = ["street","office","city","state","postal code"]
+            cap_address = [info_dict[a].strip() for a in address_parts if info_dict[a].strip() != ""]
+            dist_address = [info_dict[a+" district"].strip() for a in address_parts if info_dict[a+" district"].strip() != ""]
+
+
+            for k,v in info_dict.items():
+                if v.strip() == "":
+                    info_dict[k] = None
+                else:
+                    info_dict[k] = v.strip()
+                if k == "fax number": #sometimes use "fax", sometimes "fax number"
+                    info_dict["fax"] = info_dict[k]
+
+
+            leg.add_office('capitol',
+                            'Capitol Office',
+                            address=" ".join(cap_address),
+                            phone=info_dict["phone number"],
+                            email=info_dict["email"],
+                            fax=info_dict["fax"])
+
+            if len(dist_address) > 0:
+                leg.add_office('district',
+                                "District Office",
+                                address=" ".join(dist_address),
+                                phone=info_dict["district phone district"],
+                                fax=info_dict["district fax district"])
+
+            leg.add_source(url)
+            leg.add_source(senator_link)
+
+            self.add_committees(sen_page,leg,"upper",term)
+
+            self.save_legislator(leg)
+
+
+    def add_committees(self,leg_page,leg,chamber,term):
+        rows = leg_page.xpath("//table[contains(@id,'TabCommittees')]//tr")
+        for row in rows:
+            tds = [r.text_content() for r in row.xpath(".//td")]
+            if len(tds) == 0:
+                continue
+            role = "member"
+            comm_name = tds[1].lower().strip()
+            if tds[2].strip().lower() in ["chairperson", "ranking minority member", "vice chairperson"]:
+                role = tds[2].strip().lower()
+            
+            leg.add_role('committee member',
+                        term=term,
+                        chamber=chamber,
+                        committee=comm_name,
+                        position=role)
 
