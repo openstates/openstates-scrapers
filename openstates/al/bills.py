22bff1610b737cb893de7d36a51c6abb78655ea8
Miles Watkins <miles.w.watkins@gmail.com>
2015-02-25 15:13:31 -0500

    AL: Sessioning working intermittently for bill pages, but basic scraper functional

--- openstates/al/bills.py
+++ openstates/al/bills.py
@@ -6,6 +6,7 @@ import datetime
 import lxml.html
 import requests
 
+
 _action_re = (
     ('Introduced', 'bill:introduced'),
     ('(Forwarded|Delivered) to Governor', 'governor:received'),
@@ -44,53 +45,74 @@ class ALBillScraper(BillScraper):
     CHAMBERS = {'H': 'lower', 'S': 'upper'}
     DATE_FORMAT = '%m/%d/%Y'
 
-    def _refresh_session(self):
-        '''
-        The LIS uses ASP.NET, and this requires us to frequently
-        refresh our cookie. The cookie contains a session ID, and
-        this must be included in all requests in this scraper.
-        '''
+    # def _refresh_session(self):
+    #     '''
+    #     The LIS uses ASP.NET, and requires us to frequently refresh
+    #     our cookie. The cookie contains a session ID, and must be
+    #     included in all requests in this scraper.
+    #     '''
 
-        url = 'http://alisondb.legislature.state.al.us/Alison/ALISONLogin.aspx?SESSIONOID={}'.format(self.session_id)
-        form = {
-                '__EVENTTARGET': 'ctl01$cboSession',
-                # '__VIEWSTATEGENERATOR': 'BC7CB5E3',
-                # '__EVENTARGUMENT': '',
-                # '__LASTFOCUS': '',
-                # '__VIEWSTATE': '/wEPDwUKMTY1NDU2MTA1Mg9kFgJmD2QWAgIDD2QWDgIDDxBkEBUmFFJlZ3VsYXIgU2Vzc2lvbiAyMDE1G09yZ2FuaXphdGlvbmFsIFNlc3Npb24gMjAxNRRSZWd1bGFyIFNlc3Npb24gMjAxNBRSZWd1bGFyIFNlc3Npb24gMjAxMxpGaXJzdCBTcGVjaWFsIFNlc3Npb24gMjAxMhRSZWd1bGFyIFNlc3Npb24gMjAxMhRSZWd1bGFyIFNlc3Npb24gMjAxMRpGaXJzdCBTcGVjaWFsIFNlc3Npb24gMjAxMBpGaXJzdCBTcGVjaWFsIFNlc3Npb24gMjAwORRSZWd1bGFyIFNlc3Npb24gMjAxMBRSZWd1bGFyIFNlc3Npb24gMjAwORpGaXJzdCBTcGVjaWFsIFNlc3Npb24gMjAwOBRSZWd1bGFyIFNlc3Npb24gMjAwOBpGaXJzdCBTcGVjaWFsIFNlc3Npb24gMjAwNxRSZWd1bGFyIFNlc3Npb24gMjAwNxtPcmdhbml6YXRpb25hbCBTZXNzaW9uIDIwMDcUUmVndWxhciBTZXNzaW9uIDIwMDYaRmlyc3QgU3BlY2lhbCBTZXNzaW9uIDIwMDUUUmVndWxhciBTZXNzaW9uIDIwMDUaRmlyc3QgU3BlY2lhbCBTZXNzaW9uIDIwMDQUUmVndWxhciBTZXNzaW9uIDIwMDQbU2Vjb25kIFNwZWNpYWwgU2Vzc2lvbiAyMDAzGkZpcnN0IFNwZWNpYWwgU2Vzc2lvbiAyMDAzFFJlZ3VsYXIgU2Vzc2lvbiAyMDAzG09yZ2FuaXphdGlvbmFsIFNlc3Npb24gMjAwMxRSZWd1bGFyIFNlc3Npb24gMjAwMhtGb3VydGggU3BlY2lhbCBTZXNzaW9uIDIwMDEaVGhpcmQgU3BlY2lhbCBTZXNzaW9uIDIwMDEbU2Vjb25kIFNwZWNpYWwgU2Vzc2lvbiAyMDAxGkZpcnN0IFNwZWNpYWwgU2Vzc2lvbiAyMDAxFFJlZ3VsYXIgU2Vzc2lvbiAyMDAxFFJlZ3VsYXIgU2Vzc2lvbiAyMDAwG1NlY29uZCBTcGVjaWFsIFNlc3Npb24gMTk5ORtPcmdhbml6YXRpb25hbCBTZXNzaW9uIDIwMTEaRmlyc3QgU3BlY2lhbCBTZXNzaW9uIDE5OTkUUmVndWxhciBTZXNzaW9uIDE5OTkbT3JnYW5pemF0aW9uYWwgU2Vzc2lvbiAxOTk5FFJlZ3VsYXIgU2Vzc2lvbiAxOTk4FSYUUmVndWxhciBTZXNzaW9uIDIwMTUbT3JnYW5pemF0aW9uYWwgU2Vzc2lvbiAyMDE1FFJlZ3VsYXIgU2Vzc2lvbiAyMDE0FFJlZ3VsYXIgU2Vzc2lvbiAyMDEzGkZpcnN0IFNwZWNpYWwgU2Vzc2lvbiAyMDEyFFJlZ3VsYXIgU2Vzc2lvbiAyMDEyFFJlZ3VsYXIgU2Vzc2lvbiAyMDExGkZpcnN0IFNwZWNpYWwgU2Vzc2lvbiAyMDEwGkZpcnN0IFNwZWNpYWwgU2Vzc2lvbiAyMDA5FFJlZ3VsYXIgU2Vzc2lvbiAyMDEwFFJlZ3VsYXIgU2Vzc2lvbiAyMDA5GkZpcnN0IFNwZWNpYWwgU2Vzc2lvbiAyMDA4FFJlZ3VsYXIgU2Vzc2lvbiAyMDA4GkZpcnN0IFNwZWNpYWwgU2Vzc2lvbiAyMDA3FFJlZ3VsYXIgU2Vzc2lvbiAyMDA3G09yZ2FuaXphdGlvbmFsIFNlc3Npb24gMjAwNxRSZWd1bGFyIFNlc3Npb24gMjAwNhpGaXJzdCBTcGVjaWFsIFNlc3Npb24gMjAwNRRSZWd1bGFyIFNlc3Npb24gMjAwNRpGaXJzdCBTcGVjaWFsIFNlc3Npb24gMjAwNBRSZWd1bGFyIFNlc3Npb24gMjAwNBtTZWNvbmQgU3BlY2lhbCBTZXNzaW9uIDIwMDMaRmlyc3QgU3BlY2lhbCBTZXNzaW9uIDIwMDMUUmVndWxhciBTZXNzaW9uIDIwMDMbT3JnYW5pemF0aW9uYWwgU2Vzc2lvbiAyMDAzFFJlZ3VsYXIgU2Vzc2lvbiAyMDAyG0ZvdXJ0aCBTcGVjaWFsIFNlc3Npb24gMjAwMRpUaGlyZCBTcGVjaWFsIFNlc3Npb24gMjAwMRtTZWNvbmQgU3BlY2lhbCBTZXNzaW9uIDIwMDEaRmlyc3QgU3BlY2lhbCBTZXNzaW9uIDIwMDEUUmVndWxhciBTZXNzaW9uIDIwMDEUUmVndWxhciBTZXNzaW9uIDIwMDAbU2Vjb25kIFNwZWNpYWwgU2Vzc2lvbiAxOTk5G09yZ2FuaXphdGlvbmFsIFNlc3Npb24gMjAxMRpGaXJzdCBTcGVjaWFsIFNlc3Npb24gMTk5ORRSZWd1bGFyIFNlc3Npb24gMTk5ORtPcmdhbml6YXRpb25hbCBTZXNzaW9uIDE5OTkUUmVndWxhciBTZXNzaW9uIDE5OTgUKwMmZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2cWAWZkAgUPDxYCHgRUZXh0BTJIb3VzZSBDb252ZW5lOiZuYnNwOyAmbmJzcDswMy8wMy8yMDE1ICAgMTI6MDAgTm9vbmRkAgcPDxYCHwAFHVNlbmF0ZSBDb252ZW5lOiAwMy8wMy8yMDE1ICAgZGQCCQ8PFgIfAGVkZAILDw8WAh8AZWRkAg8PDxYCHwBlZGQCEQ88KwANAQAPFgIeCEl0ZW1XcmFwaGRkGAIFHl9fQ29udHJvbHNSZXF1aXJlUG9zdEJhY2tLZXlfXxYDBRJjdGwwMCRpbWdUeHRTZWFyY2gFF2N0bDAwJGltZ0ZpbmRMZWdpc2xhdG9yBRNjdGwwMCRpbWdJbnN0cnVtZW50BQ1jdGwwMCRUb3BNZW51Dw9kBRNTZXNzaW9uIEluZm9ybWF0aW9uZOOaG9EGekZlaxN8IIfx9JZl7aZamKEveTZcLCEm1Zl1',
-                # 'ctl01$cboSession': 'First Special Session 2009'
-                'ctl01$cboSession': self.metadata['session_details'][self.session]['display_name']
-                }
-        self.post(url, data=form)
-        # print(self.cookies)
 
     def scrape(self, session, chambers):
+        # Due to a scrapelib bug, a requests Session is necessary to maintain cookies
+        self.s = requests.Session()
+
         self.session = session
-        self.session_id = self.metadata['session_details'][session]['internal_id']
-        self.base_doc_url = 'http://alisondb.legislature.state.al.us/ALISON/SearchableInstruments/%s/PrintFiles/' % session
+        self.session_name = self.metadata['session_details'][self.session]['_scraped_name']
+        self.session_id = self.metadata['session_details'][self.session]['internal_id']
 
-        res_url = 'http://alisondb.legislature.state.al.us/Alison/SESSResList.aspx?STATUSCODES=Had%20First%20Reading%20House%20of%20Origin&BODY=999999'
-        self.scrape_for_bill_type(session, res_url)
+        SESSION_SET_URL = 'http://alisondb.legislature.state.al.us/Alison/ALISONLogin.aspx'
+        RESOLUTION_TYPE_URL = ''
+        RESOLUTION_LIST_URL = 'http://alisondb.legislature.state.al.us/Alison/SESSResList.aspx?STATUSCODES=Had%20First%20Reading%20House%20of%20Origin&BODY=999999'
+        BILL_TYPE_URL = 'http://alisondb.legislature.state.al.us/Alison/SESSBillsBySelectedStatus.aspx'
+        BILL_LIST_URL = 'http://alisondb.legislature.state.al.us/Alison/SESSBillsList.aspx?STATUSCODES=Had%20First%20Reading%20House%20of%20Origin&BODY=999999'
 
-        bill_url = 'http://alisondb.legislature.state.al.us/Alison/SESSBillsList.aspx?STATUSCODES=Had%20First%20Reading%20House%20of%20Origin&BODY=999999'
-        self.scrape_for_bill_type(session, bill_url)
+        # res_url = 'http://alisondb.legislature.state.al.us/Alison/SESSResList.aspx?STATUSCODES=Had%20First%20Reading%20House%20of%20Origin&BODY=999999'
+        # self.scrape_for_bill_type(session, res_url)
 
-    def scrape_for_bill_type(self, session, url):
-        self._refresh_session()
+        # Activate an ASP.NET session, and set the legislative session
+        doc = lxml.html.fromstring(self.s.get(SESSION_SET_URL).text)
+        (viewstate, ) = doc.xpath('//input[@id="__VIEWSTATE"]/@value')
+        (viewstategenerator, ) = doc.xpath('//input[@id="__VIEWSTATEGENERATOR"]/@value')
+        form = {
+                '__EVENTTARGET': 'ctl00$cboSession',
+                '__EVENTARGUMENT': '',
+                '__LASTFOCUS': '',
+                '__VIEWSTATE': viewstate,
+                '__VIEWSTATEGENERATOR': viewstategenerator,
+                'ctl00$cboSession': self.session_name
+                }
+        self.s.post(SESSION_SET_URL, data=form, allow_redirects=False)
 
-        html = self.get(url).text
+        # With the ASP.NET session activated, acquire a list of bills
+        doc = lxml.html.fromstring(self.s.get(BILL_TYPE_URL).text)
+        (viewstate, ) = doc.xpath('//input[@id="__VIEWSTATE"]/@value')
+        (viewstategenerator, ) = doc.xpath('//input[@id="__VIEWSTATEGENERATOR"]/@value')
+        form = {
+                '__EVENTTARGET': 'ctl00$MainDefaultContent$gvStatus$ctl02$ctl00',
+                '__EVENTARGUMENT': 'Select$0',
+                '__LASTFOCUS': '',
+                '__VIEWSTATE': viewstate,
+                '__VIEWSTATEGENERATOR': viewstategenerator,
+                'ctl00$cboSession': self.session_name,
+                'ctl00$ScriptManager1': 'ctl00$UpdatePanel1|ctl00$MainDefaultContent$gvStatus$ctl02$ctl00'
+        }
+        self.s.post(BILL_TYPE_URL, data=form, allow_redirects=False)
+
+        self.scrape_for_bill_type(BILL_LIST_URL)
+
+    def scrape_for_bill_type(self, url):
+        html = self.s.get(url).text
         doc = lxml.html.fromstring(html)
-
-        bills = doc.xpath('//table[@class="box_billstatusresults"]/tr')
+        bills = doc.xpath('//table[@class="box_billstatusresults"]/tr')[1: ]
         for bill_info in bills:
-            self._refresh_session()
 
-            (bill_id, ) = bill_info.xpath(
-                    'td[@class="CellSpace_bill"]/input/@value')
-            (sponsor_name, ) = bill_info.xpath(
-                    'td[@class="CellSpace"]/input/@value')
-            subject = bill_info.xpath('td[3]/text()')[0].strip()
-            description = bill_info.xpath('td[4]/text()')[0].strip()
+            (bill_id, ) = bill_info.xpath('td[1]/font/input/@value')
+            (sponsor, ) = bill_info.xpath('td[2]/font/input/@value')
+            subject = bill_info.xpath('td[3]/font/text()')[0].strip()
+            description = bill_info.xpath('td[4]/font/text()')[0].strip()
+            if not description:
+                description = "[No title given]"
 
             chamber = self.CHAMBERS[bill_id[0]]
             
@@ -105,7 +127,7 @@ class ALBillScraper(BillScraper):
                         "Unknown bill type for bill '{}'".format(bill_id))
 
             bill = Bill(
-                    session=session,
+                    session=self.session,
                     chamber=chamber,
                     bill_id=bill_id,
                     title=description,
@@ -119,10 +141,12 @@ class ALBillScraper(BillScraper):
 
             bill_url = 'http://alisondb.legislature.state.al.us/Alison/SESSBillResult.aspx?BILL={}'.format(bill_id)
             bill.add_source(bill_url)
-            bill_html = self.get(bill_url).text
+            bill_html = self.s.get(bill_url).text
             bill_doc = lxml.html.fromstring(bill_html)
+            print(bill_html)
+            assert False
 
-            version_url_base = 'http://alisondb.legislature.state.al.us/ALISON/SearchableInstruments/{0}/PrintFiles/{1}-'.format(session, bill_id)
+            version_url_base = 'http://alisondb.legislature.state.al.us/ALISON/SearchableInstruments/{0}/PrintFiles/{1}-'.format(self.session, bill_id)
             versions = bill_doc.xpath('//table[@class="box_versions"]/tr/td[2]/text()')
             for version in versions:
                 name = version
@@ -150,13 +174,24 @@ class ALBillScraper(BillScraper):
             birs = bill_doc.xpath('//div[@class="box_bir"]//table//table/tr')[1: ]
             for bir in birs:
                 bir_date = datetime.datetime.strptime(
-                        bir.xpath('td[2]/text()')[0], DATE_FORMAT)
-                bir_type = bir.xpath('td[2]/text()')[0].split(" ")[0]
-                bir_chamber = self.CHAMBERS[bir_type[0]]
+                        bir.xpath('td[2]/font/text()')[0], self.DATE_FORMAT)
+                bir_type = bir.xpath('td[1]/font/text()')[0].split(" ")[0][0]
+                bir_chamber = self.CHAMBERS[bir_type]
                 bir_text = "{0}: {1}".format(
-                        bir_type, bir.xpath('td[3]/text()')[0])
+                        bir_type, bir.xpath('td[3]/font/text()')[0])
 
-                (bir_vote_id, ) = bir.xpath('td[4]/input/@value')
+                bill.add_action(
+                        actor=bir_chamber,
+                        action=bir_text,
+                        date=bir_date,
+                        type="other"
+                        )
+
+                try:
+                    (bir_vote_id, ) = bir.xpath('td[4]/font/input/@value')
+                except ValueError:
+                    bir_vote_id = ''
+
                 if bir_vote_id.startswith("Roll "):
                     bir_vote_id = bir_vote_id.split(" ")[-1]
                     self.scrape_vote(
@@ -168,29 +203,32 @@ class ALBillScraper(BillScraper):
                             action_text=bir_text
                             )
 
-            action_header = actions = bill_doc.xpath('//table[@class="box_history"]/tr[1]/text()')
-            assert action_header == ["Calendar Date", "Body", "Amd/Sub", "Matter", "Committee", "Nay", "Yea", "Abstain", "Vote"]
+            # action_header = bill_doc.xpath('//table[@class="box_history"]/tr[1]/text()')
+            # assert action_header == ["Calendar Date", "Body", "Amd/Sub", "Matter", "Committee", "Nay", "Yea", "Abstain", "Vote"]
             actions = bill_doc.xpath('//table[@class="box_history"]/tr')[1: ]
             action_date = None
             for action in actions:
-
+                print('foo')
                 # If actions occur on the same day, only the first will list date
-                if action.xpath('td[1]/text()')[0].strip():
+                if action.xpath('td[1]/font/text()')[0].strip():
                     action_date = datetime.datetime.strptime(
-                            action.xpath('td[1]/text()')[0], DATE_FORMAT)
+                            action.xpath('td[1]/font/text()')[0], self.DATE_FORMAT)
 
-                (action_chamber, ) = action.xpath('td[2]/text()')
-                (action_committee, ) = action.xpath('td[5]/text()')
-                if action_committee.strip():
-                    # The committee cell is just an abbreviation, so get its full name
+                (action_chamber, ) = action.xpath('td[2]/font/text()')
+                (action_committee, ) = action.xpath('td[5]/font/text()')
+
+                # The committee cell is just an abbreviation, so get its full name
+                try:
                     action_committee = re.search(
-                            r'.*? referred to the (.*? committee on .*?)$').group(1)
+                            r'.*? referred to the (.*? committee on .*?)$',
+                            action_committee
+                            ).group(1)
                     actor = action_committee
-                else:
+                except AttributeError:
                     actor = self.CHAMBERS[action_chamber]
 
-                (action_text, ) = action.xpath('td[4]/text()')
-                action_type = self._categorize_action(action_text)
+                (action_text, ) = action.xpath('td[4]/font/text()')
+                action_type = _categorize_action(action_text)
 
                 bill.add_action(
                         actor=actor,
@@ -200,7 +238,10 @@ class ALBillScraper(BillScraper):
                         committees=action_committee if action_committee.strip() else None
                         )
 
-                vote_button = action.xpath('td[9]/text()')[0].strip()
+                try:
+                    vote_button = action.xpath('td[9]/font/text()')[0].strip()
+                except:
+                    vote_button = None
                 if vote_button:
                     vote_id = vote_button.split(" ")[-1]
                     self.scrape_vote(
@@ -214,10 +255,10 @@ class ALBillScraper(BillScraper):
 
             self.save_bill(bill)
 
-    def scrape_vote(self, bill, vote_chamber, bill_id, vote_id, action_date, action_text):
+    def scrape_vote(self, bill, vote_chamber, bill_id, vote_id, vote_date, action_text):
         url = 'http://alisondb.legislature.state.al.us/Alison/GetRollCallVoteResults.aspx?VOTE={0}&BODY={1}&INST={2}&SESS={3}'.format(
                 vote_id, vote_chamber, bill_id, self.session_id)
-        doc = lxml.html.fromstring(self.get(url).text)
+        doc = lxml.html.fromstring(self.s.get(url).text)
 
         voters = {'Y': [], 'N': [], 'P': [], 'A': []}
 
@@ -240,7 +281,7 @@ class ALBillScraper(BillScraper):
         total_nay = len(voters['N'])
         total_other = len(voters['P']) + len(voters['A'])
 
-        vote = Vote(vote_chamber, action_date, action_text, total_yea > total_nay,
+        vote = Vote(vote_chamber, vote_date, action_text, total_yea > total_nay,
                     total_yea, total_nay, total_other)
         vote.add_source(url)
         for member in voters['Y']:
