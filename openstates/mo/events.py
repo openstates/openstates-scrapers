6789c17940477868cbfde8dd444c2ede57a2a2bc
Peter Cass <pfcass>
2015-02-26 15:45:37 -0500

    Added a skeleton for mo events.py.
    Does a basic get of the html and proof-of-concept for opening it and
    accessing it w/ xpath

--- openstates/mo/events.py
+++ openstates/mo/events.py
@@ -1,6 +1,8 @@
 import re
 import datetime
+import requests
 
+from lxml import html
 from openstates.utils import LXMLMixin
 from billy.scrape.events import EventScraper, Event
 
@@ -9,4 +11,25 @@ class MOEventScraper(EventScraper, LXMLMixin):
     jurisdiction = 'mo'
 
     def scrape(self, session, chambers):
+
+        # Read from file -- keeps the data constant for debugging
+        # purposes since the actual pages changes every day
+        #f = open('/Users/pfcass/Dev/openstates/events.html')
+        #page = f.read()
+        #doc = html.fromstring(page)
+
+        # Grab the page of meetings and convert to an html list document
+        page = requests.get('http://house.mo.gov/HearingsPrint.aspx?DateOrder=true')
+        doc = html.fromstring(page.text)
+
+        str = doc.xpath("/html/body/form[@id='form1']/span[@id='lblCmtInfo']/table[@id='cmtGroup'][3]/tr[1]/td")
+        print "0: {0}".format( str[0].text_content().strip() )
+        #print "1: {0}".format( str[1].text_content().strip() )
+        #print "2: {0}".format( str[2].text_content().strip() )
+        #print "3: {0}".format( str[3].text_content().strip() )
+
+        #event = Event()
+
+        #self.save_event(event)
+
         print("Scraping events for Missouri")
