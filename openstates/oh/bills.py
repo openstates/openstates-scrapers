34d721e6d717f5924f48398f875585f10a53e963
Rachel Shorey <rshorey@gmail.com>
2015-03-02 18:08:43 -0500

    initial stab at new ohio bills format

--- openstates/oh/bills.py
+++ openstates/oh/bills.py
@@ -13,27 +13,27 @@ import lxml.html
 class OHBillScraper(BillScraper):
     jurisdiction = 'oh'
 
-    def scrape(self, chamber, session):
+    def scrape(self, session, chambers):
         if int(session) < 128:
             raise AssertionError("No data for period {}".format(session))
 
-        base_url = 'http://www.lsc.state.oh.us/status%s/' % session
+        status_report_url = "http://www.legislature.ohio.gov/legislation/status-reports"
 
-        bill_types = {'lower': [('hb', 'bill'),
-                                ('hjr', 'joint resolution'),
-                                ('hcr', 'concurrent resolution')],
-                      'upper': [('sb', 'bill'),
-                                ('sjr', 'joint resolution'),
-                                ('scr', 'concurrent resolution')]}
+        #ssl verification off due to requests bug
+        doc = self.urlopen(status_report_url,verify=False)
+        doc = lxml.html.fromstring(doc)
+        doc.make_links_absolute(status_report_url)
 
-        for bill_prefix, bill_type in bill_types[chamber]:
-            url = base_url + '%s.xlsx' % bill_prefix
+        status_table = doc.xpath("//div[contains(text(),'{}')]/following-sibling::table".format(session))[0]
+        status_links = status_table.xpath(".//a[contains(text(),'Excel')]/@href")
+
+
+        for url in status_links:
 
             try:
                 fname, resp = self.urlretrieve(url)
             except scrapelib.HTTPError:
-                # if there haven't yet been any bills of a given type
-                # then the excel url for that type will 404
+                self.logger.warning("Missing report {}".format(report))
                 continue
 
             sh = xlrd.open_workbook(fname).sheet_by_index(0)
@@ -41,78 +41,172 @@ class OHBillScraper(BillScraper):
             # once workbook is open, we can remove tempfile
             os.remove(fname)
 
-            for rownum in range(1, sh.nrows):
-                bill_no = sh.cell(rownum, 0).value
-                if isinstance(bill_no, basestring):
-                    if "*" in bill_no:
-                        continue
+            if int(session) < 131:
+                #Lots has changed with the rollover to 131
+                #but seems silly to throw out old code when
+                #we can still get historical data this way
+                #note that a few links and stuff had to be updated
+                self.old_scrape(chamber,session,sh)
 
-                bill_no = int(bill_no)
-                bill_id = '%s %s' % (bill_prefix.upper(), bill_no)
-                bill_title = str(sh.cell(rownum, 3).value)
-                bill = Bill(session, chamber, bill_id, bill_title,
-                            type=bill_type)
-                bill.add_source(url)
-                bill.add_sponsor('primary', str(sh.cell(rownum, 1).value))
+            else:
+                bill_ids_found = []
+                self.new_bulk_scrape(chamber,session,sh,bill_ids_found,url)
+                if len(bill_ids_found) == 0:
+                    self.logger.warning("No bills found in bulk data, check that it's up?")
+                self.new_site_scrape(chamber,session,bill_ids_found)
 
-                # add cosponsor
-                if sh.cell(rownum, 2).value:
-                    bill.add_sponsor('cosponsor',
-                                     str(sh.cell(rownum, 2).value))
 
-                actor = ""
+    def new_bulk_scrape(self,session,sh,bill_ids_found,url):
+        #get what we can from the bulk data download
+        #they gave it to us, we should use it, even though
+        #it doesn't seem to be complete
+        for rownum in range(1, sh.nrows):
+            bill_id = sh.cell(rownum, 0).value.replace(" ","").replace(".","")
+            bill_type,bill_no = bill_id.split("No")
+            bill_id = bill_type+bill_no.zfill(4)
 
-                # Actions start column after bill title
-                for colnum in range(4, sh.ncols - 1):
-                    action = str(sh.cell(0, colnum).value)
-                    cell = sh.cell(rownum, colnum)
-                    date = cell.value
+            bill_type = "resolution" if "R" in bill_id else "bill"
+            chamber = "lower" if "H" in bill_id else "upper"
+            bill_title = sh.cell(rownum, 3).value.strip()
 
-                    if len(action) != 0:
-                        if action.split()[0] == 'House':
-                            actor = "lower"
-                        elif action.split()[0] == 'Senate':
-                            actor = "upper"
-                        elif action.split()[-1] == 'Governor':
-                            actor = "executive"
-                        elif action.split()[0] == 'Gov.':
-                            actor = "executive"
-                        elif action.split()[-1] == 'Gov.':
-                            actor = "executive"
 
-                    if action in ('House Intro. Date', 'Senate Intro. Date'):
-                        atype = ['bill:introduced']
-                        action = action.replace('Intro. Date', 'Introduced')
-                    elif action == '3rd Consideration':
-                        atype = ['bill:reading:3', 'bill:passed']
-                    elif action == 'Sent to Gov.':
-                        atype = ['governor:received']
-                    elif action == 'Signed By Governor':
-                        atype = ['governor:signed']
-                    else:
-                        atype = ['other']
 
-                    if type(date) == float:
-                        date = str(xlrd.xldate_as_tuple(date, 0))
-                        date = datetime.datetime.strptime(
-                            date, "(%Y, %m, %d, %H, %M, %S)")
-                        bill.add_action(actor, action, date, type=atype)
+            bill = Bill(session, chamber, bill_id, bill_title,
+                        type=bill_type,subjects=[])
 
-                self.scrape_votes(bill, bill_prefix, bill_no, session)
-                self.scrape_versions(bill, bill_prefix, bill_no, session)
-                self.save_bill(bill)
+            sponsor1 = sh.cell(rownum, 1).value.strip()
+            bill.add_sponsor("primary",sponsor1)
+            sponsor2 = sh.cell(rownum, 2).value.strip()
+            if sponsor2:
+                bill.add_sponsor("primary",sponsor2)
 
-    def scrape_versions(self, bill, prefix, number, session):
-        base_url = 'http://www.legislature.state.oh.us'
+            subjects = sh.cell(rownum,4).value.split(";")
+            for subject in subjects:
+                if subject.strip():
+                    bill["subjects"].append(subject.strip())
 
-        if 'r' in prefix:
-            piece = '/res.cfm?ID=%s_%s_%s' % (session, prefix.upper(),
-                                              number)
+            bill.add_source(url)
+
+            introduced_date = sh.cell(rownum,5).value.strip()
+            if introduced_date:
+                introduced_date = datetime.datetime.strptime(introduced_date,"%d/%m/%Y")
+                bill.add_action(chamber,"Introduced",introduced_date,type="bill:introduced")
+
+            refer_date = sh.cell(rownum,6).value.strip()
+            committee = sh.cell(rownum,7).value.strip()
+            if refer_date and committee:
+                refer_date = datetime.datetime.strptime(refer_date,"%d/%m/%Y")
+                bill.add_action(chamber,"Referred to committee",refer_date,type="committee:referred",committees=committee)
+
+            comm_rep_date = sh.cell(rownum,8).value.strip()
+            comm_report = sh.cell(rownum,9).value.strip()
+            if comm_rep_date and comm_report:
+                rep_date = datetime.datetime.strptime(comm_rep_date,"%d/%m/%Y")
+                committees = committee
+                if "recommends its re-referral" in report:
+                    act_type = ["committee:passed","committee:referred"]
+                    new_comm = sh.cell(rownum,11).value.strip()
+                    committees = [committee,new_comm]
+                #TODO: lots of possibilites here, get them all in.
+                elif "recommends its passage/adoption" in report:
+                    act_type = "committee:passed:favorable"
+                bill.add_action(chamber,"Committee Reports: '{}'".format(comm_report),rep_date,type="committee:referred",committees=[committee])
+
+
+            re_refer_date = 
+
+            self.save_bill(bill)
+            bill_ids_found.append(bill_id)
+
+
+    def new_site_scrape(self,session,bill_ids_found):
+        #bulk data was missing some stuff
+        #so get remaining data from website
+        #also it took a long time to get the bulk data up
+        pass
+
+
+
+    def old_scrape(self,session,sh,url):
+        
+        for rownum in range(1, sh.nrows):
+            bill_id = sh.cell(rownum, 0).value
+
+            bill_type = "resolution" if "R" in bill_id else "bill"
+            chamber = "lower" if "H" in bill_id else "upper"
+
+            bill_title = str(sh.cell(rownum, 3).value)
+
+            bill = Bill(session, chamber, bill_id, bill_title,
+                        type=bill_type)
+            bill.add_source(url)
+            bill.add_sponsor('primary', str(sh.cell(rownum, 1).value))
+
+            # add cosponsor
+            if sh.cell(rownum, 2).value:
+                bill.add_sponsor('cosponsor',
+                                 str(sh.cell(rownum, 2).value))
+
+            actor = ""
+
+            # Actions start column after bill title
+            for colnum in range(4, sh.ncols - 1):
+                action = str(sh.cell(0, colnum).value)
+                cell = sh.cell(rownum, colnum)
+                date = cell.value
+
+                if len(action) != 0:
+                    if action.split()[0] == 'House':
+                        actor = "lower"
+                    elif action.split()[0] == 'Senate':
+                        actor = "upper"
+                    elif action.split()[-1] == 'Governor':
+                        actor = "executive"
+                    elif action.split()[0] == 'Gov.':
+                        actor = "executive"
+                    elif action.split()[-1] == 'Gov.':
+                        actor = "executive"
+
+                if action in ('House Intro. Date', 'Senate Intro. Date'):
+                    atype = ['bill:introduced']
+                    action = action.replace('Intro. Date', 'Introduced')
+                elif action == '3rd Consideration':
+                    atype = ['bill:reading:3', 'bill:passed']
+                elif action == 'Sent to Gov.':
+                    atype = ['governor:received']
+                elif action == 'Signed By Governor':
+                    atype = ['governor:signed']
+                else:
+                    atype = ['other']
+
+                if type(date) == float:
+                    date = str(xlrd.xldate_as_tuple(date, 0))
+                    date = datetime.datetime.strptime(
+                        date, "(%Y, %m, %d, %H, %M, %S)")
+                    bill.add_action(actor, action, date, type=atype)
+
+            for idx,char in enumerate(bill_id):
+                try:
+                    int(char)
+                except ValueError:
+                    continue
+
+                underscore_bill = bill_id[:idx]+"_"+bill_id[idx:]
+                break
+
+            self.scrape_votes_old(bill, underscore_bill, session)
+            self.scrape_versions_old(bill, underscore_bill, session)
+            self.save_bill(bill)
+
+    def scrape_versions_old(self, bill, billname, session):
+        base_url = 'http://archives.legislature.state.oh.us/'
+
+        if 'R' in billname:
+            piece = '/res.cfm?ID=%s_%s' % (session, billname)
         else:
-            piece = '/bills.cfm?ID=%s_%s_%s' % (session, prefix.upper(),
-                                                number)
+            piece = '/bills.cfm?ID=%s_%s' % (session, billname)
 
-        def _get_html_or_pdf_version(url):
+        def _get_html_or_pdf_version_old(url):
             doc = lxml.html.fromstring(url)
             name = doc.xpath('//font[@size="2"]/a/text()')[0]
             html_links = doc.xpath('//a[text()="(.html format)"]')
@@ -132,18 +226,18 @@ class OHBillScraper(BillScraper):
             self.warning('missing page: %s' % base_url + piece)
             return
 
-        _get_html_or_pdf_version(html)
+        _get_html_or_pdf_version_old(html)
         doc = lxml.html.fromstring(html)
         for a in doc.xpath('//a[starts-with(@href, "/bills.cfm")]/@href'):
             if a != piece:
-                _get_html_or_pdf_version(self.urlopen(base_url + a))
+                _get_html_or_pdf_version_old(self.urlopen(base_url + a))
         for a in doc.xpath('//a[starts-with(@href, "/res.cfm")]/@href'):
             if a != piece:
-                _get_html_or_pdf_version(self.urlopen(base_url + a))
+                _get_html_or_pdf_version_old(self.urlopen(base_url + a))
 
-    def scrape_votes(self, bill, bill_prefix, number, session):
-        vote_url = ('http://www.legislature.state.oh.us/votes.cfm?ID=' +
-                    session + '_' + bill_prefix + '_' + str(number))
+    def scrape_votes_old(self, bill, billname, session):
+        vote_url = ('http://archives.legislature.state.oh.us/bills.cfm?ID=' +
+                    session + '_' + billname)
 
         page = self.urlopen(vote_url)
         page = lxml.html.fromstring(page)
