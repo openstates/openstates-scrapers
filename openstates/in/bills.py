5d626fe5c20cbaddedb83a596de6b9519488ef62
Rachel Shorey <rshorey@gmail.com>
2015-03-11 16:21:35 -0400

    in bills now have sponsors!

--- openstates/in/bills.py
+++ openstates/in/bills.py
@@ -45,7 +45,45 @@ class INBillScraper(BillScraper):
     # Can turn this on or off. There are thousands of subjects and it takes hours.
     SCRAPE_SUBJECTS = True
 
+    def make_html_source(self,session,bill_id):
+        url = "http://iga.in.gov/legislative/{}/".format(session)
+        urls = {
+            "hb":"bills/house/",
+            "hr":"resolutions/house/simple/",
+            "hcr":"resolutions/house/concurrent",
+            "hjr":"resolutions/house/joint",
+            "hc":"resolutions/house/concurrent",
+            "hj":"resolutions/house/joint",
+            "sb":"bills/senate/",
+            "sr":"resolutions/senate/simple/",
+            "scr":"resolutions/senate/concurrent",
+            "sjr":"resolutions/senate/joint",
+            "sc":"resolutions/senate/concurrent",
+            "sj":"resolutions/senate/joint"
+            }
+        bill_id = bill_id.lower()
+        try:
+            int(bill_id[2])
+        except ValueError:
+            bill_prefix = bill_id[:3]
+            bill_num = bill_id[3:]
+        else:
+            bill_prefix = bill_id[:2]
+            bill_num = bill_prefix[2:]
+
+        try:
+            url += urls[bill_prefix]
+        except KeyError:
+            raise AssertionError("Unknown bill type {}, don't know how to make url".format(bill_id))
+        url += bill_num
+        return url
+
+    def get_name(self,random_json):
+        #got sick of doing this everywhere
+        return random_json["firstName"] + " " + random_json["lastName"]
+
     def scrape(self, session, chambers):
+        
         api_base_url = "https://api.iga.in.gov"
         client = ApiClient(self)
         r = client.get("bills",session=session)
@@ -55,14 +93,58 @@ class INBillScraper(BillScraper):
             bill_link = b["link"]
             api_source = api_base_url + bill_link
             bill_json = client.get("bill",session=session,bill_id=bill_id.lower())
+            
             title = bill_json["title"]
+            #sometimes title is blank
+            #if that's the case, we can check to see if
+            #the latest version has a short description
+            if not title:
+                title = bill_json["latestVersion"]["shortDescription"]
+
+            #and if that doesn't work, use the bill_id but throw a warning
+            if not title:
+                title = bill_id
+                self.logger.warning("Bill is missing a title, using bill id instead.")
+
+
             original_chamber = "lower" if bill_json["originChamber"].lower() == "house" else "upper"
             bill = Bill(session,original_chamber,bill_id,title)
-            #sources
+            
             bill.add_source(api_source,note="API key needed")
+            bill.add_source(self.make_html_source(session,bill_id))
             #documents/versions
+
+
             #sponsors
+            positions = {"Representative":"lower","Senator":"upper"}
+            for s in bill_json["authors"]:
+                bill.add_sponsor("primary",
+                    self.get_name(s),
+                    chamber=positions[s["position_title"]],
+                    official_type="author")
+
+            for s in bill_json["coauthors"]:
+                bill.add_sponsor("cosponsor",
+                    self.get_name(s),
+                    chamber=positions[s["position_title"]],
+                    official_type="coauthor")
+
+            for s in bill_json["sponsors"]:
+                bill.add_sponsor("primary",
+                    self.get_name(s),
+                    chamber=positions[s["position_title"]],
+                    official_type="sponsor")
+
+            for s in bill_json["cosponsors"]:
+                bill.add_sponsor("cosponsor",
+                    self.get_name(s),
+                    chamber=positions[s["position_title"]],
+                    official_type="cosponsor")
+
             #votes
+
+
+
             self.save_bill(bill)
 
 
