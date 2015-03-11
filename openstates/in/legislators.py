825214d8a00b9e5dda1f0c2361cafff4a323de63
Rachel Shorey <rshorey@gmail.com>
2015-03-11 11:07:12 -0400

    generalized timeout wait function into utils

--- openstates/in/legislators.py
+++ openstates/in/legislators.py
@@ -3,28 +3,11 @@ import lxml.html
 
 from billy.scrape.legislators import LegislatorScraper, Legislator
 from apiclient import ApiClient
-import requests
+from .utils import get_with_increasing_timeout
 
 
 class INLegislatorScraper(LegislatorScraper):
     jurisdiction = 'in'
-
-    def get_with_increasing_timeout(self,link,kwargs={}):
-        timeout_length = 2
-        html = None
-        while timeout_length < 65 and html is None:
-            try:
-                html = self.get(link,timeout=timeout_length,**kwargs)
-            except requests.exceptions.ConnectTimeout:
-                old_length = timeout_length
-                timeout_length **= 2
-                self.logger.debug("Timed out after {now} seconds, increasing to {next} and trying again".format(now=old_length,next=timeout_length))
-            else:
-                return html
-
-        raise AssertionError("Link failed after waiting over a minute, giving up.")
-
-
 
     def scrape(self, chamber, term):
         base_url = "https://iga.in.gov/legislative"
@@ -43,7 +26,7 @@ class INLegislatorScraper(LegislatorScraper):
             link = leg["link"]
             api_link = api_base_url+link
             html_link = base_url+link.replace("legislators/","legislators/legislator_")
-            html = self.get_with_increasing_timeout(html_link,{"verify":False})
+            html = get_with_increasing_timeout(self,html_link,fail=True,kwargs={"verify":False})
             doc = lxml.html.fromstring(html.text)
             doc.make_links_absolute(html_link)
             address, phone = doc.xpath("//address")
