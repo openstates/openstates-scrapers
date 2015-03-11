c6b24981f5a83ab276210298dc7458d577bd4f30
Rachel Shorey <rshorey@gmail.com>
2015-03-11 14:01:10 -0400

    legislator session made general

--- openstates/in/legislators.py
+++ openstates/in/legislators.py
@@ -10,11 +10,11 @@ class INLegislatorScraper(LegislatorScraper):
     jurisdiction = 'in'
 
     def scrape(self, chamber, term):
-        base_url = "https://iga.in.gov/legislative"
-        api_base_url = "https://api.iga.in.gov/2015"
         client = ApiClient(self)
         t = next((item for item in self.metadata["terms"] if item["name"] == term),None)
         session = max(t["sessions"])
+        base_url = "https://iga.in.gov/legislative"
+        api_base_url = "https://api.iga.in.gov/{}".format(session)
         chamber_name = "Senate" if chamber == "upper" else "House"
         r = client.get("chamber_legislators",session=session,chamber=chamber_name)
         all_pages = client.unpaginate(r)
