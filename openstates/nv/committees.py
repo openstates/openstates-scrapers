cf07484229d7cd9e767c0b109e656caf4ed2c250
Miles Watkins <miles.w.watkins@gmail.com>
2015-05-14 10:41:11 -0400

    NV: Clean up committee scraper syntax

--- openstates/nv/committees.py
+++ openstates/nv/committees.py
@@ -1,12 +1,11 @@
 import re
-import datetime
 
 from billy.scrape.committees import CommitteeScraper, Committee
-from billy.scrape import NoDataForPeriod
 
 import lxml.html
 
 nelis_root = 'https://www.leg.state.nv.us/App/NELIS/REL'
+
 
 class NVCommitteeScraper(CommitteeScraper):
     jurisdiction = 'nv'
@@ -26,26 +25,25 @@ class NVCommitteeScraper(CommitteeScraper):
             sessionsuffix = 'rd'
         insert = str(session) + sessionsuffix + str(term[0:4])
 
-        chamber_letter = {'lower':'A', 'upper':'S'}[chamber]
-        chamber_names = {'lower':'Assembly', 'upper':'Senate'}
+        chamber_names = {'lower': 'Assembly', 'upper': 'Senate'}
 
         insert = self.metadata['session_details'][session].get(
             '_committee_session', insert
         )
-        
-        list_url = '%s/%s/HomeCommittee/LoadCommitteeListTab' % (nelis_root,insert)
+
+        list_url = '%s/%s/HomeCommittee/LoadCommitteeListTab' % (nelis_root, insert)
         html = self.get(list_url).text
         doc = lxml.html.fromstring(html)
-        
+
         sel = 'panel%sCommittees' % chamber_names[chamber]
-        
+
         ul = doc.xpath('//ul[@id="%s"]' % sel)[0]
         coms = ul.xpath('li/div/div/div[@class="col-md-4"]/a')
-        
+
         for com in coms:
             name = com.text.strip()
-            com_id = re.match(r'.*/Committee/(?P<id>[0-9]+)/Overview',com.attrib['href']).group('id')
-            com_url = '%s/%s/Committee/FillSelectedCommitteeTab?committeeOrSubCommitteeKey=%s&selectedTab=Overview' % (nelis_root,insert,com_id)
+            com_id = re.match(r'.*/Committee/(?P<id>[0-9]+)/Overview', com.attrib['href']).group('id')
+            com_url = '%s/%s/Committee/FillSelectedCommitteeTab?committeeOrSubCommitteeKey=%s&selectedTab=Overview' % (nelis_root, insert, com_id)
             com = Committee(chamber, name)
             com.add_source(com_url)
             self.scrape_comm_members(chamber, com, com_url)
@@ -57,7 +55,7 @@ class NVCommitteeScraper(CommitteeScraper):
         links = doc.xpath('//div[@class="col-md-11"]/a[@class="bio"]')
         for link in links:
             name = link.text.strip()
-            role = link.tail.strip().replace("- ","")
+            role = link.tail.strip().replace("- ", "")
             if role == '':
                 role = 'member'
             committee.add_member(name, role)
