8b9a73ade67e209abb14b1e86c3dc16095dca32f
Paul Tagliamonte <tag@pault.ag>
2015-06-03 10:32:22 -0400

    Fix unlock script.

--- scripts/unlock_bad_photos.py
+++ scripts/unlock_bad_photos.py
@@ -10,26 +10,28 @@ def unlock(person):
     if not person.get("photo_url"):
         return
 
+    locked = person.get('_locked_fields')
+    if 'photo_url' not in locked:
+        # We're not locked, so that's fine.
+        return
+
+    print("{_id} - {photo_url}".format(**person))
+
     if requests.get(person['photo_url']).status_code % 100 == 2:
         return
 
     # Right, we've got a photo_url, but it sucks. Let's go and unlock
     # and save this homie.
 
-    locked = person.get('_locked_fields')
-    if 'photo_url' not in locked:
-        # We're not locked, so that's fine.
-        return
-
+    print(" -> Unlocked!")
     locked.remove("photo_url")
     person['_locked_fields'] = locked
     db.legislators.save(person)
 
 
 def main(state):
-    for _ in map(unlock, db.legislators.find({"abbr": state})):
+    for _ in map(unlock, db.legislators.find({"state": state})):
         pass
-
 
 
 if __name__ == "__main__":
