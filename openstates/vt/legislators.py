1809edca31371be22576e762458be64db715c26d
Miles Watkins <miles.w.watkins@gmail.com>
2015-07-27 17:47:37 -0400

    VT: Capture state office

--- openstates/vt/legislators.py
+++ openstates/vt/legislators.py
@@ -7,6 +7,7 @@ from openstates.utils import LXMLMixin
 class VTLegislatorScraper(LegislatorScraper, LXMLMixin):
     jurisdiction = 'vt'
     latest_only = True
+    CHAMBERS = {'Senator': 'upper', 'Representative': 'lower'}
 
     def scrape(self, term, chambers):
         year_slug = term[5:]
@@ -29,18 +30,30 @@ class VTLegislatorScraper(LegislatorScraper, LXMLMixin):
             page = self.lxmlize(member_url)
             (photo_url, ) = page.xpath('//img[@class="profile-photo"]/@src')
 
+            # Also grab their state email address
+            state_email = page.xpath(
+                '//dl[@class="summary-table profile-summary"]/'
+                'dt[text()="Email"]/following-sibling::dd[1]/a/text()')
+            if state_email:
+                (state_email, ) = state_email
+            else:
+                state_email = None
+
             leg = Legislator(
                 term=term,
-                chamber=('upper' if info['Title'] == 'Senator' else 'lower'),
+                chamber=self.CHAMBERS[info['Title']],
                 district=info['District'].replace(" District", ""),
-                party=info['Party'],
-                email=info['Email'],
+                party=info['Party'].replace("Democrat", "Democratic"),
                 full_name="{0} {1}".format(info['FirstName'], info['LastName']),
                 photo_url=photo_url
             )
 
-            leg.add_source(legislator_dump_url)
-            leg.add_source(member_url)
+            leg.add_office(
+                type='capitol',
+                name='Capitol Office',
+                address='Vermont State House\n115 State Street\nMontpelier, VT 05633',
+                email=state_email
+            )
 
             leg.add_office(
                 type='district',
@@ -48,14 +61,20 @@ class VTLegislatorScraper(LegislatorScraper, LXMLMixin):
                 address="{0}{1}\n{2}, {3} {4}".format(
                     info['MailingAddress1'],
                     ("\n" + info['MailingAddress2']
-                        if info['MailingAddress2']
+                        if info['MailingAddress2'].strip()
                         else ""),
                     info['MailingCity'],
                     info['MailingState'],
                     info['MailingZIP']
                 ),
-                phone=(info['HomePhone'] if info['HomePhone'] else None),
-                email=(info['HomeEmail'] if info['HomeEmail'] else None)
+                phone=(info['HomePhone'].strip() or None),
+                email=(info['Email'].strip() or
+                       info['HomeEmail'].strip() or
+                       info['WorkEmail'].strip() or
+                       None)
             )
+
+            leg.add_source(legislator_dump_url)
+            leg.add_source(member_url)
 
             self.save_legislator(leg)
