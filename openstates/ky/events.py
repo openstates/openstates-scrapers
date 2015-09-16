2ca4579e135dd3b2b1a94a194e5d0297fd25ce7e
Andy Lo <andy@causelabs.co>
2015-09-16 04:39:06 -0400

    Fixed parsing dates and times from the KY legislative calendar.

--- openstates/ky/events.py
+++ openstates/ky/events.py
@@ -1,4 +1,5 @@
 import datetime
+import re
 
 from billy.scrape import NoDataForPeriod
 from billy.scrape.events import EventScraper, Event
@@ -18,10 +19,21 @@ class KYEventScraper(EventScraper):
         page = lxml.html.fromstring(page)
 
         for div in page.xpath("//div[@style = 'MARGIN-LEFT: 20px']"):
-            date = div.xpath("string(../../span[1])").strip()
+            # Attempt to extract a date string from the most likely DOM element.
+            date = div.xpath("string(../preceding-sibling::span[div[@align]][1])").strip()
 
             try:
-                time, location = div.xpath("string(span[1])").split(',')
+                # Attempt to separate the time and location.
+                delimiter = ','
+                time, delimiter, location = div.xpath("string(span[1])").partition(delimiter)
+
+                # Strip any dates from time string.
+                time = re.sub(r'[0-9]{1,2}/[0-9]{1,2}/[0-9]{2,4}', '', time)
+
+                # Strip extraneous timezone data from time string.
+                time = re.sub(r'[\s-]*?([eE][dDsS][tT]).*', '', time)
+
+                time = time.strip()
             except ValueError:
                 # No meetings
                 continue
@@ -32,12 +44,17 @@ class KYEventScraper(EventScraper):
             if ':' not in time:
                 self.warning('skipping event with invalid time: %s', time)
                 continue
+
+            # Combine extracted date and time into a datetime string.
             when = "%s %s" % (date, time)
+
+            # Attempt to create a datetime object from datetime string.
             try:
                 when = datetime.datetime.strptime(when, "%A, %B %d, %Y %I:%M%p")
             except ValueError:
                 when = datetime.datetime.strptime(when, "%A, %B %d, %Y %I:%M %p")
 
+            # Set the timezone for datetime.
             when = self._tz.localize(when)
 
             desc = div.xpath("string(span[2])").strip()
