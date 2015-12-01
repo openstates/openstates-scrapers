12d0d1fbc4d0dd5885b08f19c425631130d0b6de
Andy Lo <alo@sunlightfoundation.com>
2015-12-01 17:14:00 -0500

    VA: Implemented legislator photo scraping.

--- openstates/va/legislators.py
+++ openstates/va/legislators.py
@@ -1,6 +1,7 @@
 import re
 import lxml.html
 from billy.scrape.legislators import Legislator, LegislatorScraper
+from openstates.utils import LXMLMixin
 
 CHAMBER_MOVES = {
     "A. Benton \"Ben\" Chafin-Elect": "upper",
@@ -8,12 +9,28 @@ CHAMBER_MOVES = {
 }
 
 
-class VALegislatorScraper(LegislatorScraper):
+class VALegislatorScraper(LegislatorScraper, LXMLMixin):
     jurisdiction = 'va'
 
-    def scrape(self, chamber, term):
-        abbr = {'upper': 'S', 'lower': 'H'}
+    def _get_lis_id(self, chamber, url):
+        """Retrieves LIS ID of legislator from URL."""
+        if chamber == 'lower':
+            regex = r'(H[0-9]+$)'
+        elif chamber == 'upper':
+            regex = r'(S[0-9]+$)'
+        else:
+            raise ValueError('Invalid chamber: {}'.format(chamber))
 
+        match = re.search(regex, url)
+
+        if match.groups:
+            lis_id = match.group(1)
+        else:
+            lis_id = None
+
+        return lis_id
+
+    def scrape(self, chamber, term):
         sessions = []
         for t in self.metadata['terms']:
             if t['name'] == term:
@@ -30,9 +47,7 @@ class VALegislatorScraper(LegislatorScraper):
         elif chamber == 'upper':
             column = 'lColRt'
 
-        html = self.get(url).text
-        doc = lxml.html.fromstring(html)
-        doc.make_links_absolute(url)
+        doc = self.lxmlize(url)
 
         for link in doc.xpath('//div[@class="%s"]/ul/li/a' % column):
             if 'resigned' in link.text:
@@ -41,6 +56,36 @@ class VALegislatorScraper(LegislatorScraper):
             self.fetch_member(link.get('href'), link.text, term, chamber)
 
     def fetch_member(self, url, name, term, chamber):
+        photo_url = ''
+
+        lis_id = self._get_lis_id(chamber, url)
+
+        if chamber == 'lower':
+            base_url = 'http://memdata.virginiageneralassembly.gov'
+            profile_url = base_url + '/images/display_image/{}'
+            photo_url = profile_url.format(lis_id)
+            #xpath_query = './/img/@src'
+        elif chamber == 'upper':
+            base_url = 'http://apps.senate.virginia.gov'
+            profile_url = base_url + '/Senator/memberpage.php?id={}'
+            xpath_query = './/img[@class="profile_pic"]/@src'
+
+            # Retrieve profile photo.
+            profile_page = self.lxmlize(profile_url.format(lis_id))
+            photo_url = self.get_node(
+                profile_page,
+                xpath_query)
+
+        # Detect whether URL points to a blank base location.
+        blank_urls = (
+            'http://memdata.virginiageneralassembly.gov/images/display_'
+            'image/',
+            'http://virginiageneralassembly.gov/house/members/photos/',
+        )
+
+        if photo_url in blank_urls:
+            photo_url = ''
+
         if name in CHAMBER_MOVES:
             if chamber != CHAMBER_MOVES[name]:
                 return  # Skip bad chambers.
@@ -70,8 +115,15 @@ class VALegislatorScraper(LegislatorScraper):
         party_district_line = doc.xpath('//h3/font/text()')[0]
         party, district = party_district_re.match(party_district_line).groups()
 
-        leg = Legislator(term, chamber, district, name.strip(),
-                         party=party_map[party], url=url)
+        leg = Legislator(
+            term=term,
+            chamber=chamber,
+            district=district,
+            full_name=name.strip(),
+            party=party_map[party],
+            url=url,
+            photo_url=photo_url,
+        )
         leg.add_source(url)
 
         for ul in doc.xpath('//ul[@class="linkNon"]'):
