d07d83994ab4174e133b4b7eeaf90add39d190ab
Andy Lo <alo@sunlightfoundation.com>
2016-01-19 15:02:31 -0500

    CO: Added some past sessions to ignore list.

--- openstates/co/__init__.py
+++ openstates/co/__init__.py
@@ -66,6 +66,10 @@ metadata = dict(
     },
     feature_flags=['influenceexplorer'],
     _ignored_scraped_sessions = [
+        '2013 Legislative Session',
+        '2012 First Special Session',
+        '2012 Legislative Session',
+        '2011 Legislative Session',
         '2010 Legislative Session',
         '2009 Legislative Session',
         '2008 Legislative Session',
