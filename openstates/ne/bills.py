a94026aeb2d34cc7d04b48577fa9a0ee458bb9ac
Andy Lo <alo@sunlightfoundation.com>
2016-01-19 13:28:50 -0500

    NE: Fixed bill scrapers for changed markup.
    Also fixed scraping for transcript documents.

--- openstates/ne/bills.py
+++ openstates/ne/bills.py
@@ -1,8 +1,9 @@
-from billy.scrape.bills import BillScraper, Bill
-from openstates.utils import LXMLMixin
 from datetime import datetime
 import lxml.html
 import urllib
+from billy.scrape.bills import BillScraper, Bill
+from openstates.utils import LXMLMixin
+
 
 class NEBillScraper(BillScraper, LXMLMixin):
     jurisdiction = 'ne'
@@ -15,104 +16,128 @@ class NEBillScraper(BillScraper, LXMLMixin):
             self.scrape_year(session, end_year)
 
     def scrape_year(self, session, year):
-        main_url = 'http://nebraskalegislature.gov/bills/search_by_date.php?SessionDay=%s' % year
+        main_url = 'http://nebraskalegislature.gov/bills/search_by_date.php?'\
+            'SessionDay={}'.format(year)
         page = self.lxmlize(main_url)
 
-        for docs in page.xpath('//div[@class="cal_content_full"]/table[@id="bill_results"]/tr/td[1]/a'):
-            bill_abbr = docs.text
+        document_links = self.get_nodes(
+            page,
+            '//div[@class="main-content"]//div[@class="panel panel-leg"]//'
+            'table[@class="table table-condensed"]/tbody/tr/td[1]/a')
+
+        for document_link in document_links:
+            bill_number = document_link.text
+            bill_link = document_link.attrib['href']
 
             #POST request for search form
-            post_dict = {'DocumentNumber': bill_abbr, 'Legislature': session}
+            #post_dict = {'DocumentNumber': bill_number, 'Legislature': session}
             #headers = urllib.urlencode(post_dict)
-            bill_resp = self.post('http://nebraskalegislature.gov/bills/search_by_number.php',
-                                  data=post_dict)
-            bill_link = bill_resp.url
-            bill_page = bill_resp.text
+            #bill_resp = self.post('http://nebraskalegislature.gov/bills/'
+            #    'search_by_number.php', data=post_dict)
+            #bill_link = bill_resp.url
+            #bill_page = bill_resp.text
 
             #scrapes info from bill page
-            self.bill_info(bill_link, session, main_url, bill_page)
+            self.bill_info(bill_link, session, main_url)
 
     #Scrapes info from the bill page
-    def bill_info(self, bill_link, session, main_url, bill_page):
+    def bill_info(self, bill_link, session, main_url):
 
-        bill_page = lxml.html.fromstring(bill_page)
-        bill_page.make_links_absolute(bill_link)
+        bill_page = self.lxmlize(bill_link)
 
-        #basic info
-        try:
-            long_title = bill_page.xpath('//div[@id="content_text"]/h2')[0].text.split()
-        except IndexError:
-            return None
-        bill_id = long_title[0]
+        long_title = self.get_node(
+            bill_page,
+            '//div[@class="main-content"]/div[1]/div/h2').text.split()
+
+        bill_number = long_title[0]
         title = ''
         for x in range(2, len(long_title)):
             title += long_title[x] + ' '
         title = title[0:-1]
 
         if not title:
-            self.error('no title, skipping %s', bill_id)
+            self.error('no title, skipping %s', bill_number)
             return
 
-        #bill_type
-        bill_type = 'resolution' if 'LR' in bill_id else 'bill'
+        bill_type = 'resolution' if 'LR' in bill_number else 'bill'
 
-        bill = Bill(session, 'upper', bill_id, title, type = bill_type)
+        bill = Bill(session, 'upper', bill_number, title, type = bill_type)
 
-        #sources
         bill.add_source(main_url)
         bill.add_source(bill_link)
 
-        #Sponsor
-        introduced_by = bill_page.xpath('//div[@id="content_text"]/div[2]/table/tr[2]/td[1]/a[1]')[0].text
+        introduced_by = self.get_node(
+            bill_page,
+            '//div[@class="main-content"]/div[3]/div[1]/ul/li[1]/a[1]/text()')
         bill.add_sponsor('primary', introduced_by)
 
-        #actions
-        for actions in bill_page.xpath('//div[@id="content_text"]/div[3]/table/tr[1]/td[1]/table/tr'):
-            date = actions[0].text
-            if 'Date' not in date:
-                date = datetime.strptime(date, '%b %d, %Y')
-                action = actions[1].text_content()
+        action_nodes = self.get_nodes(
+            bill_page,
+            '//div[@class="main-content"]/div[5]//table/tbody/tr')
 
-                if 'Governor' in action:
-                    actor = 'Governor'
-                elif 'Speaker' in action:
-                    actor = 'Speaker'
-                else:
-                    actor = 'upper'
+        for action_node in action_nodes:
+            date = self.get_node(
+                action_node,
+                './td[1]').text
+            date = datetime.strptime(date, '%b %d, %Y')
 
-                action_type = self.action_types(action)
-                bill.add_action(actor, action, date, action_type)
+            # The action node may have an anchor element within it, so
+            # we grab all the text within.
+            action = self.get_node(
+                action_node,
+                './td[2]').text_content()
 
-        # were in reverse chronological order
+            if 'Governor' in action:
+                actor = 'Governor'
+            elif 'Speaker' in action:
+                actor = 'Speaker'
+            else:
+                actor = 'upper'
+
+            action_type = self.action_types(action)
+            bill.add_action(actor, action, date, action_type)
+
+        # Were in reverse chronological order.
         bill['actions'].reverse()
 
-        #versions
-        for version in bill_page.xpath('//div[@id="content_text"]/div[2]/table/tr[2]/td[2]/a'):
-            version_url = version.attrib['href']
-            version_name = version.text
+        # Grabs bill version documents.
+        version_links = self.get_nodes(
+            bill_page,
+            '//div[@class="main-content"]/div[3]/div[2]/'
+            'div[@class="hidden-xs"]/ul[1]/li/a')
+
+        for version_link in version_links:
+            version_name = version_link.text
+            version_url = version_link.attrib['href']
             # replace Current w/ session number
             version_url = version_url.replace('Current', session)
             bill.add_version(version_name, version_url,
-                             mimetype='application/pdf')
+                mimetype='application/pdf')
 
-        #amendments
-        for amendment in bill_page.xpath('//h2[text()="Amendments"]/following-sibling::table[1]//a'):
-            amendment_name = amendment.text
-            amendment_url = amendment.attrib['href']
+        # Adds any documents related to amendments.
+        amendment_links = self.get_nodes(
+            bill_page,
+            '//div[@class="main-content"]/div[5]/div[2]/table/tr/td[1]/a')
+
+        for amendment_link in amendment_links:
+            amendment_name = amendment_link.text
+            amendment_url = amendment_link.attrib['href']
             bill.add_document(amendment_name, amendment_url)
 
-        #related transcripts
-        for transcripts in bill_page.xpath('//h2[text()="Related Transcripts"]/following-sibling::table[1]//a'):
-            transcript_name = transcripts.text
-            transcript_url = transcripts.attrib['href']
+        # Related transcripts.
+        transcript_links = self.get_nodes(
+            bill_page,
+            '//div[@class="main-content"]/div[5]/div[2]/'
+            'div[@class="hidden-xs"]/table/tr/td/a')
+
+        for transcript_link in transcript_links:
+            transcript_name = transcript_link.text
+            transcript_url = transcript_link.attrib['href']
             bill.add_document(transcript_name, transcript_url)
 
         self.save_bill(bill)
 
-
-    #Setting action types
     def action_types(self, action):
-
         if 'Date of introduction' in action:
             action_type = 'bill:introduced'
         elif 'Referred to' in action:
