f30ed6cd65981d9498f404a78c148bfbf382872d
Andy Lo <alo@sunlightfoundation.com>
2016-03-14 15:20:20 -0400

    FL: Added 2014 Organizational Session.

--- openstates/fl/__init__.py
+++ openstates/fl/__init__.py
@@ -1,5 +1,7 @@
-from billy.utils.fulltext import text_after_line_numbers
+import datetime
 import lxml.html
+from billy.utils.fulltext import text_after_line_numbers
+from billy.scrape.utils import url_xpath
 from .bills import FLBillScraper
 from .legislators import FLLegislatorScraper
 from .committees import FLCommitteeScraper
@@ -20,7 +22,7 @@ metadata = dict(
          'sessions': ['2011', '2012', '2012B'],
          'start_year': 2011, 'end_year': 2012},
         {'name': '2013-2014',
-         'sessions': ['2013', '2014', '2014A'],
+         'sessions': ['2013', '2014', '2014A', '2014O'],
          'start_year': 2013, 'end_year': 2014},
         {'name': '2015-2016',
          'sessions': ['2015', '2015A', '2015B', '2015C', '2016'],
@@ -45,6 +47,13 @@ metadata = dict(
         '2014A': {'display_name': '2014 Special Session A',
                  '_scraped_name': '2014A',
                 },
+        '2014O': {
+            'type': 'organizational',
+            'start_date': datetime.date(2014, 11, 18),
+            'end_date': datetime.date(2014, 11, 18),
+            '_scraped_name': '2014O',
+            'display_name': '2014 Organizational Session',
+        },
         '2015': {'display_name': '2015 Regular Session',
                  '_scraped_name': '2015',
                 },
@@ -64,13 +73,11 @@ metadata = dict(
     feature_flags=['influenceexplorer'],
     _ignored_scraped_sessions=[
         '2010O', '2010A', '2012O', '2010',
-        '2014O',  # 2014 Organizational session; nothing important here.
         ],
 )
 
 
 def session_list():
-    from billy.scrape.utils import url_xpath
     return url_xpath('http://flsenate.gov', '//option/text()')
 
 
