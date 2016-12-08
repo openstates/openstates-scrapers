093ef1318370b4e427b5278d0e08c2204790c6a6
Dan Roberts <droberts@askcts.com>
2016-12-08 12:41:58 -0600

    Merge from upstream

--- openstates/wa/utils.py
+++
@@ -1,9 +0,0 @@
-NS = {'wa': "http://WSLWebServices.leg.wa.gov/"}
-
-
-def xpath(elem, path):
-    """
-    A helper to run xpath with the proper namespaces for the Washington
-    Legislative API.
-    """
-    return elem.xpath(path, namespaces=NS)
