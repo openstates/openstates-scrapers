92b3a93a8f3693ff087b24466f51cf6f7f2e1144
Tim Showers <showerst@gmail.com>
2016-12-23 11:41:36 -0500

    NH: First pass at 2017 scraper

--- openstates/nh/__init__.py
+++ openstates/nh/__init__.py
@@ -20,7 +20,9 @@ metadata = {
         {'name': '2013-2014', 'sessions': ['2013', '2014'],
          'start_year': 2013, 'end_year': 2014},
         {'name': '2015-2016', 'sessions': ['2015', '2016'],
-         'start_year': 2015, 'end_year': 2016}
+         'start_year': 2015, 'end_year': 2016},
+        {'name': '2017-2018', 'sessions': ['2017'],
+         'start_year': 2017, 'end_year': 2018}
     ],
     'session_details': {
         '2011': {'display_name': '2011 Regular Session',
@@ -49,9 +51,12 @@ metadata = {
                  'zip_url': 'http://gencourt.state.nh.us/downloads/2016%20Session%20Bill%20Status%20Tables.zip',
                  '_scraped_name': '2016 Session',
                 },                
+        '2017': {'display_name': '2017 Regular Session',
+                 '_scraped_name': '2017 Session',
+                },  
     },
     'feature_flags': ['subjects', 'influenceexplorer'],
-    '_ignored_scraped_sessions': ['2013 Session'],
+    '_ignored_scraped_sessions': ['2013 Session','2017 Session Bill Status Tables Link.txt'],
 }
 
 def session_list():
