9eb7e76e1331833f06ea0d618ab4e736236c9398
James Turk <james.p.turk@gmail.com>
2017-01-12 23:07:03 -0500

    AZ: legislators first pass

--- openstates/az/legislators.py
+++ openstates/az/legislators.py
@@ -17,74 +17,57 @@ class AZLegislatorScraper(LegislatorScraper):
     def get_party(self, abbr):
         return self.parties[abbr]
 
-    def get_session_id(self, session):
-        return self.metadata['session_details'][session]['session_id']
-
-    def get_session_for_term(self, term):
-        # ideally this should be either first or second regular session
-        # and probably first and second when applicable
-        for t in self.metadata['terms']:
-            if t['name'] == term:
-                session = t['sessions'][-1]
-                if re.search('regular', session):
-                    return session
-                else:
-                    return t['sessions'][0]
-
     def scrape(self, chamber, term):
-        self.validate_term(term)
-        session = self.get_session_for_term(term)
-        try:
-            session_id = self.get_session_id(session)
-        except KeyError:
-            raise NoDataForPeriod(session)
+        # TODO: old AZ scraper allowed old sessions, they seem to be gone?
+        self.validate_term(term, latest_only=True)
 
         body = {'lower': 'H', 'upper': 'S'}[chamber]
-        url = 'http://www.azleg.gov/MemberRoster.asp?Session_ID=%s&body=%s' % (
-                                                               session_id, body)
+        url = 'http://www.azleg.gov/MemberRoster/?body=' + body
         page = self.get(url).text
+
+        # there is a bad comment closing tag on this page
+        page = page.replace('--!>', '-->')
+
         root = html.fromstring(page)
-        path = '//table[@id="%s"]/tr' % {'H': 'house', 'S': 'senate'}[body]
+
+        path = '//table//tr'
         roster = root.xpath(path)[1:]
         for row in roster:
             position = ''
-            vacated = ''
-            name, district, party, email, room, phone, fax = row.xpath('td')
+            name, district, party, email, room, phone, = row.xpath('td')
 
             if email.attrib.get('class') == 'vacantmember':
                 continue  # Skip any vacant members.
 
             link = name.xpath('string(a/@href)')
-            link = "http://www.azleg.gov" + link
             if len(name) == 1:
                 name = name.text_content().strip()
             else:
                 position = name.tail.strip()
                 name = name[0].text_content().strip()
+            if '--' in name:
+                name = name.split('--')[0].strip()
 
             linkpage = self.get(link).text
+            linkpage = linkpage.replace('--!>', '-->')
             linkroot = html.fromstring(linkpage)
             linkroot.make_links_absolute(link)
 
-            photos = linkroot.xpath("//img[@name='memberphoto']")
+            photos = linkroot.xpath("//img[contains(@src, 'MemberPhoto')]")
 
             if len(photos) != 1:
-                raise Exception
-
-            photo_url = photos[0].attrib['src']
+                self.warning('no photo on ' + link)
+                photo_url = ''
+            else:
+                photo_url = photos[0].attrib['src']
 
             district = district.text_content()
             party = party.text_content().strip()
             email = email.text_content().strip()
 
-            if ('Vacated' in email or 'Resigned' in email or 
-                'Removed' in email):
-                # comment out the following 'continue' for historical
-                # legislative sessions
-                # for the current session, if a legislator has left we will
-                # skip him/her to keep from overwriting their information
-                continue
-                vacated = re.search('[0-9]*/[0-9]*/\d{4}', email).group()
+            if email.startswith('Email: '):
+                email = email.replace('Email: ', '').lower() + '@azleg.gov'
+            else:
                 email = ''
 
             party = self.get_party(party)
@@ -99,21 +82,13 @@ class AZLegislatorScraper(LegislatorScraper):
             phone = phone.text_content().strip()
             if not phone.startswith('602'):
                 phone = "602-" + phone
-            fax = fax.text_content().strip()
-            if not fax.startswith('602'):
-                fax = "602-" + fax
-            if vacated:
-                end_date = datetime.datetime.strptime(vacated, '%m/%d/%Y')
-                leg = Legislator( term, chamber, district, full_name=name,
-                                  party=party, url=link)
-                leg['roles'][0]['end_date'] = end_date
-            else:
-                leg = Legislator(term, chamber, district, full_name=name,
-                                 party=party, url=link,
-                                 photo_url=photo_url)
+
+            leg = Legislator(term, chamber, district, full_name=name,
+                                party=party, url=link,
+                                photo_url=photo_url)
 
             leg.add_office('capitol', 'Capitol Office', address=address,
-                           phone=phone, fax=fax,  email=email)
+                           phone=phone, email=email)
 
             if position:
                 leg.add_role( position, term, chamber=chamber,
