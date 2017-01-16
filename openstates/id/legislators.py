1ed253784d4e8324a9286f1f20bad6bff197e05a
James Turk <james.p.turk@gmail.com>
2017-01-15 23:51:40 -0500

    ID: legislators scraper rewrite

--- openstates/id/legislators.py
+++ openstates/id/legislators.py
@@ -3,9 +3,8 @@ import re
 import datetime
 import lxml.html
 
-_BASE_URL = 'http://legislature.idaho.gov/%s/membership.cfm'
-
-_CHAMBERS = {'upper':'Senate', 'lower':'House'}
+_BASE_URL = 'https://legislature.idaho.gov/%s/membership/'
+_CHAMBERS = {'upper':'senate', 'lower':'house'}
 _PARTY = {
         '(R)': 'Republican',
         '(D)': 'Democratic',
@@ -14,41 +13,10 @@ _PHONE_NUMBERS = {'hom':'phone_number',
                   'bus':'business_phone',
                   'fax':'fax_number'}
 
+
 class IDLegislatorScraper(LegislatorScraper):
     """Legislator data seems to be available for the current term only."""
     jurisdiction = 'id'
-
-    def _extract_email(self, contact_form):
-        legislator_id = re.search(r'(\d+)', contact_form).group(1)
-        contact_page = self.get(contact_form).text
-        pattern = re.compile(r'legislators.email%s = "(.+?)";' % legislator_id)
-        email = pattern.search(contact_page).group(1)
-
-        return email
-
-
-    def scrape_sub(self, chamber, term, district, sub_url):
-        "Scrape basic info for a legislator's substitute."
-        page = self.get(sub_url).text
-        html = lxml.html.fromstring(page)
-        html.make_links_absolute(sub_url)
-        # substitute info div#MAINS35
-        div = html.xpath('//div[contains(@id, "MAINS")]')[0]
-        leg = {}
-        leg['img_url'] = div[0][0].get('src')
-        subfor = div[1][0].text.replace(u'\xa0', ' ').replace(': ', '')
-        full_name = div[1][2].text.replace(u'\xa0', ' ')
-        party = _PARTY[div[1][2].tail.strip()]
-        leg['contact_form'] = div[1][3].xpath('string(a/@href)')
-        leg = Legislator(term, chamber, district.strip(), full_name, party, **leg)
-        leg['roles'][0] = {'chamber': chamber, 'state': self.state,
-                           'term': term, 'role':'substitute',
-                           'legislator': subfor[subfor.rindex('for'):],
-                           'district': district.replace('District', '').strip(),
-                           'party': party,
-                           'start_date':None, 'end_date':None}
-        leg.add_source(sub_url)
-        self.save_legislator(leg)
 
     def scrape(self, chamber, term):
         """
@@ -59,72 +27,58 @@ class IDLegislatorScraper(LegislatorScraper):
         index = self.get(url).text
         html = lxml.html.fromstring(index)
         html.make_links_absolute(url)
-        base_table = html.xpath('body/table/tr/td[2]/table[2]')
-        district = None # keep track of district for substitutes
-        for row in base_table[0].xpath('tr'):
-            img_url = row.xpath('string(.//img/@src)')
-            contact_form, additional_info_url = row.xpath('.//a/@href')
-            email = self._extract_email(contact_form)
-            if "Substitute" in row.text_content():
-                # it seems like the sub always follows the person who he/she
-                # is filling in for.
-                # most sub info is provided at the additional info url
-                self.scrape_sub(chamber, term, district, additional_info_url)
-                continue
-            else:
-                full_name = " ".join(row[1][0].text_content().replace(u'\xa0', ' ').split())
-                party = _PARTY[row[1][0].tail.strip()]
 
-            pieces = [ x.strip() for x in row.itertext() if x ][6:]
+        rows = html.xpath('//div[contains(@class, "row-equal-height")]')
 
-            # The parsed HTML will be something like:
-            # ['District 4', '2', 'nd', 'term', address, phone(s), profession, committees]
-            # Sometimes there's a leadership title before all that
-            if 'District ' in pieces[1]:
-                pieces.pop(0)
-            assert pieces[0].startswith('District '), "Improper district found: {}".format(pieces[0])
-            assert pieces[3] == 'term', "Improper term found: {}".format(pieces[3])
+        for row in rows:
+            img_url = row.xpath('.//img/@src')[0]
 
-            district = pieces[0]
-            district = district.replace('District', '').strip()
-            pieces = pieces[4:]
-            if pieces[0].startswith(u'(Served '):
-                pieces.pop(0)
+            inner = row.xpath('.//div[@class="vc-column-innner-wrapper"]')[1]
 
-            address = re.sub(r'(\d{5})', r'ID \1', pieces.pop(0).strip())
-            assert re.match(r'.*\d{5}', address), "Address potentially invalid: {}".format(address)
+            name = inner.xpath('p/strong')[0].text.replace(u'\xa0', ' ').strip()
+            name = re.sub('\s+', ' ', name)
+            party = _PARTY[inner.xpath('p/strong')[0].tail.strip()]
+            email = inner.xpath('p/strong/a')[0].text
+            district = inner.xpath('p/a')[0].text
+            leg_url = inner.xpath('p/a/@href')[0]
 
-            phone = None
-            fax = None
-            for line in pieces:
-                if line.lower().startswith('home '):
-                    phone = line[len('home '):]
-                elif not phone and line.lower().startswith('bus '):
-                    phone = line[len('bus '):]
-                if line.lower().startswith('fax '):
-                    fax = line[len('fax '):]
+            address = home_phone = office_phone = fax = None
 
-                # After committees begin, no more contact information exists
-                if line == "Committees:":
-                    break
+            for br in inner.xpath('p/br'):
+                piece = br.tail or ''
+                piece = piece.strip()
 
-            leg = Legislator(term,
-                             chamber,
-                             district,
-                             full_name,
-                             party=party,
+                if re.findall(', \d{5}', piece):
+                    address = re.sub(r'(\d{5})', r'ID \1', piece).strip()
+                elif piece.startswith('Home '):
+                    home_phone = piece[5:]
+                elif piece.startswith('Bus '):
+                    office_phone = piece[4:]
+                elif piece.startswith('FAX '):
+                    fax = piece[4:]
+                print(piece)
+
+
+
+            leg = Legislator(term, chamber, district, name, party=party,
                              email=email)
 
-            leg.add_office('district',
-                           'District Office',
-                           address=address,
-                           email=email,
-                           fax=fax if fax else None,
-                           phone=phone if phone else None)
+            phone = home_phone or office_phone
+            leg.add_office('district', 'District Office',
+                           address=address, fax=fax, phone=phone)
 
             leg.add_source(url)
             leg['photo_url'] = img_url
-            leg['contact_form'] = contact_form
-            leg['url'] = additional_info_url
+            leg['url'] = leg_url
+
+            for com in inner.xpath('p/a[contains(@href, "committees")]'):
+                role = com.tail.strip()
+                if not role:
+                    role = 'member'
+                leg.add_role('committee member',
+                             term=term,
+                             chamber=chamber,
+                             committee=com.text,
+                             position=role)
 
             self.save_legislator(leg)
