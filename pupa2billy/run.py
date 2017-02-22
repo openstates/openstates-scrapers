176f53a99073106634362cda6b3b14e3c47cd893
James Turk <james.p.turk@gmail.com>
2017-02-21 22:53:15 -0500

    pupa2billy.committees

--- pupa2billy/run.py
+++ pupa2billy/run.py
@@ -4,6 +4,7 @@ import sys
 import shutil
 import importlib
 from .legislators import PupaLegislatorScraper
+from .committees import PupaCommitteeScraper
 from .bills import PupaBillScraper
 from .votes import PupaVoteScraper
 from .settings import BILLY_DATA_DIR
@@ -21,11 +22,15 @@ if __name__ == '__main__':
 
     juris_dir = os.path.join(BILLY_DATA_DIR, jurisdiction)
     os.makedirs(os.path.join(juris_dir, 'legislators'))
+    os.makedirs(os.path.join(juris_dir, 'committees'))
     os.makedirs(os.path.join(juris_dir, 'bills'))
     os.makedirs(os.path.join(juris_dir, 'votes'))
 
     ls = PupaLegislatorScraper(metadata, juris_dir, jurisdiction=jurisdiction)
     ls.scrape()
+
+    cs = PupaCommitteeScraper(metadata, juris_dir, jurisdiction=jurisdiction)
+    cs.scrape()
 
     bs = PupaBillScraper(metadata, juris_dir, jurisdiction=jurisdiction)
     bs.scrape()
