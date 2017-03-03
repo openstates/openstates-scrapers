5c5277803b7b0c042f3a0f70b6c7f4c2acf7f047
James Turk <james.p.turk@gmail.com>
2017-03-02 23:38:23 -0500

    add convert_metadata script

---
+++ scripts/convert_metadata.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+
+import csv
+import json
+import datetime
+import importlib
+
+
+class DatetimeEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime.date):
+            return repr(obj)
+        return json.JSONEncoder.default(self, obj)
+
+
+def get_districts(state):
+    with open('manual_data/districts/{}.csv'.format(state)) as f:
+        lower_max = 0
+        upper_max = 0
+        for d in csv.DictReader(f):
+            if d['chamber'] == 'lower':
+                if d['name'].isdigit():
+                    if int(d['name']) > lower_max:
+                        lower_max = int(d['name'])
+                else:
+                    lower_max = None
+                    break
+            if d['chamber'] == 'upper':
+                if d['name'].isdigit():
+                    if int(d['name']) > upper_max:
+                        upper_max = int(d['name'])
+                else:
+                    upper_max = None
+                    break
+
+    return lower_max, upper_max
+
+
+def convert(state):
+    metadata = importlib.import_module('billy_metadata.' + state).metadata
+
+    lower_max, upper_max = get_districts(state)
+
+    tmpl = """import datetime
+from pupa.scrape import Jurisdiction, Organization
+
+
+class {classname}(Jurisdiction):
+    division_id = "ocd-division/country:us/state:{abbr}"
+    classification = "government"
+    name = "{state}"
+    url = "TODO"
+    scrapers = {{
+    }}
+    parties = [
+        {{'name': 'Republican'}},
+        {{'name': 'Democratic'}}
+    ]
+    legislative_sessions = {sessions}
+    ignored_scraped_sessions = [
+{ignored}
+    ]
+
+    def get_organizations(self):
+        legislature_name = "{legislature_name}"
+        lower_chamber_name = "{lower_chamber_name}"
+        lower_seats = {lower_seats}
+        lower_title = "{lower_title}"
+        upper_chamber_name = "{upper_chamber_name}"
+        upper_seats = {upper_seats}
+        upper_title = "{upper_title}"
+
+        legislature = Organization(name=legislature_name,
+                                   classification="legislature")
+        upper = Organization(upper_chamber_name, classification='upper',
+                             parent_id=legislature._id)
+        lower = Organization(lower_chamber_name, classification='lower',
+                             parent_id=legislature._id)
+
+        for n in range(1, upper_seats+1):
+            lower.add_post(
+                label=str(n), role=upper_title,
+                division_id='{{}}/sldu:{{}}'.format(self.division_id, n))
+        for n in range(1, lower_seats+1):
+            upper.add_post(
+                label=str(n), role=lower_title,
+                division_id='{{}}/sldl:{{}}'.format(self.division_id, n))
+
+        yield legislature
+        yield upper
+        yield lower"""
+
+    sessions = [
+        {'identifier': k,
+            'classification': v['type'],
+            'name': v['display_name'],
+            '_scraped_name': v['_scraped_name'],
+            'start_date': v.get('start_date'),
+            'end_date': v.get('end_date'),
+         }
+        for k, v in sorted(metadata['session_details'].items(), reverse=True)
+    ]
+    sessions = json.dumps(sessions, sort_keys=True, indent=4,
+                          cls=DatetimeEncoder, separators=(',', ': '))
+    sessions = sessions.replace('"datetime', 'datetime').replace(')"', ')')
+    sessions = sessions.replace('null', 'None')
+
+    ignored = '        ' + '\n        '.join(
+        repr(x) + ',' for x in metadata['_ignored_scraped_sessions']
+    )
+
+    data = {
+        'abbr': metadata['abbreviation'],
+        'state': metadata['name'],
+        'classname': metadata['name'].replace(' ', ''),
+        'sessions': sessions,
+        'ignored': ignored,
+        'legislature_name': metadata['legislature_name'],
+        'lower_chamber_name': metadata['chambers']['lower']['name'],
+        'lower_title': metadata['chambers']['lower']['title'],
+        'lower_seats': lower_max,
+        'upper_chamber_name': metadata['chambers']['upper']['name'],
+        'upper_title': metadata['chambers']['upper']['title'],
+        'upper_seats': upper_max,
+    }
+    print(tmpl.format(**data))
+
+
+if __name__ == '__main__':
+    import sys
+    convert(sys.argv[1])
