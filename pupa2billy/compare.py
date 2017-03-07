67f68395db6057a188c598580ad2d70167b60c55
James Turk <james.p.turk@gmail.com>
2017-03-06 22:51:22 -0500

    fix up compare script

--- pupa2billy/compare.py
+++ pupa2billy/compare.py
@@ -1,72 +1,94 @@
 from __future__ import print_function
 import os
+import sys
 import json
 import glob
 import itertools
 
 
-def compare_dirs(dir1, dir2):
-    files1 = glob.glob(dir1 + '/*.json')
-    files2 = glob.glob(dir2 + '/*.json')
+class Comparator:
+    def __init__(self, objtype):
+        self.objtype = objtype
+        self.compared = 0
+        self.differed = 0
 
-    f1set = set(f.replace(dir1, '') for f in files1)
-    f2set = set(f.replace(dir2, '') for f in files2)
+    def summary(self):
+        print(self.objtype, self.differed, 'differed out of', self.compared)
 
-    only1 = f1set - f2set
-    only2 = f2set - f1set
+    def compare_json(self, key, val1, val2):
+        for k, v1 in val1.items():
+            v2 = val2.get(k)
+            if k == 'sponsors':
+                for v in itertools.chain(v1, v2):
+                    v.pop('chamber', None)
+            elif k == 'versions' or k == 'documents':
+                for v in itertools.chain(v1, v2):
+                    v.pop('date', None)
+            elif k == 'actions':
+                for v in itertools.chain(v1, v2):
+                    if 'other' in v['type']:
+                        v['type'] = [t for t in v['type'] if t != 'other']
+            if v1 != v2:
+                print(key, 'differ on', k, v1, '!=', v2)
+                self.differed += 1
+        self.compared += 1
 
-    if only1:
-        print('some files only found in', dir1)
-        for f in only1:
-            print('   ', f)
+    def load_json(self, dirname):
+        """ dirname => {key: jsonobj} """
+        files = glob.glob(os.path.join(dirname, self.objtype) + '/*.json')
+        return {os.path.basename(f): json.load(open(f)) for f in files}
 
-    if only2:
-        print('some files only found in', dir2)
-        for f in only2:
-            print('   ', f)
+    def compare(self, dir1, dir2):
+        files1 = self.load_json(dir1)
+        files2 = self.load_json(dir2)
 
-    if not only1 and not only2:
-        both = f1set & f2set
-        print(len(both), 'matched')
+        k1set = set(files1.keys())
+        k2set = set(files2.keys())
 
-        compare_files(files1, files2)
+        only1 = k1set - k2set
+        only2 = k2set - k1set
 
+        if only1:
+            print(self.objtype, 'some files only found in old')
+            for f in only1:
+                print('   ', f)
+        if only2:
+            print(self.objtype, 'some files only found in new')
+            for f in only2:
+                print('   ', f)
 
-def compare_json(val1, val2):
-    for k, v1 in val1.items():
-        v2 = val2.get(k)
-        if k == 'sponsors':
-            for v in itertools.chain(v1, v2):
-                v.pop('chamber', None)
-        elif k == 'versions' or k == 'documents':
-            for v in itertools.chain(v1, v2):
-                v.pop('date', None)
-        elif k == 'actions':
-            for v in itertools.chain(v1, v2):
-                if 'other' in v['type']:
-                    v['type'] = [t for t in v['type'] if t != 'other']
+        for key in k1set & k2set:
+            self.compare_json(key, files1[key], files2[key])
 
-        if v1 != v2:
-            print('differ on', k, v1, '!=', v2)
+        self.summary()
 
 
-def compare_files(files1, files2):
-    json1 = {os.path.basename(f): json.load(open(f)) for f in files1}
-    json2 = {os.path.basename(f): json.load(open(f)) for f in files2}
-
-    for key, val1 in json1.items():
-        val2 = json2[key]
-        compare_json(val1, val2)
+class VoteComparator(Comparator):
+    def load_json(self, dirname):
+        """ dirname => {key: jsonobj} """
+        files = glob.glob(os.path.join(dirname, self.objtype) + '/*.json')
+        blobs = [json.load(open(f)) for f in files]
+        all_json = {}
+        for d in blobs:
+            key = (d['bill_id'], d['motion'])
+            if key in all_json:
+                # TODO: possible to fix by keeping both around and checking them
+                print('duplicate key in votes, not all votes will get checked: ', key)
+            all_json[key] = d
+        return all_json
 
 
 def compare(dir1, dir2):
-    for subdir in ('bills', 'legislators', 'committees', 'events'):
-        compare_dirs(os.path.join(dir1, subdir), os.path.join(dir2, subdir))
+    for objtype in ('bills', 'legislators', 'committees', 'events'):
+        c = Comparator(objtype)
+        c.compare(dir1, dir2)
 
-    # don't compare vote filenames since they aren't static
+    vc = VoteComparator('votes')
+    vc.compare(dir1, dir2)
+
 
 if __name__ == '__main__':
-    # TODO: votes
-    # TODO: better reporting
-    # TODO: easier to run
-    compare('nc-billy/nc', 'nc-pupa2billy/nc')
+    if len(sys.argv) != 3:
+        print('./compare.py <dir1> <dir2>')
+        sys.exit(1)
+    compare(sys.argv[1], sys.argv[2])
