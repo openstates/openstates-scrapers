22146f864568966364b30fb1701f9201dafdbb67
Joshua Carp <jm.carp@gmail.com>
2017-03-12 13:38:54 -0400

    MI: Add pupa metadata.

--- flake8.sh
+++ flake8.sh
@@ -1,5 +1,5 @@
 python3 -m flake8 \
     openstates/fl \
+    openstates/mi \
     openstates/nc \
     pupa2billy
-
--- openstates/mi/__init__.py
+++ openstates/mi/__init__.py
@@ -1,65 +1,79 @@
-import lxml.html
-from billy.utils.fulltext import text_after_line_numbers
-from .bills import MIBillScraper
-from .legislators import MILegislatorScraper
-from .committees import MICommitteeScraper
-from .events import MIEventScraper
-
-settings = dict(SCRAPELIB_TIMEOUT=600)
-
-metadata = {
-    'name': 'Michigan',
-    'abbreviation': 'mi',
-    'capitol_timezone': 'America/New_York',
-    'legislature_name': 'Michigan Legislature',
-    'legislature_url': 'http://www.legislature.mi.gov',
-    'chambers': {
-        'upper': {'name': 'Senate', 'title': 'Senator'},
-        'lower': {'name': 'House', 'title': 'Representative'},
-    },
-    'terms': [
-        {'name': '2011-2012', 'sessions': ['2011-2012'],
-         'start_year': 2011, 'end_year': 2012},
-        {'name': '2013-2014', 'sessions': ['2013-2014'],
-         'start_year': 2013, 'end_year': 2014},
-        {'name': '2015-2016', 'sessions': ['2015-2016'],
-         'start_year': 2015, 'end_year': 2016},
-        {'name': '2017-2018', 'sessions': ['2017-2018'],
-         'start_year': 2017, 'end_year': 2018},
-    ],
-    'session_details': {
-        '2011-2012': {'type':'primary',
-                      'display_name': '2011-2012 Regular Session',
-                      '_scraped_name': '2011-2012',
-                     },
-        '2013-2014': {'type':'primary',
-                      'display_name': '2013-2014 Regular Session',
-                      '_scraped_name': '2013-2014',
-                     },
-        '2015-2016': {'type':'primary',
-                      'display_name': '2015-2016 Regular Session',
-                      '_scraped_name': '2015-2016',
-                     },
-        '2017-2018': {'type':'primary',
-                      'display_name': '2017-2018 Regular Session',
-                      '_scraped_name': '2017-2018',
-                     },
-    },
-    'feature_flags': ['subjects', 'events', 'influenceexplorer'],
-    '_ignored_scraped_sessions': ['2009-2010', '2007-2008', '2005-2006',
-                                  '2003-2004', '2001-2002', '1999-2000',
-                                  '1997-1998', '1995-1996']
-
-}
+from pupa.scrape import Jurisdiction, Organization
 
 
-def session_list():
-    from billy.scrape.utils import url_xpath
-    return url_xpath('http://www.legislature.mi.gov/mileg.aspx?'
-                     'page=LegBasicSearch', '//option/text()')
+class Michigan(Jurisdiction):
+    division_id = "ocd-division/country:us/state:mi"
+    classification = "government"
+    name = "Michigan"
+    url = "http://www.legislature.mi.gov"
+    scrapers = {
+    }
+    parties = [
+        {'name': 'Republican'},
+        {'name': 'Democratic'}
+    ]
+    legislative_sessions = [
+        {
+            "_scraped_name": "2011-2012",
+            "classification": "primary",
+            "identifier": "2011-2012",
+            "name": "2011-2012 Regular Session"
+        },
+        {
+            "_scraped_name": "2013-2014",
+            "classification": "primary",
+            "identifier": "2013-2014",
+            "name": "2013-2014 Regular Session"
+        },
+        {
+            "_scraped_name": "2015-2016",
+            "classification": "primary",
+            "identifier": "2015-2016",
+            "name": "2015-2016 Regular Session"
+        },
+        {
+            "_scraped_name": "2017-2018",
+            "classification": "primary",
+            "identifier": "2017-2018",
+            "name": "2017-2018 Regular Session"
+        }
+    ]
+    ignored_scraped_sessions = [
+        "2009-2010",
+        "2007-2008",
+        "2005-2006",
+        "2003-2004",
+        "2001-2002",
+        "1999-2000",
+        "1997-1998",
+        "1995-1996"
+    ]
 
+    def get_organizations(self):
+        legislature_name = "Michigan Legislature"
+        lower_chamber_name = "House"
+        lower_seats = 110
+        lower_title = "Representative"
+        upper_chamber_name = "Senate"
+        upper_seats = 38
+        upper_title = "Senator"
 
-def extract_text(doc, data):
-    doc = lxml.html.fromstring(data)
-    text = doc.xpath('//body')[0].text_content()
-    return text
+        legislature = Organization(name=legislature_name,
+                                   classification="legislature")
+        upper = Organization(upper_chamber_name, classification='upper',
+                             parent_id=legislature._id)
+        lower = Organization(lower_chamber_name, classification='lower',
+                             parent_id=legislature._id)
+
+        for n in range(1, upper_seats+1):
+            lower.add_post(
+                label=str(n), role=upper_title,
+                division_id='{}/sldu:{}'.format(self.division_id, n))
+        for n in range(1, lower_seats+1):
+            upper.add_post(
+                label=str(n), role=lower_title,
+                division_id='{}/sldl:{}'.format(self.division_id, n))
+
+        yield legislature
+        yield upper
+        yield lower
