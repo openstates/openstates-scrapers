4473fa7b7d4ea7dedd3929a525cab5d23a1d6407
Hitesh Garg <garg95hitesh@gmail.com>
2017-03-17 14:56:44 +0530

    DE: committee scraper for 2017

--- openstates/de/committees.py
+++ openstates/de/committees.py
@@ -1,10 +1,11 @@
 import re
 import lxml.html
-
+from pprint import pprint
 from billy.scrape.committees import CommitteeScraper, Committee
 from openstates.utils import LXMLMixin
 
-class DECommitteeScraper(CommitteeScraper,LXMLMixin):
+
+class DECommitteeScraper(CommitteeScraper, LXMLMixin):
     jurisdiction = "de"
 
     def scrape(self, chamber, term):
@@ -25,50 +26,62 @@ class DECommitteeScraper(CommitteeScraper,LXMLMixin):
         session = term2session[term]
 
         if chamber == 'lower':
-            #only scrape joint comms once
-            self.scrape_joint_committees(term,session)
+            # only scrape joint comms once
+            self.scrape_joint_committees(session)
 
+        # scrap upper and lower committees
         url = urls[chamber] % (session,)
+        self.scrape_comm(url, chamber)
+
+    def scrape_comm(self, url, chamber):
         data = self.post(url).json()['Data']
 
         for item in data:
-            committee = Committee(chamber, item['CommitteeName'])
+            pprint(item)
+            comm_name = item['CommitteeName']
+            committee = Committee(chamber, comm_name)
             chair_man = str(item.get('ChairName', 'No'))
             vice_chair = str(item.get('ViceChairName', 'No'))
             comm_id = item['CommitteeId']
-            comm_url = 'http://legis.delaware.gov/CommitteeDetail?' + \
-                        'committeeId='+ str(comm_id)
-            comm_page = lxml.html.fromstring(self.get(comm_url).text)
-            members = comm_page.xpath("//section[@class='section-short']/" + \
-                                      "div[@class='info-horizontal']/div" + \
-                                      "[@class='info-group']/div[@class" + \
-                                      "='info-value']/a/text()")
+            comm_url = self.get_comm_url(chamber, comm_id, comm_name)
+            members = self.scrape_member_info(comm_url)
 
-            committee.add_member(chair_man,'Chairman')
-            committee.add_member(vice_chair,'Vice-Chairman')
+            committee.add_member(chair_man, 'Chairman')
+            committee.add_member(vice_chair, 'Vice-Chair')
 
             for member in members:
-                if chair_man in member:
-                    committee.add_member(member,'Chairman')
-                elif vice_chair in member:
-                    committee.add_member(member,'Vice-Chairman')
-                else:
-                    committee.add_member(member)
+                # vice_chair and chair_man already added.
+                if chair_man not in member and vice_chair not in member:
+                    member = " ".join(member.split())
+                    if member:
+                        committee.add_member(member)
 
             committee.add_source(comm_url)
             committee.add_source(url)
-
+            #pprint(committee)
             self.save_committee(committee)
 
-
-    def scrape_joint_committees(self,term,session):
+    def scrape_joint_committees(self, session):
+        chamber = 'joint'
         url = 'http://legis.delaware.gov/json/Committees/' + \
-              'GetCommitteesByTypeId?assemblyId=%s&committeeTypeId=3'%(session,)
-        data = self.post(url).json()['Data']
+              'GetCommitteesByTypeId?assemblyId=%s&committeeTypeId=3' % (session, )
+        self.scrape_comm(url, chamber)
 
-        for item in data:
-            committee = Committee('joint', item['CommitteeName'])
-            committee.add_member(legislator=str(item.get('ChairName', 'No')),role='Chairman')
-            committee.add_member(legislator=str(item.get('ViceChairName', 'No')),role='Vice-Chairman')
-            committee.add_source(url)
-            self.save_committee(committee)
+    def scrape_member_info(self, comm_url):
+        comm_page = lxml.html.fromstring(self.get(comm_url).text)
+        # all members including chair_man and vice_chair
+        members = comm_page.xpath("//section[@class='section-short']/div[@class='info" +
+                                  "-horizontal']/div[@class='info-group']/div[@class=" +
+                                  "'info-value']//a/text()")
+        return members
+
+    def get_comm_url(self, chamber, comm_id, comm_name):
+        if chamber == 'joint':
+            # only Sunset url is not following pattern.
+            if comm_name == 'Joint Legislative Oversight and Sunset Committee':
+                comm_url = 'http://legis.delaware.gov/Sunset'
+            else:
+                comm_url = 'http://legis.delaware.gov/' + "".join(comm_name.split())
+        else:
+            comm_url = 'http://legis.delaware.gov/CommitteeDetail?committeeId=' + str(comm_id)
+        return comm_url
