0f0ea7be5098c13347ea0935e00717376770c2fb
Miles Watkins <miles.w.watkins@gmail.com>
2017-03-17 23:02:26 -0400

    VT: Port bills and votes to pupa

--- openstates/vt/bills.py
+++ openstates/vt/bills.py
@@ -4,42 +4,41 @@ import re
 
 import lxml.etree
 
-from billy.scrape.bills import Bill, BillScraper
-from billy.scrape.votes import Vote
+from pupa.scrape import Scraper, Bill, VoteEvent
 from openstates.utils import LXMLMixin
 
 
-class VTBillScraper(BillScraper, LXMLMixin):
-    jurisdiction = 'vt'
-
-    def scrape(self, session, chambers):
+class VTBillScraper(Scraper, LXMLMixin):
+    def scrape(self, session=None):
         HTML_TAGS_RE = r'<.*?>'
 
-        year_slug = session[5: ]
+        if session is None:
+            session = self.latest_session()
+        year_slug = session[5:]
 
         # Load all bills and resolutions via the private API
         bills_url = \
-                'http://legislature.vermont.gov/bill/loadBillsReleased/{}/'.\
-                format(year_slug)
+            'http://legislature.vermont.gov/bill/loadBillsReleased/{}/'.\
+            format(year_slug)
         bills_json = self.get(bills_url).text
         bills = json.loads(bills_json)['data'] or []
 
         bills_url = \
-                'http://legislature.vermont.gov/bill/loadBillsIntroduced/{}/'.\
-                format(year_slug)
+            'http://legislature.vermont.gov/bill/loadBillsIntroduced/{}/'.\
+            format(year_slug)
         bills_json = self.get(bills_url).text
         bills.extend(json.loads(bills_json)['data'] or [])
 
         resolutions_url = \
-                'http://legislature.vermont.gov/bill/loadAllResolutionsByChamber/{}/both'.\
-                format(year_slug)
+            'http://legislature.vermont.gov/bill/loadAllResolutionsByChamber/{}/both'.\
+            format(year_slug)
         resolutions_json = self.get(resolutions_url).text
         bills.extend(json.loads(resolutions_json)['data'] or [])
 
         # Parse the information from each bill
         for info in bills:
             # Strip whitespace from strings
-            info = { k:v.strip() for k, v in info.iteritems() }
+            info = {k:v.strip() for k, v in info.items()}
 
             # Identify the bill type and chamber
             if info['BillNumber'].startswith('J.R.H.'):
@@ -81,34 +80,34 @@ class VTBillScraper(BillScraper, LXMLMixin):
 
             else:
                 raise AssertionError(
-                        "Unknown bill type found: '{}'".
-                        format(info['BillNumber']))
+                    "Unknown bill type found: '{}'".
+                    format(info['BillNumber'])
+                )
 
             # Create the bill using its basic information
             bill = Bill(
-                    session=session,
-                    bill_id=info['BillNumber'],
-                    title=info['Title'],
-                    chamber=bill_chamber,
-                    type=bill_type
-                    )
+                identifier=info['BillNumber'],
+                legislative_session=session,
+                chamber=bill_chamber,
+                title=info['Title'],
+                classification=bill_type
+            )
             if 'resolution' in bill_type:
                 bill.add_source(resolutions_url)
             else:
                 bill.add_source(bills_url)
 
             # Load the bill's information page to access its metadata
-            bill_url = \
-                    'http://legislature.vermont.gov/bill/status/{0}/{1}'.\
-                    format(year_slug, info['BillNumber'])
+            bill_url = 'http://legislature.vermont.gov/bill/status/{0}/{1}'.\
+                format(year_slug, info['BillNumber'])
             doc = self.lxmlize(bill_url)
             bill.add_source(bill_url)
 
             # Capture sponsors
             sponsors = doc.xpath(
-                    '//dl[@class="summary-table"]/dt[text()="Sponsor(s)"]/'
-                    'following-sibling::dd[1]/ul/li'
-                    )
+                '//dl[@class="summary-table"]/dt[text()="Sponsor(s)"]/'
+                'following-sibling::dd[1]/ul/li'
+            )
             sponsor_type = 'primary'
             for sponsor in sponsors:
                 if sponsor.xpath('span/text()') == ['Additional Sponsors']:
@@ -116,50 +115,55 @@ class VTBillScraper(BillScraper, LXMLMixin):
                     continue
 
                 sponsor_name = sponsor.xpath('a/text()')[0].\
-                        replace("Rep.", "").replace("Sen.", "").strip()
+                    replace("Rep.", "").replace("Sen.", "").strip()
                 if sponsor_name and not \
-                        (sponsor_name[ :5] == "Less" and len(sponsor_name) == 5):
-                    bill.add_sponsor(sponsor_type, sponsor_name)
+                        (sponsor_name[:5] == "Less" and len(sponsor_name) == 5):
+                    bill.add_sponsorship(
+                        name=sponsor_name,
+                        classification=sponsor_type,
+                        entity_type='person',
+                        primary=(sponsor_type == 'primary')
+                    )
 
             # Capture bill text versions
             # Warning: There's a TODO in VT's source code saying 'move this to where it used to be'
             # so leave in the old and new positions
             versions = doc.xpath(
-                    '//dl[@class="summary-table"]/dt[text()="Bill/Resolution Text"]/'
-                    'following-sibling::dd[1]/ul/li/a |'
-                    '//ul[@class="bill-path"]//a'
-                    )
+                '//dl[@class="summary-table"]/dt[text()="Bill/Resolution Text"]/'
+                'following-sibling::dd[1]/ul/li/a |'
+                '//ul[@class="bill-path"]//a'
+            )
+
             for version in versions:
                 if version.xpath('text()'):
-                    bill.add_version(
-                            name=version.xpath('text()')[0],
-                            url=version.xpath('@href')[0].replace(' ', '%20'),
-                            mimetype='application/pdf'
-                            )
+                    bill.add_version_link(
+                        note=version.xpath('text()')[0],
+                        url=version.xpath('@href')[0].replace(' ', '%20'),
+                        media_type='application/pdf'
+                    )
 
             # Identify the internal bill ID, used for actions and votes
             # If there is no internal bill ID, then it has no extra information
             try:
                 internal_bill_id = re.search(
-                        r'"bill/loadBillDetailedStatus/{}/(\d+)"'.format(year_slug),
-                        lxml.etree.tostring(doc)
-                        ).group(1)
+                    r'"LegislationId": "(\d+)"',
+                    lxml.etree.tostring(doc).decode('utf-8')
+                ).group(1)
             except AttributeError:
-                self.warning("Bill {} appears to have no activity".\
-                        format(info['BillNumber']))
-                self.save_bill(bill)
+                self.warning("Bill {} appears to have no activity".format(info['BillNumber']))
+                yield bill
                 continue
 
             # Capture actions
             actions_url = 'http://legislature.vermont.gov/bill/loadBillDetailedStatus/{0}/{1}'.\
-                    format(year_slug, internal_bill_id)
+                format(year_slug, internal_bill_id)
             actions_json = self.get(actions_url).text
             actions = json.loads(actions_json)['data']
             bill.add_source(actions_url)
 
             chambers_passed = set()
             for action in actions:
-                action = { k:v.strip() for k, v in action.iteritems() }
+                action = {k:v.strip() for k, v in action.items()}
 
                 if "Signed by Governor" in action['FullStatus']:
                     actor = 'governor'
@@ -173,24 +177,27 @@ class VTBillScraper(BillScraper, LXMLMixin):
                 # Categorize action
                 if "Signed by Governor" in action['FullStatus']:
                     assert chambers_passed == set("HS")
-                    action_type = 'governor:signed'
+                    action_type = 'executive-signature'
                 elif actor == 'lower' and \
-                        any(x.lower().startswith('aspassed') for x in action['keywords'].split(';')):
-                    action_type = 'bill:passed'
+                    any(x.lower().startswith('aspassed') for x in action['keywords'].split(';')):
+                    action_type = 'passage'
                     chambers_passed.add("H")
                 elif actor == 'upper' and \
-                        any(x.lower().startswith(' aspassed') or x.lower().startswith('aspassed') for x in action['keywords'].split(';')):
-                    action_type = 'bill:passed'
+                    any(x.lower().startswith(' aspassed') or x.lower().startswith('aspassed') for x in action['keywords'].split(';')):
+                    action_type = 'passage'
                     chambers_passed.add("S")
                 else:
-                    action_type = 'other'
+                    action_type = None
 
                 bill.add_action(
-                        actor=actor,
-                        action=re.sub(HTML_TAGS_RE, "", action['FullStatus']),
-                        date=datetime.datetime.strptime(action['StatusDate'], '%m/%d/%Y'),
-                        type=action_type
-                        )
+                    description=re.sub(HTML_TAGS_RE, "", action['FullStatus']),
+                    date=datetime.datetime.strftime(
+                        datetime.datetime.strptime(action['StatusDate'], '%m/%d/%Y'),
+                        '%Y-%m-%d'
+                    ),
+                    chamber=actor,
+                    classification=action_type
+                )
 
             # Capture votes
             votes_url = 'http://legislature.vermont.gov/bill/loadBillRollCalls/{0}/{1}'.\
@@ -202,13 +209,13 @@ class VTBillScraper(BillScraper, LXMLMixin):
             for vote in votes:
                 roll_call_id = vote['VoteHeaderID']
                 roll_call_url = 'http://legislature.vermont.gov/bill/loadBillRollCallDetails/{0}/{1}'.\
-                        format(year_slug, roll_call_id)
+                    format(year_slug, roll_call_id)
                 roll_call_json = self.get(roll_call_url).text
                 roll_call = json.loads(roll_call_json)['data']
 
                 roll_call_yea = []
                 roll_call_nay = []
-                roll_call_other = []
+                roll_call_not_voting = []
                 for member in roll_call:
                     (member_name, _district) = member['MemberName'].split(" of ")
                     member_name = member_name.strip()
@@ -218,7 +225,7 @@ class VTBillScraper(BillScraper, LXMLMixin):
                     elif member['MemberVote'] == "Nay":
                         roll_call_nay.append(member_name)
                     else:
-                        roll_call_other.append(member_name)
+                        roll_call_not_voting.append(member_name)
 
                 if "Passed -- " in vote['FullStatus']:
                     did_pass = True
@@ -228,44 +235,39 @@ class VTBillScraper(BillScraper, LXMLMixin):
                     raise AssertionError("Roll call vote result is unclear")
 
                 # Check vote counts
-                yea_count = \
-                        int(re.search(r'Yeas = (\d+)', vote['FullStatus']).group(1))
-                nay_count = \
-                        int(re.search(r'Nays = (\d+)', vote['FullStatus']).group(1))
+                yea_count = int(re.search(r'Yeas = (\d+)', vote['FullStatus']).group(1))
+                nay_count = int(re.search(r'Nays = (\d+)', vote['FullStatus']).group(1))
 
-                vote_to_add = Vote(
-                        chamber=(
-                                'lower' if vote['ChamberCode'] == 'H'
-                                else 'upper'
-                                ),
-                        date=datetime.datetime.strptime(vote['StatusDate'], '%m/%d/%Y'),
-                        motion=re.sub(HTML_TAGS_RE, "", vote['FullStatus']).strip(),
-                        passed=did_pass,
-                        yes_count=yea_count,
-                        no_count=nay_count,
-                        other_count=len(roll_call_other)
-                        )
+                vote_to_add = VoteEvent(
+                    chamber=('lower' if vote['ChamberCode'] == 'H' else 'upper'),
+                    start_date=datetime.datetime.strftime(
+                        datetime.datetime.strptime(vote['StatusDate'], '%m/%d/%Y'),
+                        '%Y-%m-%d'
+                    ),
+                    motion_text=re.sub(HTML_TAGS_RE, "", vote['FullStatus']).strip(),
+                    result='pass' if did_pass else 'fail',
+                    classification='passage'
+                )
                 vote_to_add.add_source(roll_call_url)
+
+                vote_to_add.set_count('yes', yea_count)
+                vote_to_add.set_count('no', nay_count)
+                vote_to_add.set_count('not voting', len(roll_call_not_voting))
 
                 for member in roll_call_yea:
                     vote_to_add.yes(member)
                 for member in roll_call_nay:
                     vote_to_add.no(member)
-                for member in roll_call_other:
-                    vote_to_add.other(member)
+                for member in roll_call_not_voting:
+                    vote_to_add.vote('not voting', member)
 
-                try:
-                    vote_to_add.validate()
-                except ValueError as e:
-                    self.warning(e)
-
-                bill.add_vote(vote_to_add)
+                bill.add_vote_event(vote_to_add)
 
             # Capture extra information
-            # This is not in the OpenStates spec, but is available
+            # This was not in the billy spec, but is available
             # Not yet implemented
             # Witnesses: http://legislature.vermont.gov/bill/loadBillWitnessList/{year_slug}/{internal_bill_id}
             # Conference committee members: http://legislature.vermont.gov/bill/loadBillConference/{year_slug}/{bill_number}
             # Committee meetings: http://legislature.vermont.gov/committee/loadHistoryByBill/{year_slug}?LegislationId={internal_bill_id}
 
-            self.save_bill(bill)
+            yield bill
