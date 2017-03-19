e607159fe071dc3e555ade73898d76f9d6751ba4
Amit Nag <amit@Amits-MacBook-Pro.local>
2017-03-19 16:59:22 -0400

    AK: converted legislatures to people pupa

--- openstates/ak/__init__.py
+++ openstates/ak/__init__.py
@@ -1,83 +1,79 @@
-import datetime
-import lxml.html
-from billy.utils.fulltext import text_after_line_numbers
-from .bills import AKBillScraper
-from .legislators import AKLegislatorScraper
-from .committees import AKCommitteeScraper
-from .events import AKEventScraper
+from pupa.scrape import Jurisdiction, Organization
+from .people import AKPersonScraper
 
-settings = dict(SCRAPELIB_TIMEOUT=600)
-
-metadata = dict(
-    name='Alaska',
-    capitol_timezone='America/Anchorage',
-    abbreviation='ak',
-    legislature_name='Alaska State Legislature',
-    legislature_url='http://w3.legis.state.ak.us/',
-    chambers = {
-        'upper': {'name': 'Senate', 'title': 'Senator'},
-        'lower': {'name': 'House', 'title': 'Representative'},
-    },
-    terms=[
+class Alaska(Jurisdiction):
+    division_id = "ocd-division/country:us/state:ak"
+    classification = "government"
+    name = "Alaska"
+    url = "http://w3.legis.state.ak.us/"
+    scrapers = {
+        'people': AKPersonScraper,
+    }
+    parties = [
+        {'name': 'Republican'},
+        {'name': 'Democratic'}
+    ]
+    legislative_sessions = [
         {
-            'name': '27',
-            'sessions': ['27'],
-            'start_year': 2011,
-            'end_year': 2012
+            "_scraped_name": "The 27th Legislature (2011-2012)",
+            "identifier": "27",
+            "name": "27th Legislature (2011-2012)"
         },
         {
-            'name': '28',
-            'sessions': ['28'],
-            'start_year': 2013,
-            'end_year': 2014
+            "_scraped_name": "The 28th Legislature (2013-2014)",
+            "identifier": "28",
+            "name": "28th Legislature (2013-2014)"
         },
         {
-            'name': '29',
-            'sessions': ['29'],
-            'start_year': 2015,
-            'end_year': 2016
+            "_scraped_name": "The 29th Legislature (2015-2016)",
+            "identifier": "29",
+            "name": "29th Legislature (2015-2016)"
         },
         {
-            'name': '30',
-            'sessions': ['30'],
-            'start_year': 2017,
-            'end_year': 2018
-        },
-    ],
-    session_details={
-        #'26': {'display_name': '26th Legislature',
-        #       '_scraped_name': 'The 26th Legislature (2009-2010)'},
-        '27': {'display_name': '27th Legislature (2011-2012)',
-               '_scraped_name': 'The 27th Legislature (2011-2012)'},
-        '28': {'display_name': '28th Legislature (2013-2014)',
-               '_scraped_name': 'The 28th Legislature (2013-2014)'},
-        '29': {'display_name': '29th Legislature (2015-2016)',
-               '_scraped_name': 'The 29th Legislature (2015-2016)'},
-        '30': {'display_name': '30th Legislature (2017-2018)',
-               '_scraped_name': 'The 30th Legislature (2017-2018)',
-               'start_date': datetime.date(2017, 1, 17),
-               'end_date': datetime.date(2017, 4, 16)
-              },
-    },
-    _ignored_scraped_sessions=['The 26th Legislature (2009-2010)',
-                               'The 25th Legislature (2007-2008)',
-                               'The 24th Legislature (2005-2006)',
-                               'The 23rd Legislature (2003-2004)',
-                               'The 22nd Legislature (2001-2002)',
-                               'The 21st Legislature (1999-2000)',
-                               'The 20th Legislature (1997-1998)',
-                               'The 19th Legislature (1995-1996)',
-                               'The 18th Legislature (1993-1994)'],
-    feature_flags=['subjects', 'influenceexplorer'],
-)
+            "_scraped_name": "The 30th Legislature (2017-2018)",
+            "end_date": "2017-04-16",
+            "identifier": "30",
+            "name": "30th Legislature (2017-2018)",
+            "start_date": "2017-01-17"
+        }
+    ]
+    ignored_scraped_sessions = [
+        "The 26th Legislature (2009-2010)",
+        "The 25th Legislature (2007-2008)",
+        "The 24th Legislature (2005-2006)",
+        "The 23rd Legislature (2003-2004)",
+        "The 22nd Legislature (2001-2002)",
+        "The 21st Legislature (1999-2000)",
+        "The 20th Legislature (1997-1998)",
+        "The 19th Legislature (1995-1996)",
+        "The 18th Legislature (1993-1994)"
+    ]
 
-def session_list():
-    from billy.scrape.utils import url_xpath
-    return url_xpath('http://www.legis.state.ak.us/basis/start.asp',
-                     "//div[@id='rightnav']//a[contains(@href, 'start.asp?session=')]//nobr/text()")
+    def get_organizations(self):
+        legislature_name = "Alaska State Legislature"
+        lower_chamber_name = "House"
+        lower_seats = 40
+        lower_title = "Representative"
+        upper_chamber_name = "Senate"
+        upper_seats = 0
+        upper_title = "Senator"
 
-def extract_text(doc, data):
-    doc = lxml.html.fromstring(data)
-    text = doc.xpath('//pre')[0].text_content()
-    text = text_after_line_numbers(text)
-    return text
+        legislature = Organization(name=legislature_name,
+                                   classification="legislature")
+        upper = Organization(upper_chamber_name, classification='upper',
+                             parent_id=legislature._id)
+        lower = Organization(lower_chamber_name, classification='lower',
+                             parent_id=legislature._id)
+
+        for n in range(1, upper_seats+1):
+            upper.add_post(
+                label=str(n), role=upper_title,
+                division_id='{}/sldu:{}'.format(self.division_id, n))
+        for n in range(1, lower_seats+1):
+            lower.add_post(
+                label=str(n), role=lower_title,
+                division_id='{}/sldl:{}'.format(self.division_id, n))
+
+        yield legislature
+        yield upper
+        yield lower
