51cdc2b717a7d5cbda9df8b1c58d390c243bdbd3
Andrew Nichols <danweasel@gmail.com>
2017-03-30 13:09:44 +0700

    Split scrape_chamber method, apply pep8

--- openstates/ia/people.py
+++ openstates/ia/people.py
@@ -1,7 +1,6 @@
-import re
-
 from pupa.scrape import Person, Scraper
 import lxml.html
+import re
 
 
 class IAPersonScraper(Scraper):
@@ -15,8 +14,6 @@ class IAPersonScraper(Scraper):
             yield from self.scrape_chamber('lower')
 
     def scrape_chamber(self, chamber):
-        session_id = self.latest_session()
-
         url = "https://www.legis.iowa.gov/legislators/"
         if chamber == "lower":
             url += "house"
@@ -27,65 +24,77 @@ class IAPersonScraper(Scraper):
         page.make_links_absolute(url)
         table = page.xpath('//table[@id="sortableTable"]')[0]
         for link in table.xpath(".//a[contains(@href, 'legislator')]"):
-            name = link.text.strip()
-            leg_url = link.get('href')
-            district = link.xpath("string(../../td[3])")
-            party = link.xpath("string(../../td[4])")
-            email = link.xpath("string(../../td[5])")
+            yield from self.scrape_member(chamber, link)
 
-            if party == 'Democrat':
-                party = 'Democratic'
+    def scrape_member(self, chamber, link):
+        name = link.text.strip()
+        leg_url = link.get('href')
+        district = link.xpath("string(../../td[3])")
+        party = link.xpath("string(../../td[4])")
+        email = link.xpath("string(../../td[5])")
 
-            pid = re.search("personID=(\d+)", link.attrib['href']).group(1)
-            photo_url = ("https://www.legis.iowa.gov/photo"
-                         "?action=getPhoto&ga=%s&pid=%s" % (session_id, pid))
+        if party == 'Democrat':
+            party = 'Democratic'
 
-            leg = Person(
-                    name=name,
-                    primary_org=chamber,
-                    district=district,
-                    party=party,
-                    image=photo_url)
+        pid = re.search("personID=(\d+)", link.attrib['href']).group(1)
+        photo_url = ("https://www.legis.iowa.gov/photo"
+                     "?action=getPhoto&ga=%s&pid=%s" % (self.latest_session(), pid))
 
-            leg.add_link(url)
-            leg.add_source(url)
+        leg = Person(
+            name=name,
+            primary_org=chamber,
+            district=district,
+            party=party,
+            image=photo_url)
 
-            leg_page = lxml.html.fromstring(self.get(link.attrib['href']).text)
+        leg.add_link(leg_url)
+        leg.add_source(leg_url)
 
-            office_data = {
-                "Legislative E-mail:": "email",
-                "Home Phone:": "home_phone",
-                "Home Address:": "home_addr",
-                "Capitol Phone:": "office_phone",
-            }
-            metainf = {}
+        leg_page = lxml.html.fromstring(self.get(link.attrib['href']).text)
+        self.scrape_member_page(leg, leg_page)
+        yield leg
 
-            table ,= leg_page.xpath(
-                "//div[@class='legisIndent divideVert']/table"
-            )
-            for row in table.xpath(".//tr"):
-                try:
-                    key, value = (
-                        x.text_content().strip() for x in row.xpath("./td")
-                    )
-                except ValueError:
-                    continue
+    def scrape_member_page(self, leg, leg_page):
+        office_data = {
+            "Legislative E-mail:": "email",
+            "Home Phone:": "home_phone",
+            "Home Address:": "home_addr",
+            "Capitol Phone:": "office_phone",
+        }
+        metainf = {}
 
-                try:
-                    metainf[office_data[key]] = value
-                except KeyError:
-                    continue
+        table, = leg_page.xpath(
+            "//div[@class='legisIndent divideVert']/table"
+        )
+        for row in table.xpath(".//tr"):
+            try:
+                key, value = (
+                    x.text_content().strip() for x in row.xpath("./td")
+                )
+            except ValueError:
+                continue
 
-            if "home_phone" in metainf:
-                leg.add_contact_detail(type='voice', value=metainf['home_phone'], note='District Office')
-            
-            if "home_addr" in metainf:
-                leg.add_contact_detail(type='address', value=metainf['home_addr'], note='District Office')
+            try:
+                metainf[office_data[key]] = value
+            except KeyError:
+                continue
 
-            if "email" in metainf: 
-                leg.add_contact_detail(type='email', value=metainf['email'], note='Capitol Office')
-                
-            if "office_phone" in metainf:
-                leg.add_contact_detail(type='voice', value=metainf['office_phone'], note='Capitol Office')
+        if "home_phone" in metainf:
+            leg.add_contact_detail(type='voice',
+                                   value=metainf['home_phone'],
+                                   note='District Office')
 
-            yield leg
+        if "home_addr" in metainf:
+            leg.add_contact_detail(type='address',
+                                   value=metainf['home_addr'],
+                                   note='District Office')
+
+        if "email" in metainf:
+            leg.add_contact_detail(type='email',
+                                   value=metainf['email'],
+                                   note='Capitol Office')
+
+        if "office_phone" in metainf:
+            leg.add_contact_detail(type='voice',
+                                   value=metainf['office_phone'],
+                                   note='Capitol Office')
