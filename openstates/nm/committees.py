f64ee72214e2ef2e2932ee9363582b2af14a86f8
James Turk <james.p.turk@gmail.com>
2017-04-15 16:12:32 -0400

    NM: committees were losing chamber context

--- openstates/nm/committees.py
+++ openstates/nm/committees.py
@@ -43,92 +43,89 @@ class NMCommitteeScraper(Scraper, LXMLMixin):
                       'chamber_xpath': base_xpath.format('', '', '')}
         }
 
-        committee_urls = []
-
         for chamber in chambers:
             page = self.lxmlize(chamber_paths[chamber]['url'])
 
-            committee_urls = committee_urls + self.get_nodes(
-                page, chamber_paths[chamber]['chamber_xpath'])
+            committee_urls = self.get_nodes(page, chamber_paths[chamber]['chamber_xpath'])
 
-        for committee_url in committee_urls:
-            committee_page = self.lxmlize(committee_url)
+            for committee_url in committee_urls:
+                committee_page = self.lxmlize(committee_url)
 
-            c_name = committee_page.xpath(
-                '//li/a[contains(@id, "siteMapBreadcrumbs_lnkPage_")]')[
-                -1].text_content().strip()
+                c_name = committee_page.xpath(
+                    '//li/a[contains(@id, "siteMapBreadcrumbs_lnkPage_")]')[
+                    -1].text_content().strip()
 
-            if c_name:
-                members_xpath = ('//table[@id="MainContent_formView'
-                                 'CommitteeInformation_grid'
-                                 'ViewCommitteeMembers"]/tbody/tr')
-                member_nodes = self.get_nodes(committee_page, members_xpath)
+                if c_name:
+                    members_xpath = ('//table[@id="MainContent_formView'
+                                    'CommitteeInformation_grid'
+                                    'ViewCommitteeMembers"]/tbody/tr')
+                    member_nodes = self.get_nodes(committee_page, members_xpath)
 
-                tds = {
-                    'title': 0,
-                    'name': 1,
-                    'role': 3
-                }
+                    tds = {
+                        'title': 0,
+                        'name': 1,
+                        'role': 3
+                    }
 
-                members = []
+                    members = []
 
-                for member_node in member_nodes:
-                    m_title = member_node[tds['title']].text_content()
-                    m_name = \
-                        self.get_node(
-                            member_node[tds['name']],
-                            './/a[contains(@href, '
-                            '"/Members/Legislator?SponCode=")]'
-                        ).text_content()
+                    for member_node in member_nodes:
+                        m_title = member_node[tds['title']].text_content()
+                        m_name = \
+                            self.get_node(
+                                member_node[tds['name']],
+                                './/a[contains(@href, '
+                                '"/Members/Legislator?SponCode=")]'
+                            ).text_content()
 
-                    role = member_node[tds['role']].text_content()
+                        role = member_node[tds['role']].text_content()
 
-                    if m_title == 'Senator':
-                        m_chamber = 'upper'
-                    elif m_title == 'Representative':
-                        m_chamber = 'lower'
-                    else:
-                        m_chamber = None
-
-                    if role in ('Chair', 'Co-Chair', 'Vice Chair',
-                                'Member', 'Advisory', 'Ranking Member'):
-                        if chamber == 'joint':
-                            m_role = 'interim {}'.format(role.lower())
+                        if m_title == 'Senator':
+                            m_chamber = 'upper'
+                        elif m_title == 'Representative':
+                            m_chamber = 'lower'
                         else:
-                            m_role = role.lower()
+                            m_chamber = None
+
+                        if role in ('Chair', 'Co-Chair', 'Vice Chair',
+                                    'Member', 'Advisory', 'Ranking Member'):
+                            if chamber == 'joint':
+                                m_role = 'interim {}'.format(role.lower())
+                            else:
+                                m_role = role.lower()
+                        else:
+                            m_role = None
+
+                        if m_role:
+                            members.append(Member(name=m_name,
+                                                role=m_role,
+                                                chamber=m_chamber))
+
+                    # Interim committees are collected during the scraping
+                    # for joint committees, and most interim committees
+                    # have members from both chambers. However, a small
+                    # number of interim committees (right now, just 1) have
+                    # only members from one chamber, so the chamber is set
+                    # to their chamber instead of 'joint' for those
+                    # committees.
+                    if chamber == 'joint':
+                        m_chambers = set(
+                            [mem.chamber for mem in members])
+                        if len(m_chambers) == 1:
+                            chamber = m_chambers.pop()
+                    committee = Organization(name=clean_committee_name(c_name),
+                                            chamber=chamber,
+                                            classification='committee')
+                    for member in members:
+                        committee.add_member(member.name, member.role)
+                    committee.add_source(committee_url)
+                    if not committee._related:
+                        self.warning(
+                            'skipping blank committee {0} '
+                            'at {1}'.format(c_name, committee_url))
                     else:
-                        m_role = None
+                        yield committee
 
-                    if m_role:
-                        members.append(Member(name=m_name,
-                                              role=m_role,
-                                              chamber=m_chamber))
-
-                # Interim committees are collected during the scraping
-                # for joint committees, and most interim committees
-                # have members from both chambers. However, a small
-                # number of interim committees (right now, just 1) have
-                # only members from one chamber, so the chamber is set
-                # to their chamber instead of 'joint' for those
-                # committees.
-                if chamber == 'joint':
-                    m_chambers = set(
-                        [mem.chamber for mem in members])
-                    if len(m_chambers) == 1:
-                        chamber = m_chambers.pop()
-                committee = Organization(name=clean_committee_name(c_name),
-                                         chamber=chamber,
-                                         classification='committee')
-                for member in members:
-                    committee.add_member(member.name, member.role)
-                committee.add_source(committee_url)
-                if not committee._related:
-                    self.warning(
-                        'skipping blank committee {0} '
-                        'at {1}'.format(c_name, committee_url))
                 else:
-                    yield committee
-
-            else:
-                self.warning('No legislative committee found at '
-                             '{}'.format(committee_url))
+                    self.warning('No legislative committee found at '
+                                '{}'.format(committee_url))
