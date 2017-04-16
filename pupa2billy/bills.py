935eab9a600e8307f7d4797b0019bed280c8e82e
James Turk <james.p.turk@gmail.com>
2017-04-16 00:07:41 -0400

    add related entities on actions

--- pupa2billy/bills.py
+++ pupa2billy/bills.py
@@ -60,11 +60,20 @@ class PupaBillScraper(BillScraper):
 
         for action in data['actions']:
             actor = parse_psuedo_id(action['organization_id'])['classification']
+            legislators = []
+            committees = []
+            for rel in action['related_entities']:
+                if rel['entity_type'] == 'organization':
+                    committees.append(rel['name'])
+                elif rel['entity_type'] == 'person':
+                    legislators.append(rel['name'])
             bill.add_action(actor,
                             action['description'],
                             parse_date(action['date']),
-                            type=_action_categories(action['classification']))
-            # TODO: related entities
+                            type=_action_categories(action['classification']),
+                            committees=committees,
+                            legislators=legislators,
+                            )
 
         for source in data['sources']:
             bill.add_source(source['url'])
@@ -89,7 +98,9 @@ class PupaBillScraper(BillScraper):
         for title in data['other_titles']:
             bill.add_title(title['title'])
 
-        # TODO: related bills
-        # for related in data['related_bills']:
-
+        for related in data['related_bills']:
+            bill.add_companion(related['identifier'],
+                               related['legislative_session'],
+                               chamber
+                               )
         self.save_bill(bill)
