26c3e0f27cac5df95152bd94eda64636b527a7bc
Bikram Bharti <bikram.bharti.ece15@itbhu.ac.in>
2017-04-19 08:49:55 +0530

    metadata converted

--- openstates/ms/__init__.py
+++ openstates/ms/__init__.py
@@ -1,190 +1,198 @@
-import datetime
-import lxml.html
-from billy.scrape.utils import url_xpath
-from .bills import MSBillScraper
-from .legislators import MSLegislatorScraper
-from .committees import MSCommitteeScraper
+from pupa.scrape import Jurisdiction, Organization
+from openstates.utils import url_xpath
+from .people import MSLegislatorScraper
 
-metadata = {
-    'name': 'Mississippi',
-    'abbreviation': 'ms',
-    'legislature_name': 'Mississippi Legislature',
-    'legislature_url': 'http://www.legislature.ms.gov/',
-    'capitol_timezone': 'America/Chicago',
-    'chambers': {
-        'upper': {'name': 'Senate', 'title': 'Senator'},
-        'lower': {'name': 'House', 'title': 'Representative'},
-    },
-    'terms': [
+
+class Mississippi(Jurisdiction):
+    division_id = "ocd-division/country:us/state:ms"
+    classification = "government"
+    name = "Mississippi"
+    url = "http://www.legislature.ms.gov/"
+    scrapers = {
+        "people": MSLegislatorScraper
+    }
+    parties = [
+        {'name': 'Republican'},
+        {'name': 'Democratic'}
+    ]
+    legislative_sessions = [
         {
-            'name': '2008-2011',
-            'start_year': 2008,
-            'end_year': 2011,
-            'sessions': [
-                '2008',
-                '2009', '20091E', '20092E', '20093E',
-                '2010', '20101E', '20102E',
-                '2011', '20111E',
-            ],
+            "_scraped_name": "2008 Regular Session",
+            "identifier": "2008",
+            "name": "2008 Regular Session"
         },
         {
-            'name': '2012-2015',
-            'start_year': 2012, 'end_year': 2015,
-            'sessions': [
-                '2012',
-                '2013', '20131E', '20132E',
-                '2014', '20141E', '20142E',
-                '2015',
-            ],
+            "_scraped_name": "2009 Regular Session",
+            "identifier": "2009",
+            "name": "2009 Regular Session"
         },
         {
-            'name': '2016-2019',
-            'start_year': 2016, 'end_year': 2019,
-            'sessions': [
-                '2016', '20161E', '20162E', '2017'
-            ],
+            "_scraped_name": "2009 First Extraordinary Session",
+            "identifier": "20091E",
+            "name": "2009, 1st Extraordinary Session"
         },
-    ],
-    'session_details': {
-        '2008': {
-            'display_name': '2008 Regular Session',
-            '_scraped_name': '2008 Regular Session',
+        {
+            "_scraped_name": "2009 Second Extraordinary Session",
+            "identifier": "20092E",
+            "name": "2009, 2nd Extraordinary Session"
         },
-        '2009': {
-            'display_name': '2009 Regular Session',
-            '_scraped_name': '2009 Regular Session',
+        {
+            "_scraped_name": "2009 Third Extraordinary Session",
+            "identifier": "20093E",
+            "name": "2009, 3rd Extraordinary Session"
         },
-        '20091E': {
-            'display_name': '2009, 1st Extraordinary Session',
-            '_scraped_name': '2009 First Extraordinary Session',
+        {
+            "_scraped_name": "2010 Regular Session",
+            "identifier": "2010",
+            "name": "2010 Regular Session"
         },
-        '20092E': {
-            'display_name': '2009, 2nd Extraordinary Session',
-            '_scraped_name': '2009 Second Extraordinary Session',
+        {
+            "_scraped_name": "2010 First Extraordinary Session",
+            "identifier": "20101E",
+            "name": "2010, 1st Extraordinary Session"
         },
-        '20093E': {
-            'display_name': '2009, 3rd Extraordinary Session',
-            '_scraped_name': '2009 Third Extraordinary Session',
+        {
+            "_scraped_name": "2010 Second Extraordinary Session",
+            "identifier": "20102E",
+            "name": "2010, 2nd Extraordinary Session"
         },
-        '20101E': {
-            'display_name': '2010, 1st Extraordinary Session',
-            '_scraped_name': '2010 First Extraordinary Session',
+        {
+            "_scraped_name": "2011 Regular Session",
+            "identifier": "2011",
+            "name": "2011 Regular Session"
         },
-        '20102E': {
-            'display_name': '2010, 2nd Extraordinary Session',
-            '_scraped_name': '2010 Second Extraordinary Session',
+        {
+            "_scraped_name": "2011 First Extraordinary Session",
+            "identifier": "20111E",
+            "name": "2011, 1st Extraordinary Session"
         },
-        '2010': {
-            'display_name': '2010 Regular Session',
-            '_scraped_name': '2010 Regular Session',
+        {
+            "_scraped_name": "2012 Regular Session",
+            "identifier": "2012",
+            "name": "2012 Regular Session"
         },
-        '2011': {
-            'display_name': '2011 Regular Session',
-            '_scraped_name': '2011 Regular Session',
+        {
+            "_scraped_name": "2013 Regular Session",
+            "identifier": "2013",
+            "name": "2013 Regular Session"
         },
-        '20111E': {
-            'display_name': '2011, 1st Extraordinary Session',
-            '_scraped_name': '2011 First Extraordinary Session',
+        {
+            "_scraped_name": "2013 First Extraordinary Session",
+            "identifier": "20131E",
+            "name": "2013 First Extraordinary Session"
         },
-        '2012': {
-            'display_name': '2012 Regular Session',
-            '_scraped_name': '2012 Regular Session',
+        {
+            "_scraped_name": "2013 Second Extraordinary Session",
+            "identifier": "20132E",
+            "name": "2013 Second Extraordinary Session"
         },
-        '2013': {
-            'display_name': '2013 Regular Session',
-            '_scraped_name': '2013 Regular Session',
+        {
+            "_scraped_name": "2014 Regular Session",
+            "identifier": "2014",
+            "name": "2014 Regular Session"
         },
-        '20131E': {
-            'display_name': '2013 First Extraordinary Session',
-            '_scraped_name': '2013 First Extraordinary Session'
+        {
+            "_scraped_name": "2014 First Extraordinary Session",
+            "identifier": "20141E",
+            "name": "2014 First Extraordinary Session"
         },
-        '20132E': {
-            'display_name': '2013 Second Extraordinary Session',
-            '_scraped_name': '2013 Second Extraordinary Session'
+        {
+            "_scraped_name": "2014 Second Extraordinary Session",
+            "identifier": "20142E",
+            "name": "2014 Second Extraordinary Session"
         },
-        '2014': {
-            'display_name': '2014 Regular Session',
-            '_scraped_name': '2014 Regular Session'
+        {
+            "_scraped_name": "2015 Regular Session",
+            "identifier": "2015",
+            "name": "2015 Regular Session"
         },
-        '20141E': {
-            'display_name': '2014 First Extraordinary Session',
-            '_scraped_name': '2014 First Extraordinary Session'
+        {
+            "_scraped_name": "2016 Regular Session",
+            "classification": "primary",
+            "end_date": "2016-05-08",
+            "identifier": "2016",
+            "name": "2016 Regular Session",
+            "start_date": "2016-01-05"
         },
-        '20142E': {
-            'display_name': '2014 Second Extraordinary Session',
-            '_scraped_name': '2014 Second Extraordinary Session'
+        {
+            "_scraped_name": "2016 First Extraordinary Session",
+            "identifier": "20161E",
+            "name": "2016 First Extraordinary Session"
         },
-        '2015': {
-            'display_name': '2015 Regular Session',
-            '_scraped_name': '2015 Regular Session'
+        {
+            "_scraped_name": "2016 Second Extraordinary Session",
+            "identifier": "20162E",
+            "name": "2016 Second Extraordinary Session"
         },
-        '2016': {
-            'type': 'primary',
-            'start_date': datetime.date(2016, 1, 5),
-            'end_date': datetime.date(2016, 5, 8),
-            'display_name': '2016 Regular Session',
-            '_scraped_name': '2016 Regular Session',
-        },
-        '20161E': {
-            'display_name': '2016 First Extraordinary Session',
-            '_scraped_name': '2016 First Extraordinary Session'
-        },
-        '20162E': {
-            'display_name': '2016 Second Extraordinary Session',
-            '_scraped_name': '2016 Second Extraordinary Session'
-        },        
-        '2017': {
-            'type': 'primary',
-            'start_date': datetime.date(2017, 1, 3),
-            'end_date': datetime.date(2016, 4, 2),
-            'display_name': '2017 Regular Session',
-            '_scraped_name': '2017 Regular Session',
-        },
-    },
-    'feature_flags': ['subjects', 'influenceexplorer'],
-    '_ignored_scraped_sessions': [
-        '2008 First Extraordinary Session',
-        '2007 Regular Session',
-        '2007 First Extraordinary Session',
-        '2006 Regular Session',
-        '2006 First Extraordinary Session',
-        '2006 Second Extraordinary Session',
-        '2005 Regular Session',
-        '2005 First Extraordinary Session',
-        '2005 Second Extraordinary Session',
-        '2005 Third Extraordinary Session',
-        '2005 Fourth Extraordinary Session',
-        '2005 Fifth Extraordinary Session',
-        '2004 Regular Session',
-        '2004 First Extraordinary Session',
-        '2004 Second Extraordinary Session',
-        '2004 Third Extraordinary Session',
-        '2003 Regular Session',
-        '2002 Regular Session',
-        '2002 First Extraordinary Session',
-        '2002 Second Extraordinary Session',
-        '2002 Third Extraordinary Session',
-        '2001 Regular Session',
-        '2001 First Extraordinary Session',
-        '2001 Second Extraordinary Session',
-        '2000 Regular Session',
-        '2000 First Extraordinary Session',
-        '2000 Second Extraordinary Session',
-        '2000 Third Extraordinary Session',
-        '1999 Regular Session',
-        '1998 Regular Session',
-        '1997 Regular Session',
-    ],
-}
+        {
+            "_scraped_name": "2017 Regular Session",
+            "classification": "primary",
+            "end_date": "2016-04-02",
+            "identifier": "2017",
+            "name": "2017 Regular Session",
+            "start_date": "2017-01-03"
+        }
+    ]
+    ignored_scraped_sessions = [
+        "2008 First Extraordinary Session",
+        "2007 Regular Session",
+        "2007 First Extraordinary Session",
+        "2006 Regular Session",
+        "2006 First Extraordinary Session",
+        "2006 Second Extraordinary Session",
+        "2005 Regular Session",
+        "2005 First Extraordinary Session",
+        "2005 Second Extraordinary Session",
+        "2005 Third Extraordinary Session",
+        "2005 Fourth Extraordinary Session",
+        "2005 Fifth Extraordinary Session",
+        "2004 Regular Session",
+        "2004 First Extraordinary Session",
+        "2004 Second Extraordinary Session",
+        "2004 Third Extraordinary Session",
+        "2003 Regular Session",
+        "2002 Regular Session",
+        "2002 First Extraordinary Session",
+        "2002 Second Extraordinary Session",
+        "2002 Third Extraordinary Session",
+        "2001 Regular Session",
+        "2001 First Extraordinary Session",
+        "2001 Second Extraordinary Session",
+        "2000 Regular Session",
+        "2000 First Extraordinary Session",
+        "2000 Second Extraordinary Session",
+        "2000 Third Extraordinary Session",
+        "1999 Regular Session",
+        "1998 Regular Session",
+        "1997 Regular Session"
+    ]
 
+    def get_organizations(self):
+        legislature_name = "Mississippi Legislature"
+        upper_chamber_name = "Senate"
+        upper_seats = 52
+        upper_title = "Senator"
 
-def session_list():
-    return url_xpath('http://billstatus.ls.state.ms.us/sessions.htm',
-        '//a/text()')
+        legislature = Organization(name=legislature_name,
+                                   classification="legislature")
+        upper = Organization(upper_chamber_name, classification='upper',
+                             parent_id=legislature._id)
+        lower = Organization(lower_chamber_name, classification='lower',
+                             parent_id=legislature._id)
 
+        for n in range(1, upper_seats + 1):
+            upper.add_post(
+                label=str(n), role=upper_title,
+                division_id='{}/sldu:{}'.format(self.division_id, n))
+        for n in range(1, lower_seats + 1):
+            lower.add_post(
+                label=str(n), role=lower_title,
+                division_id='{}/sldl:{}'.format(self.division_id, n))
 
-def extract_text(doc, data):
-    doc = lxml.html.fromstring(data)
-    text = ' '.join(p.text_content() for p in
-        doc.xpath('//h2/following-sibling::p'))
-    return text
+        yield legislature
+        yield upper
+        yield lower
+
+    def session_list():
+        return url_xpath('http://billstatus.ls.state.ms.us/sessions.htm',
+                         '//a/text()')
