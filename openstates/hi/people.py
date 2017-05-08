c4c7183d030191917f180a9d24501d8915c77373
James Turk <james.p.turk@gmail.com>
2017-05-08 10:27:22 -0400

    HI: pupa people fixes

--- openstates/hi/people.py
+++ openstates/hi/people.py
@@ -1,7 +1,5 @@
 from pupa.scrape import Person, Scraper, Organization
-
 import lxml.html
-import re, contextlib
 
 
 HI_BASE_URL = "http://capitol.hawaii.gov"
@@ -16,14 +14,14 @@ def get_legislator_listing_url(chamber):
 
 class HIPersonScraper(Scraper):
 
-    def get_page( self, url ):
+    def get_page(self, url):
         html = self.get(url).text
         page = lxml.html.fromstring(html)
         return page
 
-    def scrape_homepage( self, url ):
-        page = self.get_page( url )
-        ret = { "source" : url, 'ctty' : [] }
+    def scrape_homepage(self, url):
+        page = self.get_page(url)
+        ret = {"source": url, 'ctty': []}
 
         table = page.xpath(
             "//table[@id='ctl00_ContentPlaceHolderCol1_GridViewMemberof']")
@@ -40,34 +38,34 @@ class HIPersonScraper(Scraper):
         ret['chamber'] = chamber
 
         if table is not None:
-            cttys = table.xpath( "./tr/td/a" )
+            cttys = table.xpath("./tr/td/a")
             for ctty in cttys:
                 ret['ctty'].append({
-                    "name" : ctty.text,
-                    "page" : "%s/%s" % (HI_BASE_URL, ctty.attrib['href']),
+                    "name": ctty.text,
+                    "page": "%s/%s" % (HI_BASE_URL, ctty.attrib['href']),
                 })
         return ret
 
-    def scrape_leg_page( self, url ):
+    def scrape_leg_page(self, url):
         page = self.get_page(url)
-        people = page.xpath( \
+        people = page.xpath(
             "//table[@id='ctl00_ContentPlaceHolderCol1_GridView1']")[0]
         people = people.xpath('./tr')[1:]
         display_order = {
-            "image"    : 0,
-            "contact"  : 1,
-            "district" : 2
+            "image": 0,
+            "contact": 1,
+            "district": 2
         }
 
         ret = []
 
         for person in people:
-            image    = person[display_order["image"]]
-            contact  = person[display_order["contact"]]
+            image = person[display_order["image"]]
+            contact = person[display_order["contact"]]
             district = person[display_order["district"]]
-            metainf  = self.scrape_contact_info( contact )
-            district = self.scrape_district_info( district )
-            homepage = self.scrape_homepage( metainf['homepage'] )
+            metainf = self.scrape_contact_info(contact)
+            district = self.scrape_district_info(district)
+            homepage = self.scrape_homepage(metainf['homepage'])
 
             image = "%s/%s" % (
                 HI_BASE_URL,
@@ -75,13 +73,13 @@ class HIPersonScraper(Scraper):
             )
 
             pmeta = {
-                "image"    : image,
-                "source"   : [ url ],
-                "district" : district,
+                "image": image,
+                "source": [url],
+                "district": district,
                 "chamber": None
             }
 
-            if homepage != None:
+            if homepage is not None:
                 pmeta['source'].append(homepage['source'])
                 pmeta['ctty'] = homepage['ctty']
                 pmeta['chamber'] = homepage['chamber']
@@ -95,9 +93,9 @@ class HIPersonScraper(Scraper):
             ret.append(pmeta)
         return ret
 
-    def br_split( self, contact ):
+    def br_split(self, contact):
         cel = []
-        els = [ cel ]
+        els = [cel]
 
         # krufty HTML requires stupid hacks
         elements = contact.xpath("./*")
@@ -106,32 +104,32 @@ class HIPersonScraper(Scraper):
                 cel = []
                 els.append(cel)
             else:
-                cel.append( element )
+                cel.append(element)
         return els
 
-    def scrape_district_info( self, district ):
+    def scrape_district_info(self, district):
         return district[2].text_content()
 
-    def scrape_contact_info( self, contact ):
-        homepage = "%s/%s" % ( # XXX: Dispatch a read on this page
+    def scrape_contact_info(self, contact):
+        homepage = "%s/%s" % (   # XXX: Dispatch a read on this page
             HI_BASE_URL,
             contact.xpath("./a")[0].attrib['href']
         )
 
-        els = self.br_split( contact )
+        els = self.br_split(contact)
 
-        def _scrape_title( els ):
+        def _scrape_title(els):
             return els[0].text_content()
 
-        def _scrape_name( els ):
+        def _scrape_name(els):
             lName = els[0].text_content()
             fName = els[2].text_content()
-            return "%s %s" % ( fName, lName )
+            return "%s %s" % (fName, lName)
 
-        def _scrape_party( els ):
+        def _scrape_party(els):
             party = {
-                "(D)" : "Democratic",
-                "(R)" : "Republican"
+                "(D)": "Democratic",
+                "(R)": "Republican"
             }
 
             try:
@@ -139,41 +137,41 @@ class HIPersonScraper(Scraper):
             except KeyError:
                 return "Other"
 
-        def _scrape_addr( els ):
+        def _scrape_addr(els):
             room_number = els[1].text_content()
-            slug        = els[0].text_content()
-            return "%s %s" % ( slug, room_number )
+            slug = els[0].text_content()
+            return "%s %s" % (slug, room_number)
 
-        def _scrape_room( els ):
+        def _scrape_room(els):
             return els[1].text_content()
 
-        def _scrape_phone( els ):
+        def _scrape_phone(els):
             return els[1].text_content()
 
-        def _scrape_fax( els ):
+        def _scrape_fax(els):
             return els[1].text_content()
 
-        def _scrape_email( els ):
+        def _scrape_email(els):
             return els[1].text_content()
 
         contact_entries = {
-            "title" : ( 0, _scrape_title ),
-            "name"  : ( 1, _scrape_name ),
-            "party" : ( 1, _scrape_party ),
-            "addr"  : ( 2, _scrape_addr ),
-            "room"  : ( 2, _scrape_room ),
-            "phone" : ( 3, _scrape_phone ),
-            "fax"   : ( 4, _scrape_fax ),
-            "email" : ( 5, _scrape_email )
+            "title": (0, _scrape_title),
+            "name": (1, _scrape_name),
+            "party": (1, _scrape_party),
+            "addr": (2, _scrape_addr),
+            "room": (2, _scrape_room),
+            "phone": (3, _scrape_phone),
+            "fax": (4, _scrape_fax),
+            "email": (5, _scrape_email)
         }
 
         ret = {
-            "homepage" : homepage
+            "homepage": homepage
         }
 
         for entry in contact_entries:
             index, callback = contact_entries[entry]
-            ret[entry] = callback( els[index] )
+            ret[entry] = callback(els[index])
         return ret
 
     def scrape_chamber(self, chamber=None):
@@ -200,33 +198,33 @@ class HIPersonScraper(Scraper):
                             image=leg['image'])
 
             for source in leg['source']:
-                person.add_source( source )
+                person.add_source(source)
 
             try:
                 for ctty in leg['ctty']:
-                    flag='Joint Legislative'
+                    flag = 'Joint Legislative'
                     if ctty['name'][:len(flag)] == flag:
                         ctty_chamber = "joint"
                     else:
                         ctty_chamber = chamber
 
-                    comm = Organization(name=ctty['name'],
-                                classification="committee",
-                                chamber=ctty_chamber)
-                    comm.add_member(person,role="member")
+                    comm = Organization(name=ctty['name'], classification="committee",
+                                        chamber=ctty_chamber)
+                    comm.add_member(person, role="member")
 
             except KeyError:
-                self.log( "XXX: Warning, %s has no scraped Committees" %
-                    leg['name'] )
+                self.warn("%s has no scraped Committees" % leg['name'])
 
             person.add_link(leg['homepage'])
-            person.add_source(leg['homepage'])
+
             if leg['addr']:
                 person.add_contact_detail(type='address', value=leg['addr'], note='Capitol Office')
             if leg['phone']:
                 person.add_contact_detail(type='voice', value=leg['phone'], note='Capitol Office')
-            if leg['phone']:
-                person.add_contact_detail(type='email', value=leg['phone'], note='Capitol Office')
+            if leg['email']:
+                person.add_contact_detail(type='email', value=leg['email'], note='Capitol Office')
+            if leg['fax']:
+                person.add_contact_detail(type='fax', value=leg['fax'], note='Capitol Office')
             yield person
 
     def scrape(self, chamber=None):
