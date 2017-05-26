a59a710f1720fcf776a5f59f533af73faeff4c38
James Turk <james.p.turk@gmail.com>
2017-05-25 23:20:27 -0700

    VI: WIP

--- openstates/vi/__init__.py
+++ openstates/vi/__init__.py
@@ -1,52 +1,65 @@
-from .bills import VIBillScraper
-#from .legislators import VTLegislatorScraper
-from .committees import VICommitteeScraper
-#from .events import VTEventScraper
+from pupa.scrape import Jurisdiction, Organization
 
-metadata = dict(
-    name='US Virgin Islands',
-    abbreviation='vi',
-    capitol_timezone='America/St_Thomas',
-    legislature_name='Senate of the Virgin Islands',
-    legislature_url='http://www.legvi.org/',
-    chambers = { 'upper': { 'name': 'Senate', 'title': 'Senator' } },
-    terms=[
-           {'name': '2013-2014',
-            'start_year': 2013,
-            'end_year': 2014,
-            'sessions': ['30']},
-           {'name': '2015-2016',
-            'start_year': 2015,
-            'end_year': 2016,
-            'sessions': ['31']},
-           {'name': '2017-2018',
-            'start_year': 2017,
-            'end_year': 2018,
-            'sessions': ['32']},
-           ],
-    session_details={'30': {'type': 'primary',
-                                   'display_name': '2013-2013 Regular Session',
-                                   '_scraped_name': '30'
-                                  },
-                     '31': {'type': 'primary',
-                                   'display_name': '2015-2016 Regular Session',
-                                   '_scraped_name': '31'
-                                  },
-                     '32': {'type': 'primary',
-                                   'display_name': '2017-2018 Regular Session',
-                                   '_scraped_name': '32'
-                                  },
-                     },
-    feature_flags=[],
-    _ignored_scraped_sessions= ['21', '22', '23', '24', '25', '26', '27', '28', '29']
 
-)
+class USVirginIslands(Jurisdiction):
+    division_id = "ocd-division/country:us/state:vi"
+    classification = "government"
+    name = "US Virgin Islands"
+    url = "TODO"
+    scrapers = {
+    }
+    parties = [
+        {'name': 'Republican'},
+        {'name': 'Democratic'}
+    ]
+    legislative_sessions = [
+        {
+            "_scraped_name": "30",
+            "classification": "primary",
+            "identifier": "30",
+            "name": "2013-2013 Regular Session"
+        },
+        {
+            "_scraped_name": "31",
+            "classification": "primary",
+            "identifier": "31",
+            "name": "2015-2016 Regular Session"
+        },
+        {
+            "_scraped_name": "32",
+            "classification": "primary",
+            "identifier": "32",
+            "name": "2017-2018 Regular Session"
+        }
+    ]
+    ignored_scraped_sessions = [
+        "21",
+        "22",
+        "23",
+        "24",
+        "25",
+        "26",
+        "27",
+        "28",
+        "29"
+    ]
 
-def session_list():
-    from billy.scrape.utils import url_xpath
-    return url_xpath(
-            'http://www.legvi.org/vilegsearch/',
-            '//select[@name="ctl00$ContentPlaceHolder$leginum"]/option/text()')
+    def get_organizations(self):
+        legislature_name = "Senate of the Virgin Islands"
+        upper_chamber_name = "Senate"
+        upper_seats = 0
+        upper_title = "Senator"
 
-def extract_text(doc, data):
-    return text_after_line_numbers(pdfdata_to_text(data))
+        legislature = Organization(name=legislature_name,
+                                   classification="legislature")
+        upper = Organization(upper_chamber_name, classification='upper',
+                             parent_id=legislature._id)
+
+        for n in range(1, upper_seats + 1):
+            upper.add_post(
+                label=str(n), role=upper_title,
+                division_id='{}/sldu:{}'.format(self.division_id, n))
+
+        yield legislature
+        yield upper
+
