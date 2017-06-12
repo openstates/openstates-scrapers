b89fc6b81bdd9ec71fe43b81743992ffbef36d9c
James Turk <james.p.turk@gmail.com>
2017-06-12 15:07:41 -0400

    IA: fix vote bill ids

--- openstates/ia/votes.py
+++ openstates/ia/votes.py
@@ -148,6 +148,8 @@ class IAVoteScraper(Scraper):
 
                 bill_id = bill_id.replace(word, letter)
 
+            bill_id = bill_id.replace('.', '')
+
             bill_chamber = dict(h='lower', s='upper')[bill_id.lower()[0]]
             self.current_id = bill_id
             votes, passed = self.parse_votes(lines)
