0b917724eb894b1db9c5a8df1f9de668639ebfba
Saxon Stiller <saxon.stiller@gmail.com>
2017-12-01 19:47:43 -0500

    remove indexcode 1130 skip as it is working now with chunked encoding prevented

--- openstates/sc/bills.py
+++ openstates/sc/bills.py
@@ -116,12 +116,6 @@ class SCBillScraper(Scraper):
         for option in doc.xpath('//option')[2:]:
             subject = option.text
             code = option.get('value')
-
-            if code == '1130':
-                # added Nov 2017, this code broke the scrape for a few weeks
-                self.warning('skipping 1130 - broken page')
-                continue
-
             url = '%s?AORB=B&session=%s&indexcode=%s' % (subject_search_url,
                                                          session_code, code)
             data = self.get(url).text
