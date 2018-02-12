dd288f29cde441da5c1dc4e742abb91cd999b2ee
Miles Watkins <miles.w.watkins@gmail.com>
2018-02-12 13:18:30 -0700

    OR: Re-enable vote scraping now that there are votes present; closes #2113

--- openstates/or/__init__.py
+++ openstates/or/__init__.py
@@ -2,7 +2,7 @@ from pupa.scrape import Jurisdiction, Organization
 from .people import ORPersonScraper
 from .committees import ORCommitteeScraper
 from .bills import ORBillScraper
-# from .votes import ORVoteScraper
+from .votes import ORVoteScraper
 
 
 class Oregon(Jurisdiction):
@@ -14,7 +14,7 @@ class Oregon(Jurisdiction):
         'people': ORPersonScraper,
         'committees': ORCommitteeScraper,
         'bills': ORBillScraper,
-        # 'votes': ORVoteScraper
+        'votes': ORVoteScraper
     }
     parties = [
         {'name': 'Republican'},
