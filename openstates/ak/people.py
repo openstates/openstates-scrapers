397eba3e55c78799d27f978b61e62f0403422dbf
James Turk <james.p.turk@gmail.com>
2018-02-19 11:35:31 -0500

    AK: remove Lincoln special case

--- openstates/ak/people.py
+++ openstates/ak/people.py
@@ -129,13 +129,7 @@ class AKPersonScraper(Scraper, LXMLMixin):
             person.add_source(leg_url)
             person.add_link(leg_url)
 
-            # scrape offices
-            if district == '40':
-                assert requests.get(leg_url).status_code == 500, \
-                        "Remove special-casing for John Lincoln"
-                # Lincoln is missing a personal page, so don't scrape it
-            else:
-                self._scrape_offices(person, leg_url, email)
+            self._scrape_offices(person, leg_url, email)
 
             yield person
 
