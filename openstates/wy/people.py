538cf07a906a4efb35b982d240444fb4c7c5ca96
Tim Showers <showerst@gmail.com>
2018-05-15 14:00:06 -0400

    WY: Add address, extras to person scraper

--- openstates/wy/people.py
+++ openstates/wy/people.py
@@ -1,12 +1,12 @@
-import re
 import json
+import datetime
 
 import lxml.html
 from pupa.scrape import Scraper, Person
 
 
 class WYPersonScraper(Scraper):
-    party_map = {'R':'republican', 'D':'democrat', 'I':'independent'}
+    party_map = {'R': 'republican', 'D': 'democrat', 'I': 'independent'}
 
     def scrape(self, chamber=None, session=None):
         session = self.latest_session()
@@ -19,22 +19,48 @@ class WYPersonScraper(Scraper):
     def scrape_chamber(self, chamber, session):
         chamber_abbrev = {'upper': 'S', 'lower': 'H'}[chamber]
 
-        url = "https://wyoleg.gov/LsoService/api/legislator/2018/{}".format(chamber_abbrev)
+        url = "https://wyoleg.gov/LsoService/api/legislator/2018/{}".format(
+            chamber_abbrev)
 
         response = self.get(url)
         people_json = json.loads(response.content.decode('utf-8'))
 
         for row in people_json:
+
+            # some fields are only available in the list json, some only in the details call
+            details_url = 'https://wyoleg.gov/LsoService/api/legislator/{}'.format(
+                row['legID'])
+            details_response = self.get(details_url)
+            details = json.loads(details_response.content.decode('utf-8'))
+
             party = self.party_map[row['party']]
+
+
+            dob = datetime.datetime.strptime(
+                details['dob'], '%m/%d/%Y %I:%M:%S %p')
+
+            dob_str = datetime.datetime.strftime(dob, "%Y-%m-%d")
+
+            photo_url = 'http://wyoleg.gov/LegislatorSummary/Photos/{}'.format(
+                details['legPhoto'])
 
             person = Person(
                 name=row['name'],
                 district=row['district'],
                 party=party,
                 primary_org=chamber,
-                # given_name='first',
-                # family_name='last',
+                birth_date=dob_str,
+                image=photo_url,
             )
+
+            if details['address']:
+                address = '{}, {} {} {}'.format(
+                    details['address'],
+                    details['city'],
+                    details['state'],
+                    details['zip']
+                )
+                person.add_contact_detail(type='address', value=address)
 
             if row['eMail']:
                 person.add_contact_detail(type='email', value=row['eMail'])
@@ -44,12 +70,25 @@ class WYPersonScraper(Scraper):
 
             person.extras['wy_leg_id'] = row['legID']
             person.extras['county'] = row['county']
+            person.extras['given_name'] = row['firstName']
+            person.extras['family_name'] = row['lastName']
+            person.extras['religion'] = details['religion']
+            person.extras['number_children'] = details['noChildren']
+            person.extras['spouse_given_name'] = details['spouseName']
+            person.extras['place_of_birth'] = details['birthPlace']
+            person.extras['occupation'] = details['occupationDesc']
+
+            if details['legEducation']:
+                person.extras['education'] = details['legEducation']
+
+            if details['civicOrgs']:
+                person.extras['civic_organizations'] = details['civicOrgs']
 
             # http://wyoleg.gov/Legislators/2018/S/2032
             leg_url = 'http://wyoleg.gov/Legislators/{}/{}/{}'.format(
-                        session,
-                        row['party'],
-                        row['legID'])
+                session,
+                row['party'],
+                row['legID'])
 
             person.add_source(leg_url)
             person.add_link(leg_url)
