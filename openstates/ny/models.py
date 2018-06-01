a4741d262a577d2847a5c1d62e4a8c44e8e874cd
Bikram Bharti <bikram.bharti99@gmail.com>
2018-06-01 10:03:59 +0530

    vote build changes in models

--- openstates/ny/models.py
+++ openstates/ny/models.py
@@ -1,14 +1,12 @@
-import collections
-import datetime
 import re
 import inspect
 
-from itertools import islice
-
-from billy.scrape.votes import Vote
 from openstates.utils import LXMLMixin
-from .utils import Urls
+from pupa.scrape import VoteEvent
 
+from .utils import Urls
+import datetime
+from pytz import timezone
 
 class AssemblyBillPage(LXMLMixin):
     '''
@@ -38,23 +36,27 @@ class AssemblyBillPage(LXMLMixin):
             'senate': senate_url})
 
     def _scrub_name(self, name):
-        junk = (re.compile(rgx) for rgx in [
+        junk = [
             r'^Rules\s+',
             '\(2nd Vice Chairperson\)',
             '\(MS\)',
             'Assemblyman',
             'Assemblywoman',
-            'Senator'])
+            'Senator']
         for rgx in junk:
-            name = rgx.sub('', name, re.I)
+            name = re.sub(rgx, '', name, re.I)
 
         # Collapse whitespace.
         name = re.sub('\s+', ' ', name)
         return name.strip('(), ')
 
-    def _build_sponsors_memo(self):
-        url = self.shared_url + '&Memo=Y'
-        self.bill.add_document('Sponsor\'s Memorandum', url)
+    def build(self):
+        '''Run all the build_* functions.
+        '''
+        for name, member in inspect.getmembers(self):
+            if inspect.ismethod(member):
+                if name.startswith('_build_'):
+                    yield member()
 
     def _build_lower_votes(self):
         url = self.shared_url + '&Floor%26nbspVotes=Y'
@@ -69,7 +71,7 @@ class AssemblyBillPage(LXMLMixin):
             pre = doc.xpath('//pre')[0].text_content().strip()
 
             no_votes = 'There are no votes for this bill in this legislative '
-            'session.'
+
 
             if pre == no_votes:
                 raise ValueError('No votes for this bill.')
@@ -77,48 +79,49 @@ class AssemblyBillPage(LXMLMixin):
         except (IndexError, ValueError) as e:
             return
 
-        actual_vote = collections.defaultdict(list)
         for table in doc.xpath('//table'):
 
             date = table.xpath('caption/span[contains(., "DATE:")]')
             date = next(date[0].itersiblings()).text
             date = datetime.datetime.strptime(date, '%m/%d/%Y')
+            date = date.replace(tzinfo=timezone('UTC'))
 
             votes = table.xpath('caption/span/span')[0].text.split(':')[1].split('/')
             yes_count, no_count = map(int, votes)
-
             passed = yes_count > no_count
-            vote = Vote('lower', date, 'Floor Vote', passed, yes_count,
-                        no_count, other_count=0)
+            vote = VoteEvent(
+                chamber='lower',
+                start_date=date,
+                motion_text='Floor Vote',
+                bill=self.bill,
+                result='pass' if passed else 'fail',
+                classification='passage'
+            )
 
+            vote.set_count('yes', yes_count)
+            vote.set_count('no', no_count)
+            absent_count = 0
+            excused_count = 0
             tds = table.xpath('tr/td/text()')
-            votes = iter(tds)
-            while True:
-                try:
-                    data = list(islice(votes, 2))
-                    name, vote_val = data
-                except (StopIteration, ValueError):
-                    # End of data. Stop.
-                    break
-                name = self._scrub_name(name)
+            votes = [tds[i:i+2] for i in range(0,len(tds),2)]
+            print(len(votes))
 
-                if vote_val.strip() == 'Y':
-                    vote.yes(name)
-                elif vote_val.strip() in ('N', 'NO'):
-                    vote.no(name)
-                else:
-                    vote.other(name)
-                    actual_vote[vote_val].append(name)
+            vote_dictionary  = {
+                'Y':'yes',
+                'NO':'no',
+                'ER': 'excused',
+                'AB': 'absent',
+                'NV': 'not voting'
+            }
+            for vote_pair in votes:
+                name, vote_val = vote_pair
+                vote.vote(vote_dictionary[vote_val], name)
+                if vote_val == 'AB':
+                    absent_count += 1
+                elif vote_val == 'ER':
+                    excused_count += 1
 
-            # The page doesn't provide an other_count.
-            vote['other_count'] = len(vote['other_votes'])
-            vote['actual_vote'] = actual_vote
-            self.bill.add_vote(vote)
-
-    def build(self):
-        '''Run all the build_* functions.
-        '''
-        for name, member in inspect.getmembers(self):
-            if inspect.ismethod(member):
-                if name.startswith('_build_'):
-                    member()
+            vote.set_count('absent', absent_count)
+            vote.set_count('excused', excused_count)
+            vote.add_source(url)
+            yield vote    
