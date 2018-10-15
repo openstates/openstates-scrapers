1cfcffdccfe320b49aeb7f67b0f608a5839e773c
Tim Showers <showerst@gmail.com>
2018-10-15 11:55:42 -0400

    DC: Update person scraper for new website

--- openstates/dc/people.py
+++ openstates/dc/people.py
@@ -5,9 +5,10 @@ import lxml.html
 
 def get_field(doc, key):
     # get text_content of parent of the element containing the key
-    elem = doc.xpath('//div[@id="member-info"]/p/strong[text()="%s"]/..' % key)
+    # elem = doc.xpath('//div[@id="member-info"]/p/strong[text()="%s"]/..' % key)
+    elem = doc.xpath('//li[contains(@class,"column")]/h4[text()="%s"]/../p')
     if elem:
-        return elem[0].text_content().replace(key, '').strip()
+        return elem[0].text_content().strip()
     else:
         return ''
 
@@ -15,7 +16,7 @@ def get_field(doc, key):
 class DCPersonScraper(Scraper):
 
     def scrape(self):
-        council_url = 'http://dccouncil.us/council'
+        council_url = 'http://dccouncil.us/councilmembers/'
         data = self.get(council_url).text
         doc = lxml.html.fromstring(data)
         doc.make_links_absolute(council_url)
@@ -29,42 +30,45 @@ class DCPersonScraper(Scraper):
             doc = lxml.html.fromstring(data)
             doc.make_links_absolute(url)
 
-            descriptor = doc.xpath('//p[@class="head-descriptor"]/text()')[0]
-            title_name = doc.xpath('//h2/text()')[0]
+            descriptor = doc.xpath('//div[contains(@class,"media-object-section")]/p[contains(@class,"h4")]/text()')[0]
+            title_name = doc.xpath('//div[contains(@class,"media-object-section")]/h1/text()')[0]
 
             # removes the title that is prepended to the name
             name = re.sub(r'^Councilmember ', '', title_name)
 
-            if 'Chairman' in descriptor:
+            if 'chairman' in descriptor.lower():
                 district = 'Chairman'
-            else:
-                district = get_field(doc, 'Represents: ')
-
-            if not district:
+            elif 'at-large' in descriptor.lower():
                 district = 'At-Large'
+            else:
+                district = descriptor.split("&bullet;")[1].strip()
 
             # party
-            party = get_field(doc, "Political Affiliation:")
+            party = get_field(doc, "Political Affiliation")
             if 'Democratic' in party:
                 party = 'Democratic'
+            elif 'Republican' in party:
+                party = 'Republican'
             else:
                 party = 'Independent'
 
-            photo_url = doc.xpath('//div[@id="member-thumb"]/img/@src')
+            photo_url = doc.xpath('//figure/a/img/@src')
             if photo_url:
                 photo_url = photo_url[0]
             else:
                 photo_url = ''
 
-            office_address = get_field(doc, "Office:")
-            phone = get_field(doc, "Tel:")
-            if phone.endswith('| Fax:'):
-                fax = None
-                phone = phone.strip('| Fax:') or None
-            else:
-                phone, fax = phone.split(' | Fax: ')
+            office_address = get_field(doc, "Office")
 
-            email = doc.xpath('//a[starts-with(text(), "Send an email")]/@href')[0].split(':')[1]
+            faxes = doc.xpath('//p[@class="byline"]/text()')
+            fax = faxes[-1].strip()
+
+            email = doc.xpath('//p[@class="byline"]/a[@class="contact-link"]')[0].text_content().strip()
+            phone = doc.xpath('//p[@class="byline"]/a[@class="contact-link"]')[1].text_content().strip()
+
+            bio = "\n".join(doc.xpath('//div[contains(@class,"js-hide")]/p/text()')).strip()
+            if doc.xpath('//p[contains(@class,"page-summary")]'):
+                short_bio = doc.xpath('//p[contains(@class,"page-summary")]')[0].text_content().strip()
 
             person = Person(
                 name=name,
@@ -72,6 +76,8 @@ class DCPersonScraper(Scraper):
                 image=photo_url,
                 primary_org='legislature',
                 district=str(district),
+                biography=bio,
+                summary=short_bio
             )
 
             person.add_source(url)
