d9b4cd6e560b58bc61c59b446faded3d2d6fbaa9
James Turk <james.p.turk@gmail.com>
2018-10-20 00:09:17 -0400

    clean up old pupa2billy unused bits

--- pupa2billy/run.py
+++ pupa2billy/run.py
@@ -3,8 +3,6 @@ import os
 import sys
 import shutil
 import importlib
-from .legislators import PupaLegislatorScraper
-from .committees import PupaCommitteeScraper
 from .bills import PupaBillScraper
 from .votes import PupaVoteScraper
 from .settings import BILLY_DATA_DIR
@@ -22,16 +20,8 @@ if __name__ == '__main__':
     except OSError:
         pass
 
-    os.makedirs(os.path.join(juris_dir, 'legislators'))
-    os.makedirs(os.path.join(juris_dir, 'committees'))
     os.makedirs(os.path.join(juris_dir, 'bills'))
     os.makedirs(os.path.join(juris_dir, 'votes'))
-
-    ls = PupaLegislatorScraper(metadata, juris_dir, jurisdiction=jurisdiction)
-    ls.scrape()
-
-    cs = PupaCommitteeScraper(metadata, juris_dir, jurisdiction=jurisdiction)
-    cs.scrape()
 
     bs = PupaBillScraper(metadata, juris_dir, jurisdiction=jurisdiction)
     bs.scrape()
