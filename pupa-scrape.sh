8e1d4cdb0318ae72ee0bf7061b6de0c6f5f9af33
James Turk <james.p.turk@gmail.com>
2018-11-09 15:16:54 -0500

    disabling billy updates, more complete removal coming soon

--- pupa-scrape.sh
+++ pupa-scrape.sh
@@ -17,7 +17,7 @@ if [ "$SKIP_BILLY" = true ]; then
   exit 0
 fi
 
-export PUPA_DATA_DIR='../openstates/_data'
-export PYTHONPATH=./billy_metadata/
-$BILLY_ENV/bin/python -m pupa2billy.run $state
-$BILLY_ENV/bin/billy-update $state --import --report
+# export PUPA_DATA_DIR='../openstates/_data'
+# export PYTHONPATH=./billy_metadata/
+# $BILLY_ENV/bin/python -m pupa2billy.run $state
+# $BILLY_ENV/bin/billy-update $state --import --report
