11519ff496bc6df399cb14bbac41b56770901da9
Miles Watkins <miles.w.watkins@gmail.com>
2019-03-22 10:13:20 -0400

    CA: Tweak MySQL error message and timing

--- openstates/ca/download.sh
+++ openstates/ca/download.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-MAX_SECONDS_TO_WAIT=60
+MAX_SECONDS_TO_WAIT=30
 
 seconds_waited=0
 until mysql -h "${MYSQL_HOST:-"localhost"}" -u "${MYSQL_USER:-"root"}" -e "SHOW DATABASES"; do
@@ -8,7 +8,7 @@ until mysql -h "${MYSQL_HOST:-"localhost"}" -u "${MYSQL_USER:-"root"}" -e "SHOW 
 
   seconds_waited=$((seconds_waited+1))
   if [ "$seconds_waited" -gt "$MAX_SECONDS_TO_WAIT" ]; then
-    echo "Error: California's MySQL was not available after $MAX_SECONDS_TO_WAIT seconds; exiting"
+    echo "ERROR: California's MySQL was not available after $MAX_SECONDS_TO_WAIT seconds; exiting"
     exit 1
   fi
 done
