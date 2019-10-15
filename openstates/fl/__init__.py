8e448593c978353a2f1bc4e90d25b7830723a653
Tim Showers <showerst@gmail.com>
2019-10-15 10:38:53 -0400

    FL: Ignore empty informational session

--- openstates/fl/__init__.py
+++ openstates/fl/__init__.py
@@ -59,6 +59,7 @@ class Florida(Jurisdiction):
     ]
     ignored_scraped_sessions = [
         *(str(each) for each in range(1997, 2010)),
+        '2019 I',  # Empty, maybe informational session
         '2010', '2010A', '2010O',
         '2012O',
         '2014O',
