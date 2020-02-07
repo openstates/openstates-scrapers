8e9ba2813bb966c0568d111d3ff39873126cb11b
James Turk <dev@jamesturk.net>
2020-02-07 07:48:03 -0500

    ID: import fix

--- openstates/id/__init__.py
+++ openstates/id/__init__.py
@@ -1,6 +1,6 @@
 from .people import IDPersonScraper
 from .bills import IDBillScraper
-from openstates.utils.lxmlize import url_xpath, State
+from openstates.utils import url_xpath, State
 
 # from .committees import IDCommitteeScraper
 
