f392419d91fcca7bfe0fa2b7b9762450db66b1a5
schneidy <schneiderman.daniel@gmail.com>
2020-03-02 17:23:54 -0500

    CA: Added rest of archived data years.

--- openstates/ca/__init__.py
+++ openstates/ca/__init__.py
@@ -34,6 +34,42 @@ class California(Jurisdiction):
         },
         {
             "classification": "primary",
+            "identifier": "19911992",
+            "name": "1991-1992 Regular Session",
+            "start_date": "1990-12-03",
+        },
+        {
+            "classification": "primary",
+            "identifier": "19931994",
+            "name": "1993-1994 Regular Session",
+            "start_date": "1992-12-07",
+        },
+        {
+            "classification": "primary",
+            "identifier": "19951996",
+            "name": "1995-1996 Regular Session",
+            "start_date": "1994-12-05",
+        },
+        {
+            "classification": "primary",
+            "identifier": "19971998",
+            "name": "1997-1998 Regular Session",
+            "start_date": "1996-12-02",
+        },
+        {
+            "classification": "primary",
+            "identifier": "19992000",
+            "name": "1999-2000 Regular Session",
+            "start_date": "1998-12-07",
+        },
+        {
+            "classification": "primary",
+            "identifier": "20012002",
+            "name": "2001-2002 Regular Session",
+            "start_date": "2000-12-04",
+        },
+        {
+            "classification": "primary",
             "identifier": "20032004",
             "name": "2003-2004 Regular Session",
             "start_date": "2002-12-02",
