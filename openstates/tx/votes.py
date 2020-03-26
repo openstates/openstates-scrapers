e304d284956de72ddb92f5a169902468ca2b0c92
schneidy <schneiderman.daniel@gmail.com>
2020-03-26 18:32:42 -0400

    TX: Split up voter names that included semicolons in them

--- openstates/tx/votes.py
+++ openstates/tx/votes.py
@@ -69,7 +69,20 @@ def names(el):
             continue
 
         name = clean_name_special_cases(name)
-        names.append(name)
+
+        if ";" in name:
+            names_still_with_semicolon = name.split(";")
+            for n in names_still_with_semicolon:
+                n = n.strip().replace(".", "")
+                if " — " in n:
+                    n = n.split()[-1]
+                names.append(n)
+                print(n)
+        else:
+            names.append(name)
+
+        # if len(name) > 20:
+        #     print(name)
 
     if names:
         # First item in the list will have stuff to ignore before an mdash
