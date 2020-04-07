8e3d6e056478f29b552e0a2679ef0bc8de2a09f9
schneidy <schneiderman.daniel@gmail.com>
2020-04-07 11:34:58 -0400

    VA: Initial commit for csv_bills

---
+++ openstates/va/csv_bills.py
@@ -0,0 +1,18 @@
+# from pupa.scrape import Scraper, Bill, VoteEvent
+
+
+# Load members of legislative
+def load_members():
+    # Grab Members.csv and save all of them locally
+    return True
+
+
+# Start scrapping Bills
+def scrape_bills():
+    # Grab BILLS.csv
+    return True
+
+
+if __name__ == "__main__":
+    load_members()
+    scrape_bills()
