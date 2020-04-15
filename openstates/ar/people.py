d863578e409f49224fd8e815835b40d1411d940d
Colby Reed <43652809+ColbyReed@users.noreply.github.com>
2020-04-15 13:45:46 -0500

    Skips persons no longer present

--- openstates/ar/people.py
+++ openstates/ar/people.py
@@ -29,7 +29,7 @@ class ARLegislatorScraper(Scraper):
             '//table[@class="screenreader"]'
             '/tbody'
             '/tr'
-            "/td"
+            "/td[not(text()[contains(.,'(Deceased)')]) and not(text()[contains(.,'(Removed)')]) and not(text()[contains(.,'(Resigned)')])]"
             "/a[1]"
         ):
             member_url = "https://www.arkleg.state.ar.us" + a.get("href").replace("../", "/")
