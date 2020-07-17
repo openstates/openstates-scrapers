651d3724dc86014b283e69c7a66dd0589c625a58
Tim Showers <showerst@gmail.com>
2020-07-17 16:54:20 -0400

    VT: Manual fix for another broken date

--- scrapers/vt/bills.py
+++ scrapers/vt/bills.py
@@ -232,6 +232,12 @@ class VTBillScraper(Scraper, LXMLMixin):
                 # Manual fix for data error in
                 # https://legislature.vermont.gov/bill/status/2020/H.511
                 action["StatusDate"] = action["StatusDate"].replace("/0209", "/2019")
+
+                # Manual fix for data error in
+                # https://legislature.vermont.gov/bill/status/2020/H.754
+                if bill_id == 'H 754' and session == "2019-2020":
+                    action["StatusDate"] = action["StatusDate"].replace("/0202", "/2020")
+
                 # https://legislature.vermont.gov/bill/status/2020/H.942
                 if bill_id == 'H 942' and session == "2019-2020":
                     action["StatusDate"] = action["StatusDate"].replace("/0200", "/2020")
