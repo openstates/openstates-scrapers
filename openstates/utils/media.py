6e7626036f18ae741fbe1854bcdbe989bd1ab024
James Turk <dev@jamesturk.net>
2020-12-15 15:35:52 -0500

    add get_media_type utility, with default fallback

--- openstates/utils/media.py
+++ openstates/utils/media.py
@@ -1,8 +1,12 @@
 import mimetypes
 
 
-# note this is just guessing based on the url,
-# not downloading/parsing anything
-def get_media_type(url):
+# note this is just guessing based on the url, not downloading/parsing anything
+def get_media_type(url, *, default=None):
     media_type = mimetypes.guess_type(url)
-    return media_type[0]
+    if media_type[0]:
+        return media_type[0]
+    elif default:
+        return default
+    else:
+        raise ValueError(f"could not guess mimetype for {url}, set default")
