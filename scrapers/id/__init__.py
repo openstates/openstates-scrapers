c7eddb4f5c682f2f85891552254bf4426b825b05
James Turk <dev@jamesturk.net>
2021-01-05 15:18:47 -0500

    ID: 2021

--- scrapers/id/__init__.py
+++ scrapers/id/__init__.py
@@ -109,14 +109,14 @@ class Idaho(State):
             # TODO: Set real end date after session completes
             "end_date": "2020-08-28",
         },
-        # {
-        #     "_scraped_name": "2021 Session",
-        #     "classification": "primary",
-        #     "identifier": "2021",
-        #     "name": "66th Legislature, 1st Regular Session (2021)",
-        #     "start_date": "2020-01-11",
-        #     "end_date": "2020-04-07",
-        # },
+        {
+            "_scraped_name": "2021 Session",
+            "classification": "primary",
+            "identifier": "2021",
+            "name": "66th Legislature, 1st Regular Session (2021)",
+            "start_date": "2020-01-11",
+            "end_date": "2020-04-07",
+        },
     ]
     ignored_scraped_sessions = [
         "2021 Session",
