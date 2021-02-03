d4e27c2b80a6f0955adaa374178ae0cd9b4cd0e6
Tim Showers <showerst@gmail.com>
2021-02-03 15:34:04 -0500

    IN: events scraper

--- scrapers/in/events.py
+++ scrapers/in/events.py
@@ -22,7 +22,6 @@ class INEventScraper(Scraper):
         page.make_links_absolute(list_url)
 
         for com_row in page.xpath('//li[contains(@class,"committee-item")]/a/@href'):
-            print("LOOP")
             yield from self.scrape_committee_page(com_row)
 
     def scrape_committee_page(self, url):
@@ -32,10 +31,6 @@ class INEventScraper(Scraper):
         com = page.xpath('//div[contains(@class, "pull-left span8")]/h1/text()')[
             0
         ].strip()
-        print(com)
-
-        if com == "(S) Corrections and Criminal Law":
-            sys.exit()
 
         for row in page.xpath('//div[contains(@id, "agenda-item")]'):
             status = "tentative"
@@ -48,9 +43,17 @@ class INEventScraper(Scraper):
             time = time_and_loc[0]
             loc = time_and_loc[1]
 
+            if loc == "":
+                loc = "See Agenda"
+
             com = com.replace("(S)", "Senate").replace("(H)", "House")
 
-            when = dateutil.parser.parse(f"{time} {date}")
+            # Indiana has a LOT of undefined times, stuff like "15 mins after adj. of elections"
+            # so just remove the time component if it won't parse, and the user can go to the agenda
+            try:
+                when = dateutil.parser.parse(f"{date} {time}")
+            except dateutil.parser._parser.ParserError:
+                when = dateutil.parser.parse(date)
             when = self._tz.localize(when)
 
             if "cancelled" in time.lower():
@@ -72,12 +75,16 @@ class INEventScraper(Scraper):
 
             if row.xpath('.//a[contains(text(), "Watch")]'):
                 vid_url = row.xpath('.//a[contains(text(), "Watch")]/@href')[0]
-                event.add_media_link("Video of Hearing", vid_url, media_type="text/html")
+                event.add_media_link(
+                    "Video of Hearing", vid_url, media_type="text/html"
+                )
 
             if row.xpath('.//tr[contains(@class,"bill-container")]/td'):
                 agenda = event.add_agenda_item("Bills under consideration")
                 for bill_row in row.xpath('.//tr[contains(@class,"bill-container")]'):
-                    bill_id = bill_row.xpath(".//a[contains(@class,'bill-name-link')]/text()")[0]
+                    bill_id = bill_row.xpath(
+                        ".//a[contains(@class,'bill-name-link')]/text()"
+                    )[0]
                     agenda.add_bill(bill_id)
 
             yield event
