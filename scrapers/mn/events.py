f16460981fc139f0b5326ac57260af7e0add9e54
test <showerst@gmail.com>
2021-03-02 10:51:58 -0500

    MN: Events: Handle blank agenda items that have bills or files

--- scrapers/mn/events.py
+++ scrapers/mn/events.py
@@ -160,6 +160,9 @@ class MNEventScraper(Scraper, LXMLMixin):
 
             if "agenda" in row:
                 for agenda_row in row["agenda"]:
+                    if agenda_row["description"].strip() == '':
+                        # sometimes they have blank agendas but bills or files
+                        agenda_row["description"] = "Agenda"
                     agenda = event.add_agenda_item(agenda_row["description"])
                     if "bill_type" in agenda_row:
                         agenda.add_bill(
