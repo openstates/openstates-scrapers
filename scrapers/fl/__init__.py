5ffe2c977bfaeb84667cc17b56870ef6b4139149
Jesse Mortenson <jessemortenson@gmail.com>
2021-06-17 19:36:39 -0500

    FL: ignore 2022 session (it is in the future)

--- scrapers/fl/__init__.py
+++ scrapers/fl/__init__.py
@@ -148,7 +148,7 @@ class Florida(State):
     ]
     ignored_scraped_sessions = [
         *(str(each) for each in range(1997, 2010)),
-        "2021A",
+        "2022",
         "2020 Org.",
         "2019 I",  # Empty, maybe informational session
         "2010",
