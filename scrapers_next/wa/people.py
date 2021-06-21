ceb9a259a5e14feb1840b1ad1d3066bca9dfa0c0
Michelle Orden <michelleorden@Michelles-MacBook-Pro.local>
2021-06-21 11:56:24 -0500

    added email, sources, and extra title

--- scrapers_next/wa/people.py
+++ scrapers_next/wa/people.py
@@ -11,11 +11,8 @@ class PartyAugmentation(HtmlListPage):
     """
 
     source = URL("https://app.leg.wa.gov/memberemail/Default.aspx")
-    # selector = CSS("#membertable tbody tr", num_items=147)
-    # dist_pos_dict = {}
 
     def find_rows(self):
-        # the first table on the page that has a bunch of rows
         # print(CSS("html body").match(self.root)[0].getchildren()[0].text_content())
         for table in CSS("#membertable").match(self.root):
             rows = CSS("tbody tr").match(table)
@@ -23,7 +20,6 @@ class PartyAugmentation(HtmlListPage):
                 return rows
 
     def process_page(self):
-        # Retrieve the member's position from the email link.
         # We need it to find the member's email address.
         # These positions are enough to discriminate the chamber too (0 = upper, 1,2 = lower)
         mapping = {}
@@ -41,48 +37,24 @@ class PartyAugmentation(HtmlListPage):
             dist = tds[2].text_content().strip()
             position = tds[3].text_content().strip()
             party = tds[4].text_content().strip()
-            # if "[" in party:
-            #    party = party.split("[")[0]
-            mapping[dist] = (name, email, position, party)
+            mapping[(dist, position)] = (name, email, party)
             # print(mapping)
         return mapping
 
-        # email = CSS("td").match(item)[1].text_content()
-        # district = CSS("td").match(item)[2].text_content()
-        # position = CSS("td").match(item)[3].text_content()
-        # print(email)
-        # print(district)
-        # print(position)
 
-        # email_link_url = (
-        #    XPath('.//a[contains(@href, "memberEmail")]').match(email_list_url)[0].get("href")
-        # )
-        # print(email_link_url)
-        # position = re.search(r"/([[0-9]+)$", email_link_url).group(1)
-        # print(position)
+# leg list if works for reps as well
+# just need to change source and selector
+# sen list and rep list with different sources
 
-        # Need to get the email from the email page by matching -
-        # with the member's district and position
-        # try:
-        #    email = (
-        #        XPath(
-        #            './/tr/td/a[contains(@href, "memberEmail/{}/{}")]/parent::td/'
-        #            "following-sibling::td[1]/text()"
-        #        )
-        #    .format(district_num, position)
-        #    .strip()
-        # )
-        # except AttributeError:
-        #    email = ""
-        # print(email)
+# class LegList
+# class SenList(LegList):
+# class RepList(LegList):
 
 
 class SenList(HtmlListPage):
     source = "https://app.leg.wa.gov/ContentParts/MemberDirectory/?a=Senate"
     selector = CSS("#allMembers .memberInformation", num_items=49)
     dependencies = {"party_mapping": PartyAugmentation()}
-
-    # PARTY_MAP = {"(R)": "Republican", "(D)": "Democratic"}
 
     def process_item(self, item):
         # name = CSS(".memberName").match_one(item).text_content()[8:-4]
@@ -92,6 +64,7 @@ class SenList(HtmlListPage):
 
         # (title_name_party,) = member.xpath('.//span[@class="memberName"]/text()')
         # print(CSS(".memberName").match(item)[0].text_content())
+
         title_name_party = CSS(".memberName").match(item)[0].text_content()
         (name, party) = re.search(
             r"^(?:Senator|Representative)\s(.+)\s\(([RD])\)$", title_name_party
@@ -206,28 +179,33 @@ class SenList(HtmlListPage):
             p.district_office.address = district_address
             if district_phone:
                 p.district_office.voice = district_phone
-        p.add_link(XPath('.//a[contains(text(), "Home Page")]/@href').match(item)[0])
 
-        print(self.dependencies["party_mapping"])
-        (dep_name, dep_email, dep_position, dep_party) = self.dependencies[
-            "party_mapping"
-        ][district_num]
+        p.add_link(XPath('.//a[contains(text(), "Home Page")]/@href').match(item)[0])
+        p.add_source(str(self.source))
+        p.add_source(str(PartyAugmentation.source))
+
+        spans = CSS("div .col-csm-8 span").match(item)
+        if len(spans) == 3:
+            p.extras["title"] = spans[1].text_content()
+
+        # print(self.party_mapping)
+        # print(district_num)
+        # print(self.party_mapping[(district_num, '0')])
+        (dep_name, dep_email, dep_party) = self.party_mapping[(district_num, "0")]
         if dep_party == "R":
             dep_party = "Republican"
         elif dep_party == "D":
             dep_party = "Democratic"
-        if dep_position == 0 and dep_name == name and dep_party == party:
+        # print(dep_name == name)
+        # print(dep_party == party)
+
+        if dep_name == name and dep_party == party:
             p.email = dep_email
+            # print(p.email)
 
         return p
 
 
 # TODO
-# email: str = ""
-# given_name: str = ""
-# family_name: str = ""
-# suffix: str = ""
-# links: list[Link] = []
-# sources: list[Link] = []
-# ids: PersonIdBlock = PersonIdBlock()
-# extras: dict = {}
+# add representatives
+# add image back in
