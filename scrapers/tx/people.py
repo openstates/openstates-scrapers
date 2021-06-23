f19d71f365f2992dba743ce5420b65f6f8b0ef51
NewAgeAirbender <paintedflame@gmail.com>
2021-06-23 13:19:10 -0500

    gets spatula working for tx senators

--- scrapers/tx/people.py
+++ scrapers/tx/people.py
@@ -1,12 +1,9 @@
 import re
 import logging
-import lxml.html
-import lxml.html.builder
 
 from openstates.scrape import Person, Scraper
 
 from utils import LXMLMixin
-from .utils import extract_phone, extract_fax
 
 # ----------------------------------------------------------------------------
 # Logging config
@@ -37,179 +34,6 @@ class TXPersonScraper(Scraper, LXMLMixin):
             ),
             flags=re.DOTALL | re.IGNORECASE,
         )
-
-    def _get_chamber_parties(self, chamber):
-        logger.info("Getting chamber parties")
-        """
-        Return a dictionary that maps each district to its representative
-        party for the given legislative chamber.
-        """
-        party_map = {"D": "Democratic", "R": "Republican"}
-
-        chamber_map = {"upper": "S", "lower": "H"}
-
-        parties = {}
-
-        # use only full-session slug for this
-        session = self.latest_session()[:2]
-
-        url = (
-            "https://lrl.texas.gov/legeLeaders/members/membersearch."
-            "cfm?leg={}&chamber={}"
-        ).format(session, chamber_map[chamber])
-        page = self.lxmlize(url)
-
-        # table is broken and doesn't have proper <tr> tags
-        # so we'll group the td tags into groups of 9
-        tds = self.get_nodes(
-            page,
-            '//div[@class="body2ndLevel"]/table//td[contains(@class, ' '"result")]',
-        )
-
-        for td_index, td in enumerate(tds):
-            # 2nd and 6th column
-            if td_index % 9 == 2:
-                district = td.text_content().strip()
-            if td_index % 9 == 6:
-                party_code = td.text_content().strip()[0]
-                party = party_map[party_code]
-                parties[district] = party
-
-        return parties
-
-    def scrape(self, chamber=None):
-        if chamber:
-            yield from self.scrape_chamber(chamber)
-        else:
-            yield from self.scrape_chamber("upper")
-            yield from self.scrape_chamber("lower")
-
-    def scrape_chamber(self, chamber):
-        logger.info(f"Scraping chamber {chamber}")
-        rosters = {
-            "lower": "https://house.texas.gov/members/",
-            "upper": "https://senate.texas.gov/members.php",
-        }
-
-        roster_url = rosters[chamber]
-        response = self.get(roster_url)
-        # auto detect encoding
-        response.encoding = response.apparent_encoding
-        roster_page = lxml.html.fromstring(response.text)
-        roster_page.make_links_absolute(roster_url)
-
-        yield from getattr(self, "_scrape_" + chamber)(roster_page, roster_url)
-
-    def _scrape_upper(self, roster_page, roster_url):
-        logger.info("Scraping uppper chamber roster")
-        """
-        Retrieves a list of members of the upper legislative chamber.
-        """
-        # TODO: email addresses could be scraped from secondary sources
-        #       https://github.com/openstates/openstates/issues/1292
-
-        member_urls = roster_page.xpath('//div[@class="mempicdiv"]/a[1]/@href')
-        # Sort by district for easier spotting of omissions:
-        member_urls.sort(key=lambda url: int(re.search(r"\d+$", url).group()))
-        # logger.warn(member_urls)
-        parties = self._get_chamber_parties("upper")
-        # logger.warn(parties)
-        for member_url in member_urls:
-            yield from self._scrape_senator(member_url, parties)
-
-    def _scrape_senator(self, url, parties):
-        # logger.info(f'Generating senator person object from {url}')
-        """
-        Returns a Person object representing a member of the upper
-        legislative chamber.
-        """
-        # Scrape legislator information from roster URL
-        # Example: view-source:https://senate.texas.gov/member.php?d=1
-        member_page = self.lxmlize(url)
-
-        photo_url = member_page.xpath('//img[@id="memhead"]/@src')[0]
-        scraped_name_district_text = member_page.xpath(
-            '//div[@class="pgtitle"]/text()'
-        )[0]
-        scraped_name, district_text = scraped_name_district_text.split(":")
-        name = " ".join(scraped_name.replace("Senator ", "").split()).strip()
-        district = str(district_text.split()[1]).strip()
-        # Vacant house "members" are named after their district numbers:
-        if re.match(r"^District \d+$", name):
-            return None
-        bio = " ".join(member_page.xpath('//div[@class="bio"]/text()'))
-        party = parties[district]
-
-        person = Person(
-            name=name,
-            district=district,
-            party=party,
-            primary_org="upper",
-            biography=bio,
-        )
-
-        if photo_url is not None:
-            person.image = photo_url
-        person.add_link(url)
-        person.add_source(url)
-
-        office_ids = []
-        # Get offices based on table headers
-        for th_tag in member_page.xpath('//table[@class="memdir"]/tr/th'):
-            # logger.warn([th_tag.xpath('text()'),th_tag.xpath('@id')])
-            id = th_tag.xpath("@id")[0] if th_tag.xpath("@id") else ""
-            label = th_tag.xpath("text()")[0].strip() if th_tag.xpath("text()") else ""
-            if id != "" and label != "":
-                office_ids.append({"id": id, "label": label})
-
-        # logger.warn(office_ids)
-        for office in office_ids:
-            # logger.warn(office)
-            row = member_page.xpath(
-                f'//table[@class="memdir"]/tr/td[@headers="{office["id"]}"]'
-            )
-            # A few member pages have broken ids for office listings:
-            if len(row) == 0:
-                row = member_page.xpath(
-                    '//table[@class="memdir"]/tr/td[@headers="dDA1"]'
-                )
-            if len(row) > 0:
-                details = " ".join(row[0].xpath("text()")).strip()
-                details = details.replace("\r", "").replace("\n", "")
-            # logger.warn(details)
-            # A few member pages have blank office listings:
-            if details == "":
-                continue
-
-            match = self.address_re.search(details)
-            if match is not None:
-                address = re.sub(
-                    " +$",
-                    "",
-                    match.group().replace("\r", "").replace("\n", ""),
-                    flags=re.MULTILINE,
-                )
-            else:
-                # No valid address found in the details.
-                continue
-
-            phone_number = extract_phone(details)
-            fax_number = extract_fax(details)
-
-            if address:
-                person.add_contact_detail(
-                    type="address", value=address, note=office["label"]
-                )
-            if phone_number:
-                person.add_contact_detail(
-                    type="voice", value=phone_number, note=office["label"]
-                )
-            if fax_number:
-                person.add_contact_detail(
-                    type="fax", value=fax_number, note=office["label"]
-                )
-
-        yield person
 
     def _scrape_lower(self, roster_page, roster_url):
         logger.info("Scraping lower chamber roster")
@@ -318,20 +142,20 @@ class TXPersonScraper(Scraper, LXMLMixin):
                 # No valid address found in the details.
                 continue
 
-            phone_number = extract_phone(details)
-            fax_number = extract_fax(details)
+            # phone_number = extract_phone(details)
+            # fax_number = extract_fax(details)
 
             if address:
                 person.add_contact_detail(
                     type="address", value=address, note=office_text["name"]
                 )
-            if phone_number:
-                person.add_contact_detail(
-                    type="voice", value=phone_number, note=office_text["name"]
-                )
-            if fax_number:
-                person.add_contact_detail(
-                    type="fax", value=fax_number, note=office_text["name"]
-                )
+            # if phone_number:
+            #     person.add_contact_detail(
+            #         type="voice", value=phone_number, note=office_text["name"]
+            #     )
+            # if fax_number:
+            #     person.add_contact_detail(
+            #         type="fax", value=fax_number, note=office_text["name"]
+            #     )
 
         yield person
--- scrapers_next/tx/people.py
+++ scrapers_next/tx/people.py
@@ -1,10 +1,144 @@
-from spatula import HtmlListPage, CSS
-from ..common.people import PeopleWorkflow
+import re
+import attr
+from spatula import HtmlListPage, HtmlPage, XPath
+from ..common.people import ScrapePerson
 
 
-class MemberList(HtmlListPage):
-    selector = CSS("#dataListMembers > tbody > tr > td", min_items=32, max_items=150)
-    source = "https://capitol.texas.gov/Members/Members.aspx?Chamber="
+@attr.s(auto_attribs=True)
+class PartialMember:
+    name: str
+    url: str
+    district: int
+    image: str
 
 
-senate = PeopleWorkflow(MemberList)
+_phone_pattern = r"\(?\d+\)?[- ]?\d{3}[-.]\d{4}"
+_phone_re = re.compile(_phone_pattern + "(?! Fax)", re.IGNORECASE)
+_fax_re = re.compile(
+    r"(?<=Fax: )%s|%s(?= \(f\)| Fax)" % (_phone_pattern, _phone_pattern), re.IGNORECASE
+)
+
+
+def extract_phone(string):
+    return next(iter(_phone_re.findall(string)), None)
+
+
+def extract_fax(string):
+    return next(iter(_fax_re.findall(string)), None)
+
+
+class SenatorList(HtmlListPage):
+    source = "https://senate.texas.gov/members.php"
+    selector = XPath('//div[@class="mempicdiv"]', num_items=31)
+
+    def process_item(self, item):
+        name = item[2].text
+        url = item[0].get("href")
+        district = re.match(r"District (\d+)", item[4].text)[1]
+        image = item[0][0].get("src")
+        return SenatorDetail(PartialMember(name, url, district, image), source=url)
+
+
+class SenatorDetail(HtmlPage):
+    input_type = PartialMember
+
+    def process_page(self):
+        party = self.root.xpath('//*[@id="mempg_top"]/div[2]/p[last()]/text()')[
+            0
+        ].strip()
+
+        p = ScrapePerson(
+            name=self.input.name,
+            state="tx",
+            party=party,
+            district=self.input.district,
+            chamber="upper",
+            image=self.input.image,
+        )
+        p.add_link(self.source.url)
+        p.add_source(self.source.url)
+        address_re = re.compile(
+            (
+                # Every representative's address starts with a room number,
+                # street number, or P.O. Box:
+                r"(?:Room|\d+|P\.?\s*O)"
+                # Just about anything can follow:
+                + ".+?"
+                # State and zip code (or just state) along with idiosyncratic
+                # comma placement:
+                + "(?:"
+                + "|".join([r", +(?:TX|Texas)(?: +7\d{4})?", r"(?:TX|Texas),? +7\d{4}"])
+                + ")"
+            ),
+            flags=re.DOTALL | re.IGNORECASE,
+        )
+
+        office_ids = []
+        # Get offices based on table headers
+        for th_tag in self.root.xpath('//table[@class="memdir"]/tr/th'):
+            id = th_tag.xpath("@id")[0] if th_tag.xpath("@id") else ""
+            label = th_tag.xpath("text()")[0].strip() if th_tag.xpath("text()") else ""
+            if id != "" and label != "":
+                office_ids.append({"id": id, "label": label})
+
+        for office in office_ids:
+            row = self.root.xpath(
+                f'//table[@class="memdir"]/tr/td[@headers="{office["id"]}"]'
+            )
+            # A few member pages have broken ids for office listings:
+            if len(row) == 0:
+                row = self.root.xpath('//table[@class="memdir"]/tr/td[@headers="dDA1"]')
+            if len(row) > 0:
+                details = " ".join(row[0].xpath("text()")).strip()
+                details = details.replace("\r", "").replace("\n", "")
+            # A few member pages have blank office listings:
+            if details == "":
+                continue
+
+            match = address_re.search(details)
+            if match is not None:
+                address = re.sub(
+                    " +$",
+                    "",
+                    match.group().replace("\r", "").replace("\n", ""),
+                    flags=re.MULTILINE,
+                )
+            else:
+                # No valid address found in the details.
+                continue
+
+            phone_number = extract_phone(details)
+            fax_number = extract_fax(details)
+
+            if address:
+                if office["label"] == "Capitol Address":
+                    p.capitol_office.address = address
+                elif office["label"] == "District Address":
+                    p.district_office.address = address
+            if phone_number:
+                if office["label"] == "Capitol Address":
+                    p.capitol_office.voice = phone_number
+                elif office["label"] == "District Address":
+                    p.district_office.voice = phone_number
+            if fax_number:
+                if office["label"] == "Capitol Address":
+                    p.capitol_office.fax = fax_number
+                elif office["label"] == "District Address":
+                    p.district_office.fax = fax_number
+
+        return p
+
+
+class RepList(HtmlListPage):
+    source = "https://house.texas.gov/members/"
+    selector = XPath('//td[@class="members-img-center"]', num_items=150)
+
+    def process_item(self, item):
+        name = item[1][2].text
+        # scraped = re.match(r'Rep\.\s(.+), (.+)', scraped_name).groups()
+        #
+        # name = f'{scraped[1]} {scraped[0]}'
+        url = item[1].get("href")
+        district = re.match(r"=(\d+)", url)
+        image = item[1][0].get("src")
+        return print(name, url, district, image)
