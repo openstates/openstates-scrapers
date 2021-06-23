769edfe02242157a9a0ee30f65a0d50420b28718
NewAgeAirbender <paintedflame@gmail.com>
2021-06-23 17:47:02 -0500

    TX: gets house members mostly working

--- scrapers/tx/people.py
+++ scrapers/tx/people.py
@@ -13,27 +13,44 @@ logger = logging.getLogger("openstates.tx-people")
 class TXPersonScraper(Scraper, LXMLMixin):
     jurisdiction = "tx"
 
-    def __init__(self, *args, **kwargs):
-        super(TXPersonScraper, self).__init__(*args, **kwargs)
+    def _get_chamber_parties(self, chamber):
+        logger.info("Getting chamber parties")
+        """
+        Return a dictionary that maps each district to its representative
+        party for the given legislative chamber.
+        """
+        party_map = {"D": "Democratic", "R": "Republican"}
 
-        self.district_re = re.compile(r"District +(\d+)")
+        chamber_map = {"upper": "S", "lower": "H"}
 
-        # Get all and only the address of a representative's office:
-        self.address_re = re.compile(
-            (
-                # Every representative's address starts with a room number,
-                # street number, or P.O. Box:
-                r"(?:Room|\d+|P\.?\s*O)"
-                # Just about anything can follow:
-                + ".+?"
-                # State and zip code (or just state) along with idiosyncratic
-                # comma placement:
-                + "(?:"
-                + "|".join([r", +(?:TX|Texas)(?: +7\d{4})?", r"(?:TX|Texas),? +7\d{4}"])
-                + ")"
-            ),
-            flags=re.DOTALL | re.IGNORECASE,
+        parties = {}
+
+        # use only full-session slug for this
+        session = self.latest_session()[:2]
+
+        url = (
+            "https://lrl.texas.gov/legeLeaders/members/membersearch."
+            "cfm?leg={}&chamber={}"
+        ).format(session, chamber_map[chamber])
+        page = self.lxmlize(url)
+
+        # table is broken and doesn't have proper <tr> tags
+        # so we'll group the td tags into groups of 9
+        tds = self.get_nodes(
+            page,
+            '//div[@class="body2ndLevel"]/table//td[contains(@class, ' '"result")]',
         )
+
+        for td_index, td in enumerate(tds):
+            # 2nd and 6th column
+            if td_index % 9 == 2:
+                district = td.text_content().strip()
+            if td_index % 9 == 6:
+                party_code = td.text_content().strip()[0]
+                party = party_map[party_code]
+                parties[district] = party
+
+        return parties
 
     def _scrape_lower(self, roster_page, roster_url):
         logger.info("Scraping lower chamber roster")
@@ -50,12 +67,10 @@ class TXPersonScraper(Scraper, LXMLMixin):
             yield from self._scrape_representative(member_url, parties)
 
     def _scrape_representative(self, url, parties):
-        # logger.info(f'Generating representative person object from {url}')
         """
         Returns a Person object representing a member of the lower
         legislative chamber.
         """
-        # url = self.get(url).text.replace('<br>', '')
         member_page = self.lxmlize(url)
 
         photo_url = member_page.xpath('//img[@class="member-photo"]/@src')[0]
@@ -81,81 +96,4 @@ class TXPersonScraper(Scraper, LXMLMixin):
 
         person = Person(name=name, district=district, party=party, primary_org="lower")
 
-        if photo_url is not None:
-            person.image = photo_url
-
-        person.add_link(url)
-        person.add_source(url)
-
-        def office_name(element):
-            """Returns the office address type."""
-            return element.xpath("preceding-sibling::h4[1]/text()")[0].rstrip(":")
-
-        offices_text = [
-            {
-                "name": office_name(p_tag),
-                "type": office_name(p_tag).replace(" Address", "").lower(),
-                "details": p_tag.text_content(),
-            }
-            for p_tag in member_page.xpath(
-                '//h4/following-sibling::p[@class="double-space"]'
-            )
-        ]
-
-        for office_text in offices_text:
-            details = office_text["details"].strip()
-
-            # A few member pages have blank office listings:
-            if details == "":
-                continue
-
-            # At the time of writing, this case of multiple district
-            # offices occurs exactly once, for the representative at
-            # District 43:
-            if details.count("Office") > 1:
-                district_offices = [
-                    district_office.strip()
-                    for district_office in re.findall(
-                        r"(\w+ Office.+?(?=\w+ Office|$))", details, flags=re.DOTALL
-                    )
-                ]
-                offices_text += [
-                    {
-                        "name": re.match(r"\w+ Office", office).group(),
-                        "type": "district",
-                        "details": re.search(
-                            r"(?<=Office).+(?=\w+ Office|$)?", office, re.DOTALL
-                        ).group(),
-                    }
-                    for office in district_offices
-                ]
-
-            match = self.address_re.search(details)
-            if match is not None:
-                address = re.sub(
-                    " +$",
-                    "",
-                    match.group().replace("\r", "").replace("\n\n", "\n"),
-                    flags=re.MULTILINE,
-                )
-            else:
-                # No valid address found in the details.
-                continue
-
-            # phone_number = extract_phone(details)
-            # fax_number = extract_fax(details)
-
-            if address:
-                person.add_contact_detail(
-                    type="address", value=address, note=office_text["name"]
-                )
-            # if phone_number:
-            #     person.add_contact_detail(
-            #         type="voice", value=phone_number, note=office_text["name"]
-            #     )
-            # if fax_number:
-            #     person.add_contact_detail(
-            #         type="fax", value=fax_number, note=office_text["name"]
-            #     )
-
-        yield person
+        return person
--- scrapers_next/tx/people.py
+++ scrapers_next/tx/people.py
@@ -1,4 +1,5 @@
 import re
+
 import attr
 from spatula import HtmlListPage, HtmlPage, XPath
 from ..common.people import ScrapePerson
@@ -17,6 +18,21 @@ _phone_re = re.compile(_phone_pattern + "(?! Fax)", re.IGNORECASE)
 _fax_re = re.compile(
     r"(?<=Fax: )%s|%s(?= \(f\)| Fax)" % (_phone_pattern, _phone_pattern), re.IGNORECASE
 )
+address_re = re.compile(
+    (
+        # Every representative's address starts with a room number,
+        # street number, or P.O. Box:
+        r"(?:Room|\d+|P\.?\s*O)"
+        # Just about anything can follow:
+        + ".+?"
+        # State and zip code (or just state) along with idiosyncratic
+        # comma placement:
+        + "(?:"
+        + "|".join([r", +(?:TX|Texas)(?: +7\d{4})?", r"(?:TX|Texas),? +7\d{4}"])
+        + ")"
+    ),
+    flags=re.DOTALL | re.IGNORECASE,
+)
 
 
 def extract_phone(string):
@@ -25,6 +41,38 @@ def extract_phone(string):
 
 def extract_fax(string):
     return next(iter(_fax_re.findall(string)), None)
+
+
+def process_address(details, p, office):
+    match = address_re.search(details)
+    if match is not None:
+        address = re.sub(
+            " +$",
+            "",
+            match.group().replace("\r", "").replace("\n", " ").replace("  ", " "),
+            flags=re.MULTILINE,
+        )
+
+    phone_number = extract_phone(details)
+    fax_number = extract_fax(details)
+
+    if address:
+        if office["label"] == "Capitol Address":
+            p.capitol_office.address = address
+        elif office["label"] == "District Address":
+            p.district_office.address = address
+    if phone_number:
+        if office["label"] == "Capitol Address":
+            p.capitol_office.voice = phone_number
+        elif office["label"] == "District Address":
+            p.district_office.voice = phone_number
+    if fax_number:
+        if office["label"] == "Capitol Address":
+            p.capitol_office.fax = fax_number
+        elif office["label"] == "District Address":
+            p.district_office.fax = fax_number
+
+    return p
 
 
 class SenatorList(HtmlListPage):
@@ -57,21 +105,6 @@ class SenatorDetail(HtmlPage):
         )
         p.add_link(self.source.url)
         p.add_source(self.source.url)
-        address_re = re.compile(
-            (
-                # Every representative's address starts with a room number,
-                # street number, or P.O. Box:
-                r"(?:Room|\d+|P\.?\s*O)"
-                # Just about anything can follow:
-                + ".+?"
-                # State and zip code (or just state) along with idiosyncratic
-                # comma placement:
-                + "(?:"
-                + "|".join([r", +(?:TX|Texas)(?: +7\d{4})?", r"(?:TX|Texas),? +7\d{4}"])
-                + ")"
-            ),
-            flags=re.DOTALL | re.IGNORECASE,
-        )
 
         office_ids = []
         # Get offices based on table headers
@@ -95,41 +128,12 @@ class SenatorDetail(HtmlPage):
             if details == "":
                 continue
 
-            match = address_re.search(details)
-            if match is not None:
-                address = re.sub(
-                    " +$",
-                    "",
-                    match.group().replace("\r", "").replace("\n", ""),
-                    flags=re.MULTILINE,
-                )
-            else:
-                # No valid address found in the details.
-                continue
-
-            phone_number = extract_phone(details)
-            fax_number = extract_fax(details)
-
-            if address:
-                if office["label"] == "Capitol Address":
-                    p.capitol_office.address = address
-                elif office["label"] == "District Address":
-                    p.district_office.address = address
-            if phone_number:
-                if office["label"] == "Capitol Address":
-                    p.capitol_office.voice = phone_number
-                elif office["label"] == "District Address":
-                    p.district_office.voice = phone_number
-            if fax_number:
-                if office["label"] == "Capitol Address":
-                    p.capitol_office.fax = fax_number
-                elif office["label"] == "District Address":
-                    p.district_office.fax = fax_number
+            process_address(details, p, office)
 
         return p
 
 
-class RepList(HtmlListPage):
+class RepresentativeList(HtmlListPage):
     source = "https://house.texas.gov/members/"
     selector = XPath('//td[@class="members-img-center"]', num_items=150)
 
@@ -139,6 +143,68 @@ class RepList(HtmlListPage):
         #
         # name = f'{scraped[1]} {scraped[0]}'
         url = item[1].get("href")
-        district = re.match(r"=(\d+)", url)
+        district = re.search(r"rict=(\d+)", url)[1]
         image = item[1][0].get("src")
-        return print(name, url, district, image)
+        return RepresentativeDetail(
+            PartialMember(name, url, district, image), source=url
+        )
+
+
+class RepresentativeDetail(HtmlPage):
+    input_type = PartialMember
+
+    def process_page(self):
+        p = ScrapePerson(
+            name=self.input.name,
+            state="tx",
+            party="Democratic",
+            district=self.input.district,
+            chamber="lower",
+            image=self.input.image,
+        )
+
+        def office_name(element):
+            """Returns the office address type."""
+            return element.xpath("preceding-sibling::h4[1]/text()")[0].rstrip(":")
+
+        offices_text = [
+            {
+                "label": office_name(p_tag),
+                "details": p_tag.text_content(),
+            }
+            for p_tag in self.root.xpath(
+                '//h4/following-sibling::p[@class="double-space"]'
+            )
+        ]
+
+        for office_text in offices_text:
+            details = office_text["details"].strip()
+
+            # A few member pages have blank office listings:
+            if details == "":
+                continue
+
+            # At the time of writing, this case of multiple district
+            # offices occurs exactly once, for the representative at
+            # District 43:
+            if details.count("Office") > 1:
+                district_offices = [
+                    district_office.strip()
+                    for district_office in re.findall(
+                        r"(\w+ Office.+?(?=\w+ Office|$))", details, flags=re.DOTALL
+                    )
+                ]
+                offices_text += [
+                    {
+                        "name": re.match(r"\w+ Office", office).group(),
+                        "type": "district",
+                        "details": re.search(
+                            r"(?<=Office).+(?=\w+ Office|$)?", office, re.DOTALL
+                        ).group(),
+                    }
+                    for office in district_offices
+                ]
+
+            process_address(details, p, office_text)
+
+        return p
