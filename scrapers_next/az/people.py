7ab6afe9f375b0fb2d8a9a5710c214475d5f7588
judgejudes <judylee2021@u.northwestern.edu>
2021-06-29 12:12:39 -0400

    moved selectorError, removed data file and hardcoded party

--- scrapers_next/az/people.py
+++ scrapers_next/az/people.py
@@ -16,45 +16,45 @@ class LegList(BrokenHtmlListPage):
     def process_item(self, item):
         try:
             name = CSS("a").match(item)[0].text_content()
-            if "--" in name:
-                name = name.split("--")[0].strip()
-
-            district = CSS("a").match(item)[1].text_content()
-
-            party = CSS("td").match(item)[2].text_content()
-            if party == "R":
-                party = "Republican"
-            elif party == "D":
-                party = "Democratic"
-
-            email = CSS("td").match(item)[3].text_content()
-            if email.startswith("Email: "):
-                email = email.replace("Email: ", "").lower() + "@azleg.gov"
-            else:
-                email = ""
-
-            room = CSS("td").match(item)[4].text_content()
-            if self.chamber == "lower":
-                address = "House of Representatives\n "
-            elif self.chamber == "upper":
-                address = "Senate\n "
-            address = address + "1700 West Washington\n " + room + "\nPhoenix, AZ 85007"
-
-            capitol_phone = CSS("td").match(item)[5].text_content()
-
-            image = CSS("td a img").match(item)
-            if image:
-                image = image[0].get("src")
-
         except SelectorError:
             self.skip("header row")
+
+        if "--" in name:
+            name = name.split("--")[0].strip()
+
+        district = CSS("a").match(item)[1].text_content()
+
+        party = CSS("td").match(item)[2].text_content()
+        if party == "R":
+            party = "Republican"
+        elif party == "D":
+            party = "Democratic"
+
+        email = CSS("td").match(item)[3].text_content()
+        if email.startswith("Email: "):
+            email = email.replace("Email: ", "").lower() + "@azleg.gov"
+        else:
+            email = ""
+
+        room = CSS("td").match(item)[4].text_content()
+        if self.chamber == "lower":
+            address = "House of Representatives\n "
+        elif self.chamber == "upper":
+            address = "Senate\n "
+        address = address + "1700 West Washington\n " + room + "\nPhoenix, AZ 85007"
+
+        capitol_phone = CSS("td").match(item)[5].text_content()
+
+        image = CSS("td a img").match(item)
+        if image:
+            image = image[0].get("src")
 
         p = ScrapePerson(
             name=name,
             state="az",
             chamber=self.chamber,
             district=district,
-            party="Democratic",
+            party=party,
             email=email,
             image=image,
         )
