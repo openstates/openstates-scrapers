ef59d0cb4b9fc055a2a70769e69b7e10dbc28b49
judgejudes <judylee2021@u.northwestern.edu>
2021-07-08 18:33:55 -0400

    scraped house commmittees and members w/ edge cases, started adding extra info

--- scrapers_next/ny/committees.py
+++ scrapers_next/ny/committees.py
@@ -8,17 +8,17 @@ class SenateCommitteeDetail(HtmlPage):
 
     def process_page(self):
         # *scrape
-        # com = self.input
-        # com.add_source(self.source.url)
+        com = self.input
+        com.add_source(self.source.url)
 
         chair_role = (
             CSS(".c-chair-block--position").match_one(self.root).text_content().lower()
         )
         chair_name = CSS(".c-chair--title").match_one(self.root).text_content()
-        print(chair_name, chair_role)
+        # print(chair_name, chair_role)
 
         # *
-        # com.add_member(chair_name, chair_role)
+        com.add_member(chair_name, chair_role)
 
         # print(XPath("//div[contains(@class, 'c-senators-container')]").match(self.root))
         try:
@@ -32,12 +32,63 @@ class SenateCommitteeDetail(HtmlPage):
                     role = "member"
                     # print('hello')
 
-                # com.add_member(name, role)
-                print(name, role)
+                com.add_member(name, role)
+                # print(name, role)
         except SelectorError:
             pass
 
-        # return com
+        return com
+
+
+class HouseCommitteeDetail(HtmlPage):
+    example_source = "https://assembly.state.ny.us/comm/?id=1"
+    # example_source = "https://assembly.state.ny.us/comm/?id=94"
+    # example_source = "https://assembly.state.ny.us/comm/?id=149"
+
+    # check out "intergenerational care"--is a subcommittee...smh but it's not listed on the list page
+    def process_page(self):
+        # *scrape
+        # com = self.input
+        # com.add_source(self.source.url)
+
+        # chair_role = CSS("#comm-chair h2").match_one(self.root).text_content().lower()
+        # chair_name = CSS(".comm-chair-name").match_one(self.root).text_content().strip()
+        # print(chair_name, chair_role)
+
+        # com.add_member(chair_name, chair_role)
+
+        # in case there are multiple chairs:
+        chairs = CSS(".chair-info").match(self.root)
+
+        # in case there are co-chairs
+        num_chairs = len(chairs)
+        # print("num chairs: ", num_chairs)
+
+        for chair in chairs:
+            # chair_role = CSS("h2").match_one(chair).text_content().lower()
+            # chair_role is preceding sibling header
+            chair_name = CSS(".comm-chair-name").match_one(chair).text_content().strip()
+            # problem: there are multiple chairs sometimes, the bottom returns multiple chair positions instead of just one
+            chair_role = (
+                XPath(f"..//preceding-sibling::header[{num_chairs}]")
+                .match_one(chair)
+                .text_content()
+                .strip()
+                .lower()
+            )
+
+            print(chair_name, chair_role)
+            # com.add_member(chair_name, chair_role)
+        for p in CSS("#comm-membership ul li").match(self.root):
+            name = p.text_content().strip()
+            # hard-code "member"?
+            role = "member"
+
+            print(name, role)
+
+        # extra info
+        address = CSS("#comm-addr .mod-inner").match_one(self.root).text_content()
+        print("address", address)
 
 
 class SenateCommitteeList(HtmlListPage):
@@ -52,3 +103,17 @@ class SenateCommitteeList(HtmlListPage):
         com = ScrapeCommittee(name=name, parent=self.chamber)
         com.add_source(self.source.url)
         return SenateCommitteeDetail(com, source=item.get("href"))
+
+
+# add address for indiv committees
+class HouseCommitteeList(HtmlListPage):
+    source = "https://assembly.state.ny.us/comm/"
+    selector = CSS(".comm-row .comm-item .comm-title a")
+    chamber = "lower"
+
+    def process_item(self, item):
+        name = item.text_content()
+        # print(name)
+        com = ScrapeCommittee(name=name, parent=self.chamber)
+        com.add_source(self.source.url)
+        return HouseCommitteeDetail(com, source=item.get("href"))
