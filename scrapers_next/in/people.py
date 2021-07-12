217fa36b3d2ae3d4e325270968283702cbfc23d4
Michelle Orden <michelleorden@Michelles-MacBook-Pro.local>
2021-07-12 17:12:44 -0500

    something weird is happening with the selector. there might be an api involved

--- scrapers_next/in/people.py
+++ scrapers_next/in/people.py
@@ -1,19 +1,52 @@
-from spatula import HtmlListPage  # , CSS, XPath, URL
+from spatula import HtmlListPage, XPath, CSS  # , CSS, , URL
 from openstates.models import ScrapePerson
 
 
 class LegList(HtmlListPage):
     def process_item(self, item):
-        return ScrapePerson()
+        # name = item.text_content()
+        name = CSS("div a").match(item)[1].text_content()
+        district = (
+            CSS("div .esg-content.eg-senators-grid-element-1")
+            .match_one(item)
+            .text_content()
+        )
+        img = CSS("div img").match_one(item).get("src")
+
+        p = ScrapePerson(
+            name=name,
+            state="in",
+            chamber=self.chamber,
+            district=district,
+            party=self.party,
+            image=img,
+        )
+        return p
 
 
-class RepList(LegList):
-    source = ""
+class RedRepList(LegList):
+    source = "https://www.indianahouserepublicans.com/members/"
     # selector = CSS("", num_items=)
     chamber = "lower"
+    party = "republican"
 
 
-class SenList(LegList):
-    source = ""
+class BlueRepList(LegList):
+    source = "https://indianahousedemocrats.org/members"
+    # selector = CSS("", num_items=)
+    chamber = "lower"
+    party = "democratic"
+
+
+class BlueSenList(LegList):
+    source = "https://www.indianasenatedemocrats.org/senators/"
+    selector = XPath(".//*[@id='esg-grid-10-1']/div/ul/li")
+    chamber = "upper"
+    party = "democratic"
+
+
+class RedSenList(LegList):
+    source = "https://www.indianasenaterepublicans.com/senators"
     # selector = CSS("", num_items=)
     chamber = "upper"
+    party = "republican"
