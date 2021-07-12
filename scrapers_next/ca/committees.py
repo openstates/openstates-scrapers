c3cf1df65aa461baebfed455b6bb858416f2e19b
Michelle Orden <michelleorden@Michelles-MacBook-Pro.local>
2021-07-12 12:31:33 -0500

    dealing with the case of joint committees, subcommittees, and everything else. joint committees have 3 different page formats that are taking care of using try/except in a for loop

--- scrapers_next/ca/committees.py
+++ scrapers_next/ca/committees.py
@@ -1,5 +1,6 @@
-from spatula import HtmlListPage, URL, XPath, HtmlPage  # , CSS
+from spatula import HtmlListPage, URL, XPath, HtmlPage, SelectorError
 from openstates.models import ScrapeCommittee
+import re
 
 '''
 class CommitteeDetail(HtmlListPage):
@@ -108,7 +109,95 @@ class CommitteeList(HtmlListPage):
 '''
 
 
-class CommitteeDetail(HtmlPage):
+class CommitteeDetail(HtmlListPage):
+    selector = XPath(
+        "//a[(contains(@href, '/sd') or "
+        "contains(@href, 'assembly.ca.gov/a')) and "
+        "(starts-with(text(), 'Senator') or "
+        "starts-with(text(), 'Assembly Member'))]/text()"
+    )
+
+    def process_item(self, item):
+        com = self.input
+        # print(item)
+
+        (mem_name, mem_role) = re.search(
+            r"""(?ux)
+                ^(?:Senator|Assembly\sMember)\s  # Legislator title
+                (.+?)  # Capture the senator's full name
+                (?:\s\((.{2,}?)\))?  # There may be role in parentheses
+                (?:\s\([RD]\))?  # There may be a party affiliation
+                \s*$
+                """,
+            item,
+        ).groups()
+
+        com.add_member(mem_name, role=mem_role if mem_role else "member")
+
+        # if not org._related:
+        #    self.warning("No members found for committee {}".format(comm_name))
+
+        return com
+
+
+class JointcommitteeDetail(HtmlPage):
+    def process_page(self):
+        com = self.input
+
+        xpaths = [
+            "//div/p/a[(contains(text(), 'Senator') or contains(text(), 'Assembly Member'))]/text()",
+            '//tbody/tr/td/a[(contains(@href, "/sd") or '
+            'contains(@href, "assembly.ca.gov/a"))]/text()',
+            "//div/p[@class = 'caption']/text()",
+        ]
+        for type in xpaths:
+            try:
+                members = XPath(type).match(self.root)
+            except SelectorError:
+                continue
+
+        print(members)
+
+        # Special case of members list being presented in text blob.
+
+        # Separate senate membership from assembly membership.
+        # This should strip the header from assembly membership
+        # string automatically.
+        # delimiter = "Assembly Membership:\n"
+        # senate_members, delimiter, assembly_members = members.partition(
+        #    delimiter
+        # )
+
+        # Strip header from senate membership string.
+        # senate_members = senate_members.replace("Senate Membership:\n", "")
+
+        # Clean membership strings.
+        # senate_members = senate_members.strip()
+        # assembly_members = assembly_members.strip()
+
+        # Parse membership strings into lists.
+        # senate_members = senate_members.split("\n")
+        # assembly_members = assembly_members.split("\n")
+
+        # members = senate_members + assembly_members
+        for member in members:
+            if not member.strip():
+                continue
+
+            (mem_name, mem_role) = re.search(
+                r"""(?ux)
+                    (.+?)  # Capture the senator's full name
+                    (?:\s\((.{2,}?)\))?  # There may be role in parentheses
+                    \s*$
+                    """,
+                member,
+            ).groups()
+            com.add_member(mem_name, role=mem_role if mem_role else "member")
+
+        return com
+
+
+class SubcommitteeDetail(HtmlPage):
     def process_page(self):
         com = self.input
         # print(self.data)
@@ -146,28 +235,34 @@ class CommitteeList(HtmlListPage):
             com = ScrapeCommittee(
                 name=comm_name, classification="committee", parent="legislature"
             )
+            com.add_source(self.source.url)
+            com.add_source(comm_url)
+            com.add_link(comm_url, note="homepage")
+            return JointcommitteeDetail(com, source=URL(comm_url))
         elif comm_name.startswith("Subcommittee"):
+            self.skip()
             # print(parent_name)
             # (parent_name,) = comm_doc.xpath(
             #    '//div[@class="banner-sitename"]/a/text()'
             # )
             # (subcom_name,) = comm_doc.xpath('//h1[@class="title"]/text()')
             com = ScrapeCommittee(
-                name=comm_name,
-                classification="subcommittee",
+                name=comm_name, classification="subcommittee", parent=""
             )
-            return CommitteeDetail(com, source=URL(comm_url))
-        else:
-            com = ScrapeCommittee(
-                name=comm_name, classification="committee", parent="upper"
-            )
+            com.add_source(self.source.url)
+            com.add_source(comm_url)
+            com.add_link(comm_url, note="homepage")
+            return SubcommitteeDetail(com, source=URL(comm_url))
 
-        # need to do for subcommittees as well
+        self.skip()
+        com = ScrapeCommittee(
+            name=comm_name, classification="committee", parent="upper"
+        )
         com.add_source(self.source.url)
         com.add_source(comm_url)
         com.add_link(comm_url, note="homepage")
 
-        return com
+        return CommitteeDetail(com, source=URL(comm_url))
 
         """
         # Special case of members list being presented in text blob.
