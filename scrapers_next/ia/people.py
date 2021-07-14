275094271445136b9b42015d34f5122764756935
judgejudes <judylee2021@u.northwestern.edu>
2021-07-14 12:51:48 -0400

    added two sources, formatted extra field names

--- scrapers_next/ia/people.py
+++ scrapers_next/ia/people.py
@@ -1,13 +1,30 @@
+import attr
 from spatula import HtmlPage, HtmlListPage, CSS, XPath
 from openstates.models import ScrapePerson
 
 
+@attr.s(auto_attribs=True)
+class PartialMember:
+    name: str
+    url: str
+    district: int
+    party: str = ""
+    email: str = ""
+    chamber: str = ""
+
+
 class LegDetail(HtmlPage):
-    # input_type = ScrapePerson
+    input_type = PartialMember
 
     example_source = (
         "https://www.legis.iowa.gov/legislators/legislator?ga=89&personID=906"
     )
+
+    def get_field(field):
+        if field.endswith(":"):
+            return field[:-1]
+        else:
+            return field
 
     def process_page(self):
 
@@ -24,6 +41,8 @@ class LegDetail(HtmlPage):
             email=self.input.email,
             image=image,
         )
+        p.add_source(self.source.url)
+        p.add_source(self.input.url)
 
         # TODO: how to get the extra info?
         # for td in CSS(".legisIndent tr").match(self.root):
@@ -33,9 +52,11 @@ class LegDetail(HtmlPage):
         table = XPath("//div[@class='legisIndent divideVert']//td//text()").match(
             self.root
         )
-        print("table", table)
+        # print("table", table)
 
-        fields = table[0::2]
+        # the fields, like "cell phone", etc. are located at every odd indice
+        # the information for each field, like the phone number, are located at every even oddice
+        fields = list(map(self.get_field, table[0::2]))
         # print('fields', fields)
         extra = table[1::2]
         # print('extra', extra)
@@ -44,6 +65,8 @@ class LegDetail(HtmlPage):
         # print("num of fields", num_of_fields)
 
         for i in num_of_fields:
+            if fields[i] == "Legislative Email":
+                continue
             p.extras[fields[i].lower()] = extra[i].strip()
             # print(fields[i],extra[i])
 
@@ -52,6 +75,7 @@ class LegDetail(HtmlPage):
 
 class LegList(HtmlListPage):
     # selector = CSS("#sortableTable tr")
+    selector = CSS("#sortableTable tbody tr")
 
     def process_item(self, item):
 
@@ -60,16 +84,17 @@ class LegList(HtmlListPage):
 
         # print(name.text_content(), district.text_content(), party.text_content(), county.text_content(), email.text_content())
 
-        p = ScrapePerson(
+        p = PartialMember(
             name=name.text_content(),
-            state="ia",
+            # state="ia",
             chamber=self.chamber,
             party=party.text_content(),
             district=district.text_content(),
             email=email.text_content(),
+            url=self.source.url,
         )
         # todo: this source isn't included in the final :(
-        p.add_source(self.source.url)
+        # p.add_source(self.source.url)
         # url = XPath("//a[contains(@href, 'personID')]").match_one(name).get("src")
 
         return LegDetail(p, source=url)
@@ -78,9 +103,8 @@ class LegList(HtmlListPage):
 class SenList(LegList):
     source = "https://www.legis.iowa.gov/legislators/senate"
     chamber = "upper"
-    selector = CSS("#sortableTable tbody tr")
 
 
-# class RepList(LegList):
-#     source = "https://www.legis.iowa.gov/legislators/house"
-#     chamber = "lower"
+class RepList(LegList):
+    source = "https://www.legis.iowa.gov/legislators/house"
+    chamber = "lower"
