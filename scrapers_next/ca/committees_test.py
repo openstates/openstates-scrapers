d2a268cfbdf7493ae4fbfce6189611d693797260
Michelle Orden <michelleorden@Michelles-MacBook-Pro.local>
2021-07-16 16:01:55 -0500

    restructured ChooseType() xpaths dictionary to simplify try/except and delete if/else

--- scrapers_next/ca/committees_test.py
+++ scrapers_next/ca/committees_test.py
@@ -8,39 +8,30 @@ class ChooseType(HtmlPage):
 
         # this link was being considered type one, but should be type four
         if self.source.url == "https://ajed.assembly.ca.gov":
-            # return None
             return TypeFour(self.input, source=self.source)
 
         xpaths = {
-            "type_one": "//div/p/a[(contains(text(), 'Senator') or contains(text(), 'Assembly Member'))]/text()",
-            "type_two": "//a[(contains(@href, '/sd') or "
+            TypeOne(
+                self.input, source=self.source
+            ): "//div/p/a[(contains(text(), 'Senator') or contains(text(), 'Assembly Member'))]/text()",
+            TypeTwo(self.input, source=self.source): "//a[(contains(@href, '/sd') or "
             "contains(@href, 'assembly.ca.gov/a')) and "
             "(starts-with(text(), 'Senator') or "
             "starts-with(text(), 'Assembly Member'))]/text()",
-            "type_three": '//tbody/tr/td/a[(contains(@href, "/sd") or '
+            TypeThree(
+                self.input, source=self.source
+            ): '//tbody/tr/td/a[(contains(@href, "/sd") or '
             'contains(@href, "assembly.ca.gov/a"))]//text()',
-            "type_four": "//p[@class = 'caption']/text()",
+            TypeFour(self.input, source=self.source): "//p[@class = 'caption']/text()",
         }
 
-        for page_type, xpath in xpaths.items():
+        for page_class, xpath in xpaths.items():
             try:
+                # this selector never gets passed to subsequent classes...
                 XPath(xpath).match(self.root)
-                break
+                return page_class
             except SelectorError:
                 continue
-
-        if page_type == "type_one":
-            # return None
-            return TypeOne(self.input, source=self.source)
-        elif page_type == "type_two":
-            # return None
-            return TypeTwo(self.input, source=self.source)
-        elif page_type == "type_three":
-            # return None
-            return TypeThree(self.input, source=self.source)
-        else:
-            # return None
-            return TypeFour(self.input, source=self.source)
 
 
 class TypeOne(HtmlPage):
@@ -76,23 +67,18 @@ class TypeOne(HtmlPage):
                 else:
                     mem_role = "member"
             elif re.search(r",\s", member):
-                # print("FORMAT 2")
                 mem_name, mem_role = member.split(",")
-                # print(mem_role)
-                # print(mem_name)
-                # mem_name, _ = mem_name.split("(")
             elif re.search(r"\(", member):
-                # print("FORMAT 1")
                 mem_name, mem_role = member.split("(")
                 mem_role = mem_role.rstrip(")")
             else:
                 mem_name = member
                 mem_role = "member"
 
-            print(mem_name.strip(), mem_role.strip())
+            if "," in mem_name.strip():
+                print(mem_name.strip(), mem_role.strip())
             com.add_member(mem_name.strip(), role=mem_role.strip())
 
-        # print("TYPE ONE")
         return com
 
 
@@ -123,7 +109,6 @@ class TypeTwo(HtmlPage):
             "(starts-with(text(), 'Senator') or "
             "starts-with(text(), 'Assembly Member'))]/text()"
         ).match(self.root)
-        # print(item)
 
         for member in members:
             (mem_name, mem_role) = re.search(
@@ -137,16 +122,16 @@ class TypeTwo(HtmlPage):
                 member,
             ).groups()
 
-            if mem_role:
-                print(mem_name.strip(), mem_role.strip())
-            else:
-                print(mem_name.strip(), "member")
+            if "," in mem_name.strip():
+                if mem_role:
+                    print(mem_name.strip(), mem_role.strip())
+                else:
+                    print(mem_name.strip(), "member")
 
             com.add_member(
                 mem_name.strip(), role=mem_role.strip() if mem_role else "member"
             )
 
-        # print("TYPE TWO")
         return com
 
 
@@ -172,7 +157,6 @@ class TypeThree(HtmlPage):
         members = XPath(
             "//tbody/tr/td/a[(contains(@href, '/sd') or contains(@href, 'assembly.ca.gov/a'))]//text()"
         ).match(self.root)
-        # print(item)
 
         for member in members:
             (mem_name, mem_role) = re.search(
@@ -184,16 +168,16 @@ class TypeThree(HtmlPage):
                 member,
             ).groups()
 
-            if mem_role:
-                print(mem_name.strip(), mem_role.strip())
-            else:
-                print(mem_name.strip(), "member")
+            if "," in mem_name.strip():
+                if mem_role:
+                    print(mem_name.strip(), mem_role.strip())
+                else:
+                    print(mem_name.strip(), "member")
 
             com.add_member(
                 mem_name.strip(), role=mem_role.strip() if mem_role else "member"
             )
 
-        # print("TYPE THREE")
         return com
 
 
@@ -227,17 +211,14 @@ class TypeFour(HtmlPage):
         except SelectorError:
             members = [CSS("p img").match(self.root)[0]]
 
-        # print(members)
         mem_num = 0
         for member in members:
             mem = member.get("alt")
-            # print(mem)
 
             # this link has bad formatting for the img alt (use p text instead)
             # https://aesm.assembly.ca.gov/
             if not mem or re.search(r"Assemblymember", mem):
                 mem = member.getnext().text_content()
-                # print(mem)
 
             # these links also has bad formatting
             # https://idd.assembly.ca.gov name is Mark Stone instead of Jim Frazier
@@ -250,14 +231,10 @@ class TypeFour(HtmlPage):
 
             mem = re.sub(r"(Senator\s|Assembly\sMember\s)", "", mem)
             mem = re.sub(r"Image\sof\s", "", mem)
-            # print(mem)
             if re.search(r",\s(V|C|\()", mem):
-                # x = re.split(r",\s(V|C|\()", mem)
-                # print(x)
-                # mem_name, mem_role = re.split(r",\s(V|C|\()", mem)
                 member_info = mem.split(",")
 
-                # some names have , Jr. or , Sr.
+                # some names have , Jr. or , Sr. or , P.h.d
                 # this handles an extra comma
                 if len(member_info) == 2:
                     mem_name = member_info[0]
@@ -267,40 +244,32 @@ class TypeFour(HtmlPage):
                     mem_name += member_info[1]
                     mem_role = member_info[2]
 
-                # mem_name = mem_name.strip()
-                # mem_role = mem_role.strip()
                 if "(" in mem_role:
                     mem_role = mem_role.strip().lstrip("(").rstrip(")")
                 if "of the" in mem_role:
                     mem_role = mem_role.split("of the")[0]
                 if mem_name == "Kevin Kiley":
                     mem_role = "Vice Chair"
-                # print(mem_name, mem_role)
             elif re.search(r"\s\((V|C)", mem):
                 mem_name, mem_role = mem.split("(")
-                # mem_name = mem_name.strip()
                 mem_role = mem_role.rstrip(")")
-                # print(mem_name, mem_role)
             elif re.search(r"\n", mem):
                 mem_name, mem_role = mem.split("\n")
-                # mem_name = mem_name.strip()
                 mem_role = mem_role.split("of the")[0]
             elif mem_num == 0:
                 mem_name = mem.strip()
                 mem_role = "Chair"
-                # print(member, "chair not listed")
             else:
                 mem_name = mem.strip()
                 mem_role = "Vice Chair"
-                # print(member, "vice chair not listed")
             mem_num += 1
 
-            print(mem_name.strip(), mem_role.strip())
+            if "," in mem_name.strip():
+                print(mem_name.strip(), mem_role.strip())
             com.add_member(
                 mem_name.strip(), role=mem_role.strip() if mem_role else "member"
             )
 
-        # print("TYPE FOUR")
         return com
 
 
@@ -317,7 +286,6 @@ class SenateCommitteeList(HtmlListPage):
         comm_url = XPath("@href").match_one(item)
 
         if comm_name.startswith("Joint"):
-            # self.skip()
             com = ScrapeCommittee(
                 name=comm_name, classification="committee", chamber="legislature"
             )
@@ -330,7 +298,6 @@ class SenateCommitteeList(HtmlListPage):
                 .getchildren()[0]
                 .text_content()
             )
-            # print(parent_comm)
             com = ScrapeCommittee(
                 name=comm_name,
                 classification="subcommittee",
@@ -385,9 +352,7 @@ class AssemblyCommitteeList(HtmlListPage):
             .lstrip("\t")
             == "Sub Committees"
         ):
-            # self.skip()
             parent_comm = item.getparent().getparent().getchildren()[0].text_content()
-            # print(parent_comm)
             com = ScrapeCommittee(
                 name=comm_name,
                 classification="subcommittee",
@@ -395,7 +360,6 @@ class AssemblyCommitteeList(HtmlListPage):
                 parent=parent_comm,
             )
         else:
-            # self.skip()
             com = ScrapeCommittee(
                 name=comm_name, classification="committee", chamber="lower"
             )
