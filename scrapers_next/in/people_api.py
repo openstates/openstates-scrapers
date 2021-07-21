b56ec98c412f11cffd78d65eba6c546e535441d0
Michelle Orden <michelleorden@Michelles-MacBook-Pro.local>
2021-07-21 16:00:45 -0500

    separated classes for house and senate. able to get 50 people for each (might be a cap at 50?). currently getting bad responses for detail pages.

--- scrapers_next/in/people_api.py
+++ scrapers_next/in/people_api.py
@@ -1,43 +1,93 @@
 from spatula import (
     JsonPage,
     URL,
+    HtmlPage,
+    CSS,
 )  # HtmlListPage, XPath, CSS, HtmlPage  # , SelectorError
 from openstates.models import ScrapePerson
 import os
 
-"""
-class LegDetailPage(JsonPage):
+
+class LegDetailPage(HtmlPage):
     def process_page(self):
+        p = self.input
+        img = CSS("div .row-fluid.span2.hidden-print img").match_one(self.root)
+        p.image = img
+        # district
+        # address
+        # phone
+
         return p
-"""
 
 
 class LegListPage(JsonPage):
     def process_page(self):
         for item in self.data["items"]:
-            self.process_item(item)
+            yield self.process_item(item)
 
     def process_item(self, item):
-        print(item)
+        name = item["firstName"] + " " + item["lastName"]
+        party = item["party"]
 
         p = ScrapePerson(
-            name="",
+            name=name,
             state="in",
-            chamber="",
+            chamber=self.chamber,
             district="",
-            party="",
+            party=party,
             image="",
         )
 
-        return p
+        p.family_name = item["lastName"]
+        p.given_name = item["firstName"]
+
+        # link from old code (no authentication needed?)
+        # getting 503 response
+        base_url = "http://iga.in.gov/legislative"
+        api_base_url = "https://api.iga.in.gov"
+        api_link = api_base_url + item["link"]
+        html_link = base_url + item["link"].replace(
+            "legislators/", "legislators/legislator_"
+        )
+        print(api_link)
+        print(html_link)
+        detail_source = URL(html_link)
+
+        # this is my logic from api
+        # getting a 403 error
+        """
+        print(item["link"])
+        link_id = item["link"].split("/")[-1]
+        link_id = link_id.split("_")[-1]
+        print(link_id)
+        detail_link = "https://api.iga.in.gov/2021/legislators/" + link_id
+        detail_link = "https://api.iga.in.gov" + item["link"]
+        print(detail_link)
+        """
+
+        p.add_source(self.source.url)
+        # p.add_source(detail_link)
+        # p.add_link(detail_link, note="homepage")
+
+        return LegDetailPage(p, source=detail_source)
 
 
-class ApiGet(LegListPage):
-    base_url = "http://iga.in.gov/legislative"
-    api_base_url = "https://api.iga.in.gov"
-
+class Senate(LegListPage):
+    chamber = "upper"
     source = URL(
-        "https://api.iga.in.gov/2021/legislators",
+        "https://api.iga.in.gov/2021/chambers/senate/legislators",
+        headers={
+            "Authorization": os.environ["INDIANA_API_KEY"],
+            "User-Agent": "openstates 2021",
+            "Accept": "application/json",
+        },
+    )
+
+
+class House(LegListPage):
+    chamber = "lower"
+    source = URL(
+        "https://api.iga.in.gov/2021/chambers/house/legislators",
         headers={
             "Authorization": os.environ["INDIANA_API_KEY"],
             "User-Agent": "openstates 2021",
