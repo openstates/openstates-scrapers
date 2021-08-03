e0ba460b4e73a1bc700c53475ba9e23ed51e302f
Michelle Orden <michelleorden@Michelles-MacBook-Pro.local>
2021-08-03 14:52:05 -0500

    finished LegList() class. working on LegDetail()

--- scrapers_next/de/people.py
+++ scrapers_next/de/people.py
@@ -1,10 +1,33 @@
-from spatula import JsonListPage
+from spatula import JsonPage, URL, HtmlPage, CSS
 from openstates.models import ScrapePerson
 
 
-class LegList(JsonListPage):
-    def process_item(self, item):
-        print(item["Data"])
+class LegDetail(HtmlPage):
+    def process_page(self):
+        p = self.input
+
+        img = CSS("img").match_one(self.root).get("src")
+        p.image = img
+
+        addr = CSS("div .info-vertical div div").match(self.root)[0].text_content()
+        addr += " "
+        addr += (
+            CSS("div .info-vertical div div").match(self.root)[1].text_content().strip()
+        )
+        print(addr)
+        p.capitol_office.address = addr
+
+        p.capitol_office.voice = (
+            CSS("div .info-vertical div div").match(self.root)[2].text_content()
+        )
+
+        p.email = CSS("div .info-vertical div div").match(self.root)[3].text_content()
+
+        return p
+
+
+class LegList(JsonPage):
+    def process_page(self):
         for item in self.data["Data"]:
             name = item["PersonFullName"]
             party_code = item["PartyCode"]
@@ -20,14 +43,22 @@ class LegList(JsonListPage):
                 district=district,
             )
 
-            return p
+            p.add_source(self.source.url)
+            detail_link = URL(
+                f"https://legis.delaware.gov/LegislatorDetail?personId={item['PersonId']}"
+            )
+            p.add_source(detail_link.url)
+
+            yield LegDetail(p, source=detail_link.url)
 
 
 class Senate(LegList):
-    source = "https://legis.delaware.gov/json/Senate/GetSenators"
+    source = URL("https://legis.delaware.gov/json/Senate/GetSenators", method="POST")
     chamber = "upper"
 
 
 class House(LegList):
-    source = "https://legis.delaware.gov/json/House/GetRepresentatives"
+    source = URL(
+        "https://legis.delaware.gov/json/House/GetRepresentatives", method="POST"
+    )
     chamber = "lower"
