1f89d40fa0cca53203f30d897c1714f946381eb6
Michelle Orden <michelleorden@Michelles-MacBook-Pro.local>
2021-08-04 11:39:20 -0500

    started writing new people scraper for id using spatula. grabbing image, name, email, district, party, and dis address from listpage

---
+++ scrapers_next/id/people.py
@@ -0,0 +1,55 @@
+from spatula import CSS, HtmlListPage, URL, XPath
+from openstates.models import ScrapePerson
+import re
+
+
+class LegList(HtmlListPage):
+    selector = CSS(
+        "div .row-equal-height.padding-two-top.sm-padding-two-top.xs-padding-two-top.hcode-inner-row"
+    )
+
+    def process_item(self, item):
+        img = CSS("img").match(item)[0].get("src")
+        name = CSS("p strong").match(item)[0].text_content()
+        email = CSS("p strong").match(item)[1].text_content()
+        district_full = CSS("p a").match(item)[1].text_content()
+        district = re.search(r"District\s(.+)", district_full).groups()[0]
+
+        all_text = CSS("p").match(item)[1].text_content()
+        party_letter = re.search(r"(.+)(\(([A-Z])\))(.+)", all_text).groups()[2]
+        party_dict = {"D": "Democratic", "R": "Republican", "I": "Independent"}
+        party = party_dict[party_letter]
+
+        p = ScrapePerson(
+            name=name,
+            state="id",
+            party=party,
+            chamber=self.chamber,
+            district=district,
+            image=img,
+            email=email,
+        )
+        addr1 = XPath(".//p/text()").match(item)[5].strip()
+        addr2 = XPath(".//p/text()").match(item)[6].strip()
+        addr3 = XPath(".//p/text()").match(item)[7].strip()
+        if re.search(r"^(\d|P\.?O)", addr1):
+            addr = addr1
+        elif re.search(r"^(\d|P\.?O)", addr2):
+            addr = addr2
+        else:
+            addr = addr3
+        p.district_office.address = addr
+
+        p.add_source(self.source.url)
+
+        return p
+
+
+class Senate(LegList):
+    source = URL("https://legislature.idaho.gov/senate/membership/")
+    chamber = "upper"
+
+
+class House(LegList):
+    source = URL("https://legislature.idaho.gov/house/membership/")
+    chamber = "lower"
