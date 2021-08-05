8f22d39552d382bb656676f7f8f2a5986bd85087
Michelle Orden <michelleorden@Michelles-MacBook-Pro.local>
2021-08-05 13:36:25 -0500

    added name, party, district, link, image, and title info. need to test all fields

--- scrapers_next/ky/people.py
+++ scrapers_next/ky/people.py
@@ -1,5 +1,22 @@
-from spatula import CSS, HtmlListPage, URL
+from spatula import CSS, HtmlListPage, URL, HtmlPage
 from openstates.models import ScrapePerson
+import re
+
+
+class LegDetail(HtmlPage):
+    def process_page(self):
+        p = self.input
+
+        img = CSS("img.leg-img").match_one(self.root).get("src")
+        p.image = img
+
+        title = (
+            CSS("div .row.profile-top h3").match_one(self.root).text_content().strip()
+        )
+        if title != "":
+            p.extras["title"] = title
+
+        return p
 
 
 class LegList(HtmlListPage):
@@ -8,21 +25,39 @@ class LegList(HtmlListPage):
     def process_page(self, item):
         name = CSS("h3").match_one(item).text_content()
         print(name)
+        if name == " - Vacant Seat":
+            self.skip()
+
+        party = CSS("small").match_one(item).text_content()
+        if party == "Democrat":
+            party = "Democratic"
+        print(party)
+
+        district = CSS("p").match(item)[0].text_content()
+        district = re.search(r"District:\s(.+)", district).groups()[0]
+        print(district)
 
         p = ScrapePerson(
             name=name,
             state="ky",
-            party="",
+            party=party,
             chamber=self.chamber,
-            district="",
-            image="",
+            district=district,
         )
+
+        detail_link = item.get("href")
+
+        p.add_source(self.source)
+        p.add_source(detail_link)
+        p.add_link(detail_link, note="homepage")
 
         return p
 
 
 class Senate(LegList):
-    source = URL("https://legislature.ky.gov/Legislators/senate")
+    source = URL(
+        "https://legislature.ky.gov/Legislators/senate", headers={"verify": "false"}
+    )
     chamber = "upper"
 
 
