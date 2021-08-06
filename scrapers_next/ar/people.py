c55a305f65862778aad55d1c556b10d221373a22
judgejudes <judylee2021@u.northwestern.edu>
2021-08-06 15:59:04 -0400

    added district office, print cleanup

--- scrapers_next/ar/people.py
+++ scrapers_next/ar/people.py
@@ -17,18 +17,18 @@ class LegDetail(HtmlPage):
 
     def process_page(self):
         # start going through the table now
-        print("hello")
+        # print("hello")
 
         table_div = CSS(".col-md-7").match(self.root)[0]
         # print(table_div.getchildren())
         # for thing in table_div.getchildren():
         #     print(thing.text_content())
 
-        for div in table_div.getchildren():
-            print("this is a div", div.text_content().strip())
-            print("TEXT", div.text_content().split(": "))
-            # print("type: ", type)
-            # print("info: ", info)
+        # for div in table_div.getchildren():
+        #     print("this is a div", div.text_content().strip())
+        #     print("TEXT", div.text_content().split(": "))
+        # print("type: ", type)
+        # print("info: ", info)
 
         # a bunch of rows
         rows = CSS(".row").match(table_div)
@@ -37,7 +37,7 @@ class LegDetail(HtmlPage):
         # TODO: party
         party = ""
         name_party = CSS("h1").match_one(self.root).text_content()
-        print("name party", name_party)
+        # print("name party", name_party)
         # if re.search("(R)", name_party):
         #     party == "Republican"
 
@@ -102,27 +102,31 @@ class LegDetail(HtmlPage):
 
         if table["Biography:"] != "":
             p.add_link(table["Biography:"])
-            print("BIOGRAPHY SHOULD BE ADDED")
+            # print("BIOGRAPHY SHOULD BE ADDED")
 
         # for every key-value pair in table that isn't phone, email, district
         for key in table:
-            print("KEY", key)
+            # print("KEY", key)
             if (
                 key == "Phone:"
                 or key == "Email:"
                 or key == "District:"
                 or key == "Biography:"
             ):
-                print("***phone, email, or district here")
+                # print("***phone, email, or district here")
                 continue
             elif table[key] != "":
                 # remove colon at the end
-                print("about to add to extras***")
+                # print("about to add to extras***")
                 # text[:-1]
                 p.extras[key[:-1].lower()] = table[key]
 
+        address = CSS(".col-md-12 p b").match_one(self.root).text_content()
+        full_address = address[:-5] + "AR " + address[-5:]
+
         p.add_source(self.source.url)
         p.add_source(self.input.url)
+        p.district_office.address = full_address
 
         return p
 
