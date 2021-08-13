52d613b27c13917bce5aff02cac369c0923018e1
judgejudes <judylee2021@u.northwestern.edu>
2021-08-13 14:34:53 -0400

    comment and print cleanup, deleted warning for party

--- scrapers_next/ct/people.py
+++ scrapers_next/ct/people.py
@@ -1,4 +1,3 @@
-# import attr
 from spatula import CsvListPage
 from openstates.models import ScrapePerson
 
@@ -7,7 +6,6 @@ class LegList(CsvListPage):
     source = "ftp://ftp.cga.ct.gov/pub/data/LegislatorDatabase.csv"
 
     def process_item(self, row):
-        # name = "{} {} {}".format(row["first name"], row["middle initial"].strip(), row["last name"])
         name = row["first name"]
         mid = row["middle initial"].strip()
         if mid:
@@ -16,19 +14,11 @@ class LegList(CsvListPage):
         suffix = row["suffix"].strip()
         if suffix:
             name += " %s" % suffix
-        print("name", name)
 
-        if row["party"] == "Democrat" or "Republican":
-            party = row["party"]
-        else:
-            # TODO: party?
-            self.logger.warning("party warning")
-        print("party", party)
+        party = row["party"]
 
         district = row["dist"].lstrip("0")
         assert district.isdigit(), "Invalid district found: {}".format(district)
-
-        print("district", district)
 
         chamber = {"H": "lower", "S": "upper"}[row["office code"]]
 
@@ -37,11 +27,9 @@ class LegList(CsvListPage):
             if not email:
                 email = ""
             elif email.startswith("http://") or email.startswith("https://"):
-                # extra_office_fields['contact_form'] = email
                 email = ""
             else:
                 raise ValueError("Problematic email found: {}".format(email))
-        print("email", email)
 
         p = ScrapePerson(
             name=name,
@@ -50,14 +38,12 @@ class LegList(CsvListPage):
             district=district,
             party=party,
             email=email,
-            # image=image,
         )
 
         legislator_url = row["URL"].replace("\\", "//").strip()
         if legislator_url != "":
             if not legislator_url.startswith("http"):
                 legislator_url = "http://"
-        print("legislator url", legislator_url)
         p.add_link(legislator_url)
 
         p.add_source(self.source.url)
@@ -68,7 +54,6 @@ class LegList(CsvListPage):
             row["room number"],
             row["capitol city"].replace("  ", " "),
         )
-        print("office address", office_address)
         p.capitol_office.address = office_address
 
         if row["capitol phone"].strip():
@@ -82,7 +67,6 @@ class LegList(CsvListPage):
                 row["home state"],
                 row["home zip code"],
             )
-            print("home address", home_address)
 
             if "Legislative Office Building" not in home_address:
                 p.district_office.address = home_address
