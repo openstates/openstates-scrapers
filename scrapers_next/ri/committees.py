29db56ce20eb90d2e4f86298cc2ab6f9bad30bde
Michelle Orden <michelleorden@Michelles-MBP.home>
2021-08-16 17:04:55 -0400

    made a dataclass for sub committees

--- scrapers_next/ri/committees.py
+++ scrapers_next/ri/committees.py
@@ -1,12 +1,71 @@
 from spatula import URL, HtmlListPage, XPath, HtmlPage, CSS
 from openstates.models import ScrapeCommittee
 import re
+from dataclasses import dataclass
+
+
+@dataclass
+class PartialSubCommittee:
+    chamber: str
+    source1: str
+    source2: str
+    link: str
 
 
 class SubCommitteeDetail(HtmlPage):
+    input_type = PartialSubCommittee
+
     def process_page(self):
-        com = self.input
-        return com
+        titles = {}
+        members_dict = {}
+        for count, td in enumerate(
+            XPath(
+                "/html/body/div/table/tr[7]/td[2]/table/tr[3]/td/div/table/tr/td/table/tr/td"
+            ).match(self.root)
+        ):
+            if td.text_content().strip() != "":
+                if re.search(
+                    r"(Rep|Sen)\.\s", CSS("div strong").match_one(td).text_content()
+                ):
+                    members_dict[count] = td.text_content().strip()
+                else:
+                    titles[count] = td.text_content().strip().lower().capitalize()
+
+        parent = CSS("font").match(self.root)[1].text_content().strip()
+        parent = re.search(
+            r"(House|Senate)\s(.+)\s(Subcommittee\sMembers)", parent
+        ).groups()[1]
+
+        for title_count, title in enumerate(titles.values()):
+            com = ScrapeCommittee(
+                name=title,
+                chamber=self.input.chamber,
+                classification="subcommittee",
+                parent=parent,
+            )
+            com.add_source(self.input.source1)
+            com.add_source(self.input.source2)
+            com.add_link(self.input.source2, note="homepage")
+
+            for mem_count, members in enumerate(members_dict.values()):
+                if title_count == mem_count:
+                    if re.search(r"Rep\.\s", members):
+                        members = members.split("Rep. ")
+                    else:
+                        members = members.split("Sen. ")
+
+                    for member in members:
+                        if member.strip() == "":
+                            continue
+                        elif ";" in member:
+                            member_name = member.split(";")[0].strip()
+                            member_role = member.split(";")[1].strip()
+                        else:
+                            member_name = member.strip()
+                            member_role = "member"
+                        com.add_member(member_name, member_role)
+
+            yield com
 
 
 class CommitteeDetail(HtmlPage):
@@ -45,25 +104,33 @@ class CommitteeList(HtmlListPage):
     source = URL("https://www.rilegislature.gov/pages/committees.aspx")
 
     def process_item(self, item):
-
         name = item.text_content()
+
+        if re.search(r"Subcommittees", name):
+            source = item.get("href")
+
+            sub = PartialSubCommittee(
+                chamber=self.chamber,
+                source1=self.source.url,
+                source2=source,
+                link=source,
+            )
+            return SubCommitteeDetail(
+                sub,
+                source=source,
+            )
 
         com = ScrapeCommittee(
             name=name,
             chamber=self.chamber,
         )
 
+        # need to do in subcommittees as well
         com.add_source(self.source.url)
-
         source = item.get("href")
         com.add_source(source)
         com.add_link(source, note="homepage")
 
-        if re.search(r"Subcommittees", name):
-            return SubCommitteeDetail(
-                com,
-                source=source,
-            )
         return CommitteeDetail(
             com,
             source=source,
@@ -71,7 +138,6 @@ class CommitteeList(HtmlListPage):
 
 
 class House(CommitteeList):
-    # selector = CSS("table .s4-wpTopTable table table tr")
     chamber = "lower"
     selector = XPath(
         "//*[@id='{4C4879FB-E046-42C7-BEC9-8F569B03E55D}-{811ABDBE-2D09-4586-8D38-D18203BD2C7E}']//a"
@@ -79,7 +145,6 @@ class House(CommitteeList):
 
 
 class Senate(CommitteeList):
-    # selector = CSS("table .s4-wpTopTable table table tr")
     chamber = "upper"
     selector = XPath(
         "//*[@id='{4C4879FB-E046-42C7-BEC9-8F569B03E55D}-{733ACC1D-0F41-4EB2-ABC2-0E815521BD5E}']//a"
@@ -87,7 +152,6 @@ class Senate(CommitteeList):
 
 
 class Joint(CommitteeList):
-    # selector = CSS("table .s4-wpTopTable table table tr")
     chamber = "legislature"
     selector = XPath(
         "//*[@id='{4C4879FB-E046-42C7-BEC9-8F569B03E55D}-{E09F0FD2-B6DF-4422-8B22-FBF09B2C85EB}']//a"
