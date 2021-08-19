40d763038c03bb8d362f70c9c57615b3283c7221
Michelle Orden <michelleorden@Michelles-MacBook-Pro.local>
2021-08-19 14:27:53 -0400

    changed source for CommitteeDetail(). changed some CSS

--- scrapers_next/wa/committees.py
+++ scrapers_next/wa/committees.py
@@ -2,29 +2,23 @@ from spatula import HtmlListPage, HtmlPage, CSS, URL, SelectorError
 from openstates.models import ScrapeCommittee
 
 
-# "//*[@id="housecommittees"]/h3/a"
-# https://leg.wa.gov/legislature/_api/search/searchcenterurl
-# "https://app.leg.wa.gov/ContentParts/LegislativeCommittees/Index"
-
-
-"//*[@id='CommitteeMembers']/table/tbody/tr[1]/td[1]/a"
-
-
 class CommitteeDetail(HtmlPage):
     def process_page(self):
         com = self.input
 
-        members = CSS("div #CommitteeMembers tbody tr").match(self.root)
+        members = CSS("div#CommitteeMembers tbody tr").match(self.root)
         # members = CSS("td a.ui-link").match(self.root)
         for member in members:
-            dirty_name = member.get("a").text_content().strip().split(", ")
+            dirty_name = CSS("a").match_one(member).text_content().strip().split(", ")
+            # dirty_name = member.get("a").text_content().strip().split(", ")
             # dirty_name = member.text_content().strip().split(", ")
             last_name = dirty_name[0]
             first_name = dirty_name[1]
             name = first_name + " " + last_name
 
             try:
-                role = member.get("td").get("text()")[-1].strip()
+                role = CSS("td").match(member)[0].get("text()")[-1].strip()
+                # role = member.get("td").get("text()")[-1].strip()
             except SelectorError:
                 role = "member"
             # role = member.getnext().getnext().getnext()
@@ -37,8 +31,6 @@ class CommitteeDetail(HtmlPage):
 
 
 class CommitteeList(HtmlListPage):
-    # source = URL("https://app.leg.wa.gov/ContentParts/LegislativeCommittees/Index", headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'})
-    # source = URL("https://leg.wa.gov/legislature/Pages/CommitteeListing.aspx")
     source = URL("https://app.leg.wa.gov/ContentParts/LegislativeCommittees/Index")
     selector = CSS("div ul li span a.ms-srch-item-link", num_items=57)
 
@@ -63,7 +55,7 @@ class CommitteeList(HtmlListPage):
             chamber = "legislative"
         elif chamber == "Legislative":
             self.skip()
-            # are these also committees?
+            # skipping Legislative Agencies
 
         com = ScrapeCommittee(
             name=name,
@@ -71,13 +63,15 @@ class CommitteeList(HtmlListPage):
         )
 
         com.add_source(self.source.url)
-        source = item.get("href")
-        com.add_source(source)
-        com.add_link(source, note="homepage")
 
-        # replace default.aspx with in MembersStaff.aspx source?
-        # https://leg.wa.gov/House/Committees/APP
+        # new source
+        href = item.get("href")
+        print(href)
+        href_lst = href.split("/")
+        print(href_lst)
+        new_source = f"https://app.leg.wa.gov/ContentParts/CommitteeMembers/?agency={href_lst[-3]}&committee={href_lst[-1]}"
 
-        # https://app.leg.wa.gov/ContentParts/CommitteeMembers/?agency=House&committee=APP
+        com.add_source(new_source)
+        com.add_link(href, note="homepage")
 
-        return CommitteeDetail(com, source=source)
+        return CommitteeDetail(com, source=new_source)
