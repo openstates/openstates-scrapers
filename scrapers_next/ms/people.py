579321c2cde2ac22b60d12c90119c3715c3ae8a5
Michelle Orden <michelleorden@Michelles-MacBook-Pro.local>
2021-08-24 17:18:55 -0500

    filled in information for Senate President Delbert Hosemann (different type of page). moved 'Home Office' info to District Office

--- scrapers_next/ms/people.py
+++ scrapers_next/ms/people.py
@@ -1,4 +1,4 @@
-from spatula import URL, HtmlPage
+from spatula import URL, HtmlPage, CSS
 from openstates.models import ScrapePerson
 from itertools import zip_longest
 from dataclasses import dataclass
@@ -26,8 +26,62 @@ class LegDetail(HtmlPage):
     input_type = PartialPerson
 
     def process_page(self):
+        # Senate President Delbert Hosemann has a completely different page
+        # what would his district be?
         if self.source.url == "http://ltgovhosemann.ms.gov/":
-            return None
+            p = ScrapePerson(
+                name=self.input.name,
+                state="ms",
+                chamber=self.input.chamber,
+                district="",
+                party="Republican",
+            )
+            p.extras["phone"] = (
+                CSS("span.fusion-contact-info-phone-number")
+                .match_one(self.root)
+                .text_content()
+                .strip()
+            )
+            p.extras["email"] = (
+                CSS("span.fusion-contact-info-email-address")
+                .match_one(self.root)
+                .text_content()
+                .strip()
+            )
+            twitter = (
+                CSS(
+                    "a.fusion-social-network-icon.fusion-tooltip.fusion-twitter.fusion-icon-twitter"
+                )
+                .match_one(self.root)
+                .get("href")
+                .split("/")[-1]
+            )
+            p.ids.twitter = twitter
+            facebook = (
+                CSS(
+                    "a.fusion-social-network-icon.fusion-tooltip.fusion-facebook.fusion-icon-facebook"
+                )
+                .match_one(self.root)
+                .get("href")
+                .split("/")[-1]
+            )
+            p.ids.facebook = facebook
+            instagram = (
+                CSS(
+                    "a.fusion-social-network-icon.fusion-tooltip.fusion-instagram.fusion-icon-instagram"
+                )
+                .match_one(self.root)
+                .get("href")
+                .split("/")[-2]
+            )
+            p.ids.instagram = instagram
+            p.extras["mailing address"] = "P.O. Box 1018 Jackson, MS, 39215"
+
+            p.add_source(self.input.source)
+            p.add_source(self.source.url)
+            p.add_link(self.source.url, note="homepage")
+
+            return p
 
         district = self.root.cssselect("district")[0].text_content()
         party = self.root.cssselect("party")[0].text_content()
@@ -107,18 +161,19 @@ class LegDetail(HtmlPage):
         h_address2 = self.root.cssselect("h_address2")[0].text_content()
         h_city = self.root.cssselect("h_city")[0].text_content()
         h_zip = self.root.cssselect("h_zip")[0].text_content()
-        h_phone = self.root.cssselect("h_phone")[0].text_content()
+        # Note that these are listed as 'Home Office' but adding to 'District Office'
         if h_address != "" and h_address2 != "":
             home_addr = (
                 h_address + " " + h_address2 + " " + h_city + ", Mississippi " + h_zip
             )
+            p.district_office.address = home_addr
         elif h_address != "":
             home_addr = h_address + " " + h_city + ", Mississippi " + h_zip
+            p.district_office.address = home_addr
 
-        if home_addr != "" and h_phone != "":
-            p.add_office(contact_type="Home Office", address=home_addr, voice=h_phone)
-        elif home_addr != "":
-            p.add_office(contact_type="Home Office", address=home_addr)
+        h_phone = self.root.cssselect("h_phone")[0].text_content()
+        if h_phone != "":
+            p.district_office.voice = h_phone
 
         cap_room = self.root.cssselect("cap_room")[0].text_content().strip()
         if cap_room != "":
