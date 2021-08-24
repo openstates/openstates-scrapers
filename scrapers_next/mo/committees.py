b3fc753a4ca5e3c4c9d306091b7ca401ee881d79
judgejudes <judylee2021@u.northwestern.edu>
2021-08-24 11:37:58 -0400

    house committees and members all scraped, including links and sources

--- scrapers_next/mo/committees.py
+++ scrapers_next/mo/committees.py
@@ -1,5 +1,5 @@
 # import re
-from spatula import HtmlPage, HtmlListPage, CSS, XPath
+from spatula import HtmlPage, HtmlListPage, CSS, XPath, SelectorError
 from openstates.models import ScrapeCommittee
 
 
@@ -92,9 +92,70 @@ class SenateCommitteeList(HtmlListPage):
             self.skip()
 
 
+class HouseCommitteeDetail(HtmlPage):
+    # example_source = "https://www.house.mo.gov/MemberGridCluster.aspx?filter=compage&category=committee&Committees.aspx?category=all&committee=2582&year=2021&code=R"
+    example_source = "https://www.house.mo.gov/MemberGridCluster.aspx?filter=compage&category=committee&Committees.aspx?category=all&committee=2571&year=2021&code=R"
+
+    def process_page(self):
+        com = self.input
+        com.add_source(self.source.url)
+        link = self.source.url.replace(
+            "MemberGridCluster.aspx?filter=compage&category=committee&", ""
+        )
+        com.add_link(link, note="homepage")
+
+        # TODO: remember to add the link after constructing it
+
+        # add members and positions
+
+        try:
+            members = CSS("#theTable tr").match(self.root)
+            # print("MEMBERS", members)
+
+            for member in members:
+                # member = member.text_content()
+                # print("NAMES", member.text_content())
+                if member.text_content().strip() == "NamePartyPosition":
+                    # print("TRIGG")
+                    continue
+
+                # member = member.text_content()
+                # info = CSS("td").match(member)
+
+                # for infos in info:
+                __, name, __, position = CSS("td").match(member)
+                # print("NAME", name.text_content())
+
+                # rearrange name
+                name = (
+                    name.text_content()
+                    .replace("Rep. ", "")
+                    .replace("Sen. ", "")
+                    .split(",")
+                )
+                name = name[1] + " " + name[0]
+                # print("FINAL NAME", name)
+
+                if position.text_content():
+                    position = position.text_content()
+                    # print("POSITION", position)
+                else:
+                    position = "member"
+                # print("POSITION", position)
+                com.add_member(name, position)
+        except SelectorError:
+            print("selector error")
+            pass
+        return com
+
+
+# com.add_member(member_name, member_pos_str)
+
+
 class HouseCommitteeList(HtmlListPage):
     source = "https://www.house.mo.gov/CommitteeHierarchy.aspx"
     selector = CSS("a")
+    chamber = "lower"
 
     def process_item(self, item):
         # this seems to be the homepage: https://www.house.mo.gov/CommitteeDetail.aspx?filter=compage&committee=2497&year=2021&code=R%20&viewCode=&cluster=true
@@ -104,16 +165,17 @@ class HouseCommitteeList(HtmlListPage):
         # craft a url: https://www.house.mo.gov/MemberGridCluster.aspx?filter=compage&category=committee&
         committee_name = item.text_content()
 
-        committee_name = committee_name.replace("Committee On ", "")
-        committee_name = committee_name.replace("Special", "")
-        committee_name = committee_name.replace("Select", "")
-        committee_name = committee_name.replace("Special", "")
-        committee_name = committee_name.replace("Joint", "")
-        committee_name = committee_name.replace(" Committee on", "").replace(
-            ", Standing", ""
+        committee_name = (
+            committee_name.replace("Joint Committee on", "")
+            .replace("Special Committee on", "")
+            .replace("Special Interim Committee on", "")
+            .replace(", Standing", "")
+            .replace(", Statutory", "")
+            .replace(", Interim", "")
+            .replace(", Special Standing", "")
         )
         committee_name = committee_name.strip()
-        print(committee_name)
+        # print(committee_name)
 
         if "Subcommittee" in committee_name:
             # Subcommittee on Appropriations - Public Safety, Corrections, Transportation and Revenue, Subcommittee
@@ -121,8 +183,31 @@ class HouseCommitteeList(HtmlListPage):
             name = committee_name.replace("Subcommittee on ", "").replace(
                 ", Subcommittee", ""
             )
-            print("name then parent", name)
+            # print("name then parent", name)
 
-            parent = XPath("..//..//preceding-sibling::a").match(item)[0].text_content()
-            print("PARENT", parent)
-            # can't get rid of "Standing" for some reason...
+            parent = (
+                XPath("..//..//preceding-sibling::a")
+                .match(item)[0]
+                .text_content()
+                .replace("Joint Committee on", "")
+                .replace("Special Committee on", "")
+                .replace("Special Interim Committee on", "")
+                .replace(", Standing", "")
+                .replace(", Interim", "")
+                .replace(", Special Standing", "")
+            )
+            # print("PARENT", parent)
+
+            com = ScrapeCommittee(
+                name=name,
+                chamber=self.chamber,
+                classification="subcommittee",
+                parent=parent,
+            )
+
+        else:
+            com = ScrapeCommittee(name=committee_name, chamber=self.chamber)
+
+        comm_link = item.get("href").replace("https://www.house.mo.gov/", "")
+        source = f"https://www.house.mo.gov/MemberGridCluster.aspx?filter=compage&category=committee&{comm_link}"
+        return HouseCommitteeDetail(com, source=source)
