689e485a3ade4f175f4837b70e0c6965a572f12a
Michelle Orden <michelleorden@Michelles-MacBook-Pro.local>
2021-08-26 12:33:18 -0500

    grabbing capitol office address, phone, and fax. grabbing images

--- scrapers_next/tn/people.py
+++ scrapers_next/tn/people.py
@@ -1,6 +1,34 @@
-from spatula import CSS, URL, HtmlListPage
+from spatula import CSS, URL, HtmlListPage, HtmlPage, XPath
 from openstates.models import ScrapePerson
 import re
+
+
+class LegDetail(HtmlPage):
+    def process_page(self):
+        p = self.input
+
+        addr_lst = XPath(
+            "/html/body/div[1]/div/div/div[2]/div/div[1]/div[2]/p[1]/text()"
+        ).match(self.root)
+        cap_address = ""
+        for line in addr_lst:
+            if re.search(r"Phone", line):
+                cap_phone = re.search(r"Phone:?\s(.+)", line).groups()[0].strip()
+                p.capitol_office.voice = cap_phone
+            elif re.search(r"Fax", line):
+                cap_fax = re.search(r"Fax:?\s(.+)", line).groups()[0].strip()
+                p.capitol_office.fax = cap_fax
+            elif re.search(r"\d?-?\d{3}-\d{3}-\d{4}", line):
+                p.extras["extra phone"] = line.strip()
+            else:
+                cap_address += line.strip()
+                cap_address += " "
+        p.capitol_office.address = cap_address.strip()
+
+        img = CSS("img.framed-photo").match_one(self.root).get("src")
+        p.image = img
+
+        return p
 
 
 class Legislators(HtmlListPage):
@@ -37,11 +65,11 @@ class Legislators(HtmlListPage):
         email = re.search(r"mailto:(.+)", email).groups()[0]
         p.email = email
 
-        # should this be appended to an address?
+        # this is also being grabbed above in capitol_office.address
         office_room = CSS("td").match(item)[5].text_content().strip()
         p.extras["office"] = office_room
 
-        return p
+        return LegDetail(p, source=detail_link)
 
 
 class Senate(Legislators):
