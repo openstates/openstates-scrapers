15024f87e69042f80a9141ddb0a85667487dd9eb
Michelle Orden <michelleorden@Michelles-MacBook-Pro.local>
2021-08-30 14:09:18 -0500

    able to create ScrapePerson() object from .txt source

--- scrapers_next/nh/people.py
+++ scrapers_next/nh/people.py
@@ -1,9 +1,10 @@
 import io
 import csv
-from spatula import URL, HtmlPage, CSS, XPath, HtmlListPage, CsvListPage
+from spatula import URL, CsvListPage  # HtmlPage, CSS, XPath, HtmlListPage
 from openstates.models import ScrapePerson
 from dataclasses import dataclass
-import re
+
+# import re
 
 
 @dataclass
@@ -12,121 +13,121 @@ class PartialPerson:
     chamber: str
     district: str
     source: str
-    staff_name: str
-    staff_email: str
+    # staff_name: str
+    # staff_email: str
 
 
 # 24 total senators
 # 395 total representatives?
 
 
-class SenDetail(HtmlPage):
-    input_type = PartialPerson
+# class SenDetail(HtmlPage):
+#     input_type = PartialPerson
 
-    def process_page(self):
-        party = CSS("span.MemberHeader").match_one(self.root).text_content().strip()
-        party = re.search(r"(.+)\(([A-Z])-.+\)", party).groups()[1]
+#     def process_page(self):
+#         party = CSS("span.MemberHeader").match_one(self.root).text_content().strip()
+#         party = re.search(r"(.+)\(([A-Z])-.+\)", party).groups()[1]
 
-        if party == "D":
-            party = "Democratic"
-        elif party == "R":
-            party = "Republican"
+#         if party == "D":
+#             party = "Democratic"
+#         elif party == "R":
+#             party = "Republican"
 
-        p = ScrapePerson(
-            name=self.input.name,
-            state="nh",
-            chamber=self.input.chamber,
-            district=self.input.district,
-            party=party,
-        )
+#         p = ScrapePerson(
+#             name=self.input.name,
+#             state="nh",
+#             chamber=self.input.chamber,
+#             district=self.input.district,
+#             party=party,
+#         )
 
-        p.add_source(self.input.source)
-        p.add_source(self.source.url)
-        p.add_link(self.source.url, note="homepage")
+#         p.add_source(self.input.source)
+#         p.add_source(self.source.url)
+#         p.add_link(self.source.url, note="homepage")
 
-        p.extras["staff name"] = self.input.staff_name
-        p.extras["staff email"] = self.input.staff_email
+#         p.extras["staff name"] = self.input.staff_name
+#         p.extras["staff email"] = self.input.staff_email
 
-        img = CSS("img.auto-style2").match_one(self.root).get("src")
-        p.image = img
+#         img = CSS("img.auto-style2").match_one(self.root).get("src")
+#         p.image = img
 
-        contact_info = XPath("//*[@id='page_content']/table/tr[2]/td//strong[3]").match(
-            self.root
-        )[0]
-        cap_addr = contact_info.getnext().tail.strip()
-        cap_addr += " "
-        cap_addr += contact_info.getnext().getnext().tail.strip()
-        cap_addr += " "
-        cap_addr += contact_info.getnext().getnext().getnext().tail.strip()
-        p.capitol_office.address = cap_addr
+#         contact_info = XPath("//*[@id='page_content']/table/tr[2]/td//strong[3]").match(
+#             self.root
+#         )[0]
+#         cap_addr = contact_info.getnext().tail.strip()
+#         cap_addr += " "
+#         cap_addr += contact_info.getnext().getnext().tail.strip()
+#         cap_addr += " "
+#         cap_addr += contact_info.getnext().getnext().getnext().tail.strip()
+#         p.capitol_office.address = cap_addr
 
-        if (
-            self.source.url
-            == "http://gencourt.state.nh.us/Senate/members/webpages/district14.aspx"
-        ):
-            phone = CSS("table tr td strong").match(self.root)[5].tail.strip()
-        else:
-            phone = (
-                XPath(
-                    "//*[@id='page_content']/table/tr[2]/td//strong[contains(text(), 'Phone:')]"
-                )
-                .match(self.root)[0]
-                .tail.strip()
-            )
-            phone = re.search(r"(\d{3}-\d{3}-\d{4})(.+)?", phone).groups()[0]
-            p.capitol_office.voice = phone
+#         if (
+#             self.source.url
+#             == "http://gencourt.state.nh.us/Senate/members/webpages/district14.aspx"
+#         ):
+#             phone = CSS("table tr td strong").match(self.root)[5].tail.strip()
+#         else:
+#             phone = (
+#                 XPath(
+#                     "//*[@id='page_content']/table/tr[2]/td//strong[contains(text(), 'Phone:')]"
+#                 )
+#                 .match(self.root)[0]
+#                 .tail.strip()
+#             )
+#             phone = re.search(r"(\d{3}-\d{3}-\d{4})(.+)?", phone).groups()[0]
+#             p.capitol_office.voice = phone
 
-        email = CSS("table tr td a").match(self.root)[1].text_content().strip()
-        p.email = email
+#         email = CSS("table tr td a").match(self.root)[1].text_content().strip()
+#         p.email = email
 
-        return p
-
-
-class Senate(HtmlListPage):
-    source = URL("http://gencourt.state.nh.us/Senate/members/senate_roster.aspx")
-    selector = CSS("div#roseterWrap > div", num_items=24)
-
-    def process_item(self, item):
-
-        header_line = CSS("div").match(item)[0].text_content().strip()
-        header_line_lst = re.search(
-            r"District\s(.+)\s-\sSenator\s(.+)", header_line
-        ).groups()
-
-        district = header_line_lst[0]
-        if district[0] == "0":
-            district = district[1]
-        name = header_line_lst[1].strip()
-
-        staff = CSS("a").match(item)[1]
-        staff_email = staff.get("href")
-        staff_email = re.search(r"mailto:(.+)", staff_email).groups()[0]
-        staff_name = staff.text_content().strip()
-
-        partial = PartialPerson(
-            name=name,
-            chamber="upper",
-            district=district,
-            source=self.source.url,
-            staff_name=staff_name,
-            staff_email=staff_email,
-        )
-
-        detail_link = CSS("a").match(item)[0].get("href")
-
-        return SenDetail(partial, source=detail_link)
+#         return p
 
 
-class House(HtmlPage):
-    source = URL("http://gencourt.state.nh.us/house/members/default.aspx")
+# class Senate(HtmlListPage):
+#     source = URL("http://gencourt.state.nh.us/Senate/members/senate_roster.aspx")
+#     selector = CSS("div#roseterWrap > div", num_items=24)
 
-    def process_page(self):
-        members = XPath("//*[@id='ContentPlaceHolder1_ddlReps']/option").match(
-            self.root
-        )
-        print(len(members))
-        # for member in members:
-        # print(member.text_content())
+#     def process_item(self, item):
+
+#         header_line = CSS("div").match(item)[0].text_content().strip()
+#         header_line_lst = re.search(
+#             r"District\s(.+)\s-\sSenator\s(.+)", header_line
+#         ).groups()
+
+#         district = header_line_lst[0]
+#         if district[0] == "0":
+#             district = district[1]
+#         name = header_line_lst[1].strip()
+
+#         staff = CSS("a").match(item)[1]
+#         staff_email = staff.get("href")
+#         staff_email = re.search(r"mailto:(.+)", staff_email).groups()[0]
+#         staff_name = staff.text_content().strip()
+
+#         partial = PartialPerson(
+#             name=name,
+#             chamber="upper",
+#             district=district,
+#             source=self.source.url,
+#             staff_name=staff_name,
+#             staff_email=staff_email,
+#         )
+
+#         detail_link = CSS("a").match(item)[0].get("href")
+
+#         return SenDetail(partial, source=detail_link)
+
+
+# class House(HtmlPage):
+#     source = URL("http://gencourt.state.nh.us/house/members/default.aspx")
+
+#     def process_page(self):
+#         members = XPath("//*[@id='ContentPlaceHolder1_ddlReps']/option").match(
+#             self.root
+#         )
+#         print(len(members))
+#         # for member in members:
+#         # print(member.text_content())
 
 
 class Legislators(CsvListPage):
@@ -140,41 +141,57 @@ class Legislators(CsvListPage):
     source = URL("http://gencourt.state.nh.us/downloads/members.txt")
 
     def process_item(self, item):
-        print(item)
-        for line in item.values():
-            # print(line)
-            member = line.split("\t")
+        lastname = item["LastName"]
+        firstname = item["FirstName"]
+        middlename = item["MiddleName"]
+        name = firstname + " " + middlename + " " + lastname
 
-            lastname = member[0]
-            firstname = member[1]
-            middlename = member[2]
-            name = firstname + " " + middlename + " " + lastname
+        legislativebody = item["LegislativeBody"]
+        if legislativebody == "H":
+            chamber = "lower"
+        elif legislativebody == "S":
+            chamber = "upper"
 
-            legislativebody = member[3]
-            if legislativebody == "H":
-                chamber = "lower"
-            elif legislativebody == "S":
-                chamber = "upper"
+        district = item["District"]
 
-            district = member[6]
+        # 39 out of 116 legislators have incomplete info (len(member) < 19)
+        # 24 senators and 15 reps
+        # if chamber == "upper":
+        #     if len(district) == 1:
+        #         district_id = "0" + district
+        #     else:
+        #         district_id = district
+        #     # detail_link = f"http://www.gencourt.state.nh.us/Senate/members/webpages/district{district_id}.aspx"
 
-            # 39 out of 116 legislators have incomplete info (len(member) < 19)
-            # 24 senators and 15 reps
-            if chamber == "upper":
-                if len(district) == 1:
-                    district_id = "0" + district
-                else:
-                    district_id = district
-                detail_link = f"http://www.gencourt.state.nh.us/Senate/members/webpages/district{district_id}.aspx"
+        party = item["party"]
+        if party == "D" or party == "d":
+            party = "Democratic"
+        elif party == "R" or party == "r":
+            party = "Republican"
 
-                partial = PartialPerson(
-                    name=name,
-                    chamber=chamber,
-                    district=district,
-                    source=self.source.url,
-                )
+        p = ScrapePerson(
+            name=name, state="nh", chamber=chamber, district=district, party=party
+        )
 
-                return SenDetail(partial, source=detail_link)
+        p.add_source(self.source.url)
+
+        p.family_name = lastname
+        p.given_name = firstname
+
+        # seatno
+        # County
+        # electedStatus
+        # Address
+        # address2
+        # city
+        # Zipcode
+        # State
+        # Phone
+        # WorkEmail
+        # GenderCode
+        # PersonTitle
+
+        return p
 
 
 # # print(len(member))
