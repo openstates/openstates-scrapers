11a0f8775b03258e1092c73580332fd9bd152193
Michelle Orden <michelleorden@Michelles-MacBook-Pro.local>
2021-09-02 17:40:40 -0500

    started writing nm scraper using spatula

---
+++ scrapers_next/nm/people.py
@@ -0,0 +1,41 @@
+from spatula import URL, CSS, HtmlListPage
+from openstates.models import ScrapePerson
+import re
+
+
+class LegList(HtmlListPage):
+    selector = CSS("div.col-xs-6.col-sm-3.col-md-2.col-lg-2")
+
+    def process_item(self, item):
+        name_party = CSS("span").match(item)[0].text_content().strip().split(" - ")
+        name = name_party[0].strip()
+        party = name_party[1].strip()
+        if party == "(D)":
+            party = "Democratic"
+        elif party == "(R)":
+            party = "Republican"
+        elif party == "(DTS)":
+            party = "Independent"
+
+        district = CSS("span").match(item)[1].text_content().strip()
+        district = re.search(r"District:\s(.+)", district).groups()[0].strip()
+
+        p = ScrapePerson(
+            name=name,
+            state="nm",
+            chamber=self.chamber,
+            district=district,
+            party=party,
+        )
+
+        return p
+
+
+class Senate(LegList):
+    source = URL("https://www.nmlegis.gov/Members/Legislator_List?T=S")
+    chamber = "upper"
+
+
+class House(LegList):
+    source = URL("https://www.nmlegis.gov/Members/Legislator_List?T=R")
+    chamber = "lower"
