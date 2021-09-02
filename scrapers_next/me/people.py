b31094a632ddceb9204ab6fb46487838831da5d1
Michelle Orden <michelleorden@Michelles-MacBook-Pro.local>
2021-09-02 10:49:26 -0500

    passing PartialPerson from House() to RepDetail()

--- scrapers_next/me/people.py
+++ scrapers_next/me/people.py
@@ -1,40 +1,70 @@
-from spatula import URL, CSS, HtmlListPage, SelectorError
-
-# from openstates.models import ScrapePerson
+from spatula import URL, CSS, HtmlListPage, HtmlPage, XPath
+from dataclasses import dataclass
+from openstates.models import ScrapePerson
 import re
 
 
-class RepList(HtmlListPage):
+@dataclass
+class PartialPerson:
+    name: str
+    party: str
+    source: str
+
+
+# is this correct?
+_party_map = {
+    "D": "Democratic",
+    "R": "Republican",
+    "I": "Independent",
+    "L": "Libertarian",
+    "Passamaquoddy Tribe": "Independent",
+}
+
+
+class RepDetail(HtmlPage):
+    input_type = PartialPerson
+
+    def process_page(self):
+        district = (
+            XPath("//*[@id='main-info']/p/span[contains(text(), 'District')]")
+            .match_one(self.root)
+            .getnext()
+        )
+        district = XPath("text()").match(district)[0].strip()
+        # https://legislature.maine.gov/house/house/MemberProfiles/Details/1193 has no district
+        if district != "":
+            district = re.search(r"(\d+)\s+-", district).groups()[0]
+
+        p = ScrapePerson(
+            name=self.input.name,
+            state="me",
+            chamber="lower",
+            district=district,
+            party=self.input.party,
+        )
+
+        p.add_source(self.input.source)
+        p.add_source(self.source.url)
+        p.add_link(self.source.url, note="homepage")
+
+        return p
+
+
+class House(HtmlListPage):
+    source = URL("https://legislature.maine.gov/house/house/MemberProfiles/ListAlpha")
     selector = CSS("table tr td", num_items=152)
 
     def process_item(self, item):
         name_dirty = CSS("br").match(item)[0].tail.strip().split(", ")
         name = name_dirty[1] + " " + name_dirty[0]
-        print(name)
-        try:
-            party = CSS("br").match(item)[2].tail.strip()
-            if re.search(r"\(([A-Z])\s-(.+)", party):
-                party = re.search(r"\(([A-Z])\s-(.+)", party).groups()[0]
-            print(party)
-        except SelectorError:
-            pass
 
-        # p = ScrapePerson(
-        #     name=name,
-        #     state="me",
-        #     chamber=self.chamber,
-        #     district=district,
-        #     party=party,
-        # )
+        party = CSS("br").match(item)[2].tail.strip()
+        if re.search(r"\(([A-Z])\s-(.+)", party):
+            party = re.search(r"\(([A-Z])\s-(.+)", party).groups()[0]
+        party = _party_map[party]
 
-        # return p
+        partial = PartialPerson(name=name, party=party, source=self.source.url)
 
+        detail_link = CSS("a").match(item)[1].get("href")
 
-# class Senate(LegList):
-#     source = URL("")
-#     chamber = "upper"
-
-
-class House(RepList):
-    source = URL("https://legislature.maine.gov/house/house/MemberProfiles/ListAlpha")
-    chamber = "lower"
+        return RepDetail(partial, source=detail_link)
