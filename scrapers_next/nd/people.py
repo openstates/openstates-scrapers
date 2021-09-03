8277da2125d075807f5e21646e8baa31feae7e0a
Michelle Orden <michelleorden@Michelles-MacBook-Pro.local>
2021-09-03 16:44:40 -0500

    grabbing all info from detail pages. connection timing out so still need to print/test

--- scrapers_next/nd/people.py
+++ scrapers_next/nd/people.py
@@ -1,6 +1,72 @@
-from spatula import URL, CSS, HtmlListPage
+from spatula import URL, CSS, HtmlListPage, HtmlPage, XPath  # , SelectorError
 from openstates.models import ScrapePerson
 import re
+
+
+class LegDetail(HtmlPage):
+    def process_page(self):
+        p = self.input
+
+        img = CSS("div.field-person-photo img").match_one(self.root).get("src")
+        # print(img)
+        p.image = img
+
+        # bio_info = CSS("div.pane-content ul li").match(self.root)
+        # occupation = bio_info[0].text_content().strip()
+        # education = bio_info[1].text_content().strip()
+        # military = bio_info[2].text_content().strip()
+        # other_pos = bio_info[3].text_content().strip()
+        # service = bio_info[4].text_content().strip()
+        # print(len(bio_info))
+
+        # try:
+        #     street = (
+        #         CSS("div.street-address").match_one(self.root).text_content().strip()
+        #     )
+        #     town = CSS("span.locality").match_one(self.root).text_content().strip()
+        #     zip_code = (
+        #         CSS("span.postal-code").match_one(self.root).text_content().strip()
+        #     )
+        #     address = street + ", " + town + ", ND " + zip_code
+        #     # print(address)
+        # except SelectorError:
+        #     pass
+
+        # Cellphone:
+        # Home Telephone:
+        # Office Telephone:
+        # try:
+        #     phones_lower = XPath("//*[@id='block-system-main']//div[contains(text(), 'phone')]").match(self.root)
+        #     for phone in phones_lower:
+        #         phone_type = phone.text_content().strip()
+        #         print(phone_type)
+        #         phone_number = phone.getnext().text_content().strip()
+        #         print(phone_number)
+        # except SelectorError:
+        #     pass
+
+        # try:
+        #     phones_upper = XPath("//*[@id='block-system-main']//div[contains(text(), 'Phone')]").match(self.root)
+        #     for phone in phones_upper:
+        #         phone_type = phone.text_content().strip()
+        #         print(phone_type)
+        #         phone_number = phone.getnext().text_content().strip()
+        #         print(phone_number)
+        # except SelectorError:
+        #     pass
+
+        # email
+        email = (
+            XPath("//*[@id='block-system-main']//div[contains(text(), 'Email')]")
+            .match_one(self.root)
+            .getnext()
+            .text_content()
+            .strip()
+        )
+        # print(email)
+        p.email = email
+
+        return p
 
 
 class LegList(HtmlListPage):
@@ -37,4 +103,10 @@ class LegList(HtmlListPage):
             party=party,
         )
 
-        return p
+        detail_link = CSS("div.name a").match_one(item).get("href")
+
+        p.add_source(self.source.url)
+        p.add_source(detail_link)
+        p.add_link(detail_link, note="homepage")
+
+        return LegDetail(p, source=detail_link)
