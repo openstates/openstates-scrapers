1b2066cf91631f0c5f356e20936a063528e8c053
Michelle Orden <michelleorden@Michelles-MacBook-Pro.local>
2021-09-07 17:06:27 -0500

    grabbing capitol and mailing addresses

--- scrapers_next/pr/people.py
+++ scrapers_next/pr/people.py
@@ -1,15 +1,105 @@
-from spatula import URL, CSS, HtmlListPage
+from spatula import URL, CSS, HtmlListPage, HtmlPage, SelectorError
 from openstates.models import ScrapePerson
 import re
+from dataclasses import dataclass
+
+
+@dataclass
+class PartialSen:
+    name: str
+    party: str
+    source: str
+
+
+class SenDetail(HtmlPage):
+    input_type = PartialSen
+
+    def process_page(self):
+        district = (
+            CSS(
+                "span#DeltaPlaceHolderMain section div.row.profile-container div ul li a"
+            )
+            .match(self.root)[1]
+            .text_content()
+            .strip()
+        )
+        if district == "Senador por Acumulación":
+            district = "At-Large"
+        elif district == "Senadora por Distrito":
+            # most are missing a district number
+            try:
+                district = (
+                    CSS("div.module-distrito")
+                    .match(self.root)[0]
+                    .text_content()
+                    .strip()
+                )
+                # print(district)
+            except SelectorError:
+                pass
+
+        p = ScrapePerson(
+            name=self.input.name,
+            state="pr",
+            chamber="upper",
+            district="",
+            party=self.input.party,
+        )
+
+        p.add_source(self.input.source)
+        p.add_source(self.source.url)
+        p.add_link(self.source.url, note="homepage")
+
+        try:
+            img = CSS("div.avatar img").match_one(self.root).get("src")
+            p.image = img
+        except SelectorError:
+            pass
+
+        email = (
+            CSS("a.contact-data.email")
+            .match_one(self.root)
+            .text_content()
+            .replace("\u200b", "")
+            .strip()
+        )
+        p.email = email
+
+        title = CSS("span.position").match_one(self.root).text_content().strip()
+        if title != "":
+            p.extras["title"] = title
+
+        # all have same phone number
+        phone = CSS("a.contact-data.tel").match_one(self.root).text_content().strip()
+        p.capitol_office.voice = phone
+
+        addresses = CSS("div.pre-footer div div div div p").match(self.root)
+        cap_addr = CSS("br").match(addresses[0])
+        capitol_address = ""
+        for line in cap_addr:
+            if line.tail.strip() != "":
+                capitol_address += line.tail.strip()
+                capitol_address += " "
+        p.capitol_office.address = capitol_address.strip()
+
+        mail_addr = CSS("br").match(addresses[1])
+        mailing_address = ""
+        for line in mail_addr:
+            if line.tail.strip() != "":
+                mailing_address += line.tail.strip()
+                mailing_address += " "
+        p.extras["Mailing address"] = mailing_address.strip()
+
+        return p
 
 
 class Senate(HtmlListPage):
     source = URL("https://senado.pr.gov/Pages/Senadores.aspx")
-    selector = CSS("ul.senadores-list li")
+    selector = CSS("ul.senadores-list li", num_items=27)
 
     def process_item(self, item):
+        # Convert names to title case as they are in all-caps
         name = CSS("span.name").match_one(item).text_content().strip()
-        # Convert to title case as some names are in all-caps
         name = re.sub(r"^Hon\.", "", name, flags=re.IGNORECASE).strip().title()
 
         party = CSS("span.partido").match_one(item).text_content().strip()
@@ -17,25 +107,11 @@ class Senate(HtmlListPage):
         if party == "Independiente":
             party = "Independent"
 
-        p = ScrapePerson(
-            name=name,
-            state="pr",
-            chamber="upper",
-            district="",
-            party=party,
-        )
-
-        title = CSS("span.position").match_one(item).text_content().strip()
-        if not re.search(r"{Position}", title):
-            p.extras["title"] = title
-
         detail_link = CSS("a").match_one(item).get("href")
 
-        p.add_source(self.source.url)
-        p.add_source(detail_link)
-        p.add_link(detail_link, note="homepage")
+        partial = PartialSen(name=name, party=party, source=self.source.url)
 
-        return p
+        return SenDetail(partial, source=detail_link)
 
 
 # class House(HtmlListPage):
