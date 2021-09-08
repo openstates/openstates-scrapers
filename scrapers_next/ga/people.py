5ebdd54d6258834964e710bdbef075ab836f00d3
judgejudes <judylee2021@u.northwestern.edu>
2021-09-08 18:21:46 -0400

    added utils for ga and updated authentication, wrote url to go to detail page, added capitol address if it exists, and some extras

--- scrapers_next/ga/people.py
+++ scrapers_next/ga/people.py
@@ -1,5 +1,63 @@
-from spatula import JsonListPage
+from spatula import JsonListPage, JsonPage, URL
 from openstates.models import ScrapePerson
+from .utils import get_token
+
+
+class PeopleDetail(JsonPage):
+    def process_page(self):
+        p = self.input
+        p.add_source(self.source.url, note="Detail page (requires authorization token)")
+
+        p.email = self.data["email"]
+
+        # capitol office
+        try:
+            capitol_office = (
+                self.data["capitolAddress"]["address1"]
+                + "; "
+                + self.data["capitolAddress"]["city"]
+                + ", GA "
+                + self.data["capitolAddress"]["zip"].strip()
+            )
+            p.capitol_office.address = capitol_office
+            p.capitol_office.voice = self.data["capitolAddress"]["phone"]
+
+            if self.data["capitolAddress"]["fax"]:
+                p.capitol_office.fax = self.data["capitolAddress"]["fax"]
+        except TypeError:
+            pass
+        # a lot of extras: birthday, committee memberships, occupation, press, religion, residence, sessionsinservice, spouse, staff with email, title,
+        # p.extras["title"]
+
+        # residence, city, georgia id
+        extras = [
+            "birthday",
+            "occupation",
+            "press",
+            "religion",
+            "spouse",
+            "staff",
+            "title",
+        ]
+        # extras = [self.data["birthday"], self.data["occupation"],self.data["press"],self.data["religion"],self.data["spouse"], self.data["staff"], self.data["title"],]
+        # for info in extras:
+        #     if info != 'null':
+        #         p.extras[]
+
+        # for self.data[info] in extras:
+        #     print("hi", info)
+
+        for type_info in extras:
+            info = self.data[type_info]
+            if info != "null":
+                # p.extras[type_info] = info
+                if type_info == "staff":
+                    p.extras["staff"] = info
+                    # p.extras["staff email"]
+                else:
+                    p.extras[type_info] = info
+
+        return p
 
 
 class DirectoryListing(JsonListPage):
@@ -9,8 +67,12 @@ class DirectoryListing(JsonListPage):
     chamber_names = {1: "house", 2: "senate"}
     party_ids = {0: "Democratic", 1: "Republican"}
 
+    # TODO: replace with utils in committees.py
+
     def process_item(self, item):
         chamber_id = item["district"]["chamberType"]
+        print("NAME", item["fullName"])
+
         p = ScrapePerson(
             state="ga",
             chamber=self.chamber_types[chamber_id],
@@ -24,9 +86,13 @@ class DirectoryListing(JsonListPage):
 
         # district address
         da = item["districtAddress"]
-        p.email = da["email"]
-        p.district_office.voice = da["phone"]
-        p.district_office.fax = da["fax"]
+        if da["email"]:
+            p.email = da["email"]
+
+        if da["phone"]:
+            p.district_office.voice = da["phone"]
+        if da["fax"]:
+            p.district_office.fax = da["fax"]
         if da["address1"]:
             p.district_office.address = da["address1"]
             if da["address2"]:
@@ -46,7 +112,7 @@ class DirectoryListing(JsonListPage):
 
         # extras
         p.extras["residence"] = item["residence"]
-        p.extras["city"] = item["city"]
+        p.extras["city"] = item["city"].strip()
         p.extras["georgia_id"] = item["id"]
         if item["dateVacated"]:
             p.end_date = item["dateVacated"]
@@ -56,9 +122,28 @@ class DirectoryListing(JsonListPage):
             f"{item['id']}?session={item['sessionId']}"
         )
         p.add_source(url)
-        p.add_link(url)
+        # TODO: get rid of below link?
+        # p.add_link(url)
 
-        return p
+        # return p
+
+        # source may instead be including &chamber=2 at the end
+        source = URL(
+            f"https://www.legis.ga.gov/api/members/detail/{item['id']}?session=1029&chamber={chamber_id}",
+            headers={"Authorization": get_token()},
+        )
+
+        # https://www.legis.ga.gov/api/members/detail/754?session=1029&chamber=2
+
+        return PeopleDetail(p, source=source)
 
 
-people = DirectoryListing(source="https://www.legis.ga.gov/api/members/list/1029")
+people = DirectoryListing(
+    source=URL(
+        "https://www.legis.ga.gov/api/members/list/1029?",
+        headers={"Authorization": get_token()},
+    )
+)
+#     source = URL(
+#     "https://www.legis.ga.gov/api/committees/List/1029",
+#     headers={"Authorization": get_token()},)
