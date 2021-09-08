34513af102128a5c4fcde40f49edb4c8e55f2a4e
Michelle Orden <michelleorden@Michelles-MacBook-Pro.local>
2021-09-08 16:07:14 -0500

    grabbing all info from json

--- scrapers_next/ut/people.py
+++ scrapers_next/ut/people.py
@@ -22,25 +22,47 @@ class LegList(JsonPage):
 
             district = leg["district"].strip()
 
-            # leg['id']
-            # leg['image']
-            # leg['serviceStart']
-            # leg['profession']
-            # leg['professionalAffiliations']
-            # leg['education']
-            # leg['recognitionsAndHonors']
-            # leg['counties']
-            # leg['address']
-            # leg['email']
-            # leg['workPhone']
-            # leg['committees']
-            # leg['legislation']
-            # leg['demographic']
-            # leg['CofI']
-            # leg['FinanceReport']
-
             p = ScrapePerson(
                 name=name, state="ut", chamber=chamber, district=district, party=party
             )
+
+            p.add_source(self.source.url)
+
+            p.image = leg["image"]
+            p.email = leg["email"].strip()
+            if leg["address"].strip() != ", , ,":
+                p.district_office.address = leg["address"].strip()
+
+            if leg.get("workPhone", None):
+                p.district_office.voice = leg["workPhone"]
+
+            p.add_link(leg["legislation"], note="legislation")
+
+            if leg["serviceStart"].strip() != "":
+                p.extras["service start"] = leg["serviceStart"].strip()
+
+            if leg["profession"].strip() != "":
+                p.extras["profession"] = leg["profession"].strip()
+
+            if leg["education"].strip() != "":
+                p.extras["education"] = leg["education"].strip()
+
+            p.extras["counties represented"] = []
+            for county in leg["counties"].split(","):
+                p.extras["counties represented"] += [county.strip()]
+
+            p.add_link(leg["demographic"], note="district demographics")
+
+            if leg.get("CofI", None):
+                for num, url in enumerate(leg["CofI"]):
+                    p.add_link(url["url"], note=f"conflict of interest {num}")
+
+            if (
+                leg["FinanceReport"][0]["url"]
+                != "http://www.disclosures.utah.gov/Search/PublicSearch"
+            ):
+                p.add_link(leg["FinanceReport"][0]["url"], note="finance report")
+
+            # leg['id']
 
             yield p
