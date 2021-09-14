c01005a956f02c19827a1c7c4eaf1bc4c7c17ad9
Michelle Orden <michelleorden@Michelles-MacBook-Pro.local>
2021-09-14 16:34:51 -0500

    grabbing images, district office addresses and phones from detail pages

--- scrapers_next/wv/people.py
+++ scrapers_next/wv/people.py
@@ -1,6 +1,41 @@
-from spatula import URL, CSS, HtmlListPage, XPath
+from spatula import URL, CSS, HtmlListPage, XPath, HtmlPage, SelectorError
 from openstates.models import ScrapePerson
 import re
+
+
+class LegDetail(HtmlPage):
+    def process_page(self):
+        p = self.input
+
+        img = XPath("//img[@valign = 'top']").match(self.root)[0].get("src")
+        p.image = img
+
+        try:
+            district = XPath(
+                "//div[@id = 'wrapleftcolr']/b[contains(text(), 'District:')]"
+            ).match_one(self.root)
+            distr_addr = ""
+            for line in district.itersiblings():
+                if line.tail is not None and line.tail.strip() != "":
+                    if re.search(r"\(\d{3}\)\s\s?\d{3}-\d{4}", line.tail.strip()):
+                        p.district_office.voice = line.tail.strip()
+                    else:
+                        distr_addr += line.tail.strip()
+                        distr_addr += " "
+            p.district_office.address = distr_addr.strip()
+        except SelectorError:
+            pass
+
+        if p.district_office.voice == "":
+            try:
+                distr_phone = XPath(
+                    "//*[@id='wrapleftcolr']/i[contains(text(), 'District Phone')]"
+                ).match_one(self.root)
+                p.district_office.voice = distr_phone.tail.strip()
+            except SelectorError:
+                pass
+
+        return p
 
 
 class LegList(HtmlListPage):
@@ -47,7 +82,7 @@ class LegList(HtmlListPage):
         p.add_source(detail_link)
         p.add_link(detail_link, note="homepage")
 
-        return p
+        return LegDetail(p, source=detail_link)
 
 
 class Senate(LegList):
