f397a8396043ffd265dfa336d6919fde97708f9d
Michelle Orden <michelleorden@Michelles-MacBook-Pro.local>
2021-09-15 10:54:29 -0500

    grabbing image from detail link

--- scrapers_next/wy/people.py
+++ scrapers_next/wy/people.py
@@ -1,5 +1,52 @@
-from spatula import URL, JsonListPage
+from spatula import URL, JsonListPage, JsonPage
 from openstates.models import ScrapePerson
+
+
+class LegDetail(JsonPage):
+    def process_page(self):
+        p = self.input
+
+        img = f"https://wyoleg.gov/LegislatorSummary/Photos/{self.data['legPhoto']}"
+        p.image = img
+
+        # self.data['county']
+        # self.data['remarks']
+        # self.data['occupationDesc']
+        # self.data['civicOrgs']
+        # self.data['legEducation']
+        # self.data['legLeadership']
+        # self.data['cityList']
+        # self.data['countyList']
+        # self.data['legPriorService']
+        # self.data['districtList']
+        # self.data['officesHeld']
+        # self.data['spouseName']
+        # self.data['noChildren']
+        # self.data['noGChildren']
+        # self.data['occupWeb']
+        # self.data['dob']
+        # self.data['birthPlace']
+        # self.data['religion']
+        # self.data['firstYrHouse']
+        # self.data['lastYrHouse']
+        # self.data['firstYrSenate']
+        # self.data['lastYrSenate']
+        # self.data['houseYears']
+        # self.data['senateYears']
+
+        # self.data['address']
+        # self.data['city']
+        # self.data['state']
+        # self.data['zip']
+        # self.data['onWeb']
+        # self.data['areaCode']
+        # self.data['phoneType']
+        # self.data['leadershipPosition']
+        # self.data['leadershipOrder']
+        # self.data['currentLeadershipPosition']
+        # self.data['legStatus']
+
+        return p
 
 
 class LegList(JsonListPage):
@@ -36,12 +83,15 @@ class LegList(JsonListPage):
         # item['leadershipOrder']
         # item['legStatus']
         # item['county']
-        # item['legID']
 
         # assuming this is a district office phone
         p.district_office.voice = item["phone"].strip()
 
-        return p
+        detail_link = f"https://wyoleg.gov/LsoService/api/legislator/{item['legID']}"
+        p.add_source(detail_link)
+        # should I add detail_link as homepage?
+
+        return LegDetail(p, source=detail_link)
 
 
 class Senate(LegList):
