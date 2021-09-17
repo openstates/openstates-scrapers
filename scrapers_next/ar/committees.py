d6e8dfb3d78ae49845d69c66fbf01ee243d1d8ee
Michelle Orden <michelleorden@Michelles-MacBook-Pro.local>
2021-09-17 12:41:49 -0500

    trying to grab subcommittees. need to work on this more

--- scrapers_next/ar/committees.py
+++ scrapers_next/ar/committees.py
@@ -1,15 +1,15 @@
-from spatula import URL, CSS, HtmlListPage
+from spatula import URL, CSS, HtmlListPage, SelectorError
 from openstates.models import ScrapeCommittee
 
 
 class SenList(HtmlListPage):
     source = URL("https://senate.arkansas.gov/senators/committees/")
-    selector = CSS("ins > ul > li > a")
+    selector = CSS("ins > ul > li")
 
     def process_item(self, item):
-        comm_name = item.text_content().strip()
+        comm_name = CSS("a").match(item)[0].text_content().strip()
 
-        previous_sibs = item.getparent().getparent().itersiblings(preceding=True)
+        previous_sibs = item.getparent().itersiblings(preceding=True)
         for sib in previous_sibs:
             if len(sib.getchildren()) == 0:
                 chamber_type = sib.text_content().strip()
@@ -27,6 +27,23 @@ class SenList(HtmlListPage):
             classification="committee",
             chamber=chamber,
         )
+
+        com.add_source(self.source.url)
+
+        try:
+            sub_committees = CSS("ul li a").match(item)
+            for sub_comm in sub_committees:
+                sub_name = sub_comm.text_content().strip()
+
+                com = ScrapeCommittee(
+                    name=sub_name,
+                    classification="subcommittee",
+                    chamber=chamber,
+                    parent=comm_name,
+                )
+                return com
+        except SelectorError:
+            pass
 
         return com
 
