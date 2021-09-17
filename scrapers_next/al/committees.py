ddfa8ea6338b1e58bc0eadae231cfa73e25fdb80
Michelle Orden <michelleorden@Michelles-MacBook-Pro.local>
2021-09-17 11:29:43 -0500

    grabbing members, roles, and extra info

--- scrapers_next/al/committees.py
+++ scrapers_next/al/committees.py
@@ -1,5 +1,39 @@
-from spatula import URL, CSS, HtmlListPage
+from spatula import URL, CSS, HtmlListPage, HtmlPage
 from openstates.models import ScrapeCommittee
+import re
+
+
+class CommDetail(HtmlPage):
+    def process_page(self):
+        com = self.input
+
+        members = CSS("table.Grid a").match(self.root)
+        for member in members:
+            name = member.text_content().strip()
+            if re.search(r"\(", name):
+                name_split = re.search(r"(.+),\s(.+)\s\((.+)\)", name).groups()
+                first_name = name_split[1]
+                last_name = name_split[0]
+                role = name_split[2]
+            else:
+                name_split = re.search(r"(.+),\s(.+)", name).groups()
+                first_name = name_split[1]
+                last_name = name_split[0]
+                role = "member"
+            first_name = re.sub("&quot", '"', first_name)
+            name = f"{first_name} {last_name}"
+
+            com.add_member(name, role)
+
+        extra_info = CSS("div table#ContentPlaceHolder1_gvClerk tr").match(self.root)
+        for info in extra_info:
+            if ":" in info.text_content().strip():
+                idx, val = info.text_content().strip().split(":")
+                com.extras[idx.strip()] = val.strip()
+            else:
+                com.extras["Room"] = info.text_content().strip()
+
+        return com
 
 
 class CommList(HtmlListPage):
@@ -12,9 +46,13 @@ class CommList(HtmlListPage):
             chamber=self.chamber,
         )
 
-        com.add_source(self.source.url)
+        detail_link = item.get("href")
 
-        return com
+        com.add_source(self.source.url)
+        com.add_source(detail_link)
+        com.add_link(detail_link, note="homepage")
+
+        return CommDetail(com, source=detail_link)
 
 
 class Senate(CommList):
@@ -33,9 +71,11 @@ class House(CommList):
     selector = CSS("li.interim_listpad a")
 
 
-class Joint(CommList):
-    source = URL(
-        "http://www.legislature.state.al.us/aliswww/ISD/House/JointInterimCommittees.aspx"
-    )
-    chamber = "legislature"
-    selector = CSS("tr td a")
+# class Joint(CommList):
+#     source = URL(
+#         "http://www.legislature.state.al.us/aliswww/ISD/House/JointInterimCommittees.aspx"
+#     )
+#     chamber = "legislature"
+#     selector = CSS("tr td a")
+# detail links are hidden.
+# "javascript:__doPostBack('ctl00$ContentPlaceHolder1$gvJICommittees','cmdCommittee$0')"
