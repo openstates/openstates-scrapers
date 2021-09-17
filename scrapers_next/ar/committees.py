697acba3661fdaa6a8a5f2e48d8f6a4ee6b79b75
Michelle Orden <michelleorden@Michelles-MacBook-Pro.local>
2021-09-17 14:04:19 -0500

    made Senate subcommittees their own class

--- scrapers_next/ar/committees.py
+++ scrapers_next/ar/committees.py
@@ -1,10 +1,46 @@
-from spatula import URL, CSS, HtmlListPage, SelectorError
+from spatula import URL, CSS, HtmlListPage
 from openstates.models import ScrapeCommittee
+
+
+class SenSubComms(HtmlListPage):
+    source = URL("https://senate.arkansas.gov/senators/committees/")
+    selector = CSS("ins > ul > li > ul > li", num_items=87)
+
+    def process_item(self, item):
+        sub_name = CSS("a").match_one(item).text_content().strip()
+
+        previous_sibs = (
+            item.getparent().getparent().getparent().itersiblings(preceding=True)
+        )
+        for sib in previous_sibs:
+            if len(sib.getchildren()) == 0:
+                chamber_type = sib.text_content().strip()
+                break
+
+        if chamber_type == "Senate Committees":
+            chamber = "upper"
+        elif chamber_type == "Joint Committees":
+            self.skip()
+        elif chamber_type == "Task Forces":
+            self.skip()
+
+        comm_name = (
+            CSS("a").match(item.getparent().getparent())[0].text_content().strip()
+        )
+
+        com = ScrapeCommittee(
+            name=sub_name,
+            classification="subcommittee",
+            chamber=chamber,
+            parent=comm_name,
+        )
+
+        return com
 
 
 class SenList(HtmlListPage):
     source = URL("https://senate.arkansas.gov/senators/committees/")
-    selector = CSS("ins > ul > li")
+    selector = CSS("ins > ul > li", num_items=45)
 
     def process_item(self, item):
         comm_name = CSS("a").match(item)[0].text_content().strip()
@@ -29,21 +65,6 @@ class SenList(HtmlListPage):
         )
 
         com.add_source(self.source.url)
-
-        try:
-            sub_committees = CSS("ul li a").match(item)
-            for sub_comm in sub_committees:
-                sub_name = sub_comm.text_content().strip()
-
-                com = ScrapeCommittee(
-                    name=sub_name,
-                    classification="subcommittee",
-                    chamber=chamber,
-                    parent=comm_name,
-                )
-                return com
-        except SelectorError:
-            pass
 
         return com
 
