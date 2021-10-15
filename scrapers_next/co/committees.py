a68fbcb2bdb8ee77b46bacfd1efbd109c98ec056
derefaacline <cderefaa@gmail.com>
2021-10-15 15:19:46 -0500

    Changed the co/committe

--- scrapers_next/co/committees.py
+++ scrapers_next/co/committees.py
@@ -1,24 +1,31 @@
-from spatula import HtmlPage, HtmlListPage, CSS, XPath, SelectorError
+from spatula import HtmlPage, HtmlListPage, CSS, XPath, SelectorError, Selector
 from openstates.models import ScrapeCommittee
+
 
 class CommitteeDetails(HtmlPage):
     example_source = "https://leg.colorado.gov/committees/agriculture-livestock-water/2021-regular-session"
     example_input = "Agriculture and Livestock"
+
     def process_page(self):
-        chamber = CSS("span.committee-title.page-qualifying-title").match_one(self.root).text
+        chamber = (
+            CSS("span.committee-title.page-qualifying-title").match_one(self.root).text
+        )
         if chamber == "House Committee of Reference":
             chamber = "lower"
-        elif chamber =="Senate Committee of Reference":
+        elif chamber == "Senate Committee of Reference":
             chamber = "upper"
         else:
             chamber = "legislature"
-        print("CHAMBER: ",chamber)
+        print("CHAMBER: ", chamber)
         com = ScrapeCommittee(name=self.input, chamber=chamber)
-        members = CSS("div div.block-content div div:nth-child(1) div div.member-details").match(self.root)
+        members = CSS(".member h4").match(self.root)
         for member in members:
-            member_name = CSS("h4 a").match_one(member).text
-            try :
-                member_role = CSS(".member-role").match_one(member).text
+            try:
+                member_name = CSS("a").match_one(member).text_content()
+                positions = ["Chair", "Vice Chair"]
+                position = CSS("span.member-role").match_one(member).text_content().lower()
+                if position in positions:
+                    member_role = position
             except SelectorError:
                 member_role = "member"
             com.add_member(member_name, member_role)
@@ -26,11 +33,11 @@ class CommitteeDetails(HtmlPage):
         com.add_link(self.source.url, note="homepage")
         return com
 
+
 class CommitteeList(HtmlListPage):
     source = "https://leg.colorado.gov/content/committees"
-    selector = CSS("div.view-content tbody a[href]",num_items=49)
+    selector = CSS("div.view-content tbody a[href]", num_items=49)
 
     def process_item(self, item):
         name = item.text
-        return CommitteeDetails(
-            name, source=CSS("a").match_one(item).get("href"))
+        return CommitteeDetails(name, source=CSS("a").match_one(item).get("href"))
