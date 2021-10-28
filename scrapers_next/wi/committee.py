97384aec0978c194d91c81957f16d45524785ecf
derefaacline <cderefaa@gmail.com>
2021-10-28 16:25:27 -0500

    Cleaned wi_committee with black and checked with flake8

--- scrapers_next/wi/committee.py
+++ scrapers_next/wi/committee.py
@@ -1,13 +1,15 @@
-from spatula import URL, CSS, HtmlListPage, SelectorError, XPath, HtmlPage
+from spatula import URL, CSS, HtmlListPage, HtmlPage
 from openstates.models import ScrapeCommittee
 
-class CommitteeDetails (HtmlPage):
+
+class CommitteeDetails(HtmlPage):
     example_source = "https://docs.legis.wisconsin.gov/2021/committees/assembly/2349"
     example_input = "2021 Assembly Committee for Review of Administrative Rules"
+
     def process_page(self):
         com = self.input
         com.add_source(self.source.url)
-        com.add_link(self.source.url, note = "homepage" )
+        com.add_link(self.source.url, note="homepage")
         members = CSS("#members p").match(self.root)
         for member in members:
             member_name = member.text_content()
@@ -17,9 +19,15 @@ class CommitteeDetails (HtmlPage):
             for position in positions:
                 if member_name.endswith(position):
                     pos_str = position.strip().replace("(", "").replace(")", "")
-            com.add_member(member_name.split(" ", 1)[1].strip().replace("(Chair)","").replace("(Co-Chair)","").replace("(Vice-Chair)",""), pos_str)
+            com.add_member(
+                member_name.split(" ", 1)[1]
+                .strip()
+                .replace("(Chair)", "")
+                .replace("(Co-Chair)", "")
+                .replace("(Vice-Chair)", ""),
+                pos_str,
+            )
         return com
-
 
 
 class CommitteeList(HtmlListPage):
@@ -28,31 +36,26 @@ class CommitteeList(HtmlListPage):
     def process_item(self, item):
         name = item.text_content().strip()
         com = ScrapeCommittee(
-            name = name,
-            classification = "committee",
-            chamber = self.chamber,
+            name=name,
+            classification="committee",
+            chamber=self.chamber,
         )
         detail_link = item.get("href")
-        com.add_source(detail_link )
+        com.add_source(detail_link)
         com.add_link(detail_link, "homepage")
-        return CommitteeDetails(com, source = detail_link)
-
-
-
-
-
+        return CommitteeDetails(com, source=detail_link)
 
 
 class SenateCommitteeList(CommitteeList):
-    source = URL ("https://docs.legis.wisconsin.gov/2021/committees/senate")
+    source = URL("https://docs.legis.wisconsin.gov/2021/committees/senate")
     chamber = "upper"
 
 
-
 class HouseCommitteeList(CommitteeList):
-    source = URL ("https://docs.legis.wisconsin.gov/2021/committees/assembly")
+    source = URL("https://docs.legis.wisconsin.gov/2021/committees/assembly")
     chamber = "lower"
 
+
 class JointCommitteeList(CommitteeList):
-    source = URL ("https://docs.legis.wisconsin.gov/2021/committees/joint")
+    source = URL("https://docs.legis.wisconsin.gov/2021/committees/joint")
     chamber = "legislature"
