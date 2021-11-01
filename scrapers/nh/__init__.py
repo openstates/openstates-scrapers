e3a289a7f69fcc4b2229ae2c0edba98b75d50130
NewAgeAirbender <paintedflame@gmail.com>
2021-11-01 15:29:47 -0500

    NH: add 2022 session

--- scrapers/nh/__init__.py
+++ scrapers/nh/__init__.py
@@ -78,6 +78,15 @@ class NewHampshire(State):
             "end_date": "2021-06-28",
             "active": True,
         },
+        {
+            "identifier": "2022",
+            "classification": "primary",
+            "name": "2022 Regular Session",
+            "start_date": "2022-01-05",
+            # TODO: update end_date
+            "end_date": "2022-06-28",
+            "active": True,
+        },
     ]
     ignored_scraped_sessions = [
         "2013 Session",
