00de8e21c76514ccb13ea21ffab37b785cc6d238
derefaacline <cderefaa@gmail.com>
2021-11-04 10:47:12 -0500

    Added the senate section

--- scrapers_next/ok/committee.py
+++ scrapers_next/ok/committee.py
@@ -1,5 +1,23 @@
 from spatula import HtmlPage, CSS, HtmlListPage, SelectorError
 from openstates.models import ScrapeCommittee
+class SenateCommitteeDetail(HtmlPage):
+    example_source = "https://oksenate.gov/committees/agriculture-and-wildlife"
+    example_input = "Agriculture and Wildlife"
+    def process_page(self):
+        com = self.input
+        try:
+            members_name = CSS(
+                "section.section.section_ind_f.section_bg-color_b div div div div a span"
+            ).match(self.root)
+            for member in members_name:
+                name = member.text_content()
+                member_role = "member"
+                com.add_member(name, member_role)
+        except SelectorError:
+            pass
+        com.add_source(self.source.url)
+        com.add_link(self.source.url, note="homepage")
+        return com
 
 class HouseCommitteeDetail(HtmlPage):
     example_source = "https://www.okhouse.gov/Committees/CommitteeMembers.aspx?CommID=417&SubCommID=0"
@@ -102,6 +120,23 @@ class SpecialCommitteeList(CommitteeList):
     selector = CSS("#ctl00_ContentPlaceHolder1_rgdSpecial_GridData td")
     source = "https://www.okhouse.gov/Committees/Default.aspx"
     chamber = "legislature"
+
+    class SenateCommitteeList(HtmlListPage):
+        source = "https://oksenate.gov/committees-list"
+        chamber = "upper"
+        selector = CSS("div.bTiles__items")
+
+        def process_item(self, item):
+            com_link = CSS("a").match(item)[0]
+            name = CSS("a div span.bTiles__title", num_items=15).match(item)[0].text_content()
+            com = ScrapeCommittee(
+                name=name, classification="committee", chamber=self.chamber
+            )
+            detail_link = com_link.get("href")
+            com.add_source(detail_link)
+            com.add_link(detail_link, note="homepage")
+            # return com
+            return SenateCommitteeDetail(com, source=detail_link)
 
 
 if __name__ == "__main__":
