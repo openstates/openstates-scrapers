3420df031cd70daf796b736adaf158587b0b4b4d
Tim Showers <showerst@gmail.com>
2021-11-12 15:43:51 -0500

    NV: 2021 first special

--- scrapers/nv/__init__.py
+++ scrapers/nv/__init__.py
@@ -124,6 +124,14 @@ class Nevada(State):
             "end_date": "2021-06-01",
             "active": True,
         },
+        {
+            "_scraped_name": "33rd (2021) Special Session",
+            "classification": "special",
+            "identifier": "2021Special33",
+            "name": "33rd (2021) Special Session",
+            "start_date": "2021-11-12",
+            "end_date": "2021-11-12",
+        },
     ]
     ignored_scraped_sessions = [
         "25th (2008) Special Session",
--- scrapers/nv/common.py
+++ scrapers/nv/common.py
@@ -6,6 +6,7 @@ session_slugs = {
     "2016Special30": "30th2016Special",
     "2020Special31": "31st2020Special",
     "2020Special32": "32nd2020Special",
+    "2021Special33": "33rd2021Special",
     "75": "75th2009",
     "76": "76th2011",
     "77": "77th2013",
