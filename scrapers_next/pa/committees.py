bd617aa1deef3765a72df708df7788de609a127c
derefaacline <cderefaa@gmail.com>
2021-12-21 12:50:24 -0600

    Reduced the redundancy in the code

--- scrapers_next/pa/committees.py
+++ scrapers_next/pa/committees.py
@@ -2,7 +2,7 @@ from spatula import CSS, HtmlPage, HtmlListPage, SelectorError
 from openstates.models import ScrapeCommittee
 
 
-class HouseCommitteeDetail(HtmlPage):
+class CommitteeDetail(HtmlPage):
     example_source = "https://www.legis.state.pa.us/cfdocs/CteeInfo/index.cfm?Code=32&CteeBody=H&SessYear=2021"
     example_name = "Aging & Older Adult Services"
     example_input = ScrapeCommittee(
@@ -14,51 +14,51 @@ class HouseCommitteeDetail(HtmlPage):
         try:
             # This section has the chair memebers the regular, democratic and minority and the roles
             # main chair
-            Chair_Member = (
+            chair_member = (
                 CSS("div.MemberInfoList-MemberWrapper.ChairWrapper div.ChairNameText a")
                 .match(self.root)[0]
                 .text.strip()
             )
             # main chair role
-            Chair_Member_role = (
+            chair_member_role = (
                 CSS(
                     "div.MemberInfoList-MemberWrapper.ChairWrapper div.ChairNameText div"
                 )
                 .match(self.root)[0]
                 .text.strip()
             )
-            com.add_member(Chair_Member, Chair_Member_role)
+            com.add_member(chair_member, chair_member_role)
             # Democratic Chair member and or the minority chair member
-            Demo_Chair_Member = (
+            demo_chair_member = (
                 CSS("div.MemberInfoList-MemberWrapper.ChairWrapper div.ChairNameText a")
                 .match(self.root)[1]
                 .text.strip()
             )
             # Democratic Chair member and or the minority chair member role
-            Demo_Chair_Member_role = (
+            demo_chair_member_role = (
                 CSS(
                     "div.MemberInfoList-MemberWrapper.ChairWrapper div.ChairNameText div"
                 )
                 .match(self.root)[1]
                 .text.strip()
             )
-            com.add_member(Demo_Chair_Member, Demo_Chair_Member_role)
+            com.add_member(demo_chair_member, demo_chair_member_role)
         except IndexError:
             pass
-        Majority_Members = CSS(
+        majority_members = CSS(
             ".Widget.CteeInfo-MajorityList .MemberInfoList-MemberWrapper.Member"
         ).match(self.root)
-        for mem in Majority_Members:
+        for mem in majority_members:
             try:
                 major_member_name = CSS("div a").match_one(mem).text.strip()
                 major_mem_position = CSS(".position").match_one(mem).text.strip()
             except SelectorError:
                 major_mem_position = "Member"
             com.add_member(major_member_name, major_mem_position)
-        Minority_Members = CSS(
+        minority_members = CSS(
             ".Widget.CteeInfo-MinorityList .MemberInfoList-MemberWrapper.Member"
         ).match(self.root)
-        for mem in Minority_Members:
+        for mem in minority_members:
             try:
                 minor_member_name = CSS("div a").match_one(mem).text.strip()
                 minor_mem_position = CSS(".position").match_one(mem).text.strip()
@@ -68,99 +68,28 @@ class HouseCommitteeDetail(HtmlPage):
         return com
 
 
-class SenateCommitteeDetail(HtmlPage):
-    source = "https://www.legis.state.pa.us/cfdocs/CteeInfo/index.cfm?Code=32&CteeBody=H&SessYear=2021"
-    input = "Aging & Older Adult Services"
+class CommitteeList(HtmlListPage):
+    selector = CSS("table tbody tr td:nth-child(1) a")
 
-    def process_page(self):
-        com = self.input
-        try:
-            # This section has the chair memebers the regular, democratic and minority and the roles
-            # main chair
-            Chair_Member = (
-                CSS("div.MemberInfoList-MemberWrapper.ChairWrapper div.ChairNameText a")
-                .match(self.root)[0]
-                .text.strip()
-            )
-            # main chair role
-            Chair_Member_role = (
-                CSS(
-                    "div.MemberInfoList-MemberWrapper.ChairWrapper div.ChairNameText div"
-                )
-                .match(self.root)[0]
-                .text.strip()
-            )
-            com.add_member(Chair_Member, Chair_Member_role)
-            # Democratic Chair member and or the minority chair member
-            Demo_Chair_Member = (
-                CSS("div.MemberInfoList-MemberWrapper.ChairWrapper div.ChairNameText a")
-                .match(self.root)[1]
-                .text.strip()
-            )
-            # Democratic Chair member and or the minority chair member role
-            Demo_Chair_Member_role = (
-                CSS(
-                    "div.MemberInfoList-MemberWrapper.ChairWrapper div.ChairNameText div"
-                )
-                .match(self.root)[1]
-                .text.strip()
-            )
-            com.add_member(Demo_Chair_Member, Demo_Chair_Member_role)
-        except IndexError:
-            pass
-        Majority_Members = CSS(
-            ".Widget.CteeInfo-MajorityList .MemberInfoList-MemberWrapper.Member"
-        ).match(self.root)
-        for mem in Majority_Members:
-            try:
-                major_member_name = CSS("div a").match_one(mem).text.strip()
-                major_mem_position = CSS(".position").match_one(mem).text.strip()
-            except SelectorError:
-                major_mem_position = "Member"
-            com.add_member(major_member_name, major_mem_position)
-        Minority_Members = CSS(
-            ".Widget.CteeInfo-MinorityList .MemberInfoList-MemberWrapper.Member"
-        ).match(self.root)
-        for mem in Minority_Members:
-            try:
-                minor_member_name = CSS("div a").match_one(mem).text.strip()
-                minor_mem_position = CSS(".position").match_one(mem).text.strip()
-            except SelectorError:
-                minor_mem_position = "Member"
-            com.add_member(minor_member_name, minor_mem_position)
-        return com
+    def process_item(self, item):
+        name = item.text_content().strip()
+        com = ScrapeCommittee(
+            name=name, classification="committee", chamber=self.chamber
+        )
+        detail_link = item.get("href")
+        com.add_source(detail_link)
+        com.add_link(detail_link, "homepage")
+        return CommitteeDetail(com, source=detail_link)
 
 
-class SenateCommitteeList(HtmlListPage):
+class SenateCommitteeList(CommitteeList):
     source = "https://www.legis.state.pa.us/cfdocs/CteeInfo/StandingCommittees.cfm?CteeBody=S"
     chamber = "upper"
-    selector = CSS("table tbody tr td:nth-child(1) a")
-
-    def process_item(self, item):
-        name = item.text_content().strip()
-        com = ScrapeCommittee(
-            name=name, classification="committee", chamber=self.chamber
-        )
-        detail_link = item.get("href")
-        com.add_source(detail_link)
-        com.add_link(detail_link, "homepage")
-        return SenateCommitteeDetail(com, source=detail_link)
 
 
-class HouseCommitteeList(HtmlListPage):
+class HouseCommitteeList(CommitteeList):
     source = "https://www.legis.state.pa.us/cfdocs/CteeInfo/StandingCommittees.cfm?CteeBody=H"
     chamber = "lower"
-    selector = CSS("table tbody tr td:nth-child(1) a")
-
-    def process_item(self, item):
-        name = item.text_content().strip()
-        com = ScrapeCommittee(
-            name=name, classification="committee", chamber=self.chamber
-        )
-        detail_link = item.get("href")
-        com.add_source(detail_link)
-        com.add_link(detail_link, "homepage")
-        return HouseCommitteeDetail(com, source=detail_link)
 
 
 if __name__ == "__main__":
