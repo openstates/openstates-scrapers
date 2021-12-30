0cd2e809a12d5744ee663829e972ba780a6f9bde
Tim Showers <showerst@gmail.com>
2021-12-30 14:49:38 -0500

    IN: remove unused var

--- scrapers/in/events.py
+++ scrapers/in/events.py
@@ -1,7 +1,7 @@
 import pytz
+import cloudscraper
 import dateutil.parser
-import lxml
-import requests
+import lxml.html
 from openstates.scrape import Scraper, Event
 
 
@@ -9,12 +9,13 @@ class INEventScraper(Scraper):
     _tz = pytz.timezone("America/Indianapolis")
     # avoid cloudflare blocks for no UA
     cf_headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"
     }
+    scraper = cloudscraper.create_scraper()
 
     def scrape(self):
         list_url = "http://iga.in.gov/legislative/2021/committees/standing"
-        page = requests.get(list_url, headers=self.cf_headers).content
+        page = self.scraper.get(list_url).content
         page = lxml.html.fromstring(page)
         page.make_links_absolute(list_url)
 
@@ -22,7 +23,7 @@ class INEventScraper(Scraper):
             yield from self.scrape_committee_page(com_row)
 
     def scrape_committee_page(self, url):
-        page = self.get(url, headers=self.cf_headers).content
+        page = self.scraper.get(url, headers=self.cf_headers).content
         page = lxml.html.fromstring(page)
         page.make_links_absolute(url)
         com = page.xpath('//div[contains(@class, "pull-left span8")]/h1/text()')[
@@ -30,7 +31,6 @@ class INEventScraper(Scraper):
         ].strip()
 
         for row in page.xpath('//div[contains(@id, "agenda-item")]'):
-            # status = "tentative"
             meta = row.xpath('div[contains(@class,"accordion-heading-agenda")]/a')[0]
 
             date = meta.xpath("text()")[0].strip()
