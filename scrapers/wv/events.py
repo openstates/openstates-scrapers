a2ae826f26900a40ff461a26b89867635989cf76
Tim Showers <showerst@gmail.com>
2022-03-21 20:18:37 -0400

    WV: event scraper fixes

--- scrapers/wv/events.py
+++ scrapers/wv/events.py
@@ -128,7 +128,7 @@ class WVEventScraper(Scraper, LXMLMixin):
         when = re.sub(r"Time Announced(.*)", "", when, flags=re.IGNORECASE)
         when = re.sub(r"\d+ min\. After Floor Session", "", when, flags=re.IGNORECASE)
         when = re.sub(
-            r"(?:Shortly)?\s*(After|following)\s*(?:the)?\s*Floor Session",
+            r"(?:Shortly)?\s*(After|following)\s*(?:the)?\s*(?:second)?\s*Floor Session",
             "",
             when,
             flags=re.IGNORECASE,
