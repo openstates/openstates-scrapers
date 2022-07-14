fa908c70f8ad77c869edb21bba69f7c3a49bda97
John Seekins <john@civiceagle.com>
2022-07-14 08:48:22 -0600

    don't use signed commits
    Signed-off-by: John Seekins <john@civiceagle.com>

--- scripts/people-update.sh
+++ scripts/people-update.sh
@@ -58,7 +58,7 @@ git add *
 # make sure all output is printed and we exit the diff screen
 echo | git diff
 # commit and push data to new branch
-git commit -as -m "${TODAY} full people update"
+git commit -a -m "${TODAY} full people update"
 git push --set-upstream origin "${TODAY}-auto-people-merge"
 popd > /dev/null || exit 1
 
