b8dba20c409255e472ddd6fd79b2b4160437d16d
John Seekins <john@civiceagle.com>
2022-08-08 12:03:39 -0600

    revert CA changes
    Signed-off-by: John Seekins <john@civiceagle.com>

--- scrapers_next/ca/people.py
+++ scrapers_next/ca/people.py
@@ -17,7 +17,7 @@ class AssemblyList(HtmlListPage):
 
         district = CSS("td").match(item)[1].text_content().strip().lstrip("0")
 
-        # vacant districts show up with an interesting name
+        # District 18 has a vacant spot
         if name == "edit":
             self.skip("skipping Vacant seat in District {}".format(district))
 
@@ -33,42 +33,30 @@ class AssemblyList(HtmlListPage):
             party=party,
             image=photo_url,
         )
+
         capitol_office_header = CSS("h3").match(item)[0].text_content()
-        capitol_office_text = XPath("./td[4]/text()").match(item)[1].strip()
-        capitol_office_full_address, capitol_office_phone = capitol_office_text.split(
-            "; "
+        capitol_office_text = (
+            XPath(
+                "//*[@id='block-views-view-members-block-1']/div/div/div/table/tbody/tr[1]/td[4]/text()"
+            )
+            .match(item)[1]
+            .strip()
         )
-        capitol_office_pobox, office_city = capitol_office_full_address.split(",", 1)
-        capitol_office_address = f"{capitol_office_header.removeprefix('Capitol Office, ')} {office_city.strip()}"
+        capitol_office_text, capitol_office_phone = capitol_office_text.split("; ")
+        capitol_office_address = capitol_office_header + capitol_office_text
 
         p.capitol_office.address = capitol_office_address
         p.capitol_office.voice = capitol_office_phone
-        p.extras["P.O. Box"] = f"{capitol_office_pobox} {office_city}"
 
         district_offices = XPath(".//td/p[1]/text()").match(item)
-        district_phones = list()
-        office = district_offices[0]
-        district_address, district_phone = office.split("; ")
-        p.district_office.address = district_address.strip()
-        p.district_office.voice = district_phone.strip()
-        district_phones.append(district_phone.strip())
 
-        if len(district_offices) > 1:
-            for office in district_offices[1:]:
-                district_address, district_phone = office.split("; ")
-                dist_phone = district_phone.strip()
-                if dist_phone not in district_phones:
-                    district_phones.append(dist_phone)
-                    p.add_office(
-                        classification="district",
-                        address=district_address.strip(),
-                        voice=dist_phone,
-                    )
-                else:
-                    p.add_office(
-                        classification="district",
-                        address=district_address.strip(),
-                    )
+        for office in district_offices:
+            district_address, district_phone = office.split("; ")
+            p.add_office(
+                classification="district",
+                address=district_address.strip(),
+                voice=district_phone.strip(),
+            )
 
         url = CSS("a").match(item)[0].get("href")
         p.add_link(url)
