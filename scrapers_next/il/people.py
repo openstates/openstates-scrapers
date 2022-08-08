7bca46fa1dc4827816173aa5de045b49680b0c8d
John Seekins <john@civiceagle.com>
2022-08-08 12:18:31 -0600

    revert IL changes
    Signed-off-by: John Seekins <john@civiceagle.com>

--- scrapers_next/il/people.py
+++ scrapers_next/il/people.py
@@ -4,9 +4,6 @@ import re
 
 
 class LegDetail(HtmlPage):
-    fax_re = re.compile(r"(.+)\sFAX")
-    email_re = re.compile(r"Email:\s(.+)")
-
     def process_page(self):
         p = self.input
 
@@ -55,11 +52,6 @@ class LegDetail(HtmlPage):
         ) = (None, None, None, None, None, None, None)
         addresses = CSS("body table tr td.member").match(self.root)
         district_addr_lines = 0
-        """
-        This terrible for loop brought to you by the IL legislature page's terrible
-        formatting! e.g.
-        <table width="100%" border="0"><tr><td class="member" width="100%" valign="top"><br><b>Springfield Office:</b></td></tr><tr><td class="member" width="100%">Senator 27th District</td></tr><tr><td class="member" width="100%">Stratton Office Building</td></tr><tr><td class="member" width="100%">Section C , Room F</td></tr><tr><td class="member" width="100%">Springfield, IL &nbsp;&nbsp;62706    </td></tr><tr><td class="member" width="100%">(217) 782-4471</td></tr><tr><td class="member" width="100%">&nbsp;</td></tr></table><table border="0" width="100%"><tr><td class="member" width="100%"></td></tr><tr><td class="member" width="100%"><b>District Office:</b></td></tr><tr><td class="member" width="100%">171 W. Wing St.</td></tr><tr><td class="member" width="100%">Suite 202</td></tr><tr><td class="member" width="100%">Arlington Heights, IL&nbsp;&nbsp;60005    </td></tr><tr><td class="member" width="100%">(847) 749-1880</td></tr></table></td></tr></table><br><table border="0" width="390"><tr><td class="member"><b>Years served: </b> <br><br><b>Committee assignments:</b> Ethics (Chair); Commerce; Health; Insurance; Local Government; App-Constitutional Offices (Sub-Vice-Chair); App-Health; Subcommittee on Medicaid (Sub-Chair); Sub. on Managed Care Organizations; Subcommittee on Special Issues (H); Redistricting- Northwest Cook (Sub-Chair).<br><br><b>Biography:</b> Lifelong resident of the Northwest suburbs. Full-time state legislator. Retired from health care industry. Bachelor's degree in history from the University of Illinois Urbana-Champaign; law degree from DePaul University College of Law. Mother of two adult children, Kevin and Gillian. Resident of Arlington Heights since 1991.
-         """
         for line_number, line in enumerate(addresses):
             line = line.text_content().strip()
             if (
@@ -80,31 +72,33 @@ class LegDetail(HtmlPage):
             if not capitol_addr:
                 capitol_addr = line
             elif line_number in [2, 3] and not line.startswith("("):
-                capitol_addr += f" {line}"
+                capitol_addr += " "
+                capitol_addr += line
             elif not cap_phone and line.startswith("("):
                 cap_phone = line
-            elif line_number in [4, 5] and "FAX" in line:
-                capitol_fax = self.fax_re.search(line).groups()[0]
+            elif (line_number in [4, 5]) and re.search(r"FAX", line):
+                capitol_fax = re.search(r"(.+)\sFAX", line).groups()[0]
             elif not district_addr:
                 district_addr = line
                 district_addr_lines += 1
             elif district_addr_lines == 1:
-                district_addr += f" {line}"
+                district_addr += " "
+                district_addr += line
                 district_addr_lines += 1
             elif (
                 district_addr_lines == 2
                 and not line.strip().startswith("(")
-                and "Email:" not in line
+                and not re.search("Email:", line)
             ):
                 district_addr += " "
                 district_addr += line
                 district_addr_lines += 1
-            elif not dis_phone and line.startswith("("):
+            elif not dis_phone:
                 dis_phone = line
-            elif "FAX" in line:
-                dis_fax = self.fax_re.search(line).groups()[0]
-            elif "Email: " in line.strip():
-                email = self.email_re.search(line).groups()[0].strip()
+            elif re.search(r"FAX", line):
+                dis_fax = re.search(r"(.+)\sFAX", line).groups()[0]
+            elif re.search(r"Email:\s", line.strip()):
+                email = re.search(r"Email:\s(.+)", line).groups()[0].strip()
 
         return (
             capitol_addr,
