17228577e928593ad79240251bc11b97051ec0e9
Chris Yamas <chrisyamas@Chriss-MacBook-Pro.local>
2022-11-09 18:30:51 -0600

    ND: events scraper consolidates agenda items into events

--- scrapers/nd/new_events.py
+++ scrapers/nd/new_events.py
@@ -1,89 +1,150 @@
-from openstates.scrape import Scraper, Event
+import re
 import pytz
 import logging
 import dateutil.parser
-from spatula import XPath, HtmlListPage
+import datetime
+from spatula import HtmlPage
+from openstates.scrape import Scraper, Event
 
 
-# TODO: Add pagination; need guidance on this
-"""
-While spatula's get_next_source() handles this for different URL's,
-does spatula have a way to handle these buttons that seem to use JavaScript
-to load the next series of events to display on the same page?
-"""
-
-# TODO: Feedback on proposed method for extracting year for event date
-"""
-The way this scraper is currently set up using spatula, the HtmlListPage
-object is creating an item for every table row (ideal for creating events.
-However, the table row only provides MM/DD + time, not year of the event
-date. My idea for how to handle this:
-    Use a selector (maybe outside of process_item()) to extract the year
-    from an HTML element elsewhere on the page (like the November 08, 2021 to
-    November 13, 2021 date range in a <span> above the table)
-        - Problem: when we hit the event week that contains two different years
-        (end of Dec to start of Jan)
-        - Proposed Solution:
-            - create two new attributes of EventList, year and year_changed
-            - :year: string of the extracted starting year for the event range
-            - :year_changed: boolean instantiated as False
-            - A selector extracts the initial event year, saves it to `year`
-            - In process_item(), a conditional statement checks if:
-                - event date prior to Jan 7 AND `year_changed == False`
-                - if both true, `year` will be incremented by 1, and
-                `year_changed` will be reassigned to True
-            - **NOTE** A conditional will need to be added when instantiating
-            `year_changed` such that if the start of the date range <span>
-            is between Jan 1st and Jan 7th upon initial page load, then
-            `year_changed` is instantiated as `True`
-                - Without a conditional check to cover this scenario, the first
-                event would trigger an erroneous incrementing of `year` by 1.
-**NOTE** Whether due to a glitch in their calendar, or some other reason (like
-their web designer demonstrating functionality for this part of their new
-page by using past events), ND's committee hearings page is showing events
-for this time LAST YEAR (in 2021).
-    - This scraper is being built assuming they will update this prior to start
-    of January 2023 legislative session.
-"""
-
-# TODO: Determine best way to store which Bill the committee work is on
-"""
-This will support Enview team in linking events to bills.
-"""
+def time_is_earlier(new, current):
+    new_magnitude = int(new[0:2] + new[3:5])
+    current_magnitude = int(current[0:2] + current[3:5])
+    if new_magnitude < current_magnitude:
+        return True
+    else:
+        return False
 
 
-class EventList(HtmlListPage):
+class EventConsolidator:
+    def __init__(self, items):
+        self.items = items
+        self.events = {}
+        self._tz = pytz.timezone("US/Central")
+
+    def consolidate(self):
+        for item in self.items:
+            date_time = item["date_time"]
+            local_date = dateutil.parser.parse(date_time)
+
+            item_date, item_time = str(local_date).split()
+
+            committee = item["committee"]
+            location = item["location"]
+            event_key = f"{item_date}-{committee}-{location}"
+
+            if not self.events.get(event_key):
+                self.events[event_key] = {}
+                self.events[event_key]["event_start_time"] = item_time
+                self.events[event_key]["item_keys"] = set()
+            else:
+                current_start = self.events[event_key]["event_start_time"]
+                if time_is_earlier(item_time, current_start):
+                    self.events[event_key]["event_start_time"] = item_time
+
+            self.events[event_key]["item_keys"].add(item_time)
+
+            agenda_item_details = {
+                "description": item["description"],
+                "bill_name": item["bill_name"],
+            }
+            self.events[event_key][item_time] = []
+            self.events[event_key][item_time].append(agenda_item_details)
+
+        print(self.events)
+        print(len(self.events))
+
+        yield from self.create_events()
+
+    def create_events(self):
+        event_objects = []
+        # TODO: Pass url to this class from EventsTable.process_page()
+        url = "https://www.ndlegis.gov/legend/committee/hearings/public-schedule/"
+        for event in self.events.keys():
+            date, com, loc = re.search(r"(.+)\-(.+)\-(.+)", event).groups()
+            print(date + com)
+            start_time = self.events[event]["event_start_time"]
+            date_time = f"{date} {start_time}"
+            date_object = dateutil.parser.parse(date_time)
+            date_with_offset = self._tz.localize(date_object)
+
+            event_obj = Event(
+                name=com,
+                location_name=loc,
+                description="Standing Committee Hearing",
+                start_date=date_with_offset,
+            )
+
+            for item_key in self.events[event]["item_keys"]:
+                agenda = self.events[event][item_key]
+                for item in agenda:
+                    time = datetime.datetime.strptime(item_key, "%H:%M:%S").strftime(
+                        "%I:%M %p"
+                    )
+                    descr_with_time = f"[{time}]: {item['description']}"
+                    item_descr = event_obj.add_agenda_item(descr_with_time)
+                    item_descr.add_bill(item["bill_name"])
+
+            event_obj.add_source(url)
+
+            event_objects.append(event_obj)
+
+        return event_objects
+
+
+class EventsTable(HtmlPage):
     events_path = "/legend/committee/hearings/public-schedule/"
     source = f"https://www.ndlegis.gov{events_path}"
-    selector = XPath("//tbody//tr[1]")
-    _tz = pytz.timezone("US/Central")
 
-    def process_item(self, item):
-        item_text_list = item.text_content().strip().split("\n")
-        item_list = [x.strip() for x in item_text_list if len(x.strip()) > 0]
+    def process_page(self):
+        date_span = self.root.xpath(".//h5[2]")[0].text_content()
+        start_year, end_year = re.search(r"(\d{4}).+(\d{4})", date_span).groups()
+        # TODO: build year_changed boolean to handle when not start_year == end_year
 
-        description = item_list[0] + ", " + item_list[4]
+        table_rows = self.root.xpath(".//tbody//tr")
+        agenda_items_list = []
 
-        raw_date_time, committee, location = item_list[1:4]
+        for row_item in table_rows:
+            item_text_list = row_item.text_content().strip().split("\n")
+            item_list = [x.strip() for x in item_text_list if len(x.strip()) > 0]
 
-        local_date = dateutil.parser.parse(raw_date_time)
-        date_with_offset = self._tz.localize(local_date)
+            # TODO: add conditions to handle blank fields in table
+            #   (try empty strings?)
+            bill, part_date, com, loc, descr = item_list
 
-        event = Event(
-            name=committee,
-            location_name=location,
-            description=description,
-            start_date=date_with_offset,
-        )
+            # TODO: conditions to handle when bill listed as "Committee Work"
+            #   or "Committee Work ####" on the schedule
+            #   (in former case: leave bill_name as empty string or None,
+            #   in latter case: may need to scrape chamber from next source/bill_link)
 
-        event.add_source(self.source.url)
+            date_time_parts = part_date.split()
+            month = int(re.search(r"\d+", date_time_parts[0]).group())
+            if month == 1 and not start_year == end_year:
+                event_year = end_year
+            else:
+                event_year = start_year
+            date_time_parts[0] = date_time_parts[0] + f"/{event_year}"
+            date_time = " ".join(date_time_parts)
 
-        return event
+            agenda_item = {
+                "bill_name": bill,
+                "date_time": date_time,
+                "committee": com,
+                "location": loc,
+                "description": descr,
+            }
+
+            agenda_items_list.append(agenda_item)
+
+        events = EventConsolidator(agenda_items_list)
+        return events.consolidate()
 
 
 class NewNDEventScraper(Scraper):
-    def scrape(self, session=None):
+    @staticmethod
+    def scrape(session=None):
         # Event object needs name, start_date, and location_name
         logging.getLogger("scrapelib").setLevel(logging.WARNING)
-        event_list = EventList()
+        # event_list = EventList()
+        event_list = EventsTable()
         yield from event_list.do_scrape()
