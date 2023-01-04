232f9d518e508418bc3e8609bf02f478b4796625
Sruthi Vedantham <sruthi@civiceagle.com>
2023-01-04 10:27:38 -0600

    house list and detail

--- scrapers_next/il/committees.py
+++ scrapers_next/il/committees.py
@@ -47,6 +47,7 @@ class SenList(HtmlListPage):
     selector = CSS(".content")
 
     def process_item(self, item):
+        # committee codes dictionary used to map subcommittees to parent committees
         comm_codes_dict = {
             "SAPP": "Appropriations",
             "SCCL": "Criminal Law",
@@ -108,6 +109,131 @@ class SenList(HtmlListPage):
             com.add_source(detail_link)
 
             return SenDetail(com, source=detail_link)
+
+        else:
+            self.skip()
+
+
+class HouseDetail(HtmlPage):
+    def process_page(self):
+        com = self.input
+        # the member page for joint commissions is slightly different
+        if list(com)[1][1] != "lower":
+            try:
+                members = CSS(".content").match(self.root)
+            except SelectorError:
+                raise SkipItem("empty committee")
+
+            members = [
+                i.text.split(",")[0].strip() for i in members
+            ]  # cleaning up member names
+            roles = CSS(".heading").match(self.root)
+            roles = [i.text for i in roles][
+                1:
+            ]  # the first "role" is the name of the committee
+
+            for i in range(len(roles)):
+                com.add_member(members[i], roles[i])
+
+        else:
+            try:
+                members = CSS(".notranslate").match(self.root)
+
+            except SelectorError:
+                raise SkipItem("empty committee")
+
+            roles = CSS(".heading").match(self.root)
+
+            members = [i.text for i in members if i.text != "D"]
+            members = [i for i in members if i != "R"]
+            roles = [i.text.split()[0].strip().strip(":") for i in roles][1:]
+
+            for i in range(len(roles)):
+                com.add_member(members[i], roles[i])
+
+        return com
+
+
+class HouseList(HtmlListPage):
+    source = URL("https://www.ilga.gov/house/committees/default.asp")
+    selector = CSS(".content")
+
+    def process_item(self, item):
+        # committee codes dictionary used to map subcommittees to parent committees
+        comm_codes_dict = {
+            "HAPI": "Appropriations- Higher Education",
+            "HAPH": "Appropriations- Human Services",
+            "HCIV": "Cities & Villages",
+            "HCON": "Consumer Protection",
+            "HENG": "Energy & Environment",
+            "SHEE": "Ethics & Elections Committee",
+            "HFIN": "Financial Institutions",
+            "HHSV": "Human Services",
+            "HINS": "Insurance",
+            "HJUA": "Judiciary- Civil",
+            "HJUC": "Judiciary- Criminal",
+            "HLBR": "Labor & Commerce",
+            "HMEH": "Mental Health & Addiction",
+            "HMAC": "Museums, Arts, & Cultural Enhancement",
+            "HPPN": "Personnel & Pensions",
+            "SHPF": "Police & Fire Committee",
+            "HPUB": "Public Utilities",
+            "HREF": "Revenue & Finance",
+            "HSGA": "State Government Administration",
+            "HTRR": "Transportation: Regulation, Roads",
+            "HVES": "Transportation: Vehicles & Safety",
+        }
+
+        if item.getchildren():
+            comm = item.getchildren()[0]
+            comm_name = comm.text
+            comm_code = [i.text for i in item.itersiblings()][0]
+
+            # random, non-committee link that's on the page
+            if comm_name == "Reports":
+                raise SkipItem("not a committee")
+
+            # regular committees
+            if comm_code != "Not Scheduled":
+                chamber = "lower"
+
+                # identifying subcommittees
+                if "-" in comm_code:
+                    this_committee_code = comm_code.split("-")[0]
+                    parent_committee = comm_codes_dict[this_committee_code]
+
+                    com = ScrapeCommittee(
+                        name=comm_name,
+                        classification="subcommittee",
+                        chamber=chamber,
+                        parent=parent_committee,
+                    )
+
+                else:
+                    com = ScrapeCommittee(
+                        name=comm_name,
+                        classification="committee",
+                        chamber=chamber,
+                    )
+
+            # we don't include task forces
+            elif "Task Force" in comm_name:
+                self.skip()
+
+            # joint commissions
+            else:
+                chamber = "legislature"
+                com = ScrapeCommittee(
+                    name=comm_name,
+                    classification="committee",
+                    chamber=chamber,
+                )
+
+            detail_link = comm.get("href")
+            com.add_source(self.source.url, note="homepage")
+            com.add_source(detail_link)
+
+            return HouseDetail(com, source=detail_link)
 
         else:
             self.skip()
