02284f6115497cba21ab02979ea7a388b62106f7
Sogo Ogundowole <dowolebolu@gmail.com>
2023-01-07 18:28:12 +0100

    refactor: use official API for Indiana

--- scrapers/in/utils.py
+++ scrapers/in/utils.py
@@ -29,3 +29,20 @@ def get_with_increasing_timeout(scraper, link, fail=False, kwargs={}):
         scraper.logger.warning(
             "Link failed after waiting over a minute, giving up and moving on."
         )
+
+
+def add_space(text):
+    """Add a space between the bill number and the bill name.
+
+    Parameters
+    ----------
+    text : str
+        The text to add a space to.
+
+    Example
+    -------
+    >>> add_space("HB1233")
+     "HB 1233"
+
+    """
+    return "{} {}".format(text[: text.index(text[-1])], text[-1])
