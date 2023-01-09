bf90e66552ea6fa2729cee587fd387824b17dde0
Tim Showers <showerst@gmail.com>
2023-01-09 16:16:11 -0500

    PA: 2023S1

--- scrapers/pa/__init__.py
+++ scrapers/pa/__init__.py
@@ -85,7 +85,7 @@ class Pennsylvania(State):
             "name": "2023-2024 Regular Session",
             "start_date": "2023-01-03",
             "end_date": "2024-11-30",
-            "active": False,
+            "active": True,
         },
         {
             "_scraped_name": "2023-2024 Special Session #1 (Victims of Sexual Abuse)",
