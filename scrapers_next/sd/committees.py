482089f30e21fa43d09ecd74dab7ba3aed8b34e4
Glydewell Burdick <glydewellb@gmail.com>
2023-01-14 21:40:02 +0000

    cleanup comments and single quotes

--- scrapers_next/sd/committees.py
+++ scrapers_next/sd/committees.py
@@ -7,6 +7,7 @@ class CommitteeList(JsonListPage):
     source_string = "https://sdlegislature.gov/api/SessionCommittees/Session/68"
     source = URL(source_string)
 
+    # I don't know if there's a method on the base class for this to be overridden
     def standardize_chamber(self, original_chamber_text):
         match original_chamber_text:
             case "H":
@@ -18,35 +19,19 @@ class CommitteeList(JsonListPage):
             case _:
                 self.skip()
 
-
     def process_item(self, item):
         committee_json = item["Committee"]
 
-        # The Full House & Senate are included in the json, tagged with the following property
-        if committee_json['FullBody']: self.skip()
+        # The Full House & Senate are included in the committe json list, tagged with the following property
+        if committee_json["FullBody"]: self.skip()
 
         com_id = item["SessionCommitteeId"]
         detail_link = f"https://sdlegislature.gov/api/SessionCommittees/Detail/{com_id}"
         homepage = f"https://sdlegislature.gov/Session/Committee/{com_id}/Detail"
 
-        # try:
-        #     title_div = (
-        #         item.getchildren()[0]
-        #         .getchildren()[0]
-        #     )
-        # except:
-        #     self.skip()
-
-        # try:
-        #     chamber = title_div.getchildren()[0].text_content()
-        # except:
-        #     self.skip()
-
-        # committee_name = item.text_content()
-
         com = ScrapeCommittee(
-            name=committee_json['Name'],
-            chamber=self.standardize_chamber(committee_json['Body'])
+            name=committee_json["Name"],
+            chamber=self.standardize_chamber(committee_json["Body"])
         )
 
         com.add_source(self.source_string)
