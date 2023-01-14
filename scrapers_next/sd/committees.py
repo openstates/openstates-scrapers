fc65a8deaea79c87fc5b08fda4da3242feeb8194
Glydewell Burdick <glydewellb@gmail.com>
2023-01-14 20:27:24 +0000

    switch to JsonListPage input

--- scrapers_next/sd/committees.py
+++ scrapers_next/sd/committees.py
@@ -1,52 +1,44 @@
-from spatula import HtmlPage, HtmlListPage, CSS, XPath, SelectorError, SkipItem, URL
+from spatula import JsonListPage, CSS, XPath, SelectorError, SkipItem, URL
 from openstates.models import ScrapeCommittee
 
 
-class CommitteeList(HtmlListPage):
-    """
-    As of review 1/14/2023, SD Leg committes page had a sidebar nav with all the committees listed.
-      There's an anchor tag with classes of v-list-item, v-list-item--link, role="listitem" that contains a href
-    to the committee detail path (not fully qualified)
-      Then there's a div child of a div child with class v-list-item__title that contains a span element with the committee type
-    (if it's one we want - there are also such structures that are the full house & senate that do not have the span elements)
-    The div itself has the text name of the committee.
-    """
+class CommitteeList(JsonListPage):
+
     source = "https://sdlegislature.gov/Session/Committees/68"
-    selector = CSS("a")
 
     def standardize_chamber(self, original_chamber_text):
         match original_chamber_text:
-            case "House":
+            case "H":
                 return "lower"
-            case "Senate":
+            case "S":
                 return "upper"
-            case "Joint":
+            case "J":
                 return "legislature"
             case _:
                 self.skip()
 
 
     def process_item(self, item):
-        detail_path = item.get("href")
-        print(detail_path)
-        try:
-            title_div = (
-                item.getchildren()[0]
-                .getchildren()[0]
-            )
-        except:
-            self.skip()
+        committee_json = item["Committee"]
+        print(committee_json)
+        # try:
+        #     title_div = (
+        #         item.getchildren()[0]
+        #         .getchildren()[0]
+        #     )
+        # except:
+        #     self.skip()
 
-        try:
-            chamber = title_div.getchildren()[0].text_content()
-        except:
-            self.skip()
+        # try:
+        #     chamber = title_div.getchildren()[0].text_content()
+        # except:
+        #     self.skip()
 
-        committee_name = item.text_content()
+        # committee_name = item.text_content()
 
         com = ScrapeCommittee(
-            name=committee_name,
-            chamber=self.standardize_chamber(chamber)
+            name="test",
+            chamber="upper"
         )
 
         return com
