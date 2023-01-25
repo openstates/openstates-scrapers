82209987d5ebb96a0a849c7a5b944cb974ce9726
Ehtisham Sabir <Shaamdbz@gmail.com>
2023-01-25 18:45:32 +0500

    Added headers to API currently api don't take session.

--- scrapers_next/in/committees.py
+++ scrapers_next/in/committees.py
@@ -1,5 +1,4 @@
 from spatula import URL, HtmlListPage, SkipItem
-
 from openstates.models import ScrapeCommittee
 
 
@@ -24,6 +23,22 @@ class CommitteeDetail(HtmlListPage):
 class CommitteeList(HtmlListPage):
     source = URL(
         "https://tlhgp53g3c.execute-api.us-east-2.amazonaws.com/beta/api/getCommittees?session_lpid=session_2021"
+        , headers={
+            'authority': 'beta.iga.in.gov',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept-language': 'en-US,en;q=0.9',
+            'cache-control': 'no-cache',
+            'pragma': 'no-cache',
+            'sec-ch-ua': '"Chromium";v="108", "Opera";v="94", "Not)A;Brand";v="99"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 OPR/94.0.0.0',
+        }
     )
 
     def process_page(self):
@@ -61,4 +76,4 @@ class CommitteeList(HtmlListPage):
                     note="API from https://beta.iga.in.gov/2022/committees/",
                 )
                 com.extras = {"committees": committee}
-                yield CommitteeDetail(com, source=URL(mem_source, timeout=30))
+                yield CommitteeDetail(com, source=URL(mem_source, timeout=30, headers=self.source.headers))
