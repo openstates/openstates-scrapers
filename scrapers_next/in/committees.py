bf14241b550fad9c93297bfcccc36ec503e66cf2
Ehtisham Sabir <Shaamdbz@gmail.com>
2023-01-26 16:05:58 +0500

    role fixed

--- scrapers_next/in/committees.py
+++ scrapers_next/in/committees.py
@@ -14,8 +14,7 @@ class CommitteeDetail(HtmlPage):
                         role = role.replace(
                             "chair", " Chair"
                         )
-                if "member" in role.lower():
-                    role = "Member"
+                role = role.lower().replace("majority ", "").replace("minority ", "").title()
                 name = each_member["first_name"] + " " + each_member["last_name"]
                 com.add_member(name, role)
 
