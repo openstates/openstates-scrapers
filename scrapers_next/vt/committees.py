5c7ba26df70ce63120e933570fcccc52b41fb2c3
ehtisham <shaamdbz@gmail.com>
2023-01-28 03:59:31 +0500

    VT committees scraper using json page https://legislature.vermont.gov/committee/loadList/2024/

---
+++ scrapers_next/vt/committees.py
@@ -0,0 +1,75 @@
+from lxml.html import fromstring
+from lxml.html.clean import Cleaner
+from lxml.etree import ParserError
+from spatula import JsonListPage, URL, SkipItem
+from openstates.models import ScrapeCommittee
+
+
+class CommitteeList(JsonListPage):
+    source = URL(
+        "https://legislature.vermont.gov/committee/loadList/2024/",
+        headers={
+            "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:108.0) Gecko/20100101 Firefox/108.0",
+        },
+    )
+
+    def process_page(self):
+        for each_committee in self.response.json()["data"]:
+            # name
+            name = each_committee["LongName"]
+
+            # chamber
+            if "house" in name.lower():
+                chamber = "lower"
+            elif "senate" in name.lower():
+                chamber = "upper"
+            else:
+                chamber = "legislature"
+
+            # classification & parent check
+            classification = "committee"
+            parent = None
+            if "subcommittee" in name.lower():
+                classification = "subcommittee"
+                parent = name.lower().split()[-1].replace(")", "")
+
+            # extracting members
+            members_html = each_committee["Members"].strip()
+            members_html = members_html.replace("</br>", "\n")
+            try:
+                doc = fromstring(members_html)
+            except ParserError:
+                SkipItem(f"No membership data found for: {name}")
+                continue
+
+            cleaner = Cleaner(allow_tags=[""], remove_unknown_tags=False)
+            members_list = cleaner.clean_html(doc).text_content()
+
+            com = ScrapeCommittee(
+                name=name,
+                chamber=chamber,
+                parent=parent,
+                classification=classification,
+            )
+
+            for member in members_list.splitlines():
+                if not member.strip():
+                    SkipItem(f"No membership data found for: {name}")
+                    continue
+                try:
+                    name, role = member.split(",")
+                except ValueError:
+                    name = member
+                    role = "Member"
+                com.add_member(name.strip(), role=role.strip())
+
+            # additional check if no member found
+            if not com.members:
+                SkipItem(f"No membership data found for: {name}")
+                continue
+
+            com.add_source(
+                self.source.url,
+                note="Committee json  of legislature.vermont.gov site",
+            )
+            yield com
