a51f19df5ec8525e0765d0896fde00c96c083125
ehtisham <shaamdbz@gmail.com>
2023-02-01 09:17:25 +0500

    UT Comm: adding helper function inside utils.py

--- scrapers_next/ut/committees.py
+++ scrapers_next/ut/committees.py
@@ -1,22 +1,12 @@
 from spatula import JsonListPage, URL
 import logging
 from openstates.models import ScrapeCommittee
-import requests
+from .utils import get_membership_dict
 
 
 class UnknownParentError(BaseException):
     def __init__(self, com_name):
         super().__init__(f"Parent unknown for subcommittee {com_name}")
-
-
-def get_membership_dict(url):
-    membership = {}
-    response = requests.get(url)
-    for each_legislator in response.json()["legislators"]:
-        member_id = each_legislator["id"]
-        name = each_legislator["formatName"]
-        membership[member_id] = name
-    return membership
 
 
 class CommitteeList(JsonListPage):
