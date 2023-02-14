d72c332dd3978031cf7ed01267bce39b54f5e317
ehtisham <shaamdbz@gmail.com>
2023-02-15 03:21:28 +0500

    NM Committee: added exception for subcommittee

--- scrapers_next/wy/committees.py
+++ scrapers_next/wy/committees.py
@@ -2,6 +2,12 @@ from spatula import URL, JsonListPage
 import logging
 from openstates.models import ScrapeCommittee
 import requests
+import re
+
+
+class UnknownSubCommFound(BaseException):
+    def __init__(self, com_name):
+        super().__init__(f"unknown for subcommittee found: {com_name}")
 
 
 class CommitteeList(JsonListPage):
@@ -9,66 +15,66 @@ class CommitteeList(JsonListPage):
     source = URL(
         f"https://web.wyoleg.gov/LsoService/api/committeeList/{year}/J", timeout=15
     )
+    num_name_re = re.compile(r"(\d+)-(.+)")
 
     def process_page(self):
         # the request was for only J type committees in the issue, but I added all
         # Joint & Standing > J
         # Select & Task Forces > SE
         # Councils & Commissions > O
-        for committees in ["SE", "O"]:
-            if committees == "J":
-                comm_resp = self.response.json()
+
+        comm_resp = self.response.json()
+        for each_comm in comm_resp["committeeList"]:
+
+            committee_url = f"https://web.wyoleg.gov/LsoService/api/committeeDetail/{self.year}/{each_comm['ownerID']}"
+            committee_info = requests.get(committee_url).json()
+
+            # name
+            name = committee_info["commName"]
+
+            num_name = self.num_name_re.search(name)
+            if num_name:
+                name = num_name.groups()[-1]
+
+            # need to identify this
+            if committee_info["senateHasChair"] and committee_info["houseHasChair"]:
+                chamber = "legislature"
+            elif committee_info["senateHasChair"]:
+                chamber = "upper"
             else:
-                comm_list_url = self.source.url.replace("/J", f"/{committees}")
-                comm_resp = requests.get(comm_list_url).json()
+                chamber = "lower"
 
-            for each_comm in comm_resp["committeeList"]:
-
-                committee_url = f"https://web.wyoleg.gov/LsoService/api/committeeDetail/{self.year}/{each_comm['ownerID']}"
-                committee_info = requests.get(committee_url).json()
-
-                # name
-                name = committee_info["commName"]
-
-                # need to identify this
-                if committee_info["senateHasChair"] and committee_info["houseHasChair"]:
-                    chamber = "legislature"
-                elif committee_info["senateHasChair"]:
-                    chamber = "upper"
+            classification = "committee"
+            parent = None
+            if "subcommittee" in name.lower():
+                if "capitol interpretive exhibits" in name.lower():
+                    pass
                 else:
-                    chamber = "lower"
-                if "subcommittee" in committee_info["commName"]:
-                    # classification & parent check
-                    classification = "subcommittee"
-                    parent = (
-                        committee_info["commName"].replace("subcommittee", "").strip()
-                    )
+                    raise UnknownSubCommFound(name)
+
+            com = ScrapeCommittee(
+                name=name,
+                chamber=chamber,
+                parent=parent,
+                classification=classification,
+            )
+
+            # extracting members
+            for each_member in committee_info["commMembers"]:
+                name = each_member["name"]
+                if each_member["chairman"].strip() == "":
+                    role = "Member"
                 else:
-                    classification = "committee"
-                    parent = None
+                    role = each_member["chairman"]
+                com.add_member(name, role=role)
+            if not com.members:
+                logging.warning(f"No membership data found for: {name}")
+                continue
 
-                com = ScrapeCommittee(
-                    name=name,
-                    chamber=chamber,
-                    parent=parent,
-                    classification=classification,
-                )
-
-                # extracting members
-                for each_member in committee_info["commMembers"]:
-                    name = each_member["name"]
-                    if each_member["chairman"].strip() == "":
-                        role = "Member"
-                    else:
-                        role = each_member["chairman"]
-                    com.add_member(name, role=role)
-                if not com.members:
-                    logging.warning(f"No membership data found for: {name}")
-                    continue
-
-                com.add_link(committee_url, note="homepage api")
-                com.add_source(
-                    committee_url,
-                    note="Committee JSON from wyoleg.gov site",
-                )
-                yield com
+            comm_html = f"https://www.wyoleg.gov/Committees/{self.year}/{each_comm['ownerID']}"
+            com.add_link(comm_html, note="homepage")
+            com.add_source(
+                committee_url,
+                note="Committee JSON from wyoleg.gov site",
+            )
+            yield com
