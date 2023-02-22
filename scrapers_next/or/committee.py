8550f786621b5d54280334bfc9bd93ca8a24e2bb
Christopher Yamas <yamas.chris@gmail.com>
2023-02-22 09:00:12 -0600

    OR comm: final reformatting changes

--- scrapers_next/or/committee.py
+++ scrapers_next/or/committee.py
@@ -1,18 +1,21 @@
-from spatula import XPath, URL, HtmlListPage, HtmlPage, CSS
+from spatula import URL, HtmlListPage, HtmlPage, CSS
 from openstates.models import ScrapeCommittee
 import re
 
-class CommitteeDetail(HtmlListPage):
-    source = URL("https://olis.oregonlegislature.gov/liz/2023R1/Committees/JCT/CommitteeMembers")
-    selector = CSS('tbody tr')
 
-    def process_item(self, item):
+class CommitteeDetail(HtmlPage):
+    example_source = URL(
+        "https://olis.oregonlegislature.gov/liz/2023R1/Committees/JCT/CommitteeMembers"
+    )
+
+    def process_page(self):
         com = self.input
-        position, name = item.getchildren()
-        name = re.sub(r"Senator |Representative ", "", name.text_content().strip())
-        position = position.text_content().strip()
-        com.add_member(name, position)
-
+        members = CSS("tbody tr").match(self.root)
+        for member in members:
+            position, name = member.getchildren()
+            name = re.sub(r"Senator |Representative ", "", name.text_content().strip())
+            position = position.text_content().strip()
+            com.add_member(name, position)
         return com
 
 
@@ -20,42 +23,49 @@ class CommitteeList(HtmlListPage):
     source = "https://olis.oregonlegislature.gov/liz/Committees/list/"
 
     def process_item(self, item):
-        link = item.get("href").split('/')
-        link = 'https://olis.oregonlegislature.gov/liz/' + link[4] +\
-                "/Committees/" + link[6] + "/CommitteeMembers"
+        url_parts = item.get("href").split("/")
+        comm_url = (
+            f"https://olis.oregonlegislature.gov/liz/{url_parts[4]}/"
+            f"Committees/{url_parts[6]}/CommitteeMembers"
+        )
 
         try:
             parent = self.parent
-        except:
+        except AttributeError:
             parent = None
 
         com = ScrapeCommittee(
-                name=item.text_content(),
-                chamber=self.chamber,
-                classification=self.classification,
-                parent=parent,
-            )
-        com.add_source(link)
+            name=item.text_content(),
+            chamber=self.chamber,
+            classification="subcommittee" if parent else "committee",
+            parent=parent,
+        )
+        print(com.name)
+        com.add_source(comm_url, note="Committee details page")
+        com.add_link(comm_url, note="homepage")
 
-        return CommitteeDetail(com, source=link,)
+        return CommitteeDetail(
+            com,
+            source=comm_url,
+        )
 
 
-class HouseCommitteeList(CommitteeList):
+class House(CommitteeList):
     selector = CSS("#HouseCommittees_search li a")
     chamber = "lower"
-    classification = "committee"
 
-class SenateCommitteeList(CommitteeList):
+
+class Senate(CommitteeList):
     selector = CSS("#SenateCommittees_search li a")
     chamber = "upper"
 
-class JointCommitteeList(CommitteeList):
-    selector = CSS("#SenateCommittees_search > li > a")
+
+class Joint(CommitteeList):
+    selector = CSS("#JointCommittees_search > li > a")
     chamber = "legislature"
 
-class WayMeansCommitteeList(CommitteeList):
+
+class WaysMeans(CommitteeList):
     selector = CSS("#JointCommittees_search > li > ul > li > a")
     chamber = "legislature"
     parent = "Ways and Means"
-    classification = "subcommittee"
-
