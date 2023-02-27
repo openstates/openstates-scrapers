725df131dabdb492a2287bb83e8f49b3583e1bd2
Tim Showers <showerst@gmail.com>
2023-02-27 11:37:30 -0500

    OK: events: get dates working

--- scrapers/ok/events.py
+++ scrapers/ok/events.py
@@ -1,11 +1,11 @@
-import re
-import json
-import dateutil.parser
-import pytz
-
-import lxml.html
-import requests
 import datetime
+import dateutil.parser
+import json
+import lxml.html
+import pytz
+import re
+import requests
+
 from openstates.scrape import Scraper, Event
 from openstates.exceptions import EmptyScrape
 
@@ -16,19 +16,29 @@ class OKEventScraper(Scraper):
     _tz = pytz.timezone("CST6CDT")
     session = requests.Session()
 
-    def scrape(self, chamber=None):
+    # usage:
+    # poetry run os-update ne \
+    # events --scrape start=2022-02-01 end=2022-03-02
+    def scrape(self, start=None, end=None):
 
-        start = datetime.datetime(2023, 2, 1).isoformat()
-        end = datetime.datetime(2023, 2, 28).isoformat()
+        if start is None:
+            delta = datetime.timedelta(days=90)
+            start = datetime.date.today() - delta
+            start = start.isoformat()
+
+            end = datetime.date.today() + delta
+            end = end.isoformat()
 
         yield from self.scrape_page(start, end)
 
     def scrape_page(self, start, end, offset=0, limit=20):
+        self.info(f"Fetching {start} - {end} offset {offset}")
+
         url = "https://www.okhouse.gov/api/events"
 
         post_data = {
-            "start": "2023-02-01T05:00:00.000Z",
-            "end": "2023-04-01T03:59:59.999Z",
+            "start": f"{start}T00:00:00.000Z",
+            "end": f"{end}T00:00:00.000Z",
             "offset": offset,
             "limit": limit,
         }
@@ -40,11 +50,10 @@ class OKEventScraper(Scraper):
         ).content
         page = json.loads(page)
 
-        if len(page["events"]["data"]) == 0:
+        if offset == 0 and len(page["events"]["data"]) == 0:
             raise EmptyScrape
 
         for row in page["events"]["data"]:
-            print(row)
             meta = row["attributes"]
 
             status = "tentative"
@@ -52,11 +61,16 @@ class OKEventScraper(Scraper):
             if meta["isCancelled"] is True:
                 status = "cancelled"
 
-            location = meta["location"]
-            if re.match(r"^room [\w\d]+$", location, flags=re.I) or re.match(
-                r"senate room [\w\d]+$", location, flags=re.I
-            ):
-                location = f"{location} 2300 N Lincoln Blvd, Oklahoma City, OK 73105"
+            if meta["location"]:
+                location = meta["location"]
+                if re.match(r"^room [\w\d]+$", location, flags=re.I) or re.match(
+                    r"senate room [\w\d]+$", location, flags=re.I
+                ):
+                    location = (
+                        f"{location} 2300 N Lincoln Blvd, Oklahoma City, OK 73105"
+                    )
+            else:
+                meta["location"] = "See agenda"
 
             when = dateutil.parser.parse(meta["startDatetime"])
 
@@ -71,7 +85,7 @@ class OKEventScraper(Scraper):
             event.add_source(f"https://www.okhouse.gov/events/{meta['slug']}")
             match_coordinates(event, {"2300 N Lincoln Blvd": (35.49293, -97.50311)})
 
-            if meta["committee"]:
+            if meta["committee"]["data"]:
                 event.add_committee(
                     meta["committee"]["data"]["attributes"]["name"], note="host"
                 )
@@ -93,4 +107,12 @@ class OKEventScraper(Scraper):
                         "Finally found an agenda with linked measure. Modify the code to handle it."
                     )
 
+                # sometimes they put the bill number here instead
+                if agenda["customId"]:
+                    item.add_bill(agenda["customId"])
+
             yield event
+
+        current_max = offset + limit
+        if page["events"]["meta"]["pagination"]["total"] > current_max:
+            yield from self.scrape_page(start, end, offset + limit, limit)
