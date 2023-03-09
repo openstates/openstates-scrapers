c3f612f4b4f33e5b4b38582d1399743f493da788
user <user@archlinux>
2023-03-09 16:54:30 -0500

    handle "upon adjournment" in start time, cleaned up comments

--- scrapers/nv/events.py
+++ scrapers/nv/events.py
@@ -16,12 +16,23 @@ bills_re = re.compile(
     r"(SJR|AR|AJR|IP|SCR|SB|ACR|SR|AB)\s{0,5}0*(\d+)", flags=re.IGNORECASE
 )
 
-date_re = re.compile(
-    r"([a-z]+ \d{1,2}, .* (am|pm))",
-    flags=re.IGNORECASE,
-)
+date_re = re.compile(r"([a-z]+ \d{1,2}, .* (am|pm))", flags=re.IGNORECASE)
 
-start_time_re = re.compile(r"(\d+) (AM|PM)")
+start_time_re = re.compile(r"Start\s*Time\s*(\d+)\s*(AM|PM)", flags=re.IGNORECASE)
+
+
+class AgendaStartTime(PdfPage):
+    def process_page(self):
+        # Simplify text to make regex simpler
+        self.text = self.text.replace(". ", "").replace(".", "")
+
+        start = start_time_re.search(self.text)
+        if not start:
+            # Default to 12am if no start time is found
+            yield "12:00 am"
+            return
+        time, ampm = start.groups(1)
+        yield f"{time}:00 {ampm}"
 
 
 class Agenda(PdfPage):
@@ -53,7 +64,6 @@ class Meetings(HtmlPage):
 
         session_link = URL(
             url=f"https://www.leg.state.nv.us/App/NELIS/REL/{session_id}/Meetings?Meetings=System.Collections.Generic.List%601%5BNvLeg.Models.Nelis.SessionManager.Views.MeetingComposite%5D&MeetingsFilters=System.DateTime%5B%5D&ShowFloorMeetings=True&ShowCommitteeMeetings=True&ShowConferenceCommittees=True",
-            # data="MeetingsFilters=3%2F9%2F2023&MeetingsFilters=4%2F9%2F2023&ShowCommitteeMeetings=true&ShowCommitteeMeetings=false&ShowFloorMeetings=false&ShowConferenceCommittees=false&X-Requested-With=XMLHttpRequest",
             method="POST",
             data={
                 "MeetingsFilters": [
@@ -74,17 +84,13 @@ class Meetings(HtmlPage):
 
 class CurrentMeetings(HtmlPage):
     _TZ = pytz.timezone("PST8PDT")
-    example_source = "https://www.leg.state.nv.us/App/NELIS/REL/82nd2023/Meetings"
 
     def process_page(self):
-        # container = XPath('//div[@id="meetings-list"]').match_one(self.root)
-
         meetings = []
         cur_row = []
-        # The container element contains meeting information - each meeting has
-        # info spread across up to 3 divs, with a <hr> after each section - The
-        # following code finds all of these sections with 3 divs and places them
-        # into the meetings variable
+        # Each meeting has info spread across up to 3 divs, with a <hr> after
+        # each section - The following code finds all of these sections with 3
+        # divs and places them into the meetings variable
         for i in self.root.getchildren():
             if i.tag == "div":
                 cur_row.append(i)
@@ -96,10 +102,17 @@ class CurrentMeetings(HtmlPage):
                 cur_row = []
 
         for i in meetings:
+            # Second element contains: a link the the agenda pdf
+            # Grabbing the second element first because the start time from the
+            # agenda pdf may be needed
+            agenda = XPath(
+                './/a[@title="View the agenda for this meeting"]/@href'
+            ).match_one(i[1])
+
             # First element contains: the title and date
             title = XPath(".//h3/a[1]/text()").match_one(i[0])
 
-            # Extract all committees involced in meeting from the title
+            # Extract all committees involved in meeting from the title
             # Some joint meetings don't start with the joint prefix so extra logic
             # is needed to get the names of all involved committees
             add_joint_prefix = False
@@ -119,22 +132,18 @@ class CurrentMeetings(HtmlPage):
             if add_joint_prefix:
                 title = f"{joint_prefix}{title}"
 
+            # Get start date
             date = XPath(".//h3/a[2]/text()").match_one(i[0])
+            # Remove day of the week information
+            date = date.split(", ", 1)[1]
+            if "[" in date:
+                # Date contains "upon adjourment" instead of start time
+                date = date.split("[")[0]
+                # Check PDF for start time
+                agenda_start_time = next(AgendaStartTime(source=agenda).do_scrape())
+                date = f"{date}{agenda_start_time}"
 
-            # Ensure date is valid and contains a start time
-            date_match = date_re.search(date)
-            if date_match:
-                date = date_match.group(1)
-                date = datetime.datetime.strptime(date, "%B %d, %Y %I:%M %p")
-            else:
-                # Invalid date or time, skip this event
-                self.logger.warning(f"Invalid date or time: {date}, skipping")
-                continue
-
-            # Second element contains: a link the the agenda pdf
-            agenda = XPath(
-                './/a[@title="View the agenda for this meeting"]/@href'
-            ).match_one(i[1])
+            date = datetime.datetime.strptime(date, "%B %d, %Y %I:%M %p")
 
             # Third element contains: a list of locations
             locations = XPath(".//li/text()").match(i[2])
@@ -147,8 +156,12 @@ class CurrentMeetings(HtmlPage):
             )
             event.add_source(self.source.url)
             event.add_document("Agenda", url=agenda, media_type="pdf")
+
+            # Add committees from title string
             for committee in committees:
                 event.add_committee(committee)
+
+            # Add bills from pdf
             for bill in Agenda(source=agenda).do_scrape():
                 event.add_bill(bill)
             yield event
