e95d76d43d3aec0023f35faa03d2ccea6da2dd84
Christopher Yamas <yamas.chris@gmail.com>
2023-03-16 14:01:59 -0400

    VT events: reformatted helper func placement

--- scrapers/vt/events.py
+++ scrapers/vt/events.py
@@ -9,21 +9,6 @@ from openstates.exceptions import EmptyScrape
 from spatula import JsonPage, URL
 
 
-class BillsInfo(JsonPage):
-    example_source = "https://legislature.vermont.gov/committee/loadBillsOut/2024?committeeId=189&sessionId=8"
-
-    def process_page(self):
-        # Bill number is in format "J.R.H.123"
-        resp = self.response.json()
-        for bill in resp["data"]:
-            # Split on final "." to separate letter and number portion
-            alpha, num = bill["BillNumber"].rsplit(".", 1)
-            # Remove "." from letter portion
-            alpha = alpha.replace(".", "")
-            # Recombine with a single space between letters and numbers
-            yield f"{alpha} {num}"
-
-
 def scrape_bills(year_slug, committee_id, session_id):
     # 4 endpoints are used for different types of bills
     bill_types = [
@@ -41,6 +26,21 @@ def scrape_bills(year_slug, committee_id, session_id):
         for bill in info.do_scrape():
             bills.add(bill)
     yield from bills
+
+
+class BillsInfo(JsonPage):
+    example_source = "https://legislature.vermont.gov/committee/loadBillsOut/2024?committeeId=189&sessionId=8"
+
+    def process_page(self):
+        # Bill number is in format "J.R.H.123"
+        resp = self.response.json()
+        for bill in resp["data"]:
+            # Split on final "." to separate letter and number portion
+            alpha, num = bill["BillNumber"].rsplit(".", 1)
+            # Remove "." from letter portion
+            alpha = alpha.replace(".", "")
+            # Recombine with a single space between letters and numbers
+            yield f"{alpha} {num}"
 
 
 class VTEventScraper(Scraper):
