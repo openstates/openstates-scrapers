f1674e3e2db660c749376a8b8fdcafc2752f7e77
Tim Showers <showerst@gmail.com>
2023-03-17 12:14:47 -0400

    Flake8 fix

---
+++ scrapers/al/actions.py
@@ -0,0 +1,42 @@
+from utils.actions import Rule, BaseCategorizer
+
+_categorizer_rules = (
+    Rule(r"Introduced and Referred", ["introduction", "referral"]),
+    Rule(r"Read First Time", "reading-1"),
+    Rule("Introduced", "introduction"),
+    Rule("(Forwarded|Delivered) to Governor", "executive-receipt"),
+    Rule("Amendment (?:.*)Offered", "amendment-introduction"),
+    Rule("Substitute (?:.*)Offered", "amendment-introduction"),
+    Rule("Amendment (?:.*)adopted", "amendment-passage"),
+    Rule("Amendment lost", "amendment-failure"),
+    Rule(
+        "Read for the first time and referred to", ["reading-1", "referral-committee"]
+    ),
+    Rule("(r|R)eferred to", "referral-committee"),
+    Rule("Read for the second time", "reading-2"),
+    Rule("(S|s)ubstitute adopted", "substitution"),
+    Rule("(m|M)otion to Adopt (?:.*)adopted", "amendment-passage"),
+    Rule("(m|M)otion to (t|T)able (?:.*)adopted", "amendment-passage"),
+    Rule("(m|M)otion to Adopt (?:.*)lost", "amendment-failure"),
+    Rule("(m|M)otion to Read a Third Time and Pass adopted", "passage"),
+    Rule("(m|M)otion to Concur In and Adopt adopted", "passage"),
+    Rule("Third Reading Passed", "passage"),
+    Rule("Reported from", "committee-passage"),
+    Rule("Indefinitely Postponed", "failure"),
+    Rule("Passed by House of Origin", "passage"),
+    Rule("Passed Second House", "passage"),
+    # memorial resolutions can pass w/o debate
+    Rule("Joint Rule 11", ["introduction", "passage"]),
+    Rule("Lost in", "failure"),
+    Rule("Favorable from", "committee-passage-favorable"),
+    # Signature event does not appear to be listed as a specific action
+    Rule("Assigned Act No", "executive-signature"),
+)
+
+
+class Categorizer(BaseCategorizer):
+    rules = _categorizer_rules
+
+    def categorize(self, text):
+        attrs = BaseCategorizer.categorize(self, text)
+        return attrs
--- scrapers/al/bills.py
+++ scrapers/al/bills.py
@@ -1,55 +1,20 @@
-import re
 import pytz
 import json
 import datetime
 from openstates.scrape import Scraper, Bill
-
-# NOTE: leaving this for now, once action data loads this week we'll probably reuse it
-_action_re = (
-    ("Introduced", "introduction"),
-    ("(Forwarded|Delivered) to Governor", "executive-receipt"),
-    ("Amendment (?:.*)Offered", "amendment-introduction"),
-    ("Substitute (?:.*)Offered", "amendment-introduction"),
-    ("Amendment (?:.*)adopted", "amendment-passage"),
-    ("Amendment lost", "amendment-failure"),
-    ("Read for the first time and referred to", ["reading-1", "referral-committee"]),
-    ("(r|R)eferred to", "referral-committee"),
-    ("Read for the second time", "reading-2"),
-    ("(S|s)ubstitute adopted", "substitution"),
-    ("(m|M)otion to Adopt (?:.*)adopted", "amendment-passage"),
-    ("(m|M)otion to (t|T)able (?:.*)adopted", "amendment-passage"),
-    ("(m|M)otion to Adopt (?:.*)lost", "amendment-failure"),
-    ("(m|M)otion to Read a Third Time and Pass adopted", "passage"),
-    ("(m|M)otion to Concur In and Adopt adopted", "passage"),
-    ("Third Reading Passed", "passage"),
-    ("Reported from", "committee-passage"),
-    ("Indefinitely Postponed", "failure"),
-    ("Passed by House of Origin", "passage"),
-    ("Passed Second House", "passage"),
-    # memorial resolutions can pass w/o debate
-    ("Joint Rule 11", ["introduction", "passage"]),
-    ("Lost in", "failure"),
-    ("Favorable from", "committee-passage-favorable"),
-    # Signature event does not appear to be listed as a specific action
-    ("Assigned Act No", "executive-signature"),
-)
-()
-
-
-def _categorize_action(action):
-    for pattern, types in _action_re:
-        if re.findall(pattern, action):
-            return types
-    return None
+from openstates.exceptions import EmptyScrape
+from .actions import Categorizer
 
 
 class ALBillScraper(Scraper):
+    categorizer = Categorizer()
     chamber_map = {"Senate": "upper", "House": "lower"}
     bill_types = {"B": "bill", "R": "resolution"}
     tz = pytz.timezone("US/Eastern")
+    chamber_map_short = {"S": "upper", "H": "lower"}
+    gql_url = "https://gql.api.alison.legislature.state.al.us/graphql"
 
     def scrape(self, session):
-        gql_url = "https://gql.api.alison.legislature.state.al.us/graphql"
 
         headers = {
             "Accept": "*/*",
@@ -65,16 +30,15 @@ class ALBillScraper(Scraper):
         while True:
             # WARNING: 2023 session id is currently hardcoded
             json_data = {
-                "query": f'{{allInstrumentOverviews(instrumentType:"B", instrumentNbr:"", body:"", sessionYear:"2023", sessionType:"2023 Regular Session", assignedCommittee:"", status:"", currentStatus:"", subject:"", instrumentSponsor:"", companionInstrumentNbr:"", effectiveDateCertain:"", effectiveDateOther:"", firstReadSecondBody:"", secondReadSecondBody:"", direction:"ASC"orderBy:"InstrumentNbr"limit:"{limit}"offset:"{offset}"  search:"" customFilters: {{}}companionReport:"", ){{ ID,SessionYear,InstrumentNbr,InstrumentSponsor,SessionType,Body,Subject,ShortTitle,AssignedCommittee,PrefiledDate,FirstRead,CurrentStatus,LastAction,ActSummary,ViewEnacted,CompanionInstrumentNbr,EffectiveDateCertain,EffectiveDateOther,InstrumentType,IntroducedUrl }}}}',
+                "query": f'{{allInstrumentOverviews(instrumentType:"B", instrumentNbr:"", body:"", sessionYear:"2023", sessionType:"2023 Regular Session", assignedCommittee:"", status:"", currentStatus:"", subject:"", instrumentSponsor:"", companionInstrumentNbr:"", effectiveDateCertain:"", effectiveDateOther:"", firstReadSecondBody:"", secondReadSecondBody:"", direction:"ASC"orderBy:"InstrumentNbr"limit:"{limit}"offset:"{offset}"  search:"" customFilters: {{}}companionReport:"", ){{ ID,SessionYear,InstrumentNbr,InstrumentUrl, InstrumentSponsor,SessionType,Body,Subject,ShortTitle,AssignedCommittee,PrefiledDate,FirstRead,CurrentStatus,LastAction,ActSummary,ViewEnacted,CompanionInstrumentNbr,EffectiveDateCertain,EffectiveDateOther,InstrumentType,IntroducedUrl,EngrossedUrl,EnrolledUrl }}}}',
                 "operationName": "",
                 "variables": [],
             }
 
-            page = self.post(gql_url, headers=headers, json=json_data)
+            page = self.post(self.gql_url, headers=headers, json=json_data)
             page = json.loads(page.content)
-
-            if len(page["data"]["allInstrumentOverviews"]) < 1:
-                return
+            if len(page["data"]["allInstrumentOverviews"]) < 1 and offset == 0:
+                raise EmptyScrape
 
             for row in page["data"]["allInstrumentOverviews"]:
                 chamber = self.chamber_map[row["Body"]]
@@ -95,45 +59,18 @@ class ALBillScraper(Scraper):
                     primary=True,
                 )
 
+                self.scrape_versions(bill, row)
+                self.scrape_actions(bill, row)
+
                 bill.add_source("https://alison.legislature.state.al.us/bill-search")
+                if row["InstrumentUrl"]:
+                    bill.add_source(row["InstrumentUrl"])
 
-                if row["IntroducedUrl"]:
-                    bill.add_version_link(
-                        "Introduced",
-                        url=row["IntroducedUrl"],
-                        media_type="application/pdf",
-                    )
-
-                # TODO: can this be removed in favor of pulling the
-                # action list via graphql?
-                if row["PrefiledDate"]:
-                    action_date = datetime.datetime.strptime(
-                        row["PrefiledDate"], "%m/%d/%Y"
-                    )
-                    action_date = self.tz.localize(action_date)
-                    bill.add_action(
-                        chamber=chamber,
-                        description="Filed",
-                        date=action_date,
-                        classification="filing",
-                    )
-
-                if row["FirstRead"] and row["FirstRead"] != "01/01/0001":
-                    action_date = datetime.datetime.strptime(
-                        row["FirstRead"], "%m/%d/%Y"
-                    )
-                    action_date = self.tz.localize(action_date)
-                    bill.add_action(
-                        chamber=chamber,
-                        description="First Reading",
-                        date=action_date,
-                        classification="reading-1",
-                    )
-
-                if row["Subject"] != "":
+                if row["Subject"]:
                     bill.add_subject(row["Subject"].strip())
 
-                # TODO: All actions
+                if row["CompanionInstrumentNbr"] != "":
+                    self.warning("AL Companion found. Code it up.")
 
                 # TODO: EffectiveDateCertain, EffectiveDateOther
 
@@ -143,4 +80,77 @@ class ALBillScraper(Scraper):
 
                 yield bill
 
+            # no need to paginate again if we max the last page
+            if len(page["data"]["allInstrumentOverviews"]) < limit:
+                return
+
             offset += limit
+
+    def scrape_versions(self, bill, row):
+        if row["IntroducedUrl"]:
+            bill.add_version_link(
+                "Introduced",
+                url=row["IntroducedUrl"],
+                media_type="application/pdf",
+            )
+        if row["EngrossedUrl"]:
+            bill.add_version_link(
+                "Engrossed",
+                url=row["EngrossedUrl"],
+                media_type="application/pdf",
+            )
+        if row["EnrolledUrl"]:
+            bill.add_version_link(
+                "Enrolled",
+                url=row["EnrolledUrl"],
+                media_type="application/pdf",
+            )
+
+    def scrape_actions(self, bill, row):
+        if row["PrefiledDate"]:
+            action_date = datetime.datetime.strptime(row["PrefiledDate"], "%m/%d/%Y")
+            action_date = self.tz.localize(action_date)
+            bill.add_action(
+                chamber=self.chamber_map[row["Body"]],
+                description="Filed",
+                date=action_date,
+                classification="filing",
+            )
+
+        headers = {
+            "Accept": "*/*",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Authorization": "Bearer undefined",
+            "Content-Type": "application/json",
+            "Origin": "https://alison.legislature.state.al.us",
+            "Referer": "https://alison.legislature.state.al.us/",
+        }
+
+        # Can this be ANDED together with the other graphql query?
+        # WARNING: 2023 session id is currently hardcoded
+        json_data = {
+            "query": f'{{instrumentHistoryBySessionYearInstNbr(sessionType:"2023 Regular Session", sessionYear:"2023", instrumentNbr:"{row["InstrumentNbr"]}", ){{ InstrumentNbr,SessionYear,SessionType,CalendarDate,Body,AmdSubUrl,Matter,Committee,Nay,Yea,Vote,VoteNbr }}}}',
+            "operationName": "",
+            "variables": [],
+        }
+
+        page = self.post(self.gql_url, headers=headers, json=json_data)
+        page = json.loads(page.content)
+
+        for row in page["data"]["instrumentHistoryBySessionYearInstNbr"]:
+            action_text = row["Matter"]
+            if row["Committee"]:
+                action_text = f'{row["Matter"]} ({row["Committee"]})'
+
+            action_date = datetime.datetime.strptime(row["CalendarDate"], "%m-%d-%Y")
+            action_date = self.tz.localize(action_date)
+
+            action_attr = self.categorizer.categorize(row["Matter"])
+            action_class = action_attr["classification"]
+
+            bill.add_action(
+                chamber=self.chamber_map_short[row["Body"]],
+                description=action_text,
+                date=action_date,
+                classification=action_class,
+            )
