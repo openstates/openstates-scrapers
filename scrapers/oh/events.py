9ad96fcc7fd26224214ca364e63e992c1ff66d6f
user <user@archlinux>
2023-03-18 09:46:30 -0400

    Added bill scraping from agenda pdfs

--- scrapers/oh/events.py
+++ scrapers/oh/events.py
@@ -14,6 +14,30 @@ from dateutil.relativedelta import relativedelta
 import dateutil.parser
 import cloudscraper
 
+from spatula import PdfPage, URL
+
+
+class Agenda(PdfPage):
+    # Bills are in the format "S. B. No. 123", "H.B. 33", "HB 234"
+    # Preprocessing step changes that format to "SB123", "HB33", or "HB234"
+    bill_re = re.compile(r"(\W|^)(SJR|HCR|HB|HR|SCR|SB|HJR|SR)\s{0,8}0*(\d+)")
+
+    def process_page(self):
+        # Remove spaces, periods, and "No." from text for simpler regex
+        self.text = (
+            self.text.upper().replace("NO", "").replace(" ", "").replace(".", "")
+        )
+
+        bills = self.bill_re.findall(self.text)
+
+        # Store bill ids in a set to remove duplicates
+        formatted_bills = set()
+        for _, alpha, num in bills:
+            # Format with space between letter and number portions
+            formatted_bills.add(f"{alpha} {num}")
+
+        yield from formatted_bills
+
 
 class OHEventScraper(Scraper):
     _tz = pytz.timezone("US/Eastern")
@@ -79,6 +103,12 @@ class OHEventScraper(Scraper):
             event = Event(
                 name=name, start_date=when, location_name=location, status=status
             )
+
+            # Scrape bill ids from the agenda PDF
+            # The server only returns data if a user agent is supplied
+            headers = {"User-Agent": "curl/7.88.1"}
+            for bill_id in Agenda(source=URL(agenda_url, headers=headers)).do_scrape():
+                event.add_bill(bill_id)
 
             match_coordinates(event, {"1 Capitol Square": (39.96019, -82.99946)})
 
