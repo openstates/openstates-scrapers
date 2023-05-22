5bbf4062935b95b1ff5b5d5aeb1981685be0bf3b
Christopher Yamas <chris.yamas@berkeley.edu>
2023-05-22 09:08:05 -0500

    Scrapers Utils: add logging warning

--- scrapers/utils/lxmlize.py
+++ scrapers/utils/lxmlize.py
@@ -1,5 +1,6 @@
 import requests
 import lxml.html
+import logging
 
 
 def url_xpath(url, path, verify=True, user_agent=None):
@@ -8,7 +9,7 @@ def url_xpath(url, path, verify=True, user_agent=None):
     try:
         doc = lxml.html.fromstring(res.text)
     except Exception as e:
-        print(
+        logging.warning(
             f"Failed to retrieve xpath from {url} :: returned:\n"
             f"CONTENT: {res.content} \n"
             f"RETURN CODE: {res.status_code}"
