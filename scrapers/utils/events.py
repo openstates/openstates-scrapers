ecfb95b3f8ba03d4bb1931b63524885901be8f99
Christopher Yamas <chris.yamas@berkeley.edu>
2023-05-24 18:11:23 -0500

    Scraper Utils: event key hash add

--- scrapers/utils/events.py
+++ scrapers/utils/events.py
@@ -1,3 +1,21 @@
+import hashlib
+import uuid
+
+
+def hash_event_key(event_key_str):
+    """
+    Used to shorten event identifier strings while maintaining uniqueness.
+    :param event_key_str: type str - event identifier of variable length
+    :return: type str - unique hash of event identifier
+    """
+    hash_val = hashlib.md5()
+    hash_val.update(event_key_str.encode("utf-8"))
+    hex_encoded_hash = hash_val.hexdigest()
+    uuid_hex = uuid.UUID(hex_encoded_hash)
+    unique_event_hash_str = str(uuid_hex)
+    return unique_event_hash_str
+
+
 # the current function to set coordinates requires a
 # valid URL and Note, which we often don't have.
 # so this will add just coordinates
