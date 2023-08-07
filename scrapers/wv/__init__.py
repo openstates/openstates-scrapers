c5cceb191a37df16a7a29c996a8349e8c7835f2f
showerst <showerst@gmail.com>
2023-08-06 20:50:16 -0400

    WV: Ignore pre-1940 legacy sessions (#4674)

--- scrapers/wv/__init__.py
+++ scrapers/wv/__init__.py
@@ -229,7 +229,7 @@ class WestVirginia(State):
     ]
     ignored_scraped_sessions = [
         "2029",
-        *(str(each) for each in range(1940, 2011)),
+        *(str(each) for each in range(1930, 2011)),
     ]
 
     def get_session_list(self):
