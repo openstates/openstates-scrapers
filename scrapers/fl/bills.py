2dcdee8c135f3fdf375877ec85c40f7bdffa9050
Tim Showers <showerst@gmail.com>
2023-08-24 16:39:11 -0400

    FL: 2024 prefiles

--- scrapers/fl/bills.py
+++ scrapers/fl/bills.py
@@ -608,6 +608,7 @@ class HouseSearchPage(HtmlListPage):
         # Keep the digits and all following characters in the bill's ID
         bill_number = re.search(r"^\w+\s(\d+\w*)$", self.input.identifier).group(1)
         session_number = {
+            "2024": "103",
             "2023B": "102",
             "2022A": "101",
             "2023": "99",
