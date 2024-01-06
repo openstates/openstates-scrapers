45152eee4877ac78eeeffdc4be62220b634df5b6
showerst <showerst@gmail.com>
2024-01-06 09:38:23 -0500

    NM: 2024 prefiles (#4779)

--- scrapers/nm/__init__.py
+++ scrapers/nm/__init__.py
@@ -184,7 +184,7 @@ class NewMexico(State):
             "name": "2023 Regular Session",
             "start_date": "2023-01-17",
             "end_date": "2023-03-18",
-            "active": True,
+            "active": False,
         },
         {
             "_scraped_name": "2024 Regular",
@@ -193,7 +193,7 @@ class NewMexico(State):
             "name": "2024 Regular Session",
             "start_date": "2024-01-16",
             "end_date": "2024-02-15",
-            "active": False,
+            "active": True,
         },
     ]
     ignored_scraped_sessions = [
