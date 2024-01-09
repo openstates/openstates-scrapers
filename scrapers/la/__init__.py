93f81f4a819aca60c64ec2303232d66b877cfcce
NewAgeAirbender <34139325+NewAgeAirbender@users.noreply.github.com>
2024-01-09 16:34:48 -0600

    LA: 24S1 no bills posted yet

--- scrapers/la/__init__.py
+++ scrapers/la/__init__.py
@@ -247,7 +247,7 @@ class Louisiana(State):
             "name": "2024 First Extraordinary Session",
             "start_date": "2024-01-15",
             "end_date": "2024-01-23",
-            "active": True,
+            "active": False,
         },
         {
             "_scraped_name": "2024 Second Extraordinary Session",
