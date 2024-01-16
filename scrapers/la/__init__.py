2854ca2e89ad0d0b4313d07c478abc3c6aefbb03
Tim Showers <showerst@gmail.com>
2024-01-15 20:37:46 -0500

    LA: 2024S1

--- scrapers/la/__init__.py
+++ scrapers/la/__init__.py
@@ -229,7 +229,7 @@ class Louisiana(State):
             "name": "2023 Regular Session",
             "start_date": "2023-04-10",
             "end_date": "2023-06-08",
-            "active": True,
+            "active": False,
         },
         {
             "_scraped_name": "2023 First Extraordinary Session",
@@ -247,7 +247,7 @@ class Louisiana(State):
             "name": "2024 First Extraordinary Session",
             "start_date": "2024-01-15",
             "end_date": "2024-01-23",
-            "active": False,
+            "active": True,
         },
         {
             "_scraped_name": "2024 Second Extraordinary Session",
