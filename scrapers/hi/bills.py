2546b27bd131a080587e6edbbfc2ddd49be8d348
NewAgeAirbender <34139325+NewAgeAirbender@users.noreply.github.com>
2024-04-02 16:26:35 -0500

    HI: add character limit

--- scrapers/hi/bills.py
+++ scrapers/hi/bills.py
@@ -137,7 +137,7 @@ class HIBillScraper(Scraper):
                 vote.set_count("yes", int(yays or 0))
                 vote.set_count("no", int(nays or 0))
                 vote.set_count("not voting", int(v["n_excused"] or 0))
-                vote.dedupe_key = string
+                vote.dedupe_key = string[:300]
                 for voter in split_specific_votes(v["yes"]):
                     voter = self.clean_voter_name(voter)
                     vote.yes(voter)
