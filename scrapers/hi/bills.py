7caf4b21d6b5e52e912f1ad6a0e7e5ea9dc9fa7d
NewAgeAirbender <34139325+NewAgeAirbender@users.noreply.github.com>
2024-04-07 07:45:02 -0500

    HI: add bill id to dedupe

--- scrapers/hi/bills.py
+++ scrapers/hi/bills.py
@@ -137,7 +137,7 @@ class HIBillScraper(Scraper):
                 vote.set_count("yes", int(yays or 0))
                 vote.set_count("no", int(nays or 0))
                 vote.set_count("not voting", int(v["n_excused"] or 0))
-                vote.dedupe_key = string[:300]
+                vote.dedupe_key = f"{bill_id}#{date}#{string[:300]}"
                 for voter in split_specific_votes(v["yes"]):
                     voter = self.clean_voter_name(voter)
                     vote.yes(voter)
