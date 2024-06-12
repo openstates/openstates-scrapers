3384c5177448db402a529a7386cf0a6cfb355a20
Tim Showers <showerst@gmail.com>
2024-06-12 14:26:50 -0400

    OK: 2024S4

--- scrapers/ok/__init__.py
+++ scrapers/ok/__init__.py
@@ -172,6 +172,15 @@ class Oklahoma(State):
             "start_date": "2024-01-29",
             # TODO: update end date
             "end_date": "2024-01-30",
+            "active": False,
+        },
+        {
+            "_scraped_name": "2024 Fourth Special Session",
+            "identifier": "2024S4",
+            "name": "2024 Third Fourth Session",
+            "start_date": "2024-06-12",
+            # TODO: update end date
+            "end_date": "2024-06-21",
             "active": True,
         },
     ]
