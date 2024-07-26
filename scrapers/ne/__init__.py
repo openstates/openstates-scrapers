38c7d45e579391d6bd05970e5553f66af0769aa2
NewAgeAirbender <34139325+NewAgeAirbender@users.noreply.github.com>
2024-07-26 09:41:38 -0500

    NE: set regular session inactive

--- scrapers/ne/__init__.py
+++ scrapers/ne/__init__.py
@@ -83,7 +83,7 @@ class Nebraska(State):
             "start_date": "2023-01-04",
             "end_date": "2024-04-19",
             "classification": "primary",
-            "active": True,
+            "active": False,
         },
         {
             "_scraped_name": "108th Legislature 1st Special Session",
