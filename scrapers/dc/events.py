939f9cecbf76a8c9ff13c70385b368b1d18c124a
Tim Showers <showerst@gmail.com>
2024-09-10 10:11:30 -0400

    Dc events rewrite wip

--- scrapers/dc/events.py
+++ scrapers/dc/events.py
@@ -1,6 +1,11 @@
 import lxml.html
 import pytz
 import re
+from dateutil import parser
+from dateutil.relativedelta import relativedelta
+import datetime
+import itertools
+import operator
 
 from ics import Calendar
 from openstates.scrape import Scraper, Event
@@ -12,7 +17,59 @@ class DCEventScraper(Scraper):
 
     bill_prefixes = {"bill": "B", "resolution": "R"}
 
-    def scrape(self):
+    def scrape(self, start_date=None, end_date=None):
+
+        if start_date is None:
+            start_date = datetime.date.today() + relativedelta(months=-3)
+        else:
+            start_date = parser.parse(start_date)
+
+        if end_date is None:
+            end_date = datetime.date.today() + relativedelta(months=+3)
+        else:
+            end_date = parser.parse(start_date)
+
+        for d in self.month_range(start_date, end_date):
+            data = self.scrape_month_json(d.month, d.year)
+            for row in data:
+                yield from self.parse_event(row)
+
+    def scrape_month_json(self, month: int, year: int):
+        self.info(f"Scraping {str(year)}-{str(month)}")
+        headers = {
+            "accept": "application/json, text/plain, */*",
+            "content-type": "application/json",
+        }
+
+        json_data = {
+            "month": month,
+            "year": year,
+            "committeeId": 0,
+            "searchText": "",
+        }
+
+        response = self.post(
+            "https://lims.dccouncil.gov/Hearings/API/Public/GetHearingsCalendar",
+            headers=headers,
+            json=json_data,
+        )
+        return response.json()
+
+    def parse_event(self, data):
+        when = parser.parse(data["hearingDateTime"])
+        when = self._tz.localize(when)
+
+        e = Event(
+            data["hearingTitle"], when, data["address"], upstream_id=data["hearingId"]
+        )
+
+        e.add_source(
+            f"https://lims.dccouncil.gov/Hearings/hearings/{data['hearingId']}"
+        )
+
+        yield e
+
+    def old_scrape(self):
         # use ical to get the full feed and start dates, which aren't cleanly in the html
         ical_url = (
             "https://dccouncil.gov/?post_type=tribe_events&ical=1&eventDisplay=list"
@@ -76,3 +133,18 @@ class DCEventScraper(Scraper):
         event.add_source(e.url)
 
         yield event
+
+    def month_range(
+        self,
+        start: datetime.date,
+        end: datetime.date,
+    ):
+        """Yields the 1st day of each month in the given date range."""
+        yield from itertools.takewhile(
+            lambda date: date < end,
+            itertools.accumulate(
+                itertools.repeat(relativedelta(months=1)),
+                operator.add,
+                initial=start,
+            ),
+        )
