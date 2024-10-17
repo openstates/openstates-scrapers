dd10b8d789a578112bad1e454f90a3d7d4664e64
Daniel Gieseke <daniel.tyler.gieseke@gmail.com>
2024-10-17 14:23:03 -0400

    Making get_secret function raise exceptionsin lieu of returning nothing
    Adding edits per lint tests
    
    adding edits per lint test

--- scrapers/utils/secrets.py
+++ scrapers/utils/secrets.py
@@ -13,6 +13,9 @@ def get_secret(secret_name, region="us-west-2"):
 
     Returns:
     - The value associated with the specified key.
+
+    Raises:
+    - Exception: If the secret cannot be retrieved or is not available.
     """
 
     client = boto3.client(service_name="secretsmanager", region_name=region)
@@ -20,12 +23,12 @@ def get_secret(secret_name, region="us-west-2"):
     try:
         secret_response = client.get_secret_value(SecretId=secret_name)
     except Exception as e:
-        logging.warning(f"Error retrieving secret: {e}")
-        return None
+        logging.error(f"Failed to retrieve secret: {secret_name}. Error details: {e}")
+        raise Exception(f"Failed to retrieve secret: {secret_name}. Error details: {e}")
 
     if "SecretString" in secret_response:
         secret = json.loads(secret_response["SecretString"])[secret_name]
         return secret
     else:
-        logging.warning("Secret is binary or unavailable in string format.")
-        return None
+        logging.error(f"Secret {secret_name} is not available in string format.")
+        raise Exception(f"Secret {secret_name} is not available in string format.")
