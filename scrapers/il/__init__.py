59fa6dac5943210ba78107aee5400e0f9354a605
Neal Paresh Patel <59032397+Desitrain22@users.noreply.github.com>
2024-11-20 11:17:10 -0500

    updated current live scrapers to use cronos

--- scrapers/il/__init__.py
+++ scrapers/il/__init__.py
@@ -10,7 +10,7 @@ class Illinois(State):
         "bills": IlBillScraper,
         "events": IlEventScraper,
     }
-    legislative_sessions = [
+    hisotrical_legislative_sessions = [
         {
             "name": "90th Regular Session",
             "identifier": "90th",
--- scrapers/ny/__init__.py
+++ scrapers/ny/__init__.py
@@ -12,7 +12,7 @@ class NewYork(State):
         "bills": NYBillScraper,
         "events": NYEventScraper,
     }
-    legislative_sessions = [
+    historical_legislative_sessions = [
         {
             "_scraped_name": "2009",
             "identifier": "2009-2010",
