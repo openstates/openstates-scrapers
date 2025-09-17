ffe103169fdaf3ddef542a892d61a40118a44730
Jesse Mortenson <jessemortenson@gmail.com>
2025-09-17 01:14:00 -0500

    CA: bills: apparently ALSO need to catch RuntimeError with StopIteration error

--- scrapers/ca/bills.py
+++ scrapers/ca/bills.py
@@ -272,7 +272,7 @@ class CABillScraper(Scraper, LXMLMixin):
             for abbr, type_ in bill_types[chamber].items():
                 try:
                     yield from self.scrape_bill_type(chamber, session, type_, abbr)
-                except StopIteration:
+                except (StopIteration, RuntimeError) as e:
                     continue
 
     def scrape_bill_type(
