cc27a5b1a67f218568062b78b0ad72bf65c0dbb6
showerst <showerst@gmail.com>
2025-10-02 09:26:16 -0400

    NM: 2025S1

--- scrapers/nm/__init__.py
+++ scrapers/nm/__init__.py
@@ -212,6 +212,16 @@ class NewMexico(State):
             "name": "2025 Regular Session",
             "start_date": "2025-01-21",
             "end_date": "2025-03-22",
+            "active": False,
+        },
+        {
+            "_scraped_name": "2025 1st Special",
+            "classification": "special",
+            "identifier": "2025S1",
+            "name": "2025 First Special Session",
+            "start_date": "2025-10-01",
+            # TODO: Correct start date
+            "end_date": "2025-10-10",
             "active": True,
         },
     ]
